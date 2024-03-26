a9ef0550055bf478a7ca4ddfaa2cba05e1837496
Nicholas Smith <nsmith@tenstorrent.com>
2024-03-26 16:23:34 -0500

    Fixup dispatch basic block types

--- lib/ttmlir/TTPasses.cpp
+++ lib/ttmlir/TTPasses.cpp
@@ -208,13 +208,23 @@ public:
 class TTLowerRewriter : public OpRewritePattern<linalg::GenericOp> {
 public:
   using OpRewritePattern<linalg::GenericOp>::OpRewritePattern;
+
+  template <typename TensorTyT>
+  static TensorTyT unparallelize(TensorTyT tensorTy) {
+    SmallVector<int64_t> shape(tensorTy.getShape());
+    assert(shape.size() > 2);
+    SmallVector<int64_t> newShape(shape.begin() + 2, shape.end());
+    return tensorTy.clone(newShape);
+  }
+
   LogicalResult matchAndRewrite(linalg::GenericOp op,
                                 PatternRewriter &rewriter) const final {
+    // Test if this generic op has already been lowered, todo remove
     if (op.getOperation()->getParentOp()->getName() ==
         OperationName("tt.dispatch", getContext()))
       return failure();
-    op.getOperation()->getParentOp()->getName().dump();
-    // Create a new dispatch op
+
+    // Create a new dispatch op, inherit the ins/outs from generic op
     auto resultTypesRange = ValueRange(op.getResults()).getTypes();
     SmallVector<Type> resultTypes(resultTypesRange.begin(),
                                   resultTypesRange.end());
@@ -222,21 +232,57 @@ public:
         op.getLoc(), resultTypes, op.getInputs(), op.getOutputs(),
         AffineMap::getMultiDimIdentityMap(2, getContext()));
 
-    // Create a new basic block for the dispatch op
+    // Create a new basic block for the dispatch op, create arguments
     Block *block = rewriter.createBlock(&dispatch.getRegion());
-
-    // Inherit the arguments from generic op
     std::size_t inputsSize = op.getInputs().size();
     std::size_t outputsSize = op.getOutputs().size();
-    auto inputTypesRange = ValueRange(op.getInputs()).getTypes();
-    auto outputTypesRange = ValueRange(op.getOutputs()).getTypes();
     SmallVector<Type> blockArgumentTypes;
-    blockArgumentTypes.append(inputTypesRange.begin(), inputTypesRange.end());
-    blockArgumentTypes.append(outputTypesRange.begin(), outputTypesRange.end());
+    for (auto inputTy : ValueRange(op.getInputs()).getTypes()) {
+      blockArgumentTypes.push_back(
+          unparallelize(dyn_cast<TensorType>(inputTy)));
+    }
+    for (auto outputTy : ValueRange(op.getOutputs()).getTypes()) {
+      blockArgumentTypes.push_back(
+          unparallelize(dyn_cast<TensorType>(outputTy)));
+    }
+    for (Value result : op.getResults()) {
+      result.setType(unparallelize(dyn_cast<TensorType>(result.getType())));
+    }
+
+    auto shiftDown = [](AffineMapAttr attr, unsigned shift,
+                        MLIRContext *ctx) -> AffineMapAttr {
+      auto affineMap = cast<AffineMapAttr>(attr).getAffineMap();
+      assert(affineMap.getNumDims() > shift);
+      assert(affineMap.getNumResults() > shift);
+      SmallVector<AffineExpr> exprs;
+      for (AffineExpr affineExpr :
+           affineMap.getMinorSubMap(affineMap.getNumResults() - shift)
+               .getResults()) {
+        auto position = dyn_cast<AffineDimExpr>(affineExpr).getPosition();
+        assert(position >= shift);
+        exprs.push_back(mlir::getAffineDimExpr(position - shift, ctx));
+      }
+      return AffineMapAttr::get(
+          AffineMap::get(affineMap.getNumDims() - shift, 0, exprs, ctx));
+    };
+
+    // Fixup the affine maps, i.e. drop the dispatch par dims
+    SmallVector<Attribute> parAffineMaps;
+    for (Attribute attr : op.getIndexingMaps()) {
+      auto affineMap = cast<AffineMapAttr>(attr);
+      parAffineMaps.push_back(shiftDown(affineMap, 2, getContext()));
+    }
+    op.setIndexingMapsAttr(ArrayAttr::get(getContext(), parAffineMaps));
+
+    SmallVector<Attribute> parIteratorTypes(op.getIteratorTypes().getValue());
+    assert(parIteratorTypes.size() > 2);
+    SmallVector<Attribute> unparIteratorTypes(parIteratorTypes.begin() + 2,
+                                              parIteratorTypes.end());
+    op.setIteratorTypesAttr(ArrayAttr::get(getContext(), unparIteratorTypes));
+
+    // Rewire the generic op arguments to reference the dispatch block arguments
     SmallVector<Location> blockArgumentLocs(inputsSize + outputsSize,
                                             dispatch.getLoc());
-
-    // Rewire the generic op arguments to the dispatch inputs
     (void)block->addArguments(blockArgumentTypes, blockArgumentLocs);
     auto blockArguments = block->getArguments();
     op.getInputsMutable().assign(blockArguments.slice(0, inputsSize));
