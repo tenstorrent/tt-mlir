dbc594a0bc7cc46bbe52c1bb0f076ec406c2d084
svuckovic <svuckovic@tenstorrent.com>
2024-08-08 13:03:04 +0000

    fix pch and add run script + readme

--- ttnn-standalone/pch.hpp
+++ ttnn-standalone/pch.hpp
@@ -2,8 +2,16 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+#include "common/bfloat16.hpp"
+#include "tensor/types.hpp"
+#include "tt_eager/tensor/tensor.hpp"
 #include "ttnn/core.hpp"
 #include "ttnn/device.hpp"
 #include "ttnn/operations/binary.hpp"
+#include "ttnn/operations/core.hpp"
+#include "ttnn/operations/creation.hpp"
+#include "ttnn/types.hpp"
 
 #include <cstddef>
+#include <iostream>
+#include <vector>
