74a3b5e4a7ae44d0c83c5c562b23bf59aa679909
Vraj Prajapati <vprajapati@tenstorrent.com>
2024-08-08 13:05:40 -0500

    Added Pipeline/Transformation from TTIR -> Flatbuffer
    Interim Update
    
    Fixed CMake

--- include/ttmlir/Bindings/Python/TTMLIRModule.h
+++ include/ttmlir/Bindings/Python/TTMLIRModule.h
@@ -8,16 +8,28 @@
 #include "mlir/Bindings/Python/PybindAdaptors.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/InitAllPasses.h"
+// #include "mlir/Parser/Parser.h"
+#include "mlir-c/Bindings/Python/Interop.h"
+#include "mlir/CAPI/IR.h"
 #include "ttmlir-c/Dialects.h"
 #include "ttmlir/Dialect/TT/IR/TT.h"
 #include "ttmlir/Dialect/TTIR/IR/TTIR.h"
 #include "ttmlir/Dialect/TTIR/Transforms/Passes.h"
+#include "ttmlir/Dialect/TTNN/Pipelines/Passes.h"
+#include "ttmlir/Dialect/TTNN/Transforms/Passes.h"
+#include "ttmlir/Dialect/TTNN/Transforms/TTNNToCpp.h"
+#include "ttmlir/RegisterAll.h"
+// #include <cstdint>
+// #include <string>
+// #include <unordered_map>
 
 namespace py = pybind11;
 
 namespace mlir::ttmlir::python {
 void populateTTModule(py::module &m);
 void populateTTKernelModule(py::module &m);
+void populateOverridesModule(py::module &m);
+void populatePassesModule(py::module &m);
 } // namespace mlir::ttmlir::python
 
 #endif // TTMLIR_BINDINGS_PYTHON_TTMLIRMODULE_H
---
+++ python/Passes.cpp
@@ -0,0 +1,59 @@
+// SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "mlir/InitAllTranslations.h"
+#include "ttmlir/Bindings/Python/TTMLIRModule.h"
+#include "ttmlir/RegisterAll.h"
+#include "ttmlir/Target/TTNN/TTNNToFlatbuffer.h"
+
+namespace mlir::ttmlir::python {
+
+void populatePassesModule(py::module &m) {
+  // When populating passes, need to first register them
+
+  mlir::tt::registerAllPasses();
+  mlir::registerAllTranslations();
+  // mlir::tt::ttnn::registerTTNNPipelines();
+
+  m.def("ttir_to_ttnn_backend_pipeline", [](MlirModule module) {
+    mlir::Operation *moduleOp = unwrap(mlirModuleGetOperation(module));
+    mlir::PassManager pm(moduleOp->getName());
+
+    mlir::DialectRegistry registry;
+    mlir::tt::registerAllDialects(registry);
+    mlir::MLIRContext *ctx = unwrap(mlirModuleGetContext(module));
+    ctx->appendDialectRegistry(registry);
+
+    const auto pipeline =
+        mlir::PassPipelineInfo::lookup("ttir-to-ttnn-backend-pipeline");
+
+    std::string options = "";
+
+    mlir::function_ref<mlir::LogicalResult(const llvm::Twine &)> err_handler =
+        [](const llvm::Twine &loc) { return mlir::failure(); };
+
+    if (mlir::failed(pipeline->addToPipeline(pm, options, err_handler))) {
+      throw std::runtime_error("Failed to add pipeline to pass manager");
+    }
+
+    if (mlir::failed(pm.run(moduleOp))) {
+      throw std::runtime_error("Failed to run pass manager");
+    }
+  });
+
+  py::class_<std::shared_ptr<void>>(m, "TTNNToFlatbufferData")
+      .def(py::init<>())
+      .def("from_module", [](MlirModule module) {
+        mlir::Operation *moduleOp = unwrap(mlirModuleGetOperation(module));
+        auto data = mlir::tt::ttnn::ttnnToFlatbuffer(moduleOp);
+      });
+
+  m.def("ttnn_to_flatbuffer", [](MlirModule module) {
+    mlir::Operation *moduleOp = unwrap(mlirModuleGetOperation(module));
+    std::shared_ptr<void> data = mlir::tt::ttnn::ttnnToFlatbuffer(moduleOp);
+    return py::capsule(data.get());
+  });
+}
+
+} // namespace mlir::ttmlir::python
