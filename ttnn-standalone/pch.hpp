345307dac18190de2c3448651a3e447a4ecc88a0
svuckovic <svuckovic@tenstorrent.com>
2024-08-08 12:10:53 +0000

    add ttnn-standalone

---
+++ ttnn-standalone/pch.hpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ttnn/core.hpp"
+#include "ttnn/device.hpp"
+#include "ttnn/operations/binary.hpp"
+
+#include <cstddef>
