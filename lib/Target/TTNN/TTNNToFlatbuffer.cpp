4b83bdbee97f3756755ed79ba489e74acd4cc377
Sasa Vuckovic <134393361+svuckovicTT@users.noreply.github.com>
2024-08-20 16:14:22 +0200

    [TTNN Dialect] Convert ttnn.to_memory_config from DPS to non-DPS op (#437)

--- lib/Target/TTNN/TTNNToFlatbuffer.cpp
+++ lib/Target/TTNN/TTNNToFlatbuffer.cpp
@@ -74,11 +74,13 @@ createOp(FlatbufferObjectCache &cache, CloseDeviceOp op) {
 
 ::flatbuffers::Offset<::tt::target::ttnn::ToMemoryConfigOp>
 createOp(FlatbufferObjectCache &cache, ToMemoryConfigOp op) {
-  auto input = getOperandThroughDPSOps(op.getInput());
-  auto output = getOperandThroughDPSOps(op.getOutput());
-  return ::tt::target::ttnn::CreateToMemoryConfigOp(
-      *cache.fbb, cache.at<::tt::target::TensorRef>(input),
-      cache.at<::tt::target::TensorRef>(output));
+  constexpr uint64_t kHostAllocatedAddress = 0;
+  constexpr uint64_t kHostAllocatedSize = 0;
+  auto input = cache.getOrCreate(op.getInput(), tensorValueToFlatbuffer,
+                                 kHostAllocatedAddress, kHostAllocatedSize);
+  auto output = cache.getOrCreate(op.getResult(), tensorValueToFlatbuffer,
+                                  kHostAllocatedAddress, kHostAllocatedSize);
+  return ::tt::target::ttnn::CreateToMemoryConfigOp(*cache.fbb, input, output);
 }
 
 ::flatbuffers::Offset<::tt::target::ttnn::EmptyOp>
