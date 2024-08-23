5f683ef4e74640bf1542d6c66a2b80516ad656e9
svuckovic <svuckovic@tenstorrent.com>
2024-08-23 13:56:04 +0000

    wip

--- tools/ttnn-standalone/ttnn-standalone.cpp
+++ tools/ttnn-standalone/ttnn-standalone.cpp
@@ -6,6 +6,7 @@
 #include "operations/core/core.hpp"
 #include "tensor/types.hpp"
 #include "ttnn-precompiled.hpp"
+#include "types.hpp"
 
 // Below is a snippet generated with:
 // ./build/bin/ttmlir-opt --ttir-load-system-desc --ttir-layout
@@ -31,8 +32,20 @@
 ttnn::Tensor forward(ttnn::Tensor v1, ttnn::Tensor v2) {
   ttnn::Device &v3 = ttnn::open_device(0);
 
-  ttnn::Tensor v4 = ttnn::empty(v1.shape(), v1.tensor_attributes->dtype,
-                                v1.tensor_attributes->layout, v3);
+  // ttnn::Tensor v4 = ttnn::empty(v1.shape(), v1.tensor_attributes->dtype,
+  //                               v1.tensor_attributes->layout, v3);
+
+  MemoryConfig memConfig = ttnn::MemoryConfig{
+      .memory_layout = ttnn::TensorMemoryLayout::SINGLE_BANK,
+      .buffer_type = ttnn::BufferType::DRAM,
+      // .shard_spec = std::nullopt,
+  };
+
+  ttnn::Tensor v4 = ttnn::to_layout(v1, ttnn::Layout::ROW_MAJOR,
+                                    ttnn::DataType::BFLOAT16, memConfig, &v3);
+  ttnn::Tensor v5 = ttnn::to_layout(v2, ttnn::Layout::ROW_MAJOR,
+                                    ttnn::DataType::BFLOAT16, memConfig, &v3);
+
   //   ttnn::Tensor v5 = ttnn::to_memory_config(v1, v4);
   //   struct MemoryConfig {
   //     TensorMemoryLayout memory_layout = TensorMemoryLayout::INTERLEAVED;  //
@@ -49,18 +62,21 @@ ttnn::Tensor forward(ttnn::Tensor v1, ttnn::Tensor v2) {
 
   // ttnn::to_memory_config(v4, q, std::optional<DataType>());
 
-  ttnn::Tensor v6 = ttnn::empty(v2.shape(), v2.tensor_attributes->dtype,
-                                v2.tensor_attributes->layout, v3);
+  // ttnn::Tensor v6 = ttnn::empty(v2.shape(), v2.tensor_attributes->dtype,
+  //                               v2.tensor_attributes->layout, v3);
   //   ttnn::Tensor v7 = ttnn::to_memory_config(v2, v6);
 
   ttnn::Tensor v9 = ttnn::empty(v2.shape(), v2.tensor_attributes->dtype,
                                 v2.tensor_attributes->layout, v3);
-  ttnn::multiply(v4, v6, std::nullopt, std::nullopt, v9, std::nullopt,
-                 std::nullopt);
-  v9 = v9.cpu(); // move to CPU
+
+  ttnn::Tensor v10 = ttnn::to_layout(v9, ttnn::Layout::TILE,
+                                     ttnn::DataType::BFLOAT16, memConfig, &v3);
+
+  ttnn::multiply(v4, v5, std::nullopt, std::nullopt, v10);
+  v10 = v10.cpu(); // move to CPU
 
   ttnn::close_device(v3);
-  return v9;
+  return v10;
 }
 
 int main() {
