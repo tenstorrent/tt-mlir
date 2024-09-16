fe4b8c7037a53c70f8574b7060ed90f25f157730
Nicholas Smith <nsmith@tenstorrent.com>
2024-09-16 17:03:51 -0500

    Refactor TTMetal dialect to be proper dialect conversion
    Also move metal backend pipeline to its own file.
    
    Closes #179
    Closes #318

--- include/ttmlir/Dialect/TTMetal/Transforms/Passes.h
+++ include/ttmlir/Dialect/TTMetal/Transforms/Passes.h
@@ -5,20 +5,12 @@
 #ifndef TTMLIR_DIALECT_TTMETAL_TRANSFORMS_PASSES_H
 #define TTMLIR_DIALECT_TTMETAL_TRANSFORMS_PASSES_H
 
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/Pass/Pass.h"
-#include "ttmlir/Dialect/TTMetal/IR/TTMetal.h"
-#include "ttmlir/Dialect/TTMetal/IR/TTMetalOps.h"
-#include <memory>
-
 namespace mlir::tt::ttmetal {
 #define GEN_PASS_DECL
 #include "ttmlir/Dialect/TTMetal/Transforms/Passes.h.inc"
 
 #define GEN_PASS_REGISTRATION
 #include "ttmlir/Dialect/TTMetal/Transforms/Passes.h.inc"
-
-void createTTIRToTTMetalBackendPipeline(OpPassManager &pm);
 } // namespace mlir::tt::ttmetal
 
 #endif
