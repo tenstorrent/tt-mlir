96cd60b936a025720468be95531719944ad94c2d
Tapasvi Patel <tpatel@tenstorrent.com>
2024-09-18 22:16:21 +0000

    #382: Added test cases for ttrt and refactored ttrt

---
+++ runtime/tools/python/test/test_run.py
@@ -0,0 +1,279 @@
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import json
+import importlib.machinery
+import sys
+import signal
+import os
+import io
+import subprocess
+import time
+import socket
+from pkg_resources import get_distribution
+import shutil
+import atexit
+import pytest
+
+import ttrt
+from ttrt.common.util import *
+from ttrt.common.api import API
+
+from util import *
+
+
+def test_flatbuffer():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_flatbuffer_cmd():
+    command = (
+        f"ttrt run {BINARY_FILE_PATH} --log-file {test_flatbuffer_cmd.__name__}_run.log"
+    )
+    sub_process_command(command)
+
+
+def test_dir_flatbuffer():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = DIRECTORY_PATH
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_dir_flatbuffer_cmd():
+    command = f"ttrt run {DIRECTORY_PATH} --log-file {test_dir_flatbuffer_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_logger():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    log_file_name = "test.log"
+    custom_logger = Logger(log_file_name)
+    run_instance = API.Run(args=custom_args, logger=custom_logger)
+    run_instance()
+
+
+def test_artifacts():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    log_file_name = "test.log"
+    custom_logger = Logger(log_file_name)
+    artifacts_folder_path = f"{os.getcwd()}/test-artifacts"
+    custom_artifacts = Artifacts(
+        logger=custom_logger, artifacts_folder_path=artifacts_folder_path
+    )
+    run_instance = API.Run(args=custom_args, artifacts=custom_artifacts)
+    run_instance()
+
+
+def test_clean_artifacts():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--clean-artifacts"] = True
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_clean_artifacts_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --clean-artifacts --log-file {test_clean_artifacts_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_save_artifacts():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--clean-artifacts"] = True
+    custom_args["--save-artifacts"] = True
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_save_artifacts_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --clean-artifacts --save-artifacts --log-file {test_save_artifacts_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_log_file():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--log-file"] = "test.log"
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_log_file_cmd():
+    command = (
+        f"ttrt run {BINARY_FILE_PATH} --log-file {test_log_file_cmd.__name__}_run.log"
+    )
+    sub_process_command(command)
+
+
+def test_artifact_dir():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--clean-artifacts"] = True
+    custom_args["--save-artifacts"] = True
+    custom_args["--artifact-dir"] = f"{os.getcwd()}/test-artifacts"
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_artifact_dir_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --clean-artifacts --save-artifacts --artifact-dir {os.getcwd()}/test-artifacts --log-file {test_artifact_dir_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_program_index():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--program-index"] = "0"
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_program_index_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --program-index 0 --log-file {test_program_index_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_loops():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--loops"] = 1
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_loops_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --loops 1 --log-file {test_loops_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_init():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--init"] = "randn"
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_init_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --init randn --log-file {test_init_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+@pytest.mark.skip
+def test_identity():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--identity"] = True
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+@pytest.mark.skip
+def test_identity_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --identity --log-file {test_identity_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_non_zero():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--non-zero"] = True
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_non_zero_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --non-zero --log-file {test_non_zero_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_rtol():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--rtol"] = 1e-05
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_rtol_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --rtol 1e-05 --log-file {test_rtol_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_atol():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--atol"] = 1e-08
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_atol_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --atol 1e-08 --log-file {test_atol_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_seed():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--seed"] = 1
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_seed_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --seed 1 --log-file {test_seed_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_load_kernels_from_disk():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--load-kernels-from-disk"] = True
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_load_kernels_from_disk_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --load-kernels-from-disk --log-file {test_load_kernels_from_disk_cmd.__name__}_run.log"
+    sub_process_command(command)
+
+
+def test_disable_async_ttnn():
+    API.initialize_apis()
+    custom_args = {}
+    custom_args["binary"] = BINARY_FILE_PATH
+    custom_args["--disable-async-ttnn"] = True
+    run_instance = API.Run(args=custom_args)
+    run_instance()
+
+
+def test_disable_async_ttnn_cmd():
+    command = f"ttrt run {BINARY_FILE_PATH} --disable-async-ttnn --log-file {test_disable_async_ttnn_cmd.__name__}_run.log"
+    sub_process_command(command)
---
+++ runtime/tools/python/ttrt/common/query.py
@@ -0,0 +1,191 @@
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import json
+import importlib.machinery
+import sys
+import signal
+import os
+import io
+import subprocess
+import time
+import socket
+from pkg_resources import get_distribution
+import shutil
+import atexit
+
+from ttrt.common.util import *
+
+
+class Query:
+    registered_args = {}
+
+    @staticmethod
+    def initialize_api():
+        Query.register_arg(
+            name="--clean-artifacts",
+            type=bool,
+            default=False,
+            choices=[True, False],
+            help="clean all artifacts from previous runs",
+        )
+        Query.register_arg(
+            name="--save-artifacts",
+            type=bool,
+            default=False,
+            choices=[True, False],
+            help="save all artifacts during run",
+        )
+        Query.register_arg(
+            name="--log-file",
+            type=str,
+            default="",
+            choices=None,
+            help="log file to dump ttrt output to",
+        )
+        Query.register_arg(
+            name="--artifact-dir",
+            type=str,
+            default=f"{os.getcwd()}/ttrt-artifacts",
+            choices=None,
+            help="provides a directory path to save artifacts to",
+        )
+        Query.register_arg(
+            name="--quiet",
+            type=bool,
+            default=False,
+            choices=[True, False],
+            help="suppress system desc from being printed",
+        )
+
+    def __init__(self, args={}, logger=None, artifacts=None):
+        for name, attributes in Query.registered_args.items():
+            if type(args) == dict:
+                if name in args.keys():
+                    self[name] = args[name]
+                else:
+                    self[name] = attributes["default"]
+            else:
+                # argument got parsed to hyphen's for underscrolls and leading hyphen's removed - need to put back
+                converted_name = name
+                if name != "binary":
+                    converted_name = converted_name.lstrip("-")
+                    converted_name = converted_name.replace("-", "_")
+                self[name] = getattr(args, converted_name)
+
+        self.logger = logger if logger != None else Logger(self["--log-file"])
+        self.logging = self.logger.get_logger()
+        self.globals = Globals(self.logger)
+        self.file_manager = FileManager(self.logger)
+        self.artifacts = (
+            artifacts
+            if artifacts != None
+            else Artifacts(
+                self.logger,
+                self.file_manager,
+                artifacts_folder_path=self["--artifact-dir"],
+            )
+        )
+        self.system_desc = None
+        self.device_ids = None
+
+    def preprocess(self):
+        self.logging.debug(f"preprocessing query API")
+
+        if self["--clean-artifacts"]:
+            self.artifacts.clean_artifacts()
+
+        if self["--save-artifacts"]:
+            self.artifacts.create_artifacts()
+
+        self.logging.debug(f"finished preprocessing query API")
+
+    def check_constraints(self):
+        self.logging.debug(f"checking constraints for query API")
+        self.logging.debug(f"finished constraints for query API")
+
+    def execute(self):
+        self.logging.debug(f"executing query API")
+
+        import ttrt.runtime
+
+        try:
+            self.logging.debug(f"getting system descriptor")
+            (
+                self.system_desc,
+                self.device_ids,
+            ) = ttrt.runtime.get_current_system_desc()
+
+            if not self["--quiet"]:
+                self.logging.info(self.system_desc.as_json())
+        except Exception as e:
+            raise Exception(f"an unexpected error occurred: {e}")
+
+        self.logging.debug(f"finished executing query API")
+
+    def postprocess(self):
+        self.logging.debug(f"postprocessing query API")
+
+        if self["--save-artifacts"]:
+            self.artifacts.save_system_desc(self.system_desc)
+
+        self.logging.debug(f"finished postprocessing query API")
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+    def __call__(self):
+        self.logging.debug(
+            f"----------------------------starting query API----------------------------"
+        )
+
+        self.preprocess()
+        self.check_constraints()
+        self.execute()
+        self.postprocess()
+
+        self.logging.debug(
+            f"----------------------------finished query API----------------------------"
+        )
+
+    def get_system_desc_as_dict(self):
+        return json.loads(self.system_desc.as_json())
+
+    @staticmethod
+    def register_arg(name, type, default, choices, help):
+        Query.registered_args[name] = {
+            "type": type,
+            "default": default,
+            "choices": choices,
+            "help": help,
+        }
+
+    @staticmethod
+    def generate_subparser(subparsers):
+        query_parser = subparsers.add_parser(
+            "query", help="query information about the current system"
+        )
+        query_parser.set_defaults(api=Query)
+
+        for name, attributes in Query.registered_args.items():
+            if attributes["type"] == bool:
+                query_parser.add_argument(
+                    f"{name}",
+                    action="store_true",
+                    help=attributes["help"],
+                )
+            else:
+                query_parser.add_argument(
+                    f"{name}",
+                    type=attributes["type"],
+                    default=attributes["default"],
+                    choices=attributes["choices"],
+                    help=attributes["help"],
+                )
+
+        return query_parser
