04e9dab0f8c49ce1075c3a59ec96628c8e2f6a47
Marko Rakita <mrakita@tenstorrent.com>
2024-09-27 15:25:02 +0000

    Hacky fix for full op layout

--- runtime/lib/ttnn/operations/creation/full.cpp
+++ runtime/lib/ttnn/operations/creation/full.cpp
@@ -38,6 +38,21 @@ void run(const ::tt::target::ttnn::FullOp *op, ProgramContext &context) {
       ::ttnn::full(shape, fillValue, outputDataType, outputLayout, outputDevice,
                    outputMemoryConfig);
 
+  // When tensor is created with full op it is usually used as input to other
+  // ops, not as output like in the case of empty op. That is why we really need
+  // it to be in desired original layout, which we cannot deduce because of
+  // issue #272, but it is more likely to be TILE layout so converting to that
+  // one here. Unfortunately ttnn::to_layout works only with bfp16, so for other
+  // types we are doing this hack which is safe to do only for single value
+  // tensors.
+  if (outputDataType == ::ttnn::DataType::BFLOAT16) {
+    out =
+        ::ttnn::to_layout(out, ::ttnn::TILE_LAYOUT, std::nullopt, std::nullopt,
+                          static_cast<::ttnn::Device *>(nullptr));
+  } else if (shape.volume() == 1) {
+    out.set_layout(::ttnn::Layout::TILE);
+  }
+
   tensorPool.insert_or_assign(op->out()->global_id(), out);
 }
 } // namespace tt::runtime::ttnn::operations::creation
