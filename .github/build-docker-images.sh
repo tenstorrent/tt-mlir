0142a5cda0ff1c14ffb0aae71d0b08c1d39dffc6
Vladimir Milosevic <157983820+vmilosevic@users.noreply.github.com>
2024-10-21 14:11:59 +0200

    Docker build as part of workflow (#928)
    Updating the workflow to make the Docker image build the first step in the build and test pipeline. Instead of triggering Docker builds manually, the workflow will check if the required Docker images already exist on GHCR (GitHub Container Registry). If they don’t, the workflow will initiate the build.
    
    How do we determine which Docker image is needed?
    
    The image tag is based on a hash of the Dockerfiles and the environment CMake file. If any of these change, a new Docker image build will be triggered. Although we could refine this to only trigger builds for specific changes (e.g., LLVM version in the environment file), for now, any change will trigger a new build. See get-docker-tag.sh
    
    If a Docker image with the calculated tag already exists, the job will quickly complete and pass the image tag to the subsequent build and test jobs, which will use this specific image instead of the "latest" tag. This ensures the workflow always uses the correct Docker image.
    
    How do we know which image to use in IRD?
    
    Each time a Docker image is built, it will also be tagged as "latest." In most cases, this "latest" image will work for IRD. However, if it doesn’t, developers can run the get-docker-tag.sh script to retrieve the exact image tag needed for their code and use that in IRD.

---
+++ .github/build-docker-images.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+REPO=tenstorrent/tt-mlir
+BASE_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-base-ubuntu-22-04
+CI_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-ci-ubuntu-22-04
+BASE_IRD_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-base-ird-ubuntu-22-04
+IRD_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-ird-ubuntu-22-04
+
+# Compute the hash of the Dockerfile
+DOCKER_TAG=$(./.github/get-docker-tag.sh)
+echo "Docker tag: $DOCKER_TAG"
+
+# Are we on main branch
+ON_MAIN=$(git branch --show-current | grep -q main && echo "true" || echo "false")
+
+build_and_push() {
+    local image_name=$1
+    local dockerfile=$2
+    local on_main=$3
+    local from_image=$4
+
+
+    if docker manifest inspect $image_name:$DOCKER_TAG > /dev/null; then
+        echo "Image $image_name:$DOCKER_TAG already exists"
+    else
+        echo "Building image $image_name:$DOCKER_TAG"
+        docker build \
+            --progress=plain \
+            --build-arg FROM_TAG=$DOCKER_TAG \
+            ${from_image:+--build-arg FROM_IMAGE=$from_image} \
+            -t $image_name:$DOCKER_TAG \
+            -t $image_name:latest \
+            -f $dockerfile .
+
+        echo "Pushing image $image_name:$DOCKER_TAG"
+        docker push $image_name:$DOCKER_TAG
+
+        # If we are on main branch also push the latest tag
+        if [ "$on_main" = "true" ]; then
+            echo "Pushing image $image_name:latest"
+            docker push $image_name:latest
+        fi
+    fi
+}
+
+build_and_push $BASE_IMAGE_NAME .github/Dockerfile.base $ON_MAIN
+build_and_push $BASE_IRD_IMAGE_NAME .github/Dockerfile.ird $ON_MAIN base
+build_and_push $CI_IMAGE_NAME .github/Dockerfile.ci $ON_MAIN
+build_and_push $IRD_IMAGE_NAME .github/Dockerfile.ird $ON_MAIN ci
+
+echo "All images built and pushed successfully"
+echo "CI_IMAGE_NAME:"
+echo $CI_IMAGE_NAME:$DOCKER_TAG
