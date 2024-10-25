1c1f4fe73eec60140cb875b57a0f70347a8f6882
Vladimir Milosevic <vmilosevic@tenstorrent.com>
2024-10-25 13:13:19 +0000

    Split image build workflows

--- .github/build-docker-images.sh
+++ .github/build-docker-images.sh
@@ -5,12 +5,26 @@
 
 set -e
 
-CONFIG=${1:-debug}
+# By default build the release configuration
+
+# Check the first argument
+if [ -z "$1" ]; then
+    CONFIG="release"
+elif [[ "$1" == "debug" ]]; then
+    CONFIG="debug"
+else
+    echo "Error: Invalid argument. Use 'debug' or leave empty for default build."
+    exit 1
+fi
+
 REPO=tenstorrent/tt-mlir
-BASE_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-base-ubuntu-22-04-$CONFIG
-CI_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-ci-ubuntu-22-04-$CONFIG
-BASE_IRD_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-base-ird-ubuntu-22-04-$CONFIG
-IRD_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-ird-ubuntu-22-04-$CONFIG
+BASE_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-base-ubuntu-22-04
+CI_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-ci-ubuntu-22-04
+IRD_IMAGE_NAME=ghcr.io/$REPO/tt-mlir-ird-ubuntu-22-04
+if [ "$CONFIG" = "debug" ]; then
+    IRD_IMAGE_NAME=$IRD_IMAGE_NAME-debug
+    CI_IMAGE_NAME=$CI_IMAGE_NAME-debug
+fi
 
 echo "Building images for config: $CONFIG"
 
@@ -25,7 +39,6 @@ build_and_push() {
     local image_name=$1
     local dockerfile=$2
     local on_main=$3
-    local from_image=$4
 
 
     if docker manifest inspect $image_name:$DOCKER_TAG > /dev/null; then
@@ -36,7 +49,6 @@ build_and_push() {
             --progress=plain \
             --build-arg FROM_TAG=$DOCKER_TAG \
             --build-arg CONFIG=$CONFIG \
-            ${from_image:+--build-arg FROM_IMAGE=$from_image} \
             -t $image_name:$DOCKER_TAG \
             -t $image_name:latest \
             -f $dockerfile .
@@ -53,10 +65,7 @@ build_and_push() {
 }
 
 build_and_push $BASE_IMAGE_NAME .github/Dockerfile.base $ON_MAIN
-build_and_push $BASE_IRD_IMAGE_NAME .github/Dockerfile.ird $ON_MAIN base
 build_and_push $CI_IMAGE_NAME .github/Dockerfile.ci $ON_MAIN
-build_and_push $IRD_IMAGE_NAME .github/Dockerfile.ird $ON_MAIN ci
+build_and_push $IRD_IMAGE_NAME .github/Dockerfile.ird $ON_MAIN
 
 echo "All images built and pushed successfully"
-echo "CI_IMAGE_NAME:"
-echo $CI_IMAGE_NAME:$DOCKER_TAG
