b1ae668bd908cc2dbbb1b972a3d3aa00c41dce0b
Jacob DeSousa <jdesousa@tenstorrent.com>
2024-10-25 20:08:43 +0000

    Bootstrap D2M Matmul

---
+++ include/ttmlir/Conversion/TTIRToTTMetal/TTIRMatmulToTTMetal.h
@@ -0,0 +1,6 @@
+#include "mlir/IR/PatternMatch.h"
+#include "ttmlir/Dialect/TTIR/IR/TTIROps.h"
+
+namespace mlir::tt::ttmetal {
+    class TTIRToTTMetalMatmulRewriter;
+} // namespace mlir::tt::ttmetal
