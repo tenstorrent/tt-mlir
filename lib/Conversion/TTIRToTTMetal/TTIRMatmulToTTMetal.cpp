e4c910574892c8dd640c8e78280fb2f139717143
Jacob DeSousa <jdesousa@tenstorrent.com>
2024-10-27 23:36:57 +0000

    Add v0 matmul constraints

--- lib/Conversion/TTIRToTTMetal/TTIRMatmulToTTMetal.cpp
+++ lib/Conversion/TTIRToTTMetal/TTIRMatmulToTTMetal.cpp
@@ -1,18 +1,51 @@
 #include "ttmlir/Conversion/TTIRToTTMetal/TTIRMatmulToTTMetal.h"
 
-namespace mlir::tt::ttmetal {
+namespace mlir::tt {
+namespace ttmetal {
+
+constexpr uint32_t TILE_HEIGHT = 32;
+constexpr uint32_t TILE_WIDTH = 32;
 
 class TTIRToTTMetalMatmulRewriter : public OpRewritePattern<ttir::MatmulOp> {
 public:
   using OpRewritePattern<ttir::MatmulOp>::OpRewritePattern;
 
   LogicalResult matchAndRewrite(ttir::MatmulOp op, PatternRewriter &rewriter) const final {
-    auto operand_constraints = mlir::dyn_cast<mlir::ArrayAttr>(op.getOperand(3));
-    assert(std::find(operand_constraints.begin(), operand_constraints.end(),
-                     mlir::tt::OperandConstraint(5))); // TODO(jdesousa): Check for tile layout constriant tile == 5
+    RankedTensorType tensorA = op.getA().getType();
+    RankedTensorType tensorB = op.getB().getType();
+    RankedTensorType outputTensor = op.getOutput().getType();
+    ArrayAttr constraints = op.getOperandConstraints();
+
+    // Operands must be DRAM OR L1 AND Tile Layout
+    if ((std::find(constraints.begin(), constraints.end(), OperandConstraint::DRAM) == constraints.end() &&
+        std::find(constraints.begin(), constraints.end(), OperandConstraint::L1) == constraints.end()) ||
+        std::find(constraints.begin(), constraints.end(), OperandConstraint::Tile) == constraints.end()) {
+          return failure();
+    }
+
+    uint32_t tensorARank = tensorA.getRank();
+    uint32_t tensorBRank = tensorB.getRank();
+    uint32_t outputTensorRank = outputTensor.getRank();
+
+    // Input A must be tile aligned
+    if ((tensorA.getShape()[tensorARank - 1] % TILE_WIDTH != 0 || tensorA.getShape()[tensorARank - 2] % TILE_HEIGHT != 0)) {
+      return failure();
+    }
+
+    // Input B must be tile aligned
+    if ((tensorB.getShape()[tensorBRank - 1] % TILE_WIDTH != 0 ||
+         tensorB.getShape()[tensorBRank - 2] % TILE_HEIGHT != 0)) {
+      return failure();
+    }
+
+    // Output must be tile aligned
+    if ((outputTensor.getShape()[outputTensorRank - 1] % TILE_WIDTH != 0 ||
+         outputTensor.getShape()[outputTensorRank - 2] % TILE_HEIGHT != 0)) {
+      return failure();
+    }
 
     return success();
   }
 };
-
+}
 } // namespace mlir::tt::ttmetal
