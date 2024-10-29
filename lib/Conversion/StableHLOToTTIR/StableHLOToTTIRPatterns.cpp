3d4325cb1e4764d5588844bfbf6e423ddb1dc979
uazizTT <uaziz@tenstorrent.com>
2024-10-29 15:34:35 +0000

    Refactor for code review.

--- lib/Conversion/StableHLOToTTIR/StableHLOToTTIRPatterns.cpp
+++ lib/Conversion/StableHLOToTTIR/StableHLOToTTIRPatterns.cpp
@@ -866,8 +866,7 @@ void addElementwiseUnaryOpsConversionPatterns(MLIRContext *ctx,
   patterns.add<StableHLOToTTIROpDefaultConversionPattern<
       mlir::stablehlo::CeilOp, mlir::tt::ttir::CeilOp>>(typeConverter, ctx);
   patterns.add<StableHLOToTTIROpDefaultConversionPattern<
-      mlir::stablehlo::ConvertOp, mlir::tt::ttir::TypecastOp>>(typeConverter,
-                                                               ctx);
+      mlir::stablehlo::CeilOp, mlir::tt::ttir::CeilOp>>(typeConverter, ctx);
   patterns.add<StableHLOToTTIROpDefaultConversionPattern<
       mlir::stablehlo::CosineOp, mlir::tt::ttir::CosOp>>(typeConverter, ctx);
   patterns.add<StableHLOToTTIROpDefaultConversionPattern<
