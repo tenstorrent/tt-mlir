f4be0f8441bd760938d5784028e6a49246cd6956
svuckovic <svuckovic@tenstorrent.com>
2024-11-12 09:26:42 +0000

    fix broken include

--- runtime/lib/ttnn/operations/deletion/dealloc.cpp
+++ runtime/lib/ttnn/operations/deletion/dealloc.cpp
@@ -2,16 +2,16 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 #include "dealloc.h"
-#include "operations/core/core.hpp"
 #include "tt/runtime/detail/logger.h"
 #include "tt/runtime/detail/ttnn.h"
+#include "ttnn/operations/core/core.hpp"
 
 namespace tt::runtime::ttnn::operations::deletion {
 void run(const ::tt::target::ttnn::DeallocOp *op, ProgramContext &context) {
   ProgramTensorPool &tensorPool = context.getTensorPool();
   ::ttnn::Tensor &tensor = tensorPool.at(op->in()->global_id());
   DEBUG_ASSERT(tensor.is_allocated());
-  ttnn::operations::core::deallocate(tensor);
+  ::ttnn::operations::core::deallocate(tensor);
 
   // The tensor should be deallocated after the deallocate call.
   // Still this assert may be hit in the future for multidevice/async ttnn
