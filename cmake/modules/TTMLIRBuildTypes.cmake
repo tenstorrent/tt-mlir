83437743a91416b66e4d034bec16e66e6bde347b
Nicholas Smith <nsmith@tenstorrent.com>
2024-11-13 22:03:18 -0600

    Add code coverage build type

--- cmake/modules/TTMLIRBuildTypes.cmake
+++ cmake/modules/TTMLIRBuildTypes.cmake
@@ -12,6 +12,14 @@ set(CMAKE_SHARED_LINKER_FLAGS_ASAN
     "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fsanitize=address" CACHE STRING
     "Linker lags to be used to create shared libraries for Asan build type." FORCE)
 
+# Coverage
+set(CMAKE_C_FLAGS_COVERAGE
+    "${CMAKE_C_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping" CACHE STRING
+    "Flags used by the C compiler for Asan build type or configuration." FORCE)
+  set(CMAKE_CXX_FLAGS_COVERAGE
+    "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping" CACHE STRING
+    "Flags used by the C++ compiler for Asan build type or configuration." FORCE)
+
   # Assert build type
 set(CMAKE_C_FLAGS_ASSERT
   "${CMAKE_C_FLAGS_RELEASE}" CACHE STRING
