30c4a69b3bfe17a6bf7d35f5feceb75ea100effc
Vincent Wells <vwells@tenstorrent.com>
2024-11-14 17:14:36 -0600

    maybe this will work?

--- runtime/lib/common/system_desc.cpp
+++ runtime/lib/common/system_desc.cpp
@@ -250,7 +250,7 @@ static std::unique_ptr<::tt::runtime::SystemDesc> getCurrentSystemDescImpl(
       getAllDeviceConnections(devices);
   // Store CPUDesc
   std::vector<::flatbuffers::Offset<tt::target::CPUDesc>> cpuDescs;
-  cpuDescs.emplace_back(::tt::target::CPUDesc{::tt::target::CPURole::Host, std::string("TARGET_TRIPLE")});
+  cpuDescs.emplace_back(::tt::target::CPUDesc{::tt::target::CPURole::Host, std::string(TARGET_TRIPLE)});
 
   // Create SystemDesc
   auto systemDesc = ::tt::target::CreateSystemDescDirect(
