4ed0821dcbe4c83433f3396775f1d87e8ce2395d
Stefan Djordjevic <157365107+sdjordjevicTT@users.noreply.github.com>
2024-11-18 19:40:06 +0100

    Implementing leaky relu op in tt-mlir (#1290)

--- runtime/lib/ttnn/operations/eltwise/unary/unary.cpp
+++ runtime/lib/ttnn/operations/eltwise/unary/unary.cpp
@@ -6,6 +6,7 @@
 #include "tt/runtime/detail/ttnn.h"
 #include "tt/runtime/ttnn/operations/eltwise/unary/utils.h"
 #include "tt/runtime/ttnn/operations/utils.h"
+#include "ttmlir/Target/TTNN/program_generated.h"
 #include "ttnn/operations/copy.hpp"
 
 namespace tt::runtime::ttnn::operations::unary {
@@ -42,6 +43,21 @@ static void runEltwiseUnaryWithFastAndApproximateModeOp(
 
   ::ttnn::Tensor out =
       ttnnOp(*in, false /* parameter */, outputMemoryConfig, std::nullopt);
+  tensorPool.insert_or_assign(op->out()->global_id(), out);
+}
+
+static void runEltwiseUnaryWithFloatParameterOp(
+    const ::tt::target::ttnn::EltwiseOp *op, ProgramTensorPool &tensorPool,
+    const std::function<::ttnn::Tensor(const ::ttnn::Tensor &, float,
+                                       const ::tt::tt_metal::MemoryConfig &)>
+        &ttnnOp) {
+  ::ttnn::Tensor *in = nullptr;
+  getEltwiseUnaryOpInputTensor(op, tensorPool, &in);
+
+  float parameter = op->params_as_EltwiseOpWithFloatParams()->parameter();
+  ::tt::tt_metal::MemoryConfig outputMemoryConfig =
+      utils::createMemoryConfig(op->out());
+  ::ttnn::Tensor out = ttnnOp(*in, parameter, outputMemoryConfig);
   tensorPool.insert_or_assign(op->out()->global_id(), out);
 }
 
@@ -120,6 +136,10 @@ void run(const ::tt::target::ttnn::EltwiseOp *op, ProgramContext &context) {
   }
   case ::tt::target::ttnn::EltwiseOpType::Expm1: {
     runEltwiseUnaryOp(op, tensorPool, ::ttnn::expm1);
+    break;
+  }
+  case ::tt::target::ttnn::EltwiseOpType::LeakyRelu: {
+    runEltwiseUnaryWithFloatParameterOp(op, tensorPool, ::ttnn::leaky_relu);
     break;
   }
   default:
