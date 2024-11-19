da48625ee9a4d277563363573bd8752b207a3d23
Vincent Wells <vwells@tenstorrent.com>
2024-11-19 09:24:27 -0600

    +format

--- lib/Dialect/TTIR/Transforms/HoistCPUOps.cpp
+++ lib/Dialect/TTIR/Transforms/HoistCPUOps.cpp
@@ -23,21 +23,43 @@ public:
 
   LogicalResult matchAndRewrite(MaximumOp op,
                                 PatternRewriter &rewriter) const final {
+    // Retrieve the parent module of the current operation
+    auto parentModule = op->getParentOfType<mlir::ModuleOp>();
+
+    // Check if a "cpu_module" already exists
+    mlir::ModuleOp cpuModule;
+    for (auto &op : parentModule.getBody()->getOperations()) {
+      if (auto module = llvm::dyn_cast<mlir::ModuleOp>(&op)) {
+        if (module->hasAttr("cpu_module")) {
+          cpuModule = module;
+          break;
+        }
+      }
+    }
+
+    // If no CPU module exists, create one
+    if (!cpuModule) {
+      rewriter.setInsertionPointToEnd(parentModule.getBody());
+      cpuModule = rewriter.create<mlir::ModuleOp>(loc);
+      cpuModule->setAttr("cpu_module", rewriter.getUnitAttr());
+    }
+
     auto resultTy = op.getResultTypes();
     auto loc = op.getLoc();
     auto hoistFuncTy = rewriter.getFunctionType({resultTy}, {resultTy});
 
     // define hoisted func, w placeholder attr for CPU execution
+    rewriter.setInsertionPointToEnd(cpuModule.getBody());
     auto hoistFunc =
         rewriter.create<func::FuncOp>(loc, "cpu_maximum_func", hoistFuncTy);
     hoistFunc->setAttr("target", rewriter.getStringAttr("CPU"));
 
     rewriter.setInsertionPointToEnd(hoistFunc.addEntryBlock());
-    // auto cpuMaxOp = rewriter.create<MaximumOp>(loc, resultTy,
-    // op.getOperand(0),
-    //                                            op.getOperand(1));
-    rewriter.create<func::ReturnOp>(loc, op.getResults());
+    auto cpuMaxOp = rewriter.create<MaximumOp>(loc, resultTy, op.getOperand(0),
+                                               op.getOperand(1));
+    rewriter.create<func::ReturnOp>(loc, cpuMaxOp.getResults());
 
+    rewriter.setInsertionPoint(op);
     auto funcAttr =
         FlatSymbolRefAttr::get(rewriter.getContext(), hoistFunc.getName());
     auto callOp = rewriter.create<func::CallOp>(
