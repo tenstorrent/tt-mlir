838dc85fb9cc15552ec7666935d3bde088f9ab24
Vincent Wells <vwells@tenstorrent.com>
2024-11-21 14:27:30 -0600

    linalg to llvm conversion

--- lib/Dialect/TTIR/Pipelines/TTIRPipelines.cpp
+++ lib/Dialect/TTIR/Pipelines/TTIRPipelines.cpp
@@ -4,7 +4,19 @@
 
 #include "ttmlir/Dialect/TTIR/Pipelines/TTIRPipelines.h"
 
+#include "mlir/Conversion/ArithToLLVM/ArithToLLVM.h"
+#include "mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
+#include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVMPass.h"
+#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
+#include "mlir/Conversion/TensorToLinalg/TensorToLinalgPass.h"
+#include "mlir/Dialect/Bufferization/Pipelines/Passes.h"
+#include "mlir/Dialect/Bufferization/Transforms/Passes.h"
+#include "mlir/Dialect/Linalg/Passes.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
 #include "mlir/Pass/PassManager.h"
+#include "mlir/Pass/PassRegistry.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
 #include "mlir/Transforms/Passes.h"
 
 #include "ttmlir/Conversion/Passes.h"
@@ -34,6 +46,43 @@ void createStableHLOToTTIRPipeline(
 }
 #endif
 
+void createLinalgToLLVMPipeline(OpPassManager &manager,
+                                const LinalgToLLVMPipelineOptions &options) {
+  manager.addPass(mlir::createCanonicalizerPass());
+
+  manager.addPass(mlir::createConvertElementwiseToLinalgPass());
+  manager.addPass(mlir::createConvertTensorToLinalgPass());
+
+  // One-shot bufferize, from
+  // https://mlir.llvm.org/docs/Bufferization/#ownership-based-buffer-deallocation
+  mlir::bufferization::OneShotBufferizationOptions bufferizationOptions;
+  bufferizationOptions.bufferizeFunctionBoundaries = true;
+  manager.addPass(
+      mlir::bufferization::createOneShotBufferizePass(bufferizationOptions));
+  mlir::bufferization::BufferDeallocationPipelineOptions deallocationOptions;
+  mlir::bufferization::buildBufferDeallocationPipeline(manager,
+                                                       deallocationOptions);
+
+  manager.addPass(mlir::createConvertLinalgToLoopsPass());
+
+  // Needed to lower memref.subview
+  manager.addPass(mlir::memref::createExpandStridedMetadataPass());
+
+  manager.addPass(mlir::createConvertSCFToCFPass());
+  manager.addPass(mlir::createConvertControlFlowToLLVMPass());
+  manager.addPass(mlir::createArithToLLVMConversionPass());
+  manager.addPass(mlir::createConvertFuncToLLVMPass());
+  manager.addPass(mlir::createFinalizeMemRefToLLVMConversionPass());
+  manager.addPass(mlir::createReconcileUnrealizedCastsPass());
+
+  // Cleanup
+  if (options.cleanupOutputEnabled) {
+    manager.addPass(mlir::createCanonicalizerPass());
+    manager.addPass(mlir::createSCCPPass());
+    manager.addPass(mlir::createCSEPass());
+    manager.addPass(mlir::createSymbolDCEPass());
+  }
+}
 //===----------------------------------------------------------------------===//
 // Pipeline registration.
 //===----------------------------------------------------------------------===//
@@ -45,5 +94,8 @@ void registerTTIRPipelines() {
       "Pipeline lowering stablehlo to ttir dialect.",
       mlir::tt::ttir::createStableHLOToTTIRPipeline);
 #endif
+  mlir::PassPipelineRegistration<LinalgToLLVMPipelineOptions>(
+      "linalg-to-llvm-pipeline", "Pipeline lowering linalg to llvm dialect.",
+      mlir::tt::ttir::createLinalgToLLVMPipeline);
 }
 } // namespace mlir::tt::ttir
