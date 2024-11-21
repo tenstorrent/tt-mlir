10049604e2b6279816de5f6ebeb6014f7232f7a8
Collin Tod <collintod@tenstorrent.com>
2024-11-21 17:11:22 +0000

    Convert to `frombuffer` and comment out len checks

--- runtime/tools/python/ttrt/common/golden.py
+++ runtime/tools/python/ttrt/common/golden.py
@@ -127,19 +127,19 @@ def golden(binary, programContext, opContext):
         op_golden_tensor = binary.get_debug_info_golden(loc)
         op_output_tensor = ttrt.runtime.get_op_output_tensor(opContext, programContext)
 
-        if len(op_golden_tensor) == 0:
-            print("Golden tensor is empty - skipping golden comparison")
-            return
+        # if len(op_golden_tensor) == 0:
+        #    print("Golden tensor is empty - skipping golden comparison")
+        #    return
 
-        if len(op_output_tensor) == 0:
-            print("Output tensor is empty - skipping golden comparison")
-            return
+        # if len(op_output_tensor) == 0:
+        #    print("Output tensor is empty - skipping golden comparison")
+        #    return
 
-        if len(op_golden_tensor) != len(op_output_tensor):
-            print(
-                "Golden and output tensor sizes do not match - skipping golden comparison"
-            )
-            return
+        # if len(op_golden_tensor) != len(op_output_tensor):
+        #    print(
+        #        "Golden and output tensor sizes do not match - skipping golden comparison"
+        #    )
+        #    return
 
         golden_tensor_torch = torch.frombuffer(
             op_golden_tensor, dtype=torch.float32
