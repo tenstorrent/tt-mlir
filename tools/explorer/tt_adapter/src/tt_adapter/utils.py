aae1d586ff46ef42c5b1475d330ef8551dca618d
vprajapati-tt <vprajapati@tenstorrent.com>
2024-11-22 22:42:42 +0000

    Removed stale import

--- tools/explorer/tt_adapter/src/tt_adapter/utils.py
+++ tools/explorer/tt_adapter/src/tt_adapter/utils.py
@@ -6,7 +6,6 @@ import ttmlir
 
 def parse_mlir_file(model_path):
     with ttmlir.ir.Context() as ctx, open(model_path, "r") as model_file:
-        ttmlir.dialects.ttkernel.register_dialect(ctx)
         ttmlir.dialects.ttir.register_dialect(ctx)
         ttmlir.dialects.tt.register_dialect(ctx)
         ttmlir.dialects.ttnn.register_dialect(ctx)
