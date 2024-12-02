b5dbb9e9a7608400cab75c5906d90548dce0f39b
Vincent Wells <vwells@tenstorrent.com>
2024-12-02 17:26:14 -0600

    lotta fake headers

--- lib/Target/LLVM/LLVMToDynamicLib.cpp
+++ lib/Target/LLVM/LLVMToDynamicLib.cpp
@@ -21,7 +21,6 @@
 
 #include "mlir/Conversion/LLVMCommon/Pattern.h"
 #include "mlir/Pass/Pass.h"
-#include "mlir/Target/LLVMIR.h"
 #include "mlir/Target/LLVMIR/LLVMConversion.h"
 #include "mlir/Target/LLVMIR/LLVMTypeConverter.h"
 #include "llvm/IR/Module.h"
