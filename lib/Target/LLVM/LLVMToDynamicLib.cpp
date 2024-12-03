ff3902a2624890334f878e0962c0c726c90984a3
Vincent Wells <vwells@tenstorrent.com>
2024-12-03 09:48:35 -0600

    ??

--- lib/Target/LLVM/LLVMToDynamicLib.cpp
+++ lib/Target/LLVM/LLVMToDynamicLib.cpp
@@ -782,11 +782,11 @@ translateLLVMToDyLib(Operation *op, llvm::raw_ostream &,
     return llvm::failure();
   }
 
-  if (llvm::failed(verifyAllLLVM(*moduleOp))) {
+  if (llvm::failed(verifyAllLLVM(moduleOp))) {
     return llvm::failure();
   }
   llvm::LLVMContext llvmContext;
-  auto llvmModule = convertToLLVMModule(*moduleOp, llvmContext);
+  auto llvmModule = convertToLLVMModule(moduleOp, llvmContext);
   if (llvm::failed(compileAndLinkToSharedLibrary(*llvmModule.get(), llvmContext,
                                                  "temp.so"))) {
     return llvm::failure();
