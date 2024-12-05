9b9985e98c451fa52f1ce93408cceb44c09f4b40
Aleksandar Zecevic <azecevic@tenstorrent.com>
2024-12-05 09:58:12 +0000

    Changed invoke method in the runtime so it doesn't fail for <4D tensors

--- runtime/lib/ttnn/operations/data_movement/permute.cpp
+++ runtime/lib/ttnn/operations/data_movement/permute.cpp
@@ -5,9 +5,7 @@
 #include "permute.h"
 
 #include "tt/runtime/detail/logger.h"
-#include "tt/runtime/detail/ttnn.h"
-#include "tt/runtime/ttnn/operations/utils.h"
-#include "tt/runtime/ttnn/utils.h"
+#include "ttnn/common/constants.hpp"
 
 #include <vector>
 
@@ -21,7 +19,13 @@ void run(const ::tt::target::ttnn::PermuteOp *op, ProgramContext &context) {
   std::vector<int64_t> permutation(op->permutation()->begin(),
                                    op->permutation()->end());
 
-  ::ttnn::Tensor out = ::ttnn::permute(in, permutation);
+  // Has to called with the verbose invoke, since calling it with composite=true
+  // results in an error with the message "Only 4D tensor are supported for
+  // permute."
+  ::ttnn::Tensor out =
+      ::ttnn::permute(/*queue_id=*/::ttnn::DefaultQueueId, /*input_tensor=*/in,
+                      /*dims=*/permutation, /*memory_config=*/std::nullopt,
+                      /*composite=*/false, /*pad_value=*/std::nullopt);
   tensorPool.insert_or_assign(op->out()->global_id(), out);
 }
 } // namespace tt::runtime::ttnn::operations::data_movement
