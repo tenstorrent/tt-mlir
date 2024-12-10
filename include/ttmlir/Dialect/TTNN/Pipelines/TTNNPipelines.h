e9886346da807caaadd289e3e5ecec938eff193b
dgolubovicTT <dgolubovic@tenstorrent.com>
2024-12-10 13:15:52 +0000

    Workaround for embedding on mlir

--- include/ttmlir/Dialect/TTNN/Pipelines/TTNNPipelines.h
+++ include/ttmlir/Dialect/TTNN/Pipelines/TTNNPipelines.h
@@ -117,7 +117,7 @@ struct TTIRToTTNNBackendPipelineOptions
   //
   Option<bool> workaroundPassEnabled{*this, "enable-workaround-pass",
                                      llvm::cl::desc("Enable workaround pass."),
-                                     llvm::cl::init(false)};
+                                     llvm::cl::init(true)};
 };
 
 // TTIR to EmitC pipeline options.
