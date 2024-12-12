33dbf2e405a3de04fc4436b14bcc8b7bda1fbcb9
Collin Tod <collintod@tenstorrent.com>
2024-12-12 20:06:48 +0000

    Warning Documentation

--- runtime/tools/python/ttrt/__init__.py
+++ runtime/tools/python/ttrt/__init__.py
@@ -4,6 +4,12 @@
 
 import importlib.machinery
 
+# NOTE: it is _VERY_ important that this import & setup call is _BEFORE_ any
+# other `ttrt` imports and _AFTER_ all system imports to ensure a well ordered
+# setup of the pybound `.so`. Otherwise, undefined behaviour ensues related to
+# the timing of when `TTMETAL_HOME` environment variable is set. DO NOT MOVE
+# w.r.t. other imports. This is a temporary workaround until `TT_METAL_HOME` is
+# not used anymore in TTMetal
 import ttrt.library_tweaks
 
 ttrt.library_tweaks.set_tt_metal_home()
