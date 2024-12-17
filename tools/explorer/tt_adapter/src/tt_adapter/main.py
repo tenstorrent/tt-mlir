8c37b9ddb71e1a1aa87a5fcfcfdaac3bee472ede
Vraj Prajapati <vprajapati@tenstorrent.com>
2024-12-17 11:44:36 -0600

    Send Perf Data to Explorer Frontend (#1608)
    - A few small changes to explorer to send perf data to the frontend.
    - Changed `to_adapter_format` to allow for multiple objects
    - Added `add_to_dataclass` to append perf_data to Graph dataclass.

--- tools/explorer/tt_adapter/src/tt_adapter/main.py
+++ tools/explorer/tt_adapter/src/tt_adapter/main.py
@@ -56,7 +56,9 @@ class TTAdapter(model_explorer.Adapter):
 
         # Convert TTIR to Model Explorer Graphs and Display/Return
         graph, perf_data = mlir.build_graph(module, perf_trace)
-        return {"graphs": [graph], "perf_data": perf_data}
+        if perf_data:
+            graph = utils.add_to_dataclass(graph, "perf_data", perf_data.graphsData)
+        return {"graphs": [graph]}
 
     def execute(
         self, model_path: str, settings: Dict
--- tools/explorer/tt_adapter/src/tt_adapter/utils.py
+++ tools/explorer/tt_adapter/src/tt_adapter/utils.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 import ttmlir
-from dataclasses import make_dataclass
+from dataclasses import make_dataclass, is_dataclass, asdict
 
 
 def parse_mlir_file(model_path):
@@ -18,5 +18,15 @@ def to_dataclass(obj: dict, dc_name: str = "tempClass"):
     return make_dataclass(dc_name, ((k, type(v)) for k, v in obj.items()))(**obj)
 
 
-def to_adapter_format(obj: dict):
-    return {"graphs": [to_dataclass(obj)]}
+def add_to_dataclass(dataclass, new_attr_name: str, new_attr_value):
+    if not is_dataclass(dataclass):
+        return None
+    classname = dataclass.__class__.__name__
+    dataclass = asdict(dataclass)
+    dataclass[new_attr_name] = new_attr_value
+    return to_dataclass(dataclass, dc_name=classname)
+
+
+def to_adapter_format(*objs):
+    res = [x if is_dataclass(x) else to_dataclass(x) for x in objs]
+    return {"graphs": res}
