6aae3febcb2023029ff3f308e144fe75ca93640f
svuckovic <svuckovic@tenstorrent.com>
2024-12-20 12:40:00 +0000

    fix utilCreateVec

--- lib/Conversion/TTNNToEmitC/Utils.cpp
+++ lib/Conversion/TTNNToEmitC/Utils.cpp
@@ -31,8 +31,8 @@ bool insertVecCreateFnIfNotExists(PatternRewriter &rewriter, Operation *op) {
 
   static constexpr const char *vecCreateFnAsStr = R"(
 template <typename... T>
-std::vector<ttnn::Tensor> utilCreateVec(const T &&...t) {
-  return std::vector<ttnn::Tensor>{std::forward(t)...};
+std::vector<ttnn::Tensor> utilCreateVec(T &&...t) {
+  return std::vector<ttnn::Tensor>{std::forward<T>(t)...};
 }
 )";
 
