cb83f43fb1c09468b10c47ad2d1d4f21eb519cb6
Aleksandar Zecevic <azecevic@tenstorrent.com>
2024-12-26 10:57:13 +0000

    Refactoring of TTIRTraits.cpp
    - improved comments in few places

--- lib/Dialect/TTIR/IR/TTIRTraits.cpp
+++ lib/Dialect/TTIR/IR/TTIRTraits.cpp
@@ -4,17 +4,19 @@
 
 #include "ttmlir/Dialect/TTIR/IR/TTIRTraits.h"
 
-// Op is foldable iff:
-// 1. argment and result types are the same.
-// 2. argument is defined by the same op.
+// Check if all operands and result have the same type. Function assumes op has
+// at least one operand and exactly one result.
+static bool operandAndResultSameType(mlir::Operation *op) {
+  return llvm::all_equal(op->getOperandTypes()) &&
+         op->getOperand(0).getType() == op->getResult(0).getType();
+}
+
+// If Op has TTIRInvolution trait, then it's foldable if:
+// 1. Argument and result types are the same.
+// 2. Argument is defined by the same op.
 // 3. 1) is true for the producing op of the argument.
 // op(op(T a, T r0), T r1)
 bool mlir::tt::ttir::OpTrait::impl::verifyInvolution(mlir::Operation *op) {
-  // Dependant trait of TTIRInvolution is DestionationStyleOpInterface, hence
-  // operands include the result.
-  auto operandAndResultSameType = [](Operation *op) {
-    return llvm::all_equal(op->getOperandTypes());
-  };
   if (!operandAndResultSameType(op)) {
     return false;
   }
@@ -25,17 +27,12 @@ bool mlir::tt::ttir::OpTrait::impl::verifyInvolution(mlir::Operation *op) {
   return operandAndResultSameType(producerOp);
 }
 
-// Op is foldable iff:
-// 1. argment and result types are the same.
-// 2. argument is defined by the same op.
+// If Op has TTIRIdempotence trait, then it's foldable if:
+// 1. Argument and result types are the same.
+// 2. Argument is defined by the same op.
 // 3. 1) is true for the producing op of the argument.
 // op(op(T a, T r0), T r1)
 bool mlir::tt::ttir::OpTrait::impl::verifyIdempotence(mlir::Operation *op) {
-  // Dependant trait of TTIRIdempotence is DestionationStyleOpInterface, hence
-  // operands include the result.
-  auto operandAndResultSameType = [](mlir::Operation *op) {
-    return llvm::all_equal(op->getOperandTypes());
-  };
   if (!operandAndResultSameType(op)) {
     return false;
   }
@@ -46,7 +43,7 @@ bool mlir::tt::ttir::OpTrait::impl::verifyIdempotence(mlir::Operation *op) {
   return operandAndResultSameType(producerOp);
 }
 
-// Op is foldable iff:
+// If Op has TTIRBinaryIdempotence trait, then it's foldable if:
 // 1. Both inputs are the same.
 // 2. Inputs and result types are the same.
 bool mlir::tt::ttir::OpTrait::impl::verifyBinaryIdempotence(
