afecd830b3f2cd0f0f21e1fcff968bad2e949ede
Bill Teng <bteng@tenstorrent.com>
2024-12-28 06:21:33 +0000

    comment out printer

--- lib/Target/TTKernel/TTKernelToCpp.cpp
+++ lib/Target/TTKernel/TTKernelToCpp.cpp
@@ -25,71 +25,71 @@
 namespace mlir::tt::ttkernel {
 
 /// This pass illustrates the IR nesting through printing.
-struct Printer{
+// struct Printer{
 
-  /// The three methods below are mutually recursive and follow the nesting of
-  /// the IR: operation->region->block->operation->...
-  public:
-  void printOperation(Operation *op) {
-    // Print the operation itself and some of its properties
-    printIndent() << "visiting op: '" << op->getName() << "' with "
-                  << op->getNumOperands() << " operands and "
-                  << op->getNumResults() << " results\n";
-    // Print the operation attributes
-    if (!op->getAttrs().empty()) {
-      printIndent() << op->getAttrs().size() << " attributes:\n";
-      for (NamedAttribute attr : op->getAttrs())
-        printIndent() << " - '" << attr.getName().getValue() << "' : '"
-                      << attr.getValue() << "'\n";
-    }
+//   /// The three methods below are mutually recursive and follow the nesting of
+//   /// the IR: operation->region->block->operation->...
+//   public:
+//   void printOperation(Operation *op) {
+//     // Print the operation itself and some of its properties
+//     printIndent() << "visiting op: '" << op->getName() << "' with "
+//                   << op->getNumOperands() << " operands and "
+//                   << op->getNumResults() << " results\n";
+//     // Print the operation attributes
+//     if (!op->getAttrs().empty()) {
+//       printIndent() << op->getAttrs().size() << " attributes:\n";
+//       for (NamedAttribute attr : op->getAttrs())
+//         printIndent() << " - '" << attr.getName().getValue() << "' : '"
+//                       << attr.getValue() << "'\n";
+//     }
 
-    // Recurse into each of the regions attached to the operation.
-    printIndent() << " " << op->getNumRegions() << " nested regions:\n";
-    auto indent = pushIndent();
-    for (Region &region : op->getRegions())
-      printRegion(region);
-  }
+//     // Recurse into each of the regions attached to the operation.
+//     printIndent() << " " << op->getNumRegions() << " nested regions:\n";
+//     auto indent = pushIndent();
+//     for (Region &region : op->getRegions())
+//       printRegion(region);
+//   }
 
-  void printRegion(Region &region) {
-    // A region does not hold anything by itself other than a list of blocks.
-    printIndent() << "Region with " << region.getBlocks().size()
-                  << " blocks:\n";
-    auto indent = pushIndent();
-    for (Block &block : region.getBlocks())
-      printBlock(block);
-  }
+//   void printRegion(Region &region) {
+//     // A region does not hold anything by itself other than a list of blocks.
+//     printIndent() << "Region with " << region.getBlocks().size()
+//                   << " blocks:\n";
+//     auto indent = pushIndent();
+//     for (Block &block : region.getBlocks())
+//       printBlock(block);
+//   }
 
-  void printBlock(Block &block) {
-    // Print the block intrinsics properties (basically: argument list)
-    printIndent()
-        << "Block with " << block.getNumArguments() << " arguments, "
-        << block.getNumSuccessors()
-        << " successors, and "
-        // Note, this `.size()` is traversing a linked-list and is O(n).
-        << block.getOperations().size() << " operations\n";
+//   void printBlock(Block &block) {
+//     // Print the block intrinsics properties (basically: argument list)
+//     printIndent()
+//         << "Block with " << block.getNumArguments() << " arguments, "
+//         << block.getNumSuccessors()
+//         << " successors, and "
+//         // Note, this `.size()` is traversing a linked-list and is O(n).
+//         << block.getOperations().size() << " operations\n";
 
-    // Block main role is to hold a list of Operations: let's recurse.
-    auto indent = pushIndent();
-    for (Operation &op : block.getOperations())
-      printOperation(&op);
-  }
+//     // Block main role is to hold a list of Operations: let's recurse.
+//     auto indent = pushIndent();
+//     for (Operation &op : block.getOperations())
+//       printOperation(&op);
+//   }
 
-  /// Manages the indentation as we traverse the IR nesting.
-  int indent;
-  struct IdentRAII {
-    int &indent;
-    IdentRAII(int &indent) : indent(indent) {}
-    ~IdentRAII() { --indent; }
-  };
-  void resetIndent() { indent = 0; }
-  IdentRAII pushIndent() { return IdentRAII(++indent); }
+//   /// Manages the indentation as we traverse the IR nesting.
+//   int indent;
+//   struct IdentRAII {
+//     int &indent;
+//     IdentRAII(int &indent) : indent(indent) {}
+//     ~IdentRAII() { --indent; }
+//   };
+//   void resetIndent() { indent = 0; }
+//   IdentRAII pushIndent() { return IdentRAII(++indent); }
 
-  llvm::raw_ostream &printIndent() {
-    for (int i = 0; i < indent; ++i)
-      llvm::outs() << "  ";
-    return llvm::outs();
-  }
-};
+//   llvm::raw_ostream &printIndent() {
+//     for (int i = 0; i < indent; ++i)
+//       llvm::outs() << "  ";
+//     return llvm::outs();
+//   }
+// };
 
 static llvm::LogicalResult translateModuleToCpp(
     Operation *op, llvm::raw_ostream &os, const ttkernel::ThreadType &threadType) {
