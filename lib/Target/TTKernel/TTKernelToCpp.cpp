b60809916f2a32d2031b389abf1168fa767218bf
Bill Teng <bteng@tenstorrent.com>
2025-01-01 02:51:19 +0000

    clang format

--- lib/Target/TTKernel/TTKernelToCpp.cpp
+++ lib/Target/TTKernel/TTKernelToCpp.cpp
@@ -6,35 +6,35 @@
 #include <cstddef>
 #include <memory>
 
+#include "mlir/Conversion/ArithToEmitC/ArithToEmitCPass.h"
+#include "mlir/Conversion/FuncToEmitC/FuncToEmitCPass.h"
+#include "mlir/Conversion/SCFToEmitC/SCFToEmitC.h"
 #include "mlir/Dialect/EmitC/IR/EmitC.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/Pass/PassManager.h"
 #include "mlir/Support/LLVM.h"
 #include "mlir/Support/LogicalResult.h"
+#include "mlir/Target/Cpp/CppEmitter.h"
+#include "ttmlir/Conversion/TTKernelToEmitC/TTKernelToEmitC.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/Support/LogicalResult.h"
 #include "llvm/Support/raw_ostream.h"
-#include "mlir/Target/Cpp/CppEmitter.h"
-#include "mlir/Pass/PassManager.h"
-#include "ttmlir/Conversion/TTKernelToEmitC/TTKernelToEmitC.h"
-#include "mlir/Conversion/ArithToEmitC/ArithToEmitCPass.h"
-#include "mlir/Conversion/SCFToEmitC/SCFToEmitC.h"
-#include "mlir/Conversion/FuncToEmitC/FuncToEmitCPass.h"
 #include <llvm/Support/raw_ostream.h>
 
 namespace mlir::tt::ttkernel {
 
-static llvm::LogicalResult translateModuleToCpp(
-    Operation *op, llvm::raw_ostream &os, const ttkernel::ThreadType &threadType) {
+static llvm::LogicalResult
+translateModuleToCpp(Operation *op, llvm::raw_ostream &os,
+                     const ttkernel::ThreadType &threadType) {
 
-    ModuleOp module = dyn_cast<ModuleOp>(op);
-    assert(module && "Expected ModuleOp as top level operation");
-    return mlir::tt::emitKernelAsCpp(module, os, threadType);
-
+  ModuleOp module = dyn_cast<ModuleOp>(op);
+  assert(module && "Expected ModuleOp as top level operation");
+  return mlir::tt::emitKernelAsCpp(module, os, threadType);
 }
 
-LogicalResult translateTTKernelToCpp(
-    Operation *op, llvm::raw_ostream &os, const ttkernel::ThreadType &threadType) {
+LogicalResult translateTTKernelToCpp(Operation *op, llvm::raw_ostream &os,
+                                     const ttkernel::ThreadType &threadType) {
   return translateModuleToCpp(op, os, threadType);
 }
 
