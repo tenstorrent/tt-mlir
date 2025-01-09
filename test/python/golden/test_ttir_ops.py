3c124ffb4a60fe85046597d3d3b5f4435080547b
Collin Tod <collintod@tenstorrent.com>
2025-01-09 16:57:00 +0000

    Add lowewring tests for rest of eltwise ops

--- test/python/golden/test_ttir_ops.py
+++ test/python/golden/test_ttir_ops.py
@@ -16,6 +16,21 @@ def test_exp(in0: Operand, builder: TTIRBuilder):
 
 
 @compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_expm1(in0: Operand, builder: TTIRBuilder):
+    return builder.expm1(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_ceil(in0: Operand, builder: TTIRBuilder):
+    return builder.ceil(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_floor(in0: Operand, builder: TTIRBuilder):
+    return builder.floor(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
 def test_abs(in0: Operand, builder: TTIRBuilder):
     return builder.abs(in0)
 
@@ -25,14 +40,67 @@ def test_logical_not(in0: Operand, builder: TTIRBuilder):
     return builder.logical_not(in0)
 
 
+# TODO: uncomment once we have control over generated input types (bitwise ops
+# don't support floats)
+# @compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+# def test_bitwise_not(in0: Operand, builder: TTIRBuilder):
+#    return builder.bitwise_not(in0)
+
+
 @compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
 def test_neg(in0: Operand, builder: TTIRBuilder):
     return builder.neg(in0)
 
 
 @compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_sign(in0: Operand, builder: TTIRBuilder):
+    return builder.sign(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_sin(in0: Operand, builder: TTIRBuilder):
+    return builder.sin(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_cos(in0: Operand, builder: TTIRBuilder):
+    return builder.cos(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_tan(in0: Operand, builder: TTIRBuilder):
+    return builder.tan(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_tanh(in0: Operand, builder: TTIRBuilder):
+    return builder.tanh(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_log(in0: Operand, builder: TTIRBuilder):
+    return builder.log(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_log1p(in0: Operand, builder: TTIRBuilder):
+    return builder.log1p(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
 def test_relu(in0: Operand, builder: TTIRBuilder):
     return builder.relu(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_gelu(in0: Operand, builder: TTIRBuilder):
+    return builder.gelu(in0)
+
+
+# TODO: figured out required param `parameter` in `LeakyReluOp.__init__`
+# @compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+# def test_leaky_relu(in0: Operand, builder: TTIRBuilder):
+#    return builder.leaky_relu(in0)
 
 
 @compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
@@ -53,6 +121,11 @@ def test_sigmoid(in0: Operand, builder: TTIRBuilder):
 @compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
 def test_reciprocal(in0: Operand, builder: TTIRBuilder):
     return builder.reciprocal(in0)
+
+
+@compile_to_flatbuffer([(128, 128)], targets=["ttnn"])
+def test_is_finite(in0: Operand, builder: TTIRBuilder):
+    return builder.is_finite(in0)
 
 
 @compile_to_flatbuffer(
@@ -95,6 +168,52 @@ def test_logical_and(in0: Operand, in1: Operand, builder: TTIRBuilder):
 )
 def test_logical_or(in0: Operand, in1: Operand, builder: TTIRBuilder):
     return builder.logical_or(in0, in1)
+
+
+@compile_to_flatbuffer(
+    [
+        (64, 64),
+        (64, 64),
+    ],
+    targets=["ttnn"],
+)
+def test_logical_xor(in0: Operand, in1: Operand, builder: TTIRBuilder):
+    return builder.logical_xor(in0, in1)
+
+
+# TODO: uncomment once we have control over generated input types (bitwise ops
+# don't support floats)
+# @compile_to_flatbuffer(
+#    [
+#        (64, 64),
+#        (64, 64),
+#    ],
+#    targets=["ttnn"],
+# )
+# def test_bitwise_and(in0: Operand, in1: Operand, builder: TTIRBuilder):
+#    return builder.bitwise_and(in0, in1)
+#
+#
+# @compile_to_flatbuffer(
+#    [
+#        (64, 64),
+#        (64, 64),
+#    ],
+#    targets=["ttnn"],
+# )
+# def test_bitwise_or(in0: Operand, in1: Operand, builder: TTIRBuilder):
+#    return builder.bitwise_or(in0, in1)
+#
+#
+# @compile_to_flatbuffer(
+#    [
+#        (64, 64),
+#        (64, 64),
+#    ],
+#    targets=["ttnn"],
+# )
+# def test_bitwise_xor(in0: Operand, in1: Operand, builder: TTIRBuilder):
+#    return builder.bitwise_xor(in0, in1)
 
 
 @compile_to_flatbuffer(
@@ -192,8 +311,43 @@ def test_div(in0: Operand, in1: Operand, builder: TTIRBuilder):
     ],
     targets=["ttnn"],
 )
+def test_remainder(in0: Operand, in1: Operand, builder: TTIRBuilder):
+    return builder.remainder(in0, in1)
+
+
+@compile_to_flatbuffer(
+    [
+        (64, 64),
+        (64, 64),
+    ],
+    targets=["ttnn"],
+)
 def test_maximum(in0: Operand, in1: Operand, builder: TTIRBuilder):
     return builder.maximum(in0, in1)
+
+
+@compile_to_flatbuffer(
+    [
+        (64, 64),
+        (64, 64),
+    ],
+    targets=["ttnn"],
+)
+def test_minimum(in0: Operand, in1: Operand, builder: TTIRBuilder):
+    return builder.minimum(in0, in1)
+
+
+# TODO: uncomment when we have control over the input types
+# @compile_to_flatbuffer(
+#    [
+#        (64, 64),
+#        (64, 64),
+#        (64, 64),
+#    ],
+#    targets=["ttnn"],
+# )
+# def test_where(in0: Operand, in1: Operand, in2: Operand, builder: TTIRBuilder):
+#    return builder.where(in0, in1, in2)
 
 
 @compile_to_flatbuffer(
