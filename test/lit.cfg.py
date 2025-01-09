f5ec1dc22f95b3bdc88741e58aa1af097b80c13f
svuckovic <svuckovic@tenstorrent.com>
2025-01-09 13:33:56 +0000

    remove tools/ttnn-standalone from PATH in llvm-lit; remove prints

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -101,30 +101,18 @@ llvm_config.with_environment(
 
 # Add `TT_MLIR_HOME` to lit environment.
 if "TT_MLIR_HOME" in os.environ:
-    print(f"{os.environ['TT_MLIR_HOME']}")
     llvm_config.with_environment("TT_MLIR_HOME", os.environ["TT_MLIR_HOME"])
-
-    # Additionally, add 'tools/ttnn-standalone' to $PATH
-    llvm_config.with_environment(
-        "PATH",
-        [
-            os.path.join(os.getenv("TT_MLIR_HOME"), "tools/ttnn-standalone"),
-        ],
-        append_path=True,
-    )
 else:
     raise OSError("Error: TT_MLIR_HOME not set")
 
 # Add `TT_METAL_HOME` to lit environment.
 if "TT_METAL_HOME" in os.environ:
-    print(f"{os.environ['TT_METAL_HOME']}")
     llvm_config.with_environment("TT_METAL_HOME", os.environ["TT_METAL_HOME"])
 else:
     raise OSError("Error: TT_METAL_HOME not set")
 
 # Add `TT_METAL_BUILD_HOME` to lit environment.
 if "TT_METAL_BUILD_HOME" in os.environ:
-    print(f"{os.environ['TT_METAL_BUILD_HOME']}")
     llvm_config.with_environment(
         "TT_METAL_BUILD_HOME", os.environ["TT_METAL_BUILD_HOME"]
     )
@@ -133,7 +121,6 @@ else:
 
 # Add `ARCH_NAME` to lit environment.
 if "ARCH_NAME" in os.environ:
-    print(f"ARCH_NAME={os.environ['ARCH_NAME']}")
     llvm_config.with_environment("ARCH_NAME", os.environ["ARCH_NAME"])
 else:
     raise OSError("Error: ARCH_NAME not set.")
