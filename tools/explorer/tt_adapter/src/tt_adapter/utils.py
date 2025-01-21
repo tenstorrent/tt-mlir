df7062a63826ad46d557f74c63b287b9f9f86137
vprajapati-tt <vprajapati@tenstorrent.com>
2025-01-21 16:17:46 +0000

    Interim

--- tools/explorer/tt_adapter/src/tt_adapter/utils.py
+++ tools/explorer/tt_adapter/src/tt_adapter/utils.py
@@ -4,6 +4,11 @@
 import ttmlir
 from dataclasses import make_dataclass, is_dataclass, asdict
 
+import importlib
+import logging
+
+TTRT_INSTALLED = importlib.util.find_spec("ttrt") is not None
+
 
 def parse_mlir_file(model_path):
     with ttmlir.ir.Context() as ctx, open(model_path, "r") as model_file:
@@ -12,6 +17,18 @@ def parse_mlir_file(model_path):
         ttmlir.dialects.ttnn.register_dialect(ctx)
         module = ttmlir.ir.Module.parse(model_file.read(), ctx)
         return module
+
+
+def parse_flatbuffer_file(fb_path):
+    if not TTRT_INSTALLED:
+        logging.error(
+            "TTRT is not installed in Python Environment, unable to parse Flatbuffer"
+        )
+        return None
+
+    from ttrt.common.util import Binary
+
+    fbb = Binary(file_path=fb_path)
 
 
 def to_dataclass(obj: dict, dc_name: str = "tempClass"):
@@ -36,4 +53,4 @@ def to_adapter_format(*objs):
 def make_editable_kv(kv, editable):
     obj = asdict(kv)
     obj["editable"] = editable
-    return make_dataclass("KeyValue", ((k, type(v)) for k, v in obj.items()))(**obj)
+    return to_dataclass(obj, "KeyValue")
