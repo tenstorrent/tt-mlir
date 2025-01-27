91363b73b8e977476fc932e128943692ec1d550b
Vincent Wells <vwells@tenstorrent.com>
2025-01-27 14:55:50 +0000

    don't try to build clang to get asan so

--- cmake/modules/TTMLIRBuildTypes.cmake
+++ cmake/modules/TTMLIRBuildTypes.cmake
@@ -1,15 +1,15 @@
 # Asan build type
 set(CMAKE_C_FLAGS_ASAN
-    "${CMAKE_C_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer" CACHE STRING
+    "${CMAKE_C_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer -shared-libasan" CACHE STRING
     "Flags used by the C compiler for Asan build type or configuration." FORCE)
 set(CMAKE_CXX_FLAGS_ASAN
-    "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer" CACHE STRING
+    "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer -shared-libasan" CACHE STRING
     "Flags used by the C++ compiler for Asan build type or configuration." FORCE)
 set(CMAKE_EXE_LINKER_FLAGS_ASAN
-    "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -fsanitize=address" CACHE STRING
+    "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -fsanitize=address -shared-libasan" CACHE STRING
     "Linker flags to be used to create executables for Asan build type." FORCE)
 set(CMAKE_SHARED_LINKER_FLAGS_ASAN
-    "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fsanitize=address" CACHE STRING
+    "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fsanitize=address -shared-libasan" CACHE STRING
     "Linker lags to be used to create shared libraries for Asan build type." FORCE)
 
   # Assert build type
