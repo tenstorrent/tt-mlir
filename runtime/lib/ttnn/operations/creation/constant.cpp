93dd12391fc08ea18d4836a0e92d3e1418876eae
Aleksandar Zecevic <azecevic@tenstorrent.com>
2025-02-02 19:17:15 +0000

    ConstantOp runtime refactoring
    - TODO: place utility functions in appropriate files

--- runtime/lib/ttnn/operations/creation/constant.cpp
+++ runtime/lib/ttnn/operations/creation/constant.cpp
@@ -7,25 +7,64 @@
 #include "tt/runtime/detail/logger.h"
 #include "tt/runtime/ttnn/operations/utils.h"
 #include "tt/runtime/ttnn/utils.h"
-#include <ttnn/operations/functions.hpp>
-#include <type_traits>
 
 namespace tt::runtime::ttnn::operations::creation {
 
+template <::tt::target::DataType DataType>
+struct NativeDType {
+  using type = std::monostate;
+};
+template <>
+struct NativeDType<::tt::target::DataType::Float32> {
+  using type = float;
+};
+template <>
+struct NativeDType<::tt::target::DataType::BFloat16> {
+  using type = bfloat16;
+};
+template <>
+struct NativeDType<::tt::target::DataType::UInt32> {
+  using type = uint32_t;
+};
+template <>
+struct NativeDType<::tt::target::DataType::UInt16> {
+  using type = uint16_t;
+};
+template <>
+struct NativeDType<::tt::target::DataType::UInt8> {
+  using type = uint8_t;
+};
+
+template <::tt::target::DataType DataType>
+using NativeDTypeT = typename NativeDType<DataType>::type;
+
 template <typename T>
-constexpr bool is_host_type_v =
+constexpr bool IsHostTypeV =
     std::is_constructible_v<::tt::tt_metal::OwnedBuffer,
                             ::tt::tt_metal::owned_buffer::Buffer<T>>;
 
 template <typename T>
-::tt::tt_metal::OwnedBuffer buffer(const ::flatbuffers::Vector<uint8_t> *data) {
-  if constexpr (is_host_type_v<T>) {
-    auto size = data->size() / sizeof(T);
+static T getElement(const ::flatbuffers::Vector<uint8_t> *data, size_t i) {
+  if constexpr (std::is_same_v<T, bfloat16>) {
+    return bfloat16(
+        ::flatbuffers::IndirectHelper<uint16_t>::Read(data->data(), i));
+  } else {
+    return ::flatbuffers::IndirectHelper<T>::Read(data->data(), i);
+  }
+}
+
+template <typename T>
+static ::tt::tt_metal::OwnedBuffer
+makeBuffer(const ::flatbuffers::Vector<uint8_t> *data) {
+  if constexpr (IsHostTypeV<T>) {
+    size_t size = data->size() / sizeof(T);
+    LOG_ASSERT(data->size() % sizeof(T) == 0, "Invalid data size");
+
     ::tt::tt_metal::owned_buffer::Buffer<T> ownedBuffer =
         tt::tt_metal::owned_buffer::create<T>(size);
 
     for (size_t i = 0; i < size; ++i) {
-      ownedBuffer[i] = *data->GetAs<T>(i);
+      ownedBuffer[i] = getElement<T>(data, i);
     }
     return ownedBuffer;
   } else {
@@ -34,44 +73,47 @@ template <typename T>
   }
 }
 
+constexpr size_t DTypeMinV = static_cast<size_t>(tt::target::DataType::MIN);
+constexpr size_t DTypeMaxV = static_cast<size_t>(tt::target::DataType::MAX);
+constexpr size_t DTypeCountV = DTypeMaxV - DTypeMinV + 1;
+
+using BufferCreatorFn =
+    ::tt::tt_metal::OwnedBuffer (*)(const ::flatbuffers::Vector<uint8_t> *);
+
+template <size_t... Is>
+constexpr auto makeBufferTable(std::index_sequence<Is...>) {
+  return std::array<BufferCreatorFn, sizeof...(Is)>{
+      [](const ::flatbuffers::Vector<uint8_t> *data)
+          -> tt::tt_metal::OwnedBuffer {
+        return makeBuffer<
+            NativeDTypeT<static_cast<::tt::target::DataType>(DTypeMinV + Is)>>(
+            data);
+      }...};
+}
+
+constexpr auto bufferTable =
+    makeBufferTable(std::make_index_sequence<DTypeCountV>{});
+
+static tt::tt_metal::OwnedBuffer
+makeTypedBuffer(::tt::target::DataType dtype,
+                const ::flatbuffers::Vector<uint8_t> *data) {
+  return bufferTable[static_cast<size_t>(dtype)](data);
+}
+
 void run(const ::tt::target::ttnn::ConstantOp *op, ProgramContext &context) {
   ::ttnn::SimpleShape shape(::tt::runtime::ttnn::utils::toShapeFromFBShape(
       *op->out()->desc()->shape()));
 
+  ::tt::tt_metal::OwnedBuffer ownedBuffer = makeTypedBuffer(
+      op->out()->desc()->layout()->memory_desc()->data_type(), op->data());
+
   ::ttnn::DataType dtype =
       ::tt::runtime::ttnn::operations::utils::getDataType(op->out());
 
-  const ::flatbuffers::Vector<uint8_t> *rawData = op->data();
-  ::tt::tt_metal::OwnedBuffer owned_buffer;
-  switch (dtype) {
-  case ::ttnn::DataType::FLOAT32:
-    owned_buffer = buffer<float>(rawData);
-    break;
-  case ::ttnn::DataType::UINT8:
-    owned_buffer = buffer<uint8_t>(rawData);
-    break;
-  case ::ttnn::DataType::UINT16:
-    owned_buffer = buffer<uint16_t>(rawData);
-    break;
-  case ::ttnn::DataType::INT32:
-    owned_buffer = buffer<int32_t>(rawData);
-    break;
-  case ::ttnn::DataType::UINT32:
-    owned_buffer = buffer<uint32_t>(rawData);
-    break;
-  case ::ttnn::DataType::BFLOAT16:
-    owned_buffer = buffer<bfloat16>(rawData);
-    break;
-  case ::ttnn::DataType::BFLOAT4_B:
-    owned_buffer = buffer<bfloat4_b>(rawData);
-    break;
-  default:
-    LOG_FATAL("Unsupported data type");
-  }
-
-  ::ttnn::Tensor out(::tt::tt_metal::OwnedStorage{owned_buffer}, shape, dtype,
+  ::ttnn::Tensor out(::tt::tt_metal::OwnedStorage(ownedBuffer), shape, dtype,
                      ::ttnn::Layout::ROW_MAJOR);
 
   context.getTensorPool().insert_or_assign(op->out()->global_id(), out);
 }
+
 } // namespace tt::runtime::ttnn::operations::creation
