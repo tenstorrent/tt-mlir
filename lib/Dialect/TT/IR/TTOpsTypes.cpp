4f0eb32904a3fc1a6a4765296655452b628507a0
Vlad Roubtsov <vroubtsov@tenstorrent.com>
2025-02-05 00:24:39 +0000

    clang-tidy found an #include reordering

--- lib/Dialect/TT/IR/TTOpsTypes.cpp
+++ lib/Dialect/TT/IR/TTOpsTypes.cpp
@@ -11,9 +11,9 @@
 #include <llvm/ADT/SmallVector.h>
 #include <llvm/ADT/StringExtras.h>
 #include <llvm/ADT/TypeSwitch.h>
-#include <mlir/IR/BuiltinTypes.h>
 #include <mlir/IR/Builders.h>
 #include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/BuiltinTypes.h>
 #include <mlir/IR/DialectImplementation.h>
 #include <mlir/Support/LLVM.h>
 
