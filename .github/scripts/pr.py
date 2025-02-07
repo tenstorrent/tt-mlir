aba4fb8edb07131c664703c08afa351af427ff7d
John McGrath <jmcgrath@tenstorrent.com>
2025-02-07 09:58:07 -0600

    test

--- .github/scripts/pr.py
+++ .github/scripts/pr.py
@@ -21,13 +21,17 @@ pull_request: dict = json.loads(os.environ.get('pull_request', '{}'))
 changed_files: dict = json.loads(os.environ.get('changed_files', '{}'))
 
 def to_github_output(outputs: dict):
+    print_builder = 'Here are your available outputs:\n'
+    for x, z in outputs.items():
+        print_builder += f'\tOutput values for job: {x}\n'
+        for y, t in z.items():
+            print_builder += f'\tfromJson(needs.pr-skip-check.outputs.results).{x}.{y}\n'
+            print_builder += f'\tvalue: {t}'
+        
+
+    # TODO: add test flag
     github_output = os.environ['GITHUB_OUTPUT']
     payload =json.dumps(outputs)
-    print_builder = ''
-    print_builder += 'Here are your available outputs'
-    for x in outputs:
-        pass
-    # TODO: add debug flag
     with open(github_output, "a") as myfile:
         myfile.write(f"results={payload}")
         
@@ -40,7 +44,7 @@ def files_regex(action: str, manifest: dict) -> dict:
     # https://github.com/tj-actions/changed-files?tab=readme-ov-file#outputs-
     changed_files_list: list = changed_files.get(action).split(' ')
     changed_files_regex = re.compile('|'.join(manifest))
-    return  all([changed_files_regex.match(x) for x in changed_files_list])
+    return all([changed_files_regex.match(x) for x in changed_files_list])
     
 def draft_pr(manifest: str):
     is_draft = pull_request.get("draft")
@@ -51,12 +55,12 @@ def draft_pr(manifest: str):
 def skip(manifest: dict) -> bool:
     skip_manifest = manifest.get('skip')
     if not skip_manifest:
-        return False
+        return "false"
     if draft_pr(skip_manifest.get('draft_pr')):
-        return True
+        return "true"
     if files_regex('all_changed_and_modified_files', skip_manifest.get('files_regex')):
-        return True
-    return False
+        return "true"
+    return "false"
 
 def main():
     json_config: Path = Path.joinpath(Path(__file__).resolve().parent, 'pr.json')
