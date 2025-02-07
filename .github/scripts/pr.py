5bed99c6a3b7f1abe5c728328af0363aa4d152b7
John McGrath <jmcgrath@tenstorrent.com>
2025-02-07 00:35:04 -0600

    test

--- .github/scripts/pr.py
+++ .github/scripts/pr.py
@@ -6,26 +6,68 @@
 import os
 import json
 import re
-from typing import Dict
+from pathlib import Path
 
-no_build_re = re.compile('^')
+os.environ["pull_request"] = '{"draft": "false"}'
+os.environ["changed_files"] =  '{ "all_changed_and_modified_files": ".github/scripts/pr.py docs/src/build.md" }' 
 
-def to_github_output(output: str):
-    payload =json.dumps({"test": "hello_word"})
-    with open(output, "a") as myfile:
+#os.environ["pull_request"] = '{"draft": "true"}'
+#os.environ["changed_files"] =  '{ "all_changed_and_modified_files": ".github/scripts/pr.py docs/src/build.md" }' 
+#
+#os.environ["pull_request"] = '{"draft": "false"}'
+#os.environ["changed_files"] =  '{ "all_changed_and_modified_files": "docs/src/build.md" }' 
+
+pull_request: dict = json.loads(os.environ.get('pull_request', '{}'))
+changed_files: dict = json.loads(os.environ.get('changed_files', '{}'))
+
+def to_github_output(outputs: dict):
+    github_output = os.environ['GITHUB_OUTPUT']
+    payload =json.dumps(outputs)
+    print_builder = ''
+    print_builder += 'Here are your available outputs'
+    for x in outputs:
+        pass
+    # TODO: add debug flag
+    with open(github_output, "a") as myfile:
         myfile.write(f"results={payload}")
+        
+    
 
-def pr(changed_files: Dict, pull_request: dict, output: str ):
-    run_test_and_build = True
-    all_changed_and_modified_files = changed_files.get('all_changed_and_modified_files').split(' ')
-    print(all_changed_and_modified_files)
-    to_github_output(output)
-
+def files_regex(action: str, manifest: dict) -> dict:
+    if not manifest:
+        return False
+    # Avaible actions
+    # https://github.com/tj-actions/changed-files?tab=readme-ov-file#outputs-
+    changed_files_list: list = changed_files.get(action).split(' ')
+    changed_files_regex = re.compile('|'.join(manifest))
+    return  all([changed_files_regex.match(x) for x in changed_files_list])
+    
+def draft_pr(manifest: str):
+    is_draft = pull_request.get("draft")
+    if manifest == 'true' and is_draft == 'true':
+        return True
+    return False
+        
+def skip(manifest: dict) -> bool:
+    skip_manifest = manifest.get('skip')
+    if not skip_manifest:
+        return False
+    if draft_pr(skip_manifest.get('draft_pr')):
+        return True
+    if files_regex('all_changed_and_modified_files', skip_manifest.get('files_regex')):
+        return True
+    return False
 
 def main():
-    output = os.environ['GITHUB_OUTPUT']
-    changed_files = json.loads(os.environ.get('changed_files'))
-    pull_request = json.loads(os.environ.get('pull_request'))
-    pr(changed_files, pull_request, output)
+    json_config: Path = Path.joinpath(Path(__file__).resolve().parent, 'pr.json')
+    manifests: dict = json.loads(json_config.read_text())
+
+    outputs = {}
+
+    for manifest in manifests:
+        job_name = manifest['job_name']
+        outputs[job_name] = {} 
+        outputs[job_name]['skip']= skip(manifest)
+    to_github_output(outputs)
 
 main()
--- .github/scripts/test_pr.py
+++ .github/scripts/test_pr.py
@@ -1,14 +1,12 @@
-import pytest
+#import pytest
 from typing import Dict
-from .pr import pr
+from pr import main
+import os
 
-
-
-@pytest.mark.parametrize(
-    "changed_files, pull_request",
-    [
-        ({ "all_changed_and_modified_files": ".github/scripts/pr.py docs/src/build.md" }, {"draft": "false"}),
-    ],
-)
-def test_build_test_skip(changed_files: Dict, pull_request: Dict):
-    pr(changed_files, pull_request)
+#@pytest.fixture(scope="session", autouse=True)
+def test_build_test_skip():
+        os.environ["pull_request"] = {"draft": "false"}
+        os.environ["changed_files"] =  { "all_changed_and_modified_files": ".github/scripts/pr.py docs/src/build.md" } 
+        main()
+    
+test_build_test_skip()
