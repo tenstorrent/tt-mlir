150356b1a72ff5529451493470792a2007b564b2
John McGrath <jmcgrath@tenstorrent.com>
2025-02-06 20:35:35 -0600

    test

--- .github/scripts/pr.py
+++ .github/scripts/pr.py
@@ -10,19 +10,21 @@ from typing import Dict
 
 no_build_re = re.compile('^')
 
-def to_github_output():
-    pass
+def to_github_output(output: str):
+    with open(output, "a") as myfile:
+        myfile.write("test=hello-world")
 
-def pr(changed_files: Dict, pull_request: dict ):
+def pr(changed_files: Dict, pull_request: dict, output: str ):
     run_test_and_build = True
     all_changed_and_modified_files = changed_files.get('all_changed_and_modified_files').split(' ')
     print(all_changed_and_modified_files)
+    to_github_output(output)
 
 
 def main():
-    print(os.environ['output'])
-    #changed_files = json.loads(os.environ.get('changed_files'))
-    #pull_request = json.loads(os.environ.get('pull_request'))
-    #pr(changed_files, pull_request)
+    output = os.environ['output']
+    changed_files = json.loads(os.environ.get('changed_files'))
+    pull_request = json.loads(os.environ.get('pull_request'))
+    pr(changed_files, pull_request, output)
 
 main()
