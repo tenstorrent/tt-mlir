af5ba74186488cad0ef9ee111433ffe2fae5b0d5
Jacob DeSousa <jdesousa@tenstorrent.com>
2025-02-10 20:39:50 +0000

    include style

--- lib/Dialect/TTIR/IR/TTIRDialect.cpp
+++ lib/Dialect/TTIR/IR/TTIRDialect.cpp
@@ -4,12 +4,12 @@
 
 #include "ttmlir/Dialect/TT/IR/TT.h"
 #include "ttmlir/Dialect/TTIR/IR/TTIR.h"
+#include "ttmlir/Dialect/TTIR/IR/TTIROps.h"
+#include "ttmlir/Dialect/TTIR/IR/TTIRTileOps.h"
 
 #include "mlir/InitAllDialects.h"
 #include "mlir/Interfaces/FoldInterfaces.h"
 #include "mlir/Transforms/InliningUtils.h"
-#include "ttmlir/Dialect/TTIR/IR/TTIROps.h"
-#include "ttmlir/Dialect/TTIR/IR/TTIRTileOps.h"
 #include "llvm/ADT/StringSet.h"
 #include "llvm/ADT/TypeSwitch.h"
 
