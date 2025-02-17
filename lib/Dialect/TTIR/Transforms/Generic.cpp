f1d21709d3fef2d9cb3f1437fee57169cd813604
Vlad Roubtsov <vroubtsov@tenstorrent.com>
2025-02-17 23:47:37 +0000

    fix style and #include formatting

--- lib/Dialect/TTIR/Transforms/Generic.cpp
+++ lib/Dialect/TTIR/Transforms/Generic.cpp
@@ -5,12 +5,12 @@
 #include "ttmlir/Dialect/TT/IR/TT.h"
 #include "ttmlir/Dialect/TTIR/Transforms/Passes.h"
 
-#include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Dialect/Linalg/IR/Linalg.h"
-#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include <mlir/Dialect/Arith/IR/Arith.h>
+#include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/Dialect/Linalg/IR/Linalg.h>
+#include <mlir/Dialect/Tensor/IR/Tensor.h>
 
-namespace mlir::tt::ttir {  
+namespace mlir::tt::ttir {
 #include "ttmlir/Dialect/TTIR/Transforms/Passes.h.inc"
 
 } // namespace mlir::tt::ttir
