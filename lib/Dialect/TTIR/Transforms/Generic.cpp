ac5491c0576f252c22ff96701e321eaecdd1f8b2
Vlad Roubtsov <vroubtsov@tenstorrent.com>
2025-02-17 23:21:53 +0000

    restore Generic.cpp as an empty file with stock #includes

---
+++ lib/Dialect/TTIR/Transforms/Generic.cpp
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ttmlir/Dialect/TT/IR/TT.h"
+#include "ttmlir/Dialect/TTIR/Transforms/Passes.h"
+
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+
+namespace mlir::tt::ttir {  
+#include "ttmlir/Dialect/TTIR/Transforms/Passes.h.inc"
+
+} // namespace mlir::tt::ttir
