234eb00bdbb22fd91f9d74f21dd3fc23370aa5fb
Aleksandar Zecevic <azecevic@tenstorrent.com>
2025-02-18 18:43:44 +0100

    Remove `QueueId` from ops in runtime (#2173)
    ### Ticket
    https://github.com/tenstorrent/tt-mlir/issues/2167
    
    ### Problem description
    With https://github.com/tenstorrent/tt-metal/pull/17640 `QueueId` is now
    optional in all calls. There were 3 cases where we couldn't use
    signature without `QueueId`, that restriction is now removed.
    
    ### What's changed
    Removed `QueueId` and stuff related to it from `conv_transpose2d`, `pad`
    and `max_pool2d`.
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- runtime/lib/ttnn/operations/pool/maxpool2d.cpp
+++ runtime/lib/ttnn/operations/pool/maxpool2d.cpp
@@ -14,24 +14,19 @@ namespace tt::runtime::ttnn::operations::pool {
 
 void run(const ::tt::target::ttnn::MaxPool2dOp *op, ProgramContext &context) {
   ProgramTensorPool &tensorPool = context.getTensorPool();
-  const ::ttnn::operations::pool::Pool2DOp<
-      ::ttnn::operations::pool::Pool2DType::MAX_POOL2D>
-      operation = ::ttnn::operations::pool::Pool2DOp<
-          ::ttnn::operations::pool::Pool2DType::MAX_POOL2D>();
 
   ::ttnn::Tensor input = tensorPool.at(op->in()->global_id());
   DEBUG_ASSERT(input.is_allocated());
 
   ::ttnn::MemoryConfig outMemConfig =
       ::tt::runtime::ttnn::utils::createMemoryConfig(op->out());
-  ::ttnn::Tensor out =
-      operation.invoke(::ttnn::DefaultQueueId, input, op->batch_size(),
-                       op->input_height(), op->input_width(), op->channels(),
-                       {op->kernel_height(), op->kernel_width()},
-                       {op->stride_height(), op->stride_width()},
-                       {op->padding_height(), op->padding_width()},
-                       {op->dilation_height(), op->dilation_width()},
-                       outMemConfig, std::nullopt);
+  ::ttnn::Tensor out = ::ttnn::max_pool2d(
+      input, op->batch_size(), op->input_height(), op->input_width(),
+      op->channels(), std::array{op->kernel_height(), op->kernel_width()},
+      std::array{op->stride_height(), op->stride_width()},
+      std::array{op->padding_height(), op->padding_width()},
+      std::array{op->dilation_height(), op->dilation_width()}, outMemConfig,
+      std::nullopt);
 
   tensorPool.insert_or_assign(op->out()->global_id(), out);
 }
