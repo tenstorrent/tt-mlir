979a9ce34bdd7709d06bd4ec321c58b19ef55f2e
ajakovljevicTT <ajakovljevic@tenstorrent.com>
2025-02-19 12:43:01 +0000

    Channel handle fix

--- lib/Conversion/StableHLOToTTIR/StableHLOToTTIRPatterns.cpp
+++ lib/Conversion/StableHLOToTTIR/StableHLOToTTIRPatterns.cpp
@@ -1280,17 +1280,6 @@ public:
           return failure();
         }
 
-        // channelType is supposed to be DEVICE_TO_DEVICE for CCL ops.
-        // Currently, we ensure if it is DEVICE_TO_DEVICE commmuincaiton.
-        // Consider preserving this information in the future if the attribute
-        // is non-DEVICE_TO_DEVICE values.
-        std::cerr << "DDDD" << std::endl;
-        auto channelType = static_cast<int32_t>(srcChannelHandleAttr.getType());
-        channelType = kChannelTypeDeviceToDevice;
-        if (channelType != kChannelTypeDeviceToDevice) {
-          return failure();
-        }
-
         std::cerr << "EEEE" << std::endl;
         IntegerAttr channelHandleAttr = rewriter.getSI32IntegerAttr(
             static_cast<int32_t>(srcChannelHandleAttr.getHandle()));
