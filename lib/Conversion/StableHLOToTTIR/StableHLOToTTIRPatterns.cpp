18ede091e339582339d395824e349b7779bb0a07
Andrej Jakovljevic <ajakovljevic@tenstorrent.com>
2025-02-20 16:26:30 +0000

    Channel fix

--- lib/Conversion/StableHLOToTTIR/StableHLOToTTIRPatterns.cpp
+++ lib/Conversion/StableHLOToTTIR/StableHLOToTTIRPatterns.cpp
@@ -1220,6 +1220,7 @@ public:
         // Consider preserving this information in the future if the attribute
         // is non-DEVICE_TO_DEVICE values.
         auto channelType = static_cast<int32_t>(channelHandle.getType());
+        channelType = kChannelTypeDeviceToDevice;
         if (channelType != kChannelTypeDeviceToDevice) {
           return failure();
         }
