b603dcbd342f3d63aeb8366b3a0b7b67f4e52e3b
Aleksandar Zecevic <azecevic@tenstorrent.com>
2025-02-24 22:26:32 +0000

    Setup namespaces

---
+++ include/ttmlir/Conversion/TTNNToEmitC/EmitCConversion.h
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef TTMLIR_CONVERSION_TTNNTOEMITC_EMITCCONVERSION_H
+#define TTMLIR_CONVERSION_TTNNTOEMITC_EMITCCONVERSION_H
+
+// This namespace contains mock declarations of TTNN types for the purpose of
+// conversion.
+namespace ttnn {} // namespace ttnn
+
+namespace mlir {
+namespace tt {
+namespace ttnn_to_emitc {} // namespace ttnn_to_emitc
+} // namespace tt
+} // namespace mlir
+
+#endif // TTMLIR_CONVERSION_TTNNTOEMITC_EMITCCONVERSION_H
