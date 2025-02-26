72f1a1b9940680a0ea517d90db7a98be2d280041
Aleksandar Zecevic <azecevic@tenstorrent.com>
2025-02-26 23:11:19 +0100

    Create `ttnn.neg` as non-DPS op (#2284)
    ### Ticket
    https://github.com/tenstorrent/tt-mlir/issues/2283
    
    ### Problem description
    In one place in TTIR->TTNN conversion, `NegOp` is created as DPS op,
    even though it isn't.
    
    ### What's changed
    Since `NegOp` is elementwise op and they were recently changed from DPS
    to non DPS op it was an error to create it like a DPS op, because it
    took empty tensor as an input. Added `static_assert`s in DPSOp creation
    functions to check if target op is actually a DPS op.
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- include/ttmlir/Utils.h
+++ include/ttmlir/Utils.h
@@ -366,6 +366,9 @@ auto splitAndCall(mlir::PatternRewriter &rewriter, mlir::Location loc,
 template <typename OpTy, typename... ArgsTy>
 OpTy createDPSOp(mlir::PatternRewriter &rewriter, mlir::Location loc,
                  mlir::RankedTensorType outputType, ArgsTy &&...args) {
+  static_assert(
+      OpTy::template hasTrait<mlir::DestinationStyleOpInterface::Trait>());
+
   auto output = rewriter.create<mlir::tensor::EmptyOp>(
       loc, outputType.getShape(), outputType.getElementType(),
       outputType.getEncoding());
@@ -393,6 +396,9 @@ OpTy createDPSOp(mlir::PatternRewriter &rewriter, mlir::Location loc,
                  llvm::ArrayRef<int64_t> outputShape,
                  mlir::Type outputElementType, mlir::Attribute outputEncoding,
                  ArgsTy &&...args) {
+  static_assert(
+      OpTy::template hasTrait<mlir::DestinationStyleOpInterface::Trait>());
+
   auto outputType = mlir::RankedTensorType::get(outputShape, outputElementType,
                                                 outputEncoding);
   return createDPSOp<OpTy>(rewriter, loc, outputType,
@@ -414,6 +420,9 @@ template <typename OpTy, typename... ArgsTy>
 OpTy replaceOpWithNewDPSOp(mlir::PatternRewriter &rewriter, mlir::Operation *op,
                            mlir::RankedTensorType outputType,
                            ArgsTy &&...args) {
+  static_assert(
+      OpTy::template hasTrait<mlir::DestinationStyleOpInterface::Trait>());
+
   auto newOp = createDPSOp<OpTy>(rewriter, op->getLoc(), outputType,
                                  std::forward<ArgsTy>(args)...);
   rewriter.replaceOp(op, newOp.getOperation());
@@ -440,6 +449,9 @@ OpTy replaceOpWithNewDPSOp(mlir::PatternRewriter &rewriter, mlir::Operation *op,
                            llvm::ArrayRef<int64_t> outputShape,
                            mlir::Type outputElementType,
                            mlir::Attribute outputEncoding, ArgsTy &&...args) {
+  static_assert(
+      OpTy::template hasTrait<mlir::DestinationStyleOpInterface::Trait>());
+
   auto newOp =
       createDPSOp<OpTy>(rewriter, op->getLoc(), outputShape, outputElementType,
                         outputEncoding, std::forward<ArgsTy>(args)...);
