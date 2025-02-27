3c127a3c9fad77696b7e0f04589844420860ae12
Stefan Djordjevic <157365107+sdjordjevicTT@users.noreply.github.com>
2025-02-27 01:08:40 +0100

    Removing simple builders from eltwise binary and ternary ops. (#2295)
    ### Ticket
    Closes https://github.com/tenstorrent/tt-mlir/issues/2294
    
    ### Problem description
    Frontends reported a few issues when the broadcast is applied to eltwise
    ops.
    
    ### What's changed
    We removed the simple builders for eltwise binary and ternary ops. This
    is the main reason for bugs in the code, hence we are temporarily
    removing them to unblock the uplift. We have created the following issue
    to follow up on these builders later:
    https://github.com/tenstorrent/tt-mlir/issues/2263
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes

--- test/unittests/Optimizer/TestShardSolver.cpp
+++ test/unittests/Optimizer/TestShardSolver.cpp
@@ -148,7 +148,7 @@ TEST_F(ShardSolverBase, VerifyProduceMaxCoreUsage) {
   mlir::Value lhs = func.getBody().getBlocks().front().getArgument(0);
   mlir::Value rhs = func.getBody().getBlocks().front().getArgument(1);
   mlir::Operation *op =
-      builder.create<AddOp>(builder.getUnknownLoc(), lhs, rhs);
+      builder.create<AddOp>(builder.getUnknownLoc(), lhs, rhs, lhs.getType());
   mlir::Operation *firstOp = op;
 
   prepareOpForShardSolver(op, opL1MemSpecs, l1ChainedOps);
@@ -172,7 +172,7 @@ TEST_F(ShardSolverBase, VerifyProduceMaxCoreUsage) {
   lhs = func.getBody().getBlocks().front().getArgument(0);
   rhs = op->getResult(0);
 
-  op = builder.create<AddOp>(builder.getUnknownLoc(), lhs, rhs);
+  op = builder.create<AddOp>(builder.getUnknownLoc(), lhs, rhs, lhs.getType());
   prepareOpForShardSolver(op, opL1MemSpecs, l1ChainedOps);
   addLayoutForOp(op, legalLayouts, BufferType::L1,
                  TensorMemoryLayout::WidthSharded, 1, 4);
@@ -181,7 +181,7 @@ TEST_F(ShardSolverBase, VerifyProduceMaxCoreUsage) {
   addLayoutForOp(op, legalLayouts, BufferType::L1,
                  TensorMemoryLayout::BlockSharded, 1, 1);
 
-  op = builder.create<AddOp>(builder.getUnknownLoc(), lhs, rhs);
+  op = builder.create<AddOp>(builder.getUnknownLoc(), lhs, rhs, lhs.getType());
   prepareOpForShardSolver(op, opL1MemSpecs, l1ChainedOps);
   addLayoutForOp(op, legalLayouts, BufferType::L1,
                  TensorMemoryLayout::WidthSharded, 1, 4);
@@ -192,7 +192,7 @@ TEST_F(ShardSolverBase, VerifyProduceMaxCoreUsage) {
 
   lhs = opL1MemSpecs[opL1MemSpecs.size() - 2].op->getResult(0);
   rhs = opL1MemSpecs[opL1MemSpecs.size() - 1].op->getResult(0);
-  op = builder.create<AddOp>(builder.getUnknownLoc(), lhs, rhs);
+  op = builder.create<AddOp>(builder.getUnknownLoc(), lhs, rhs, lhs.getType());
   prepareOpForShardSolver(op, opL1MemSpecs, l1ChainedOps);
   addLayoutForOp(op, legalLayouts, BufferType::L1,
                  TensorMemoryLayout::WidthSharded, 1, 2);
