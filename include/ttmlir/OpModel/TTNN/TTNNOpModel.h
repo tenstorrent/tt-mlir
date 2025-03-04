e8249045e20f1fe70792edbd41cdbe69a8dd9387
Armin Ale <arminale@tenstorrent.com>
2025-03-04 22:12:36 +0000

    unit test with segfault

--- include/ttmlir/OpModel/TTNN/TTNNOpModel.h
+++ include/ttmlir/OpModel/TTNN/TTNNOpModel.h
@@ -128,7 +128,7 @@ namespace ToLayoutOpInterface {
 llvm::Expected<std::tuple<size_t, size_t, size_t>>
 getOpConstraints(llvm::ArrayRef<int64_t> inputShape,
                  mlir::tt::ttnn::TTNNLayoutAttr inputLayout,
-                 mlir::tt::ttnn::LayoutAttr outputPageLayout,
+                 mlir::tt::ttnn::Layout outputPageLayout,
                  std::optional<mlir::tt::DataTypeAttr> outputDtype,
                  mlir::tt::ttnn::TTNNLayoutAttr outputLayout,
                  bool passDevicePtr);
@@ -136,7 +136,7 @@ getOpConstraints(llvm::ArrayRef<int64_t> inputShape,
 llvm::Expected<size_t>
 getOpRuntime(llvm::ArrayRef<int64_t> inputShape,
              mlir::tt::ttnn::TTNNLayoutAttr inputLayout,
-             mlir::tt::ttnn::LayoutAttr outputPageLayout,
+             mlir::tt::ttnn::Layout outputPageLayout,
              std::optional<mlir::tt::DataTypeAttr> outputDtype,
              mlir::tt::ttnn::TTNNLayoutAttr outputLayout, bool passDevicePtr);
 
