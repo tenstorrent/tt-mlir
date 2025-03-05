dc0670ba29438c083a64136cfc4737168826036b
Nick Smith <127986401+nsmithtt@users.noreply.github.com>
2025-03-04 19:13:03 -0600

    Generic region to loops (#2306)
    This change adds a few new passes:
    
    ## `linalg.generic` to affine passes
    
    Simply calls the upstream pass for converting a `linalg.generic` into an
    affine loop nest.
    
    ## Lower affine pass
    
    Again, just uses an upstream pass for converting affine into SCF and
    arith.
    
    ## Linearize memref accesses pass
    
    A custom TTIR pass that takes a nested loop structure over n-dimensional
    memrefs and linearizes them into a single dimension. This is a useful
    because circular buffers in metal are only one-dimensional.
        
    Example, this pass will convert the following code:        
    ```mlir        
      affine.for %arg5 = 0 to 2 {        
        affine.for %arg6 = 0 to 4 {        
          %0 = affine.load %arg2[%arg5, %arg6] : memref<2x4x!tt.tile<32x32, f32>, #l1_>        
          %1 = affine.load %arg3[%arg5, %arg6] : memref<2x4x!tt.tile<32x32, f32>, #l1_>        
          %2 = "ttir.tile_maximum"(%0, %1) : (!tt.tile<32x32, f32>, !tt.tile<32x32, f32>) -> !tt.tile<32x32, f32>        
          affine.store %2, %arg4[%arg5, %arg6] : memref<2x4x!tt.tile<32x32, f32>, #l1_>                                                       
        }        
      }                                                                              
    ```
                                                                 
    Into:
    ```mlir                                                                                    
      %collapse_shape = memref.collapse_shape %arg2 [[0, 1]] : memref<2x4x!tt.tile<32x32, f32>, #l1_> into memref<8x!tt.tile<32x32, f32>, #l1_>        
      %collapse_shape_0 = memref.collapse_shape %arg3 [[0, 1]] : memref<2x4x!tt.tile<32x32, f32>, #l1_> into memref<8x!tt.tile<32x32, f32>, #l1_>        
      %collapse_shape_1 = memref.collapse_shape %arg4 [[0, 1]] : memref<2x4x!tt.tile<32x32, f32>, #l1_> into memref<8x!tt.tile<32x32, f32>, #l1_>        
      affine.for %arg5 = 0 to 2 {                                    
        affine.for %arg6 = 0 to 4 {                                                       
          %0 = affine.load %collapse_shape[%arg5 * 4 + %arg6] : memref<8x!tt.tile<32x32, f32>, #l1_>        
          %1 = affine.load %collapse_shape_0[%arg5 * 4 + %arg6] : memref<8x!tt.tile<32x32, f32>, #l1_>                                                                         
          %2 = "ttir.tile_maximum"(%0, %1) : (!tt.tile<32x32, f32>, !tt.tile<32x32, f32>) -> !tt.tile<32x32, f32>        
          affine.store %2, %collapse_shape_1[%arg5 * 4 + %arg6] : memref<8x!tt.tile<32x32, f32>, #l1_>        
        }                                                         
      }                                     
    ```
    
    Closes #1910
    Closes #1911

---
+++ lib/Dialect/TTIR/Transforms/GenericLinearizeMemref.cpp
@@ -0,0 +1,126 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ttmlir/Dialect/TT/IR/TT.h"
+#include "ttmlir/Dialect/TTIR/Transforms/Passes.h"
+#include "ttmlir/Utils.h"
+
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+#include <numeric>
+
+namespace mlir::tt::ttir {
+#define GEN_PASS_DEF_TTIRGENERICLINEARIZEMEMREF
+#include "ttmlir/Dialect/TTIR/Transforms/Passes.h.inc"
+
+namespace {
+struct TTIRGenericLinearizeMemrefRewriter final
+    : public OpRewritePattern<GenericOp> {
+public:
+  using OpRewritePattern<GenericOp>::OpRewritePattern;
+
+  static bool isLinearizedMemref(BlockArgument arg) {
+    auto memref = mlir::cast<MemRefType>(arg.getType());
+    if (memref.getShape().size() == 1) {
+      return true;
+    }
+
+    return llvm::all_of(arg.getUsers(), [](Operation *user) {
+      return mlir::isa<memref::CollapseShapeOp>(user);
+    });
+  }
+
+  static mlir::AffineMap linearizeAffineMap(::mlir::MLIRContext *context,
+                                            mlir::AffineMap map,
+                                            ArrayRef<int64_t> shape) {
+    auto evaledShape = ttmlir::utils::evalShape(map, shape);
+    mlir::AffineExpr indexing = getAffineConstantExpr(0, context);
+    mlir::AffineExpr volumeExpr = getAffineConstantExpr(1, context);
+
+    assert(map.getNumResults() > 0);
+    for (int i = map.getNumResults() - 1; i >= 0; i--) {
+      mlir::AffineExpr linearIdx = getAffineDimExpr(i, context);
+      mlir::AffineExpr dim = getAffineConstantExpr(evaledShape[i], context);
+      indexing = linearIdx * volumeExpr + indexing;
+      volumeExpr = volumeExpr * dim;
+    }
+
+    mlir::AffineMap linearResult =
+        mlir::AffineMap::get(map.getNumResults(), 0, indexing, context);
+    return linearResult.compose(map);
+  }
+
+  LogicalResult matchAndRewrite(GenericOp op,
+                                PatternRewriter &rewriter) const final {
+    Block *entry = &op.getRegion().front();
+    rewriter.setInsertionPointToStart(entry);
+    auto args = entry->getArguments();
+    if (llvm::all_of(args, isLinearizedMemref)) {
+      return failure();
+    }
+
+    rewriter.modifyOpInPlace(op, [&]() {
+      for (auto arg : args) {
+        if (isLinearizedMemref(arg)) {
+          continue;
+        }
+        auto memref = mlir::cast<MemRefType>(arg.getType());
+        auto shape = memref.getShape();
+        SmallVector<ReassociationIndices, 4> collapsedDims = {
+            llvm::to_vector(llvm::seq<int64_t>(0, shape.size()))};
+        assert(memref::CollapseShapeOp::isGuaranteedCollapsible(
+                   memref, collapsedDims) &&
+               "linearizeAffineMap assumes that the shape is collapsible aka "
+               "has contiguous memory layout");
+        auto linearMap = linearizeAffineMap(
+            rewriter.getContext(), memref.getLayout().getAffineMap(), shape);
+        auto linearizedArg = rewriter.create<memref::CollapseShapeOp>(
+            arg.getLoc(), arg, collapsedDims);
+        rewriter.replaceAllUsesExcept(arg, linearizedArg->getResult(0),
+                                      linearizedArg);
+        for (auto *user : linearizedArg->getUsers()) {
+          if (auto load = mlir::dyn_cast<affine::AffineLoadOp>(user)) {
+            load.setMap(linearMap.compose(load.getMap()));
+          } else if (auto store = mlir::dyn_cast<affine::AffineStoreOp>(user)) {
+            store.setMap(linearMap.compose(store.getMap()));
+          }
+        }
+      }
+    });
+
+    return success();
+  }
+};
+} // namespace
+
+namespace {
+class TTIRGenericLinearizeMemref
+    : public impl::TTIRGenericLinearizeMemrefBase<TTIRGenericLinearizeMemref> {
+public:
+  using impl::TTIRGenericLinearizeMemrefBase<
+      TTIRGenericLinearizeMemref>::TTIRGenericLinearizeMemrefBase;
+
+  void runOnOperation() final {
+    RewritePatternSet patterns(&getContext());
+    patterns.add<TTIRGenericLinearizeMemrefRewriter>(&getContext());
+    FrozenRewritePatternSet patternSet(std::move(patterns));
+    if (failed(applyPatternsGreedily(getOperation(), patternSet))) {
+      signalPassFailure();
+    }
+  }
+  void getDependentDialects(mlir::DialectRegistry &registry) const override {
+    registry.insert<mlir::tt::ttir::TTIRDialect>();
+    registry.insert<mlir::tt::TTDialect>();
+    registry.insert<mlir::arith::ArithDialect>();
+  }
+};
+} // namespace
+
+} // namespace mlir::tt::ttir
