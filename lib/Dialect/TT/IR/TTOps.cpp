80223c69cccb88a82c85dfae45cc47e184f600c0
Vincent Wells <vwells@tenstorrent.com>
2025-03-12 18:51:45 +0000

    buildable

--- lib/Dialect/TT/IR/TTOps.cpp
+++ lib/Dialect/TT/IR/TTOps.cpp
@@ -115,97 +115,51 @@ LogicalResult DeviceModuleOp::verify() { return verifyModuleWrapper(*this); }
 
 LogicalResult CPUModuleOp::verify() { return verifyModuleWrapper(*this); }
 
-<<<<<<< HEAD
 LogicalResult LoadCachedOp::verify() {
-  // Verify that the callee exists and has the right type.
+  // Verify that the callee exists
   FlatSymbolRefAttr calleeAttr = this->getCalleeAttr();
-  func::FuncOp funcOp =
+  func::FuncOp calleeFunc =
       SymbolTable::lookupNearestSymbolFrom<func::FuncOp>(*this, calleeAttr);
-  if (!funcOp) {
+  if (!calleeFunc) {
     return emitOpError() << "'" << calleeAttr.getValue()
                          << "' does not reference a function";
   }
 
-  FunctionType fnType = funcOp.getFunctionType();
-
-  if (fnType.getNumInputs() != this->getNumOperands()) {
-    return emitOpError("Incorrect number of operands for callee -- expected ")
-           << fnType.getNumInputs() << " but got: " << this->getNumOperands();
+  // Get the function where this op resides
+  Operation *op = this->getOperation();
+  func::FuncOp parentFunc = op->getParentOfType<func::FuncOp>();
+  if (!parentFunc) {
+    return emitOpError() << "must be nested inside a function";
   }
+  FunctionType parentFnType = parentFunc.getFunctionType();
 
-  for (unsigned i = 0; i < fnType.getNumInputs(); ++i) {
-    if (this->getOperand(i).getType() != fnType.getInput(i)) {
-      return emitOpError("Operand type mismatch at index ") << i;
+  // Verify that all indices in input_indices are valid
+  llvm::ArrayRef<int32_t> inputIndices = this->getInputIndices();
+
+  // Verify each input index is valid for the parent function's signature
+  for (const int32_t index : inputIndices) {
+    if (index < 0 ||
+        static_cast<unsigned>(index) >= parentFnType.getNumInputs()) {
+      return emitOpError("input index ")
+             << index << " out of range; enclosing function has "
+             << parentFnType.getNumInputs() << " parameters";
     }
   }
 
+  FunctionType calleeFnType = calleeFunc.getFunctionType();
   // Verify result count.
-  if (fnType.getNumResults() != this->getNumResults()) {
+  if (calleeFnType.getNumResults() != this->getNumResults()) {
     return emitOpError("Incorrect number of results for callee -- expected ")
-           << fnType.getNumResults() << " but got: " << this->getNumResults();
+           << calleeFnType.getNumResults()
+           << " but got: " << this->getNumResults();
   }
 
   // Verify result types.
-  for (unsigned i = 0; i < fnType.getNumResults(); ++i) {
-    if (this->getResult(i).getType() != fnType.getResult(i)) {
+  for (unsigned i = 0; i < calleeFnType.getNumResults(); ++i) {
+    if (this->getResult(i).getType() != calleeFnType.getResult(i)) {
       return emitOpError("Result type mismatch at index ") << i;
     }
   }
-=======
-LogicalResult verifyTTLoadCachedOp(TT_LoadCachedOp op) {
-  // Verify that the callee exists and has the right type
-  SymbolRefAttr calleeAttr = op.getCalleeAttr();
-  Operation *callee = SymbolTable::lookupNearestSymbolFrom(op, calleeAttr);
-  if (!callee)
-    return op.emitOpError() << "'" << calleeAttr.getValue()
-                            << "' does not reference a valid function";
-
-  auto fnType = dyn_cast<FunctionType>(
-      callee->getAttrOfType<TypeAttr>("function_type").getValue());
-  if (!fnType)
-    return op.emitOpError("callee does not have function type");
-
-  // Verify input arity
-  if (fnType.getNumInputs() != op.getInputs().size())
-    return op.emitOpError("incorrect number of arguments for callee");
-
-  // Verify input types
-  for (unsigned i = 0; i < fnType.getNumInputs(); ++i) {
-    if (op.getInputs()[i].getType() != fnType.getInput(i))
-      return op.emitOpError("argument type mismatch at index ") << i;
-  }
-
-  // Verify result types by checking that the function returns a tuple
-  // and the tuple elements match our result types
-  // This assumes you have a TT_TupleOp defined elsewhere
-
-  // Look for a return op with tt.tuple
-  bool foundTupleReturn = false;
-  func::FuncOp funcOp = cast<func::FuncOp>(callee);
-  for (Block &block : funcOp.getBody()) {
-    if (auto returnOp =
-            dyn_cast_or_null<func::ReturnOp>(block.getTerminator())) {
-      if (returnOp.getNumOperands() == 1) {
-        if (auto tupleOp = dyn_cast_or_null<TT_TupleOp>(
-                returnOp.getOperand(0).getDefiningOp())) {
-          foundTupleReturn = true;
-
-          if (tupleOp.getNumResults() != op.getNumResults())
-            return op.emitOpError(
-                "callee returns tuple with wrong number of elements");
-
-          for (unsigned i = 0; i < op.getNumResults(); ++i) {
-            if (op.getResult(i).getType() != tupleOp.getOperand(i).getType())
-              return op.emitOpError("result type mismatch at index ") << i;
-          }
-        }
-      }
-    }
-  }
-
-  if (!foundTupleReturn)
-    return op.emitOpError("callee does not return a tuple");
->>>>>>> 91beea6d4 (still working out schema)
 
   return success();
 }
