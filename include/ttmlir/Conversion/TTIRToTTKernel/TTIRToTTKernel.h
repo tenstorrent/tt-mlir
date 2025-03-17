e6f7de6655be156bf10471fb38a32fdb328fbb31
Jacob DeSousa <jdesousa@tenstorrent.com>
2025-03-17 17:46:01 +0000

    Cleanup pass arcitecture

--- include/ttmlir/Conversion/TTIRToTTKernel/TTIRToTTKernel.h
+++ include/ttmlir/Conversion/TTIRToTTKernel/TTIRToTTKernel.h
@@ -10,17 +10,13 @@
 
 namespace mlir::tt {
 
-void populateTTIRToTTKernelPatternsPhase1(MLIRContext *ctx,
-                                          RewritePatternSet &patterns,
-                                          TypeConverter &typeConverter);
+void populateTTIRToTTKernelInnerRegionPatterns(MLIRContext *ctx,
+                                               RewritePatternSet &patterns,
+                                               TypeConverter &typeConverter);
 
-void populateTTIRToTTKernelPatternsPhase2(MLIRContext *ctx,
-                                          RewritePatternSet &patterns,
-                                          TypeConverter &typeConverter);
-
-void populateTTIRToTTKernelPatternsPhase3(MLIRContext *ctx,
-                                          RewritePatternSet &patterns,
-                                          TypeConverter & /*typeConverter*/);
+void populateTTIRToTTKernelTopLevelPatterns(MLIRContext *ctx,
+                                            RewritePatternSet &patterns,
+                                            TypeConverter & /*typeConverter*/);
 
 std::unique_ptr<OperationPass<ModuleOp>> createConvertTTIRToTTKernelPass();
 
