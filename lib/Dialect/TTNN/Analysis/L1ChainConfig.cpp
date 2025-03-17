4e0d82a32a61616b7b9f37c566bd29e27afc1576
Ognjen Djuricic <160603639+odjuricicTT@users.noreply.github.com>
2025-03-17 14:26:34 +0100

    Add boilerplate for op configs in optimizer (#2442)
    * Add OpConfig as struct that covers both output layout and op specific
    configurations
    * Change all optimizer analysis to use OpConfig instead of just output
    layout
    * All configs are still null

--- lib/Dialect/TTNN/Analysis/L1ChainConfig.cpp
+++ lib/Dialect/TTNN/Analysis/L1ChainConfig.cpp
@@ -3,7 +3,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "ttmlir/Dialect/TTNN/Analysis/L1ChainConfig.h"
+
 #include "ttmlir/Dialect/TTNN/Analysis/Edge.h"
+#include "ttmlir/Dialect/TTNN/Analysis/OpConfig.h"
 #include "ttmlir/Dialect/TTNN/Analysis/ShardSolver.h"
 
 namespace mlir::tt::ttnn {
@@ -19,8 +21,7 @@ void L1ChainConfig::resolve() {
 }
 
 ShardSolver L1ChainConfig::resolveWithSolver(
-    const llvm::DenseMap<Operation *, std::vector<TTNNLayoutAttr>>
-        &legalLayouts,
+    const llvm::DenseMap<Operation *, std::vector<OpConfig>> &legalConfigs,
     unsigned usableL1CacheSize,
     const std::unordered_set<Edge> &overrideReshardEdges) {
   assert(state == L1ChainState::Built);
@@ -28,7 +29,7 @@ ShardSolver L1ChainConfig::resolveWithSolver(
   // Reconcile adjacent shard specs.
   // Generate reshard specs where needed.
   //
-  ShardSolver shardSolver(legalLayouts, opL1MemSpecs, l1ChainedOps,
+  ShardSolver shardSolver(legalConfigs, opL1MemSpecs, l1ChainedOps,
                           usableL1CacheSize, overrideReshardEdges);
 
   state = shardSolver.resolve() ? L1ChainState::Resolved : L1ChainState::Failed;
@@ -37,14 +38,14 @@ ShardSolver L1ChainConfig::resolveWithSolver(
 }
 
 void L1ChainConfig::complete(
-    const llvm::DenseMap<Operation *, TTNNLayoutAttr> &selectedOpLayout,
+    const llvm::DenseMap<Operation *, OpConfig> &selectedOpConfig,
     std::unordered_set<Edge> &memReconfigEdges) {
   assert(state == L1ChainState::Resolved);
   for (auto &opL1MemSpec : opL1MemSpecs) {
-    auto legalLayoutsIter = selectedOpLayout.find(opL1MemSpec.op);
-    assert(legalLayoutsIter != selectedOpLayout.end());
+    auto legalConfigsIter = selectedOpConfig.find(opL1MemSpec.op);
+    assert(legalConfigsIter != selectedOpConfig.end());
 
-    opL1MemSpec.layout = legalLayoutsIter->second;
+    opL1MemSpec.config = legalConfigsIter->second;
   }
 
   this->memReconfigEdges.swap(memReconfigEdges);
