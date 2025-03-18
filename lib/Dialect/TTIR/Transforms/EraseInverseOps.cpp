ada55edb9224bafeb5223bcda26454ec44788fe0
Lewis Panos <lpanos@tenstorrent.com>
2025-03-18 01:07:20 +0000

    Add patterns to erase back to back TMs. Add a test for commuting a permute up through an eltwise binary. Add a test showcasing that the permute ops between convolutions are erased.

--- lib/Dialect/TTIR/Transforms/EraseInverseOps.cpp
+++ lib/Dialect/TTIR/Transforms/EraseInverseOps.cpp
@@ -10,49 +10,47 @@
 #include "mlir/IR/Operation.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "llvm/Support/LogicalResult.h"
+#include <cstdint>
 #include <llvm/Support/ErrorHandling.h>
 #include <mlir/IR/BuiltinTypes.h>
 #include <mlir/IR/OperationSupport.h>
+#include <mlir/IR/PatternMatch.h>
 
 namespace mlir::tt::ttir {
 #define GEN_PASS_DEF_TTIRERASEINVERSEOPS
 #include "ttmlir/Dialect/TTIR/Transforms/Passes.h.inc"
 
+namespace {
 class TTIRCommuteTmsAboveElementwiseRewriter : public RewritePattern {
 public:
   TTIRCommuteTmsAboveElementwiseRewriter(MLIRContext *ctx)
       : RewritePattern(MatchAnyOpTypeTag(), /*benefit=*/1, ctx) {}
 
-  LogicalResult matchAndRewrite(Operation *op,
-                                PatternRewriter &rewriter) const override {
-
-    if (!op->hasTrait<Elementwise::Trait>()) {
+  LogicalResult match(Operation *op) const override {
+    if (failed(checkTrait(op))) {
       // The op should support implicit broadcast to fold them.
       return failure();
     }
+    return shouldCommute(op);
+  }
 
+  void rewrite(Operation *op, PatternRewriter &rewriter) const override {
     SmallVector<Operation *> users(op->getUsers());
-    if (failed(checkAllUsersAreIdenticalTms(users))) {
-      return failure();
-    }
-
     if (isa<ttir::TransposeOp>(users[0])) {
-      commuteTmsThroughEltwise<ttir::TransposeOp>(op, users, op->getOperand(0),
+      commuteTmsThroughEltwise<ttir::TransposeOp>(op, users, op->getOperands(),
                                                   rewriter);
     } else if (isa<ttir::PermuteOp>(users[0])) {
-      commuteTmsThroughEltwise<ttir::PermuteOp>(op, users, op->getOperand(0),
+      commuteTmsThroughEltwise<ttir::PermuteOp>(op, users, op->getOperands(),
                                                 rewriter);
     } else if (isa<ttir::ReshapeOp>(users[0])) {
-      commuteTmsThroughEltwise<ttir::ReshapeOp>(op, users, op->getOperand(0),
+      commuteTmsThroughEltwise<ttir::ReshapeOp>(op, users, op->getOperands(),
                                                 rewriter);
     } else {
       llvm_unreachable("users[0] must be one of ttir::TransposeOp, "
                        "ttir::PermuteOp, ttir::ReshapeOp");
     }
-    return success();
   }
 
-private:
   LogicalResult
   checkAllUsersAreIdenticalTms(SmallVector<Operation *> users) const {
     Operation *firstUser = users[0];
@@ -65,27 +63,65 @@ private:
         isa<ttir::TransposeOp, ttir::PermuteOp, ttir::ReshapeOp>(firstUser));
   }
 
+  LogicalResult checkAllOperandsHaveSameShape(ValueRange operands) const {
+    RankedTensorType firstOperandType =
+        cast<RankedTensorType>(operands[0].getType());
+    for (Value operand : operands) {
+      RankedTensorType operandType = cast<RankedTensorType>(operand.getType());
+      if (operandType.getShape() != firstOperandType.getShape()) {
+        return failure();
+      }
+    }
+    return success();
+  }
+
+private:
+  LogicalResult virtual shouldCommute(Operation *op) const {
+    llvm_unreachable("shouldCommute must be overridden");
+  };
+
+  LogicalResult virtual checkTrait(Operation *op) const {
+    llvm_unreachable("checkTrait must be overridden");
+  };
+
   template <typename TMOpType>
   void commuteTmsThroughEltwise(Operation *op, SmallVector<Operation *> users,
-                                Value operand,
+                                ValueRange operands,
                                 PatternRewriter &rewriter) const {
     Operation *user = users[0];
     auto newEltwiseType = cast<RankedTensorType>(user->getResult(0).getType());
 
-    mlir::tensor::EmptyOp newTransposeDPS = rewriter.create<tensor::EmptyOp>(
-        op->getLoc(), newEltwiseType.getShape(),
-        newEltwiseType.getElementType());
-    TMOpType newTranspose = cast<TMOpType>(rewriter.clone(*user));
+    SmallVector<mlir::tensor::EmptyOp> newTransposeDPSOperands;
+    SmallVector<TMOpType> newTransposes;
+    for (uint32_t operandIdx = 0; operandIdx < op->getNumOperands() - 1;
+         operandIdx++) {
+      newTransposeDPSOperands.push_back(rewriter.create<tensor::EmptyOp>(
+          op->getLoc(), newEltwiseType.getShape(),
+          newEltwiseType.getElementType()));
+
+      TMOpType newTranspose = cast<TMOpType>(rewriter.clone(*user));
+      newTranspose->setOperand(newTranspose->getNumOperands() - 1,
+                               newTransposeDPSOperands[operandIdx]);
+      newTransposes.push_back(newTranspose);
+    }
+
     mlir::tensor::EmptyOp newEltwiseDPS = rewriter.create<tensor::EmptyOp>(
         op->getLoc(), newEltwiseType.getShape(),
         newEltwiseType.getElementType());
     Operation *newEltwise = rewriter.clone(*op);
 
-    newEltwise->setOperand(0, newTranspose->getResult(0));
-    newEltwise->setOperand(1, newEltwiseDPS->getResult(0));
+    // Do not want to put a clone on the DPS operand
+    for (uint32_t operandIdx = 0; operandIdx < newEltwise->getNumOperands() - 1;
+         operandIdx++) {
+      newTransposes[operandIdx]->setOperand(0, operands[operandIdx]);
+      newTransposes[operandIdx]->setOperand(
+          1, newTransposeDPSOperands[operandIdx]);
+      newEltwise->setOperand(operandIdx,
+                             newTransposes[operandIdx]->getResult(0));
+    }
+    newEltwise->setOperand(newEltwise->getNumOperands() - 1,
+                           newEltwiseDPS->getResult(0));
     newEltwise->getResult(0).setType(newEltwiseType);
-    newTranspose->setOperand(0, operand);
-    newTranspose->setOperand(1, newTransposeDPS->getResult(0));
 
     for (auto *user : users) {
       rewriter.replaceOp(user, newEltwise);
@@ -93,20 +129,195 @@ private:
   }
 };
 
+class TTIRCommuteTmsAboveElementwiseUnaryRewriter
+    : public TTIRCommuteTmsAboveElementwiseRewriter {
+public:
+  TTIRCommuteTmsAboveElementwiseUnaryRewriter(MLIRContext *ctx)
+      : TTIRCommuteTmsAboveElementwiseRewriter(ctx) {}
+
+private:
+  LogicalResult checkTrait(Operation *op) const override {
+    return success(op->hasTrait<ElementwiseUnary::Trait>());
+  }
+
+  LogicalResult shouldCommute(Operation *op) const override {
+    // For now we always want to commute through unary elementwise ops if all
+    // the users are identical
+    SmallVector<Operation *> users(op->getUsers());
+    return success(succeeded(checkAllUsersAreIdenticalTms(users)) &&
+                   succeeded(checkAllOperandsHaveSameShape(op->getOperands())));
+  };
+};
+
+class TTIRCommuteTmsAboveElementwiseBinaryRewriter
+    : public TTIRCommuteTmsAboveElementwiseRewriter {
+public:
+  TTIRCommuteTmsAboveElementwiseBinaryRewriter(MLIRContext *ctx)
+      : TTIRCommuteTmsAboveElementwiseRewriter(ctx) {}
+
+private:
+  LogicalResult checkTrait(Operation *op) const override {
+    return success(op->hasTrait<ElementwiseBinary::Trait>());
+  }
+
+  LogicalResult shouldCommute(Operation *op) const override {
+    // For now we always want to commute through unary elementwise ops if all
+    // the users are identical
+    SmallVector<Operation *> users(op->getUsers());
+    return success(succeeded(checkAllUsersAreIdenticalTms(users)) &&
+                   succeeded(checkAllOperandsHaveSameShape(op->getOperands())));
+  };
+};
+} // namespace
+
+namespace {
+class TTIREraseInverseTransposes : public OpRewritePattern<ttir::TransposeOp> {
+public:
+  using OpRewritePattern<ttir::TransposeOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ttir::TransposeOp op,
+                                PatternRewriter &rewriter) const override {
+
+    // Erase with operand
+    if (!op->getOperand(0).getDefiningOp()) {
+      return failure();
+    }
+    ttir::TransposeOp operand =
+        dyn_cast<ttir::TransposeOp>(op->getOperand(0).getDefiningOp());
+    if (!operand) {
+      return failure();
+    }
+
+    auto opDim0 = op.getDim0();
+    auto opDim1 = op.getDim1();
+    auto operandDim0 = operand.getDim0();
+    auto operandDim1 = operand.getDim1();
+
+    if ((opDim0 == operandDim1 && opDim1 == operandDim0) ||
+        (opDim0 == operandDim0 && opDim1 == operandDim1)) {
+      rewriter.replaceOp(op, operand->getOperand(0));
+      return success();
+    }
+    return failure();
+  }
+};
+} // namespace
+
+namespace {
+class TTIREraseInversePermutations : public OpRewritePattern<ttir::PermuteOp> {
+public:
+  using OpRewritePattern<ttir::PermuteOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ttir::PermuteOp op,
+                                PatternRewriter &rewriter) const override {
+
+    // Erase with operand
+    if (!op->getOperand(0).getDefiningOp()) {
+      return failure();
+    }
+    ttir::PermuteOp operand =
+        dyn_cast<ttir::PermuteOp>(op->getOperand(0).getDefiningOp());
+    if (!operand) {
+      return failure();
+    }
+
+    // Apply the permutation of this op to the permuatation of the operand
+    // If the result is the identity permutation, erase the ops
+    ArrayRef<int64_t> opPemutation = op.getPermutation();
+    ArrayRef<int64_t> operandPermutation = operand.getPermutation();
+
+    SmallVector<int64_t> newPermutation;
+    for (int64_t i = 0; i < static_cast<int64_t>(opPemutation.size()); i++) {
+      if (operandPermutation[opPemutation[i]] != i) {
+        return failure();
+      };
+    }
+
+    rewriter.replaceOp(op, operand->getOperand(0));
+    return success();
+  }
+};
+} // namespace
+
+namespace {
+class TTIREraseInverseReshapes : public OpRewritePattern<ttir::ReshapeOp> {
+public:
+  using OpRewritePattern<ttir::ReshapeOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ttir::ReshapeOp op,
+                                PatternRewriter &rewriter) const override {
+
+    // Erase with operand
+    if (!op->getOperand(0).getDefiningOp()) {
+      return failure();
+    }
+    ttir::ReshapeOp operand =
+        dyn_cast<ttir::ReshapeOp>(op->getOperand(0).getDefiningOp());
+    if (!operand) {
+      return failure();
+    }
+
+    // if the input shape of the operand is the same as the output shape of this
+    // op, erase the ops
+    auto opShape =
+        cast<RankedTensorType>(op->getResult(0).getType()).getShape();
+    auto inputShape =
+        cast<RankedTensorType>(operand->getOperand(0).getType()).getShape();
+
+    if (opShape != inputShape) {
+      return failure();
+    }
+    rewriter.replaceOp(op, operand->getOperand(0));
+    return success();
+  }
+};
+} // namespace
+
 class TTIREraseInverseOps
     : public impl::TTIREraseInverseOpsBase<TTIREraseInverseOps> {
 public:
   using impl::TTIREraseInverseOpsBase<
       TTIREraseInverseOps>::TTIREraseInverseOpsBase;
   void runOnOperation() final {
-    RewritePatternSet patterns(&getContext());
-    patterns.add<TTIRCommuteTmsAboveElementwiseUnaryRewriter>(&getContext());
-    FrozenRewritePatternSet patternSet(std::move(patterns));
+    RewritePatternSet commutePatterns(&getContext());
+    commutePatterns.add<TTIRCommuteTmsAboveElementwiseUnaryRewriter>(
+        &getContext());
+    commutePatterns.add<TTIRCommuteTmsAboveElementwiseBinaryRewriter>(
+        &getContext());
+    FrozenRewritePatternSet commutePatternSet(std::move(commutePatterns));
 
-    if (failed(applyPatternsGreedily(getOperation(), patternSet))) {
-      signalPassFailure();
-      return;
-    }
+    RewritePatternSet erasePatterns(&getContext());
+    erasePatterns.add<TTIREraseInverseTransposes>(&getContext());
+    erasePatterns.add<TTIREraseInversePermutations>(&getContext());
+    erasePatterns.add<TTIREraseInverseReshapes>(&getContext());
+    FrozenRewritePatternSet erasePatternSet(std::move(erasePatterns));
+
+    // We want to commute all TMs upwards as much as possible so they are are
+    // placed back to back Then we can erase back to back inverses.
+    //
+    //
+    // Because there are multiple TMs we wish to commute and erase, we must
+    // continuously run the commute and erase patterns until the graph stops
+    // changing. This is because erasing a pair of TMs may free up a path
+    // for another pair of TMs to be erased.
+    //
+    // We do have some canonicalizatios for these ops will erase back to back
+    // ops, however they are not run during this pass (yet). Maybe we can call
+    // them instead.
+    GreedyRewriteConfig rewriteConfig = GreedyRewriteConfig();
+    bool changed = false;
+    do {
+      if (failed(applyPatternsGreedily(getOperation(), commutePatternSet,
+                                       rewriteConfig, &changed))) {
+        signalPassFailure();
+        return;
+      }
+      if (failed(applyPatternsGreedily(getOperation(), erasePatternSet,
+                                       rewriteConfig, &changed))) {
+        signalPassFailure();
+        return;
+      }
+    } while (changed);
   }
 
   void getDependentDialects(mlir::DialectRegistry &registry) const override {
