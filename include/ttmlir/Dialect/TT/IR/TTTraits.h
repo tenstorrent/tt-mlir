b0706f6e3bac4dc8ad4928a2a362a552eab4b334
Vincent Wells <vwells@tenstorrent.com>
2025-03-19 16:34:26 +0000

    missed a file

---
+++ include/ttmlir/Dialect/TT/IR/TTTraits.h
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef TTMLIR_DIALECT_TT_IR_TTTRAITS_H
+#define TTMLIR_DIALECT_TT_IR_TTTRAITS_H
+
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Support/LLVM.h"
+
+namespace mlir::tt::Trait {
+template <typename Ty>
+class TTCreationOpTrait
+    : public mlir::OpTrait::TraitBase<Ty, TTCreationOpTrait> {};
+
+template <typename Ty>
+class TTIgnoreConstEvalTrait
+    : public mlir::OpTrait::TraitBase<Ty, TTIgnoreConstEvalTrait> {};
+
+} // namespace mlir::tt::Trait
+
+#endif
