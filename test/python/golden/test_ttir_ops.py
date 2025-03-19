01eee41878975d643ea5edf388458f0a24c383ca
jgrimTT <jgrim@tenstorrent.com>
2025-03-19 14:36:06 +0000

    Fixed merge issues

--- test/python/golden/test_ttir_ops.py
+++ test/python/golden/test_ttir_ops.py
@@ -537,64 +537,64 @@ def test_broadcast(in0: Operand, in1: Operand, builder: TTIRBuilder):
     return builder.broadcast(in0, in1, [1, 16, 1])
 
 
- @compile_to_flatbuffer(
-     [
-         (1, 32, 32, 64),
-         (64, 32, 3, 3),
-         (1, 1, 1, 64),
-         (1, 16, 28, 64),
-     ],
-     inputs_types=[torch.bfloat16, torch.bfloat16, torch.bfloat16, torch.bfloat16],
-     targets=["ttnn"],
- )
- def test_conv2d(
-     in0: Operand, weight: Operand, bias: Operand, in1: Operand, builder: TTIRBuilder
- ):
-     stride = DenseI32ArrayAttr.get([2, 1])
-     padding = DenseI32ArrayAttr.get([2, 1])
-     dilation = DenseI32ArrayAttr.get([2, 1])
-     groups = IntegerAttr.get(IntegerType.get_signless(32), 2)
-     return builder.conv2d(
-         in0,
-         weight,
-         bias,
-         in1,
-         stride=stride,
-         padding=padding,
-         dilation=dilation,
-         groups=groups,
-     )
-  
-  
 @compile_to_flatbuffer(
-     [
-         (3, 8, 8, 256),
-         (256, 256, 3, 3),
-         (1, 1, 1, 256),
-         (1, 10, 10, 256),
-     ],
-     inputs_types=[torch.bfloat16, torch.bfloat16, torch.bfloat16, torch.bfloat16],
-     targets=["ttnn"],
- )
- def test_conv_transpose2d(
-     in0: Operand, weight: Operand, bias: Operand, in1: Operand, builder: TTIRBuilder
- ):
-     stride = IntegerAttr.get(IntegerType.get_signless(32), 1)
-     padding = IntegerAttr.get(IntegerType.get_signless(32), 0)
-     output_padding = IntegerAttr.get(IntegerType.get_signless(32), 0)
-     dilation = IntegerAttr.get(IntegerType.get_signless(32), 1)
-     groups = IntegerAttr.get(IntegerType.get_signless(32), 1)
-     return builder.conv_transpose2d(
-         in0,
-         weight,
-         bias,
-         in1,
-         stride=stride,
-         padding=padding,
-         output_padding=output_padding,
-         dilation=dilation,
-         groups=groups,
-     )
+    [
+        (1, 32, 32, 64),
+        (64, 32, 3, 3),
+        (1, 1, 1, 64),
+        (1, 16, 28, 64),
+    ],
+    inputs_types=[torch.bfloat16, torch.bfloat16, torch.bfloat16, torch.bfloat16],
+    targets=["ttnn"],
+)
+def test_conv2d(
+    in0: Operand, weight: Operand, bias: Operand, in1: Operand, builder: TTIRBuilder
+):
+    stride = DenseI32ArrayAttr.get([2, 1])
+    padding = DenseI32ArrayAttr.get([2, 1])
+    dilation = DenseI32ArrayAttr.get([2, 1])
+    groups = IntegerAttr.get(IntegerType.get_signless(32), 2)
+    return builder.conv2d(
+        in0,
+        weight,
+        bias,
+        in1,
+        stride=stride,
+        padding=padding,
+        dilation=dilation,
+        groups=groups,
+    )
+
+
+@compile_to_flatbuffer(
+    [
+        (3, 8, 8, 256),
+        (256, 256, 3, 3),
+        (1, 1, 1, 256),
+        (1, 10, 10, 256),
+    ],
+    inputs_types=[torch.bfloat16, torch.bfloat16, torch.bfloat16, torch.bfloat16],
+    targets=["ttnn"],
+)
+def test_conv_transpose2d(
+    in0: Operand, weight: Operand, bias: Operand, in1: Operand, builder: TTIRBuilder
+):
+    stride = IntegerAttr.get(IntegerType.get_signless(32), 1)
+    padding = IntegerAttr.get(IntegerType.get_signless(32), 0)
+    output_padding = IntegerAttr.get(IntegerType.get_signless(32), 0)
+    dilation = IntegerAttr.get(IntegerType.get_signless(32), 1)
+    groups = IntegerAttr.get(IntegerType.get_signless(32), 1)
+    return builder.conv_transpose2d(
+        in0,
+        weight,
+        bias,
+        in1,
+        stride=stride,
+        padding=padding,
+        output_padding=output_padding,
+        dilation=dilation,
+        groups=groups,
+    )
 
 
 @compile_to_flatbuffer(
