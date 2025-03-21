5dfd71343ee74da66b0e29983d6198de9467d26c
Wooseok Lee <wooseoklee@tenstorrent.com>
2025-03-21 14:38:49 -0500

    Details TensorMeshShardingAttr with extra info and fix bug in (#2514)
    maintaining multi-device tensors with pre-sharded inputs.
    
    ### Ticket
    https://github.com/tenstorrent/tt-mlir/issues/2429
    
    ### Problem description
    1. Some multi-device tensors need extra information to understand the
    actual shape of tensors across devices.
    2. Some ops with pre-sharded input tensors try to compute single-device
    tensor with multi-device tensor.
    
    ### What's changed
    1. Elaborate multi-device tensors with extra sharding info if tensor
    shape is full shape.
        - Single device tensor has no TensorMeshShardingAttr.
            tensor<784x16384xf32>
    
        - Multi-device tensors have TensorMeshShardingAttr.
    (i) multi-device tensor without tensor mesh shard axis indicates all
    devices in "mesh"
              have tensors with the given shape. e.g., 784x16384 for
                tensor<784x16384xf32, #tt.mesh_sharding<"mesh">>
    
    (ii) multi-device tensor with tensor mesh shard axis indicate all
    devices in "mesh"
    have sharded tensor shape defined by the TensorMeshShardingAxisAttr.
    e.g., 196x16384 for
    tensor<784x16384xf32, #tt.mesh_sharding<"mesh" : [ 4(1), 1]>>.
    Here, 4(1) indicates shard_shape(shard_dim), so 784 should be sharded by
    4
    at "mesh"'s second hardware dimension. 1 indicates no sharding, so 16384
    is not
              being sharded.
    
    2. Insert extra mesh_shard op for pre-sharded tensors that are computing
    with single-device tensors.
    
    ### Checklist
    - [X] New/Existing tests provide coverage for changes

--- include/ttmlir/Conversion/StableHLOToTTIR/ShardingUtils.h
+++ include/ttmlir/Conversion/StableHLOToTTIR/ShardingUtils.h
@@ -52,15 +52,24 @@ public:
   // mesh_shard op needs to be created or not.
   bool checkAndUpdateShardyArgSharding(
       mlir::PatternRewriter &rewriter, mlir::func::FuncOp funcOp,
-      mlir::Value argOperand, mlir::sdy::TensorShardingAttr shardingAttr,
-      mlir::tt::TensorMeshShardingAttr tensorMeshShardingAttr);
+      mlir::Value argOperand, mlir::sdy::TensorShardingAttr shardingAttr);
 
   // Check and update ret sharding attribute and determine if mesh_shard op
   // needs to be created or not.
-  bool checkAndUpdateShardyRetSharding(
-      mlir::PatternRewriter &rewriter, mlir::func::FuncOp funcOp,
-      uint64_t retIdx, mlir::sdy::TensorShardingAttr shardingAttr,
-      mlir::tt::TensorMeshShardingAttr tensorMeshShardingAttr);
+  bool
+  checkAndUpdateShardyRetSharding(mlir::PatternRewriter &rewriter,
+                                  mlir::func::FuncOp funcOp, uint64_t retIdx,
+                                  mlir::sdy::TensorShardingAttr shardingAttr);
+
+  // Parse Shardy sharing attribuite. Make this as public for
+  // potential frontend use.
+  llvm::Expected<bool>
+  parseSdySharding(mlir::sdy::TensorShardingAttr sdySharding,
+                   mlir::sdy::MeshAttr meshAttr);
+
+  // Given parsed MeshSharding info, get TensorMeshShardingAttr.
+  mlir::tt::TensorMeshShardingAttr
+  getTensorMeshShardingAttr(mlir::PatternRewriter &rewriter);
 
   // Getter functions.
   mlir::tt::MeshShardDirection getShardDirection() const {
@@ -93,8 +102,8 @@ private:
   // op will be ignored at runtime by simply copying input tensor to output.
   void setDummyShardingOp() { shardType = mlir::tt::MeshShardType::Identity; }
 
-  // Decide wheter to create mesh_shard op and shard_type. Detailed description
-  // can be found in function body.
+  // Decide wheter to create mesh_shard op and shard_type. Detailed
+  // description can be found in function body.
   bool determineMeshShardOpCreationAndShardType(bool foundArgSharding);
 
 private:
@@ -105,6 +114,7 @@ private:
   llvm::SmallVector<int64_t> shardDims{-1};
   llvm::SmallVector<int64_t> meshShape{-1};
   llvm::SmallVector<int64_t> deviceIds{-1};
+  std::string meshName;
   bool lastTileDimReplicate = false;
 };
 
