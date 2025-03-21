fce6cf690a488e40667788487b2315cd448ea3fc
Radenko Pavlovic <133032400+rpavlovicTT@users.noreply.github.com>
2025-03-21 10:39:31 +0100

    Implement op constraints & runtime API for MaxPool2D (#2523)
    ### Ticket
    
    Related to https://github.com/tenstorrent/tt-mlir/issues/2277
    
    ### Problem description
    
    Optimizer needs constraints and runtime model for every op in order to
    shard them properly. This commit adds support for MaxPool 2D op in that
    regard.
    
    ### What's changed
    
    Implementation for GetOpConstraints & GetOpRuntime for MaxPool2D.
    Unit tests for MaxPool2D API.
    
    ### Checklist
    
    - [x]  New/Existing tests provide coverage for changes

--- lib/Target/TTNN/TTNNToFlatbuffer.cpp
+++ lib/Target/TTNN/TTNNToFlatbuffer.cpp
@@ -1386,12 +1386,10 @@ createMaxPool2dOp(FlatbufferObjectCache &cache, MaxPool2dOp op) {
   ::flatbuffers::Offset<::flatbuffers::Vector<int32_t>> dilation =
       toFlatbuffer(cache, op.getDilation());
 
-  auto device = getOperandThroughDPSOps(op.getDevice());
   return ::tt::target::ttnn::CreateMaxPool2dOp(
-      *cache.fbb, in, out, cache.at<::tt::target::DeviceRef>(device),
-      op.getBatchSize(), op.getInputHeight(), op.getInputWidth(),
-      op.getChannels(), kernelSize, stride, padding, dilation,
-      op.getCeilMode());
+      *cache.fbb, in, out, op.getBatchSize(), op.getInputHeight(),
+      op.getInputWidth(), op.getChannels(), kernelSize, stride, padding,
+      dilation, op.getCeilMode());
 }
 
 ::flatbuffers::Offset<::tt::target::ttnn::RepeatInterleaveOp>
