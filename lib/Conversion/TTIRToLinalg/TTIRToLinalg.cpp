432a73ef68bc14e1f1dbc2c3513d157d61fbad10
Radovan Božić <radovan.bozic@htecgroup.com>
2025-03-24 21:22:40 +0100

    Lowering of softmax op from TTIR to the Linalg dialect (#2582)
    ### Ticket
    Closes #2580 
    
    ### Problem description
    Currently, the TTIR dialect doesn't support lowering the `softmax`
    operation to the Linalg dialect. Since Linalg provides `linalg.softmax`
    opeartion, it should be possible to implement this lowering.
    
    ### What's changed
    Implemented a basic lowering of `softmax` operation to the Linalg
    dialect. While lowering we distinguish between two cases for dimension
    attribute:
        
    1) Negative dimension: we treated it as an index from the end, so we
    subtract it from the tensor's shape size.
    2) Positive dimension: we just passed it through
        
    If dimension is bigger than shape size, then `ttmlir-opt` reports an
    error before conversion.
    
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Vincent Wells <vwells@tenstorrent.com>

--- lib/Conversion/TTIRToLinalg/TTIRToLinalg.cpp
+++ lib/Conversion/TTIRToLinalg/TTIRToLinalg.cpp
@@ -182,7 +182,6 @@ public:
     return success();
   }
 };
-
 } // namespace
 
 namespace {
@@ -210,6 +209,30 @@ public:
 };
 } // namespace
 
+namespace {
+class SoftmaxOpConversionPattern : public OpConversionPattern<ttir::SoftmaxOp> {
+public:
+  using OpConversionPattern<ttir::SoftmaxOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(ttir::SoftmaxOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+
+    Value input = adaptor.getInput();
+    const size_t inputSize =
+        dyn_cast<RankedTensorType>(input.getType()).getShape().size();
+    const int32_t dimension = (op.getDimension() < 0)
+                                  ? op.getDimension() + inputSize
+                                  : op.getDimension();
+
+    rewriter.replaceOpWithNewOp<linalg::SoftmaxOp>(
+        op, this->getTypeConverter()->convertType(op.getType()), input,
+        adaptor.getOutput(), dimension);
+    return success();
+  }
+};
+} // namespace
+
 namespace mlir::tt {
 
 void populateTTIRToLinalgPatterns(MLIRContext *ctx, RewritePatternSet &patterns,
@@ -228,8 +251,8 @@ void populateTTIRToLinalgPatterns(MLIRContext *ctx, RewritePatternSet &patterns,
       ElementwiseOpConversionPattern<ttir::CeilOp, linalg::CeilOp>,
       ElementwiseOpConversionPattern<ttir::FloorOp, linalg::FloorOp>,
       ElementwiseOpConversionPattern<ttir::TanhOp, linalg::TanhOp>,
-      ElementwiseOpConversionPattern<ttir::ReciprocalOp, linalg::ReciprocalOp>>(
-      typeConverter, ctx);
+      ElementwiseOpConversionPattern<ttir::ReciprocalOp, linalg::ReciprocalOp>,
+      SoftmaxOpConversionPattern>(typeConverter, ctx);
 }
 
 } // namespace mlir::tt
