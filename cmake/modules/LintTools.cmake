1950035f111d6e936d3c272039e18a09827e250d
Nicholas Smith <nsmith@tenstorrent.com>
2025-03-27 17:38:05 +0000

    Remove clang-tidy __cpp_structured_bindings hack

--- cmake/modules/LintTools.cmake
+++ cmake/modules/LintTools.cmake
@@ -1,5 +1,5 @@
 # clang-tidy setup
-add_custom_target(clang-tidy COMMAND run-clang-tidy.py -p ${PROJECT_BINARY_DIR} -export-fixes clang-tidy-fixes.yaml -warnings-as-errors '*' -extra-arg-before=-DDISABLE_STATIC_ASSERT_TESTS -extra-arg-before=-D__cpp_structured_bindings=202400
+add_custom_target(clang-tidy COMMAND run-clang-tidy.py -p ${PROJECT_BINARY_DIR} -export-fixes clang-tidy-fixes.yaml -warnings-as-errors '*' -extra-arg-before=-DDISABLE_STATIC_ASSERT_TESTS
   DEPENDS
     mlir-headers
     mlir-generic-headers
