c01e4f3083f3d9ad14d820df37b86b87a3f9ceb2
sgligorijevicTT <189116645+sgligorijevicTT@users.noreply.github.com>
2025-04-01 14:58:12 +0000

    formatting

--- runtime/tools/python/ttrt/binary/__init__.py
+++ runtime/tools/python/ttrt/binary/__init__.py
@@ -19,6 +19,6 @@ def as_dict(bin):
     tmp = bin.as_json()
     # Flatbuffers emits 'nan' and 'inf'
     # But Python's JSON accepts only 'NaN' and 'Infinity' and nothing else
-    tmp = tmp.replace('nan', 'NaN')
-    tmp = tmp.replace('inf', 'Infinity')
+    tmp = tmp.replace("nan", "NaN")
+    tmp = tmp.replace("inf", "Infinity")
     return json.loads(tmp)
