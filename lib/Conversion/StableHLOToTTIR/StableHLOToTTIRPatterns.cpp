25a2daf1fbfd28cb2935003a641c996e789c4429
Aleksandar Zecevic <azecevic@tenstorrent.com>
2025-04-04 23:36:36 +0000

    nit

--- lib/Conversion/StableHLOToTTIR/StableHLOToTTIRPatterns.cpp
+++ lib/Conversion/StableHLOToTTIR/StableHLOToTTIRPatterns.cpp
@@ -824,7 +824,6 @@ public:
 
     ttir::utils::replaceOpWithNewDPSOp<mlir::tt::ttir::DequantizeOp>(
         rewriter, srcOp, outputType, adaptor.getOperand());
-    ttir::AddOp addOp;
     return success();
   }
 
