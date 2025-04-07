7d5879e4bbddd9c1eb3cb213e7223600de5879c1
Anurag <anuragsingh@tenstorrent.com>
2025-04-07 22:54:23 +0000

    run clang-tidy, eliminate dequantize converter, fail when user provides bad type and negative test

--- lib/Dialect/TTIR/Transforms/Quantization.cpp
+++ lib/Dialect/TTIR/Transforms/Quantization.cpp
@@ -17,17 +17,23 @@ namespace mlir::tt::ttir {
 
 // Helper function to convert integer bit width string to integer type.
 static Type getIntegerTypeFromString(MLIRContext *context, StringRef bitWidth) {
-  if (bitWidth == "int8")
-    return IntegerType::get(context, 8, IntegerType::Signed);
-  if (bitWidth == "int16")
-    return IntegerType::get(context, 16, IntegerType::Signed);
-  if (bitWidth == "int32")
+  if (bitWidth.empty()) {
+    // Default to int32.
     return IntegerType::get(context, 32, IntegerType::Signed);
-  if (bitWidth == "int64")
+  }
+  if (bitWidth == "int8") {
+    return IntegerType::get(context, 8, IntegerType::Signed);
+  }
+  if (bitWidth == "int16") {
+    return IntegerType::get(context, 16, IntegerType::Signed);
+  }
+  if (bitWidth == "int32") {
+    return IntegerType::get(context, 32, IntegerType::Signed);
+  }
+  if (bitWidth == "int64") {
     return IntegerType::get(context, 64, IntegerType::Signed);
-
-  // Default to int32.
-  return IntegerType::get(context, 32, IntegerType::Signed);
+  }
+  return nullptr;
 }
 
 static std::pair<int64_t, int64_t> getStorageTypeMinMax(IntegerType intType) {
@@ -41,27 +47,33 @@ static std::pair<int64_t, int64_t> getStorageTypeMinMax(IntegerType intType) {
 static Type convertQuantizedType(Type type, Type targetIntType) {
   if (auto quantType = mlir::dyn_cast<quant::QuantizedType>(type)) {
     // Use the target integer type's min and max values.
+    IntegerType intType = mlir::cast<IntegerType>(targetIntType);
     std::pair<int64_t, int64_t> storageTypeMinMax =
-        getStorageTypeMinMax(mlir::cast<IntegerType>(targetIntType));
+        getStorageTypeMinMax(intType);
     int64_t storageTypeMin = storageTypeMinMax.first;
     int64_t storageTypeMax = storageTypeMinMax.second;
 
-    if (auto uniformType =
+    assert(intType.getWidth() >=
+               quantType.getStorageType().getIntOrFloatBitWidth() &&
+           "Target integer type is smaller than quantized type. Out of range.");
+
+    if (quant::UniformQuantizedType uniformType =
             mlir::dyn_cast<quant::UniformQuantizedType>(quantType)) {
       return quant::UniformQuantizedType::get(
           uniformType.getFlags(), targetIntType, uniformType.getExpressedType(),
           uniformType.getScale(), uniformType.getZeroPoint(), storageTypeMin,
           storageTypeMax);
-    } else if (auto perAxisType =
-                   mlir::dyn_cast<quant::UniformQuantizedPerAxisType>(
-                       quantType)) {
+    }
+    if (quant::UniformQuantizedPerAxisType perAxisType =
+            mlir::dyn_cast<quant::UniformQuantizedPerAxisType>(quantType)) {
       return quant::UniformQuantizedPerAxisType::get(
           perAxisType.getFlags(), targetIntType, perAxisType.getExpressedType(),
           perAxisType.getScales(), perAxisType.getZeroPoints(),
           perAxisType.getQuantizedDimension(), storageTypeMin, storageTypeMax);
     }
-  } else if (auto tensorType = mlir::dyn_cast<RankedTensorType>(type)) {
-    if (auto elementType =
+  }
+  if (RankedTensorType tensorType = mlir::dyn_cast<RankedTensorType>(type)) {
+    if (quant::QuantizedType elementType =
             mlir::dyn_cast<quant::QuantizedType>(tensorType.getElementType())) {
       return RankedTensorType::get(
           tensorType.getShape(),
@@ -91,7 +103,8 @@ protected:
 
   // Helper to convert a type to use the target integer bit width.
   Type convertType(Type type) const {
-    return convertQuantizedType(type, targetIntType);
+    Type newType = convertQuantizedType(type, targetIntType);
+    return newType;
   }
 };
 
@@ -108,8 +121,9 @@ public:
     Type resultType = convertType(op.getResult().getType());
     Type outputType = convertType(op.getOutput().getType());
 
-    if (resultType == op.getResult().getType())
+    if (resultType == op.getResult().getType()) {
       return failure();
+    }
 
     // Create a new empty tensor.
     Value newOutputOp = createEmptyTensor(op.getLoc(), outputType, rewriter);
@@ -119,31 +133,6 @@ public:
         op.getLoc(), resultType, op.getInput(), newOutputOp);
 
     // Replace the old operation with the new one.
-    rewriter.replaceOp(op, newOp.getResult());
-
-    return success();
-  }
-};
-
-class DequantizeOpTypeConverter : public QuantOpTypeConverterBase,
-                                  public OpRewritePattern<ttir::DequantizeOp> {
-public:
-  DequantizeOpTypeConverter(MLIRContext *context, Type targetIntType)
-      : QuantOpTypeConverterBase(context, targetIntType),
-        OpRewritePattern<ttir::DequantizeOp>(context) {}
-
-  LogicalResult matchAndRewrite(ttir::DequantizeOp op,
-                                PatternRewriter &rewriter) const override {
-    Value input = op.getInput();
-    Type inputType = input.getType();
-    Type convertedType = convertType(inputType);
-
-    if (convertedType == inputType)
-      return failure();
-
-    ttir::DequantizeOp newOp = rewriter.create<ttir::DequantizeOp>(
-        op.getLoc(), op.getResult().getType(), input, op.getOutput());
-
     rewriter.replaceOp(op, newOp.getResult());
 
     return success();
@@ -165,8 +154,9 @@ public:
     Type outputType = convertType(op.getOutput().getType());
 
     if (resultType == op.getResult().getType() &&
-        inputType == op.getInput().getType())
+        inputType == op.getInput().getType()) {
       return failure();
+    }
 
     // Create a new output tensor.
     Value newOutputOp = createEmptyTensor(op.getLoc(), outputType, rewriter);
@@ -197,8 +187,24 @@ public:
     ModuleOp module = getOperation();
 
     Type targetIntType = getIntegerTypeFromString(context, targetBitWidth);
+    assert(targetIntType &&
+           ("Invalid target bit width: " + targetBitWidth).c_str());
 
     module.walk([&](func::FuncOp funcOp) {
+      // Exit early if no relevant ops found in the function.
+      bool hasQuantOps = false;
+      funcOp.walk([&](Operation *op) {
+        if (isa<ttir::QuantizeOp, ttir::DequantizeOp, ttir::RequantizeOp>(op)) {
+          hasQuantOps = true;
+          return WalkResult::interrupt();
+        }
+        return WalkResult::advance();
+      });
+
+      if (!hasQuantOps) {
+        return;
+      }
+
       bool needsUpdate = false;
       SmallVector<Type, 4> newInputTypes;
       SmallVector<Type, 4> newResultTypes;
@@ -208,6 +214,20 @@ public:
         newInputTypes.push_back(newType);
         if (newType != inputType) {
           needsUpdate = true;
+        }
+      }
+
+      // Update block arguments only if function input was modified.
+      if (needsUpdate && !funcOp.empty()) {
+        Block &entryBlock = funcOp.getBody().front();
+
+        for (unsigned i = 0;
+             i < newInputTypes.size() && i < entryBlock.getNumArguments();
+             ++i) {
+          BlockArgument arg = entryBlock.getArgument(i);
+          if (arg.getType() != newInputTypes[i]) {
+            arg.setType(newInputTypes[i]);
+          }
         }
       }
 
@@ -227,19 +247,6 @@ public:
           FunctionType::get(context, newInputTypes, newResultTypes);
 
       funcOp.setType(newFuncType);
-
-      if (!funcOp.empty()) {
-        Block &entryBlock = funcOp.getBody().front();
-
-        for (unsigned i = 0;
-             i < newInputTypes.size() && i < entryBlock.getNumArguments();
-             ++i) {
-          BlockArgument arg = entryBlock.getArgument(i);
-          if (arg.getType() != newInputTypes[i]) {
-            arg.setType(newInputTypes[i]);
-          }
-        }
-      }
     });
 
     RewritePatternSet patterns(context);
