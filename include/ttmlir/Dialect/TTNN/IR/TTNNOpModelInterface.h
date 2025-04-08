6e18a5d5789b9195161fed1d236cda4dd4c48d68
Armin Ale <arminale@tenstorrent.com>
2025-04-08 15:59:49 +0000

    fix comment

--- include/ttmlir/Dialect/TTNN/IR/TTNNOpModelInterface.h
+++ include/ttmlir/Dialect/TTNN/IR/TTNNOpModelInterface.h
@@ -5,9 +5,9 @@
 #ifndef TTMLIR_DIALECT_TTNN_IR_TTNNOPMODELINTERFACE_H
 #define TTMLIR_DIALECT_TTNN_IR_TTNNOPMODELINTERFACE_H
 
+#include "ttmlir/Dialect/TTNN/IR/TTNNOpsAttrs.h"
 // This include is required for llvm::Expected in the tablegen'd
 // TTNNOpModelInterface methods
-#include "ttmlir/Dialect/TTNN/IR/TTNNOpsAttrs.h"
 #include "llvm/Support/Error.h"
 
 #endif
