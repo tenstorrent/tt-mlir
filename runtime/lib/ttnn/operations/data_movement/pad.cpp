5c71b7f6dd024cd2c92c04c30779ed9a70e46a70
Vladimir Milosevic <157983820+vmilosevic@users.noreply.github.com>
2025-04-09 14:53:56 +0200

    Uplift third_party/tt-metal to 678a464f7f15d3581fae286df0017100882e7975 2025-04-09 (#2886)
    This PR uplifts the third_party/tt-metal to the
    678a464f7f15d3581fae286df0017100882e7975
    
    - Update pad op after metal commit 8d50fef
    
    ---------
    
    Co-authored-by: kmabeeTT <118925087+kmabeeTT@users.noreply.github.com>
    Co-authored-by: brataTT <achoudhury@tenstorrent.com>

--- runtime/lib/ttnn/operations/data_movement/pad.cpp
+++ runtime/lib/ttnn/operations/data_movement/pad.cpp
@@ -22,9 +22,9 @@ void run(const ::tt::target::ttnn::PadOp *op, ProgramContext &context) {
 
   ::ttnn::Tensor out;
 
-  std::vector<std::pair<uint32_t, uint32_t>> padding;
+  ::ttnn::SmallVector<::ttnn::operations::data_movement::PadSpecDim> padding;
   for (uint32_t i = 0; i < op->padding()->size(); i += 2) {
-    padding.emplace_back(op->padding()->Get(i), op->padding()->Get(i + 1));
+    padding.push_back({op->padding()->Get(i), op->padding()->Get(i + 1)});
   }
 
   std::optional<::ttnn::MemoryConfig> outputMemoryConfig =
