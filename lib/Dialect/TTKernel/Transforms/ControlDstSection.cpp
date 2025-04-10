eec25f05a79b34ba77ca8508166e0c887e3932de
Jacob DeSousa <jdesousa@tenstorrent.com>
2025-04-10 19:22:38 +0000

    TTIRToTTKernel Conversion + ControlDstSection pass + EraseFunctionArgs pass + tests

---
+++ lib/Dialect/TTKernel/Transforms/ControlDstSection.cpp
@@ -0,0 +1,66 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ttmlir/Dialect/TTKernel/Transforms/Passes.h"
+
+#include "ttmlir/Dialect/TTKernel/IR/TTKernel.h"
+#include "ttmlir/Dialect/TTKernel/IR/TTKernelOps.h"
+
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Support/LLVM.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+namespace mlir::tt::ttkernel {
+#define GEN_PASS_DEF_TTKERNELCONTROLDSTSECTION
+#include "ttmlir/Dialect/TTKernel/Transforms/Passes.h.inc"
+
+namespace {
+
+class TTKernelTileRegsRewriter : public OpRewritePattern<ttkernel::PackTileOp> {
+public:
+  using OpRewritePattern<ttkernel::PackTileOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ttkernel::PackTileOp op,
+                                PatternRewriter &rewriter) const final {
+    if (!op->getBlock()->getOps<ttkernel::TileRegsCommitOp>().empty()) {
+      return failure();
+    }
+
+    rewriter.moveOpAfter(
+        rewriter.create<ttkernel::TileRegsReleaseOp>(op->getLoc()), op);
+    auto regsWait = rewriter.create<ttkernel::TileRegsWaitOp>(op->getLoc());
+    rewriter.moveOpBefore(regsWait, op);
+    rewriter.moveOpBefore(
+        rewriter.create<ttkernel::TileRegsCommitOp>(op->getLoc()), regsWait);
+
+    rewriter.moveOpAfter(
+        rewriter.create<ttkernel::TileRegsAcquireOp>(op->getLoc()),
+        op->getBlock(), op->getBlock()->begin());
+
+    return success();
+  };
+};
+
+} // namespace
+
+namespace {
+class TTKernelControlDstSection
+    : public impl::TTKernelControlDstSectionBase<TTKernelControlDstSection> {
+public:
+  using impl::TTKernelControlDstSectionBase<
+      TTKernelControlDstSection>::TTKernelControlDstSectionBase;
+
+  void runOnOperation() final {
+    RewritePatternSet patterns(&getContext());
+    patterns.add<TTKernelTileRegsRewriter>(&getContext());
+
+    if (failed(applyPatternsGreedily(getOperation(), std::move(patterns)))) {
+      signalPassFailure();
+      return;
+    }
+  }
+};
+} // namespace
+
+} // namespace mlir::tt::ttkernel
