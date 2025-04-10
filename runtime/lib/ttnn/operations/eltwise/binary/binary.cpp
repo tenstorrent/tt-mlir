ad17245348a9ce694ef9f6ca13ed3b04126588b3
brataTT <achoudhury@tenstorrent.com>
2025-04-10 12:20:14 +0000

    update binary op params after metal change d7a8dcd

--- runtime/lib/ttnn/operations/eltwise/binary/binary.cpp
+++ runtime/lib/ttnn/operations/eltwise/binary/binary.cpp
@@ -17,8 +17,10 @@ static void runEltwiseBinaryOp(
         const std::optional<const ::ttnn::DataType> &,
         const std::optional<::ttnn::MemoryConfig> &,
         std::optional<::ttnn::Tensor>,
-        std::optional<::ttnn::operations::unary::FusedActivations>,
-        std::optional<::ttnn::operations::unary::UnaryWithParam>)> &ttnnOp) {
+        tt::stl::Span<const ::ttnn::operations::unary::UnaryWithParam>,
+        tt::stl::Span<const ::ttnn::operations::unary::UnaryWithParam>,
+        tt::stl::Span<const ::ttnn::operations::unary::UnaryWithParam>)>
+        &ttnnOp) {
 
   const ::ttnn::Tensor &lhs = tensorPool.getTTNNTensorAndValidate(op->lhs());
   const ::ttnn::Tensor &rhs = tensorPool.getTTNNTensorAndValidate(op->rhs());
@@ -37,7 +39,7 @@ static void runEltwiseBinaryOp(
              "Memory config must exist for device tensors");
 
   ::ttnn::Tensor out = ttnnOp(lhs, rhs, outputDataType, outputMemoryConfig,
-                              std::nullopt, std::nullopt, std::nullopt);
+                              std::nullopt, {}, {}, {});
 
   tensorPool.insertTTNNTensorAndValidate(op->out(), out);
 }
