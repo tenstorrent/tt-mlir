f0be64ed15bf2e290b2b93ef812afc37ed6ef71c
Nicholas Smith <nsmith@tenstorrent.com>
2025-04-11 20:45:35 -0500

    emitC conversion

--- lib/Dialect/TTMetal/Pipelines/TTMetalPipelines.cpp
+++ lib/Dialect/TTMetal/Pipelines/TTMetalPipelines.cpp
@@ -77,6 +77,7 @@ void createTTIRToTTMetalBackendPipeline(
   pm.addPass(ttkernel::createTTKernelControlDstSection());
   pm.addPass(mlir::createCanonicalizerPass());
   pm.addPass(createConvertTTIRToTTMetalPass());
+  createOptimizationPasses(pm);
 }
 
 //===----------------------------------------------------------------------===//
