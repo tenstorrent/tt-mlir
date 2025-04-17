3a1a02f915c83edb6eb7240c467d953bf59a1d2f
Anurag <anuragsingh@tenstorrent.com>
2025-04-17 17:07:07 +0000

    Adding per-axis quant/dequant/requant.

--- lib/Dialect/TTNN/IR/TTNNOps.cpp
+++ lib/Dialect/TTNN/IR/TTNNOps.cpp
@@ -343,19 +343,19 @@ static ::mlir::LogicalResult verifyQuantizeOpCommon(
     llvm::function_ref<mlir::InFlightDiagnostic()> emitOpError,
     ::mlir::RankedTensorType inputType, ::mlir::RankedTensorType outputType,
     std::optional<uint32_t> axis) {
-  // Sanity check to make sure that input rank matches the rank of the output
-  // tensor.
+  // Verify that the input rank matches the rank of the output tensor.
   if (inputType.getRank() != outputType.getRank()) {
     return emitOpError() << "Input tensor rank of " << inputType.getRank()
-                         << " does not match output tensor rank of "
+                         << " does not match the output tensor rank of "
                          << outputType.getRank();
   }
 
-  // Shapes of input and output of a quantize operation must be the same.
+  // Verify that the shapes of the input and output of a quantize operation are
+  // the same.
   if (inputType.getShape() != outputType.getShape()) {
     return emitOpError() << "Output tensor shape ("
                          << ttmlir::utils::join(outputType.getShape(), ",") +
-                                ") must match the inferred shape: (" +
+                                ") must match the input tensor shape: (" +
                                 ttmlir::utils::join(inputType.getShape(), ",") +
                                 ")";
   }
@@ -365,9 +365,61 @@ static ::mlir::LogicalResult verifyQuantizeOpCommon(
   if (axis.has_value()) {
     uint32_t axisValue = axis.value();
     if (axisValue < 0 || axisValue >= inputType.getRank()) {
-      return emitOpError() << "Axis value " << axisValue
-                           << " is out of range for tensor of rank "
-                           << inputType.getRank();
+      return emitOpError()
+             << "Axis value " << axisValue
+             << " is out of the range for the input tensor of rank "
+             << inputType.getRank();
+    }
+  }
+
+  for (auto tensorType : {inputType, outputType}) {
+    auto elemType = tensorType.getElementType();
+    // Verify that the scales size matches the axis size for per-axis
+    // quantization on both input and output types. This aligns with the
+    // runtime's behavior.
+    if (auto quantPerAxisType =
+            mlir::dyn_cast<mlir::quant::UniformQuantizedPerAxisType>(
+                elemType)) {
+      int64_t axis = quantPerAxisType.getQuantizedDimension();
+      auto shape = tensorType.getShape();
+      auto scales = quantPerAxisType.getScales();
+      if (scales.size() != static_cast<size_t>(shape[axis])) {
+        return emitOpError()
+               << "Number of scales (" << scales.size()
+               << ") does not match the size of the quantized axis ("
+               << shape[axis] << ")";
+      }
+    }
+    // Verify that the zero point is in the range of the storage type
+    // (per-tensor). This aligns with the frontends' behavior.
+    if (auto quantType =
+            mlir::dyn_cast<mlir::quant::UniformQuantizedType>(elemType)) {
+      int64_t zp = quantType.getZeroPoint();
+      int64_t min = quantType.getStorageTypeMin();
+      int64_t max = quantType.getStorageTypeMax();
+      if (zp < min || zp > max) {
+        return emitOpError() << "Zero point " << zp
+                             << " is out of the range for storage type "
+                             << quantType.getStorageType();
+      }
+    }
+
+    // Verify that the zero point is in the range of the storage type
+    // (per-axis). This aligns with the frontends' behavior.
+    if (auto quantPerAxisType =
+            mlir::dyn_cast<mlir::quant::UniformQuantizedPerAxisType>(
+                elemType)) {
+      llvm::ArrayRef<int64_t> zps = quantPerAxisType.getZeroPoints();
+      int64_t min = quantPerAxisType.getStorageTypeMin();
+      int64_t max = quantPerAxisType.getStorageTypeMax();
+      for (size_t i = 0; i < zps.size(); ++i) {
+        int64_t zp = zps[i];
+        if (zp < min || zp > max) {
+          return emitOpError() << "Zero point " << zp
+                               << " is out of the range for storage type "
+                               << quantPerAxisType.getStorageType();
+        }
+      }
     }
   }
 
@@ -443,18 +495,21 @@ static ::mlir::LogicalResult verifyQuantizeOpCommon(
   auto inputElemType = inputTensorType.getElementType();
   auto resultElemType = resultTensorType.getElementType();
 
-  if (!mlir::isa<mlir::quant::UniformQuantizedType,
-                 mlir::quant::UniformQuantizedPerAxisType>(inputElemType)) {
-    return emitOpError() << "Input element type must be UniformQuantizedType "
-                            "or UniformQuantizedPerAxisType, but got "
-                         << inputElemType;
-  }
+  auto inputIsPerAxis =
+      mlir::isa<mlir::quant::UniformQuantizedPerAxisType>(inputElemType);
+  auto outputIsPerAxis =
+      mlir::isa<mlir::quant::UniformQuantizedPerAxisType>(resultElemType);
+  auto inputIsPerTensor =
+      mlir::isa<mlir::quant::UniformQuantizedType>(inputElemType);
+  auto outputIsPerTensor =
+      mlir::isa<mlir::quant::UniformQuantizedType>(resultElemType);
 
-  if (!mlir::isa<mlir::quant::UniformQuantizedType,
-                 mlir::quant::UniformQuantizedPerAxisType>(resultElemType)) {
-    return emitOpError() << "Result element type must be UniformQuantizedType "
-                            "or UniformQuantizedPerAxisType, but got "
-                         << resultElemType;
+  if (!((inputIsPerAxis && outputIsPerAxis) ||
+        (inputIsPerTensor && outputIsPerTensor))) {
+    return emitOpError()
+           << "Input and result element types must both be per-axis "
+              "or both be per-tensor quantized types, but got "
+           << inputElemType << " and " << resultElemType;
   }
 
   return verifyQuantizeOpCommon([&]() { return emitOpError(); },
--- test/ttmlir/Silicon/TTNN/n150/quantization/simple_quantize.mlir
+++ test/ttmlir/Silicon/TTNN/n150/quantization/simple_quantize.mlir
@@ -3,14 +3,55 @@
 // RUN: ttmlir-translate --ttnn-to-flatbuffer %t.mlir > %t.ttnn
 
 module {
-  func.func @quantize_test(%arg0: tensor<1x3x320x320xf32>) -> tensor<1x3x320x320x!quant.uniform<i32:f32, 0.1>> {
-    // CHECK-LABEL: func.func @quantize_test(
-    %0 = ttir.empty() : tensor<1x3x320x320x!quant.uniform<i32:f32, 0.1>>
-    %1 = "ttir.quantize"(%arg0, %0) : (tensor<1x3x320x320xf32>, tensor<1x3x320x320x!quant.uniform<i32:f32, 0.1>>) -> tensor<1x3x320x320x!quant.uniform<i32:f32, 0.1>>
-    // CHECK: "ttnn.quantize"(%arg0)
-    // CHECK-SAME: {output_dtype = #tt.supportedDataTypes<si32>, scale = 1.000000e-01 : f32, zero_point = 0 : i32}
+  func.func @quantize_per_tensor_scale_per_tensor_zp(%arg0: tensor<1x3x320x320xf32>) -> tensor<1x3x320x320x!quant.uniform<i32:f32, 1.000000e-01>> {
+    // CHECK-LABEL: func.func @quantize_per_tensor_scale_per_tensor_zp(
+    %0 = ttir.empty() : tensor<1x3x320x320x!quant.uniform<i32:f32, 1.000000e-01>>
+    // CHECK: "ttnn.constant"
+    // CHECK-SAME: value = dense<1.000000e-01> : tensor<1xf32,
+    // CHECK-SAME: -> tensor<1xf32,
+    // CHECK: "ttnn.constant"
+    // CHECK-SAME: value = dense<0> : tensor<1xi32,
+    // CHECK-SAME: -> tensor<1xsi32,
+    // CHECK: "ttnn.quantize"
+    // CHECK-SAME: {output_dtype = #tt.supportedDataTypes<si32>}
     // CHECK-SAME: tensor<1x3x320x320xf32
     // CHECK-SAME: -> tensor<1x3x320x320x!quant.uniform<i32:f32, 1.000000e-01>,
-    return %1 : tensor<1x3x320x320x!quant.uniform<i32:f32, 0.1>>
+    %1 = "ttir.quantize"(%arg0, %0) : (tensor<1x3x320x320xf32>, tensor<1x3x320x320x!quant.uniform<i32:f32, 1.000000e-01>>) -> tensor<1x3x320x320x!quant.uniform<i32:f32, 1.000000e-01>>
+    return %1 : tensor<1x3x320x320x!quant.uniform<i32:f32, 1.000000e-01>>
+  }
+  func.func @quantize_per_axis_scale_per_tensor_zp(%arg0: tensor<1x3x320x320xf32>) -> tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01,2.000000e-01,3.000000e-01}>> {
+    // CHECK-LABEL: func.func @quantize_per_axis_scale_per_tensor_zp(
+    %0 = ttir.empty() : tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01,2.000000e-01,3.000000e-01}>>
+    // CHECK: "ttnn.constant"()
+    // CHECK-SAME: value = dense<
+    // CHECK-SAME: 1.000000e-01, 2.000000e-01, 3.000000e-01
+    // CHECK-SAME: -> tensor<3xf32,
+    // CHECK: "ttnn.full"
+    // CHECK-SAME: fillValue = 0.000000e+00 : f32
+    // CHECK-SAME: -> tensor<3xui32,
+    // CHECK: "ttnn.quantize"
+    // CHECK-SAME: {axis = 1 : i32, output_dtype = #tt.supportedDataTypes<si32>}
+    // CHECK-SAME: tensor<1x3x320x320xf32
+    // CHECK-SAME: -> tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01,2.000000e-01,3.000000e-01}>,
+    %1 = "ttir.quantize"(%arg0, %0) : (tensor<1x3x320x320xf32>, tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01,2.000000e-01,3.000000e-01}>>) -> tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01,2.000000e-01,3.000000e-01}>>
+    return %1 : tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01,2.000000e-01,3.000000e-01}>>
+  }
+  func.func @quantize_per_axis_scale_per_axis_zp(%arg0: tensor<1x3x320x320xf32>) -> tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01:10,2.000000e-01:20,3.000000e-01:30}>> {
+    // CHECK-LABEL: func.func @quantize_per_axis_scale_per_axis_zp(
+    %0 = ttir.empty() : tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01:10,2.000000e-01:20,3.000000e-01:30}>>
+    // CHECK: "ttnn.constant"()
+    // CHECK-SAME: value = dense<
+    // CHECK-SAME: 1.000000e-01, 2.000000e-01, 3.000000e-01
+    // CHECK-SAME: -> tensor<3xf32,
+    // CHECK: "ttnn.constant"()
+    // CHECK-SAME: value = dense<
+    // CHECK-SAME: 10, 20, 30
+    // CHECK-SAME: -> tensor<3xsi32,
+    // CHECK: "ttnn.quantize"
+    // CHECK-SAME: <{axis = 1 : i32, output_dtype = #tt.supportedDataTypes<si32>}>
+    // CHECK-SAME: tensor<1x3x320x320xf32
+    // CHECK-SAME: -> tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01:10,2.000000e-01:20,3.000000e-01:30}>,
+    %1 = "ttir.quantize"(%arg0, %0) : (tensor<1x3x320x320xf32>, tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01:10,2.000000e-01:20,3.000000e-01:30}>>) -> tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01:10,2.000000e-01:20,3.000000e-01:30}>>
+    return %1 : tensor<1x3x320x320x!quant.uniform<i32:f32:1, {1.000000e-01:10,2.000000e-01:20,3.000000e-01:30}>>
   }
 }
