4d13d9216a8c0a7958c8d5692c13712d3230858d
Brata Choudhury <achoudhury@tenstorrent.com>
2025-04-17 10:05:30 -0400

    Allow getCurrentSystemDesc to take in Device as optional parameter (#2917)
    ### Ticket
    [Torch Issue 580](https://github.com/tenstorrent/tt-torch/issues/580)
    
    ### Problem description
    Creating system descriptor currently opens a device by itself. Which
    means callers cannot have the same device open when
    `getCurrentSystemDesc` is called.
    
    ### What's changed
    Allow `getCurrentSystemDesc` to accept a device as argument so it
    doesn't need to create one itself and callers can have the same device
    open when this function is called.
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes
    - [x] Tested usage in tt-torch [[link to torch
    branch]](https://github.com/tenstorrent/tt-torch/compare/main...brata/sysdesc_device)

--- runtime/lib/common/system_desc.cpp
+++ runtime/lib/common/system_desc.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 #include "tt/runtime/detail/common.h"
 #include "tt/runtime/detail/logger.h"
+#include "tt/runtime/runtime.h"
 #include "tt/runtime/types.h"
 #include "tt/runtime/utils.h"
 #include "ttmlir/Target/TTNN/Target.h"
@@ -274,30 +275,53 @@ static std::unique_ptr<::tt::runtime::SystemDesc> getCurrentSystemDescImpl(
   return std::make_unique<::tt::runtime::SystemDesc>(handle);
 }
 
-std::pair<::tt::runtime::SystemDesc, DeviceIds> getCurrentSystemDesc(
-    std::optional<DispatchCoreType> dispatchCoreType = std::nullopt) {
-  size_t numDevices = ::tt::tt_metal::GetNumAvailableDevices();
-  ::tt::tt_metal::DispatchCoreType type =
-      tt::runtime::common::getDispatchCoreType(dispatchCoreType);
-  std::vector<chip_id_t> deviceIds(numDevices);
-  std::iota(deviceIds.begin(), deviceIds.end(), 0);
+static std::shared_ptr<::tt::tt_metal::distributed::MeshDevice>
+createNewMeshDevice(size_t numDevices,
+                    std::optional<DispatchCoreType> dispatchCoreType) {
   ::tt::tt_metal::distributed::MeshShape meshShape{
       1, static_cast<uint32_t>(numDevices)};
-  std::shared_ptr<::tt::tt_metal::distributed::MeshDevice> meshDevice =
-      ::tt::tt_metal::distributed::MeshDevice::create(
-          ::tt::tt_metal::distributed::MeshDeviceConfig(meshShape),
-          DEFAULT_L1_SMALL_SIZE, DEFAULT_TRACE_REGION_SIZE, 1, type);
+
+  ::tt::tt_metal::DispatchCoreType type =
+      tt::runtime::common::getDispatchCoreType(dispatchCoreType);
+
+  return ::tt::tt_metal::distributed::MeshDevice::create(
+      ::tt::tt_metal::distributed::MeshDeviceConfig(meshShape),
+      DEFAULT_L1_SMALL_SIZE, DEFAULT_TRACE_REGION_SIZE, 1, type);
+}
+
+std::pair<::tt::runtime::SystemDesc, DeviceIds>
+getCurrentSystemDesc(std::optional<DispatchCoreType> dispatchCoreType,
+                     std::optional<Device> meshDevice) {
+  const size_t numDevices = ::tt::tt_metal::GetNumAvailableDevices();
+
+  std::shared_ptr<::tt::tt_metal::distributed::MeshDevice> meshDevicePtr;
+  if (meshDevice.has_value()) {
+    meshDevicePtr =
+        meshDevice.value().asSharedPtr<::tt::tt_metal::distributed::MeshDevice>(
+            getCurrentRuntime());
+  } else {
+    meshDevicePtr = createNewMeshDevice(numDevices, dispatchCoreType);
+  }
+
   LOG_DEBUG("Device grid size = { ",
-            meshDevice->compute_with_storage_grid_size().x, ", ",
-            meshDevice->compute_with_storage_grid_size().y, " }");
+            meshDevicePtr->compute_with_storage_grid_size().x, ", ",
+            meshDevicePtr->compute_with_storage_grid_size().y, " }");
+
   std::exception_ptr eptr = nullptr;
   std::unique_ptr<::tt::runtime::SystemDesc> desc;
+  std::vector<chip_id_t> deviceIds;
   try {
-    desc = getCurrentSystemDescImpl(*meshDevice);
+    desc = getCurrentSystemDescImpl(*meshDevicePtr);
+
+    deviceIds = meshDevicePtr->get_device_ids();
+    std::sort(deviceIds.begin(), deviceIds.end());
   } catch (...) {
     eptr = std::current_exception();
   }
-  meshDevice->close();
+  if (!meshDevice.has_value()) {
+    // Close if mesh device was created in this scope (not passed from caller)
+    meshDevicePtr->close();
+  }
   if (eptr) {
     std::rethrow_exception(eptr);
   }
