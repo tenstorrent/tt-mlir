95497f972162570d5d0628471814a0fbc3da48a6
Anurag <anuragsingh@tenstorrent.com>
2025-04-17 20:14:10 +0000

    responding to comments

--- lib/Dialect/TTIR/Transforms/Quantization.cpp
+++ lib/Dialect/TTIR/Transforms/Quantization.cpp
@@ -12,104 +12,105 @@
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/Transforms/WalkPatternRewriteDriver.h"
 
+#include <limits>
+
 namespace mlir::tt::ttir {
 #define GEN_PASS_DEF_TTIRQUANTDATATYPECONVERSIONPASS
 #include "ttmlir/Dialect/TTIR/Transforms/Passes.h.inc"
 
 namespace {
 
-// Helper function to convert integer bit width string to integer type.
-static Type getIntegerTypeFromString(MLIRContext *context, StringRef bitWidth) {
-  // Default to int32 if empty.
-  if (bitWidth.empty()) {
+// Helper function to convert integer bit width to IntegerType.
+static IntegerType getIntegerTypeFromBitWidth(MLIRContext *context,
+                                              uint32_t bitWidth) {
+  // Default to int32 if 0 or invalid.
+  switch (bitWidth) {
+  case 8:
+  case 16:
+  case 32:
+  case 64:
+    return IntegerType::get(context, bitWidth, IntegerType::Signed);
+  default:
+    // Fallback to int32 if the bit width is not supported.
     return IntegerType::get(context, 32, IntegerType::Signed);
   }
-
-  struct BitWidthMapping {
-    StringRef name;
-    unsigned width;
-  };
-
-  static const BitWidthMapping mappings[] = {
-      {"int8", 8}, {"int16", 16}, {"int32", 32}, {"int64", 64}};
-
-  for (const BitWidthMapping &mapping : mappings) {
-    if (bitWidth == mapping.name) {
-      return IntegerType::get(context, mapping.width, IntegerType::Signed);
-    }
-  }
-
-  return nullptr;
 }
 
 static std::pair<int64_t, int64_t> getStorageTypeMinMax(IntegerType intType) {
-  // Only need to return signed min and max.
-  int64_t min = -(1LL << (intType.getWidth() - 1));
-  int64_t max = (1LL << (intType.getWidth() - 1)) - 1;
-  return {min, max};
+  switch (intType.getWidth()) {
+  case 8:
+    return {std::numeric_limits<int8_t>::min(),
+            std::numeric_limits<int8_t>::max()};
+  case 16:
+    return {std::numeric_limits<int16_t>::min(),
+            std::numeric_limits<int16_t>::max()};
+  case 32:
+    return {std::numeric_limits<int32_t>::min(),
+            std::numeric_limits<int32_t>::max()};
+  case 64:
+    return {std::numeric_limits<int64_t>::min(),
+            std::numeric_limits<int64_t>::max()};
+  default:
+    // Fallback for unsupported widths.
+    return {-(1LL << (intType.getWidth() - 1)),
+            (1LL << (intType.getWidth() - 1)) - 1};
+  }
 }
 
 // Convert quantized type to use the target integer bit width.
-static Type convertQuantizedType(Type type, Type targetIntType) {
-  if (quant::QuantizedType quantType = dyn_cast<quant::QuantizedType>(type)) {
-    // Use the target integer type's min and max values.
-    IntegerType intType = cast<IntegerType>(targetIntType);
-    std::pair<int64_t, int64_t> storageTypeMinMax =
-        getStorageTypeMinMax(intType);
-    int64_t storageTypeMin = storageTypeMinMax.first;
-    int64_t storageTypeMax = storageTypeMinMax.second;
+static Type convertQuantizedType(quant::QuantizedType quantType,
+                                 IntegerType targetIntType) {
+  // Use the target integer type's min and max values.
+  std::pair<int64_t, int64_t> storageTypeMinMax =
+      getStorageTypeMinMax(targetIntType);
+  int64_t storageTypeMin = storageTypeMinMax.first;
+  int64_t storageTypeMax = storageTypeMinMax.second;
 
-    assert(intType.getWidth() >=
-               quantType.getStorageType().getIntOrFloatBitWidth() &&
-           "Target integer type is smaller than quantized type. Out of range.");
+  assert(targetIntType.getWidth() >=
+             quantType.getStorageType().getIntOrFloatBitWidth() &&
+         "Target integer type is smaller than quantized type. Out of range.");
 
-    if (quant::UniformQuantizedType uniformType =
-            dyn_cast<quant::UniformQuantizedType>(quantType)) {
-      return quant::UniformQuantizedType::get(
-          uniformType.getFlags(), targetIntType, uniformType.getExpressedType(),
-          uniformType.getScale(), uniformType.getZeroPoint(), storageTypeMin,
-          storageTypeMax);
-    }
-    if (quant::UniformQuantizedPerAxisType perAxisType =
-            dyn_cast<quant::UniformQuantizedPerAxisType>(quantType)) {
-      return quant::UniformQuantizedPerAxisType::get(
-          perAxisType.getFlags(), targetIntType, perAxisType.getExpressedType(),
-          perAxisType.getScales(), perAxisType.getZeroPoints(),
-          perAxisType.getQuantizedDimension(), storageTypeMin, storageTypeMax);
-    }
+  if (quant::UniformQuantizedType uniformType =
+          dyn_cast<quant::UniformQuantizedType>(quantType)) {
+    return quant::UniformQuantizedType::get(
+        uniformType.getFlags(), targetIntType, uniformType.getExpressedType(),
+        uniformType.getScale(), uniformType.getZeroPoint(), storageTypeMin,
+        storageTypeMax);
   }
-  if (RankedTensorType tensorType = dyn_cast<RankedTensorType>(type)) {
-    if (quant::QuantizedType elementType =
-            dyn_cast<quant::QuantizedType>(tensorType.getElementType())) {
-      return RankedTensorType::get(
-          tensorType.getShape(),
-          convertQuantizedType(elementType, targetIntType),
-          tensorType.getEncoding());
-    }
+  if (quant::UniformQuantizedPerAxisType perAxisType =
+          dyn_cast<quant::UniformQuantizedPerAxisType>(quantType)) {
+    return quant::UniformQuantizedPerAxisType::get(
+        perAxisType.getFlags(), targetIntType, perAxisType.getExpressedType(),
+        perAxisType.getScales(), perAxisType.getZeroPoints(),
+        perAxisType.getQuantizedDimension(), storageTypeMin, storageTypeMax);
   }
-  return type;
+  return quantType;
 }
 
 class QuantDataTypeConverter : public TypeConverter {
 private:
-  Type targetIntType;
+  IntegerType targetIntType;
 
 public:
-  QuantDataTypeConverter(Type targetIntType) : targetIntType(targetIntType) {
+  QuantDataTypeConverter(IntegerType targetIntType)
+      : targetIntType(targetIntType) {
     addConversion([](Type type) -> Type { return type; });
 
-    addConversion([targetIntType](RankedTensorType type) -> Type {
-      Type elementType = type.getElementType();
-      if (!isa<quant::QuantizedType>(elementType)) {
-        return type;
-      }
-      Type newElementType = convertQuantizedType(elementType, targetIntType);
-      if (newElementType == elementType) {
-        return type;
-      }
-      return RankedTensorType::get(type.getShape(), newElementType,
-                                   type.getEncoding());
-    });
+    addConversion(
+        [targetIntType](RankedTensorType type) -> std::optional<Type> {
+          Type elementType = type.getElementType();
+          if (!isa<quant::QuantizedType>(elementType)) {
+            return std::nullopt;
+          }
+          auto quantElementType = dyn_cast<quant::QuantizedType>(elementType);
+          Type newElementType =
+              convertQuantizedType(quantElementType, targetIntType);
+          if (newElementType == elementType) {
+            return type;
+          }
+          return RankedTensorType::get(type.getShape(), newElementType,
+                                       type.getEncoding());
+        });
 
     addConversion([targetIntType](quant::QuantizedType type) -> Type {
       return convertQuantizedType(type, targetIntType);
@@ -131,10 +132,10 @@ struct TTIRQuantDataTypeConversionPass
   void runOnOperation() final {
     MLIRContext *context = &getContext();
 
-    Type targetIntType = getIntegerTypeFromString(context, targetBitWidth);
+    IntegerType targetIntType =
+        getIntegerTypeFromBitWidth(context, targetBitWidth);
     assert(targetIntType &&
-           ("Invalid target bit width: " + targetBitWidth).c_str());
-
+           "Invalid quantization bit width (must be 8, 16, 32, or 64).");
     RewritePatternSet patterns(context);
     QuantDataTypeConverter converter(targetIntType);
     patterns.add<UniformTypeRewriter>(converter, context);
--- test/ttmlir/Dialect/TTIR/Transforms/quant_data_type_conversion_negative.mlir
+++ test/ttmlir/Dialect/TTIR/Transforms/quant_data_type_conversion_negative.mlir
@@ -1,9 +1,7 @@
-// RUN: not ttmlir-opt --ttir-quant-data-type-conversion="quant-data-type=int8" %s 2>&1 | FileCheck %s --check-prefix=CHECK-BAD-INT32
-// RUN: not ttmlir-opt --ttir-quant-data-type-conversion="quant-data-type=float32" %s 2>&1 | FileCheck %s --check-prefix=CHECK-BAD-FLOAT32
+// RUN: not ttmlir-opt --ttir-quant-data-type-conversion="quant-bit-width=8" %s 2>&1 | FileCheck %s
 // XFAIL: *
 func.func @requantize_datatype_negative(%arg0: tensor<1x3x320x320x!quant.uniform<i32:f32, 1.000000e-01>>) -> tensor<1x3x320x320x!quant.uniform<i32:f32, 2.000000e-01>> {
-  // CHECK-BAD-INT32: Assertion `intType.getWidth() >= quantType.getStorageType().getIntOrFloatBitWidth() && "Target integer type is smaller than quantized type. Out of range."' failed.
-  // CHECK-BAD-FLOAT32: Assertion `targetIntType && ("Invalid target bit width: " + targetBitWidth).c_str()' failed.
+  // CHECK: Assertion `intType.getWidth() >= quantType.getStorageType().getIntOrFloatBitWidth() && "Target integer type is smaller than quantized type. Out of range."' failed.
   %0 = ttir.empty() : tensor<1x3x320x320x!quant.uniform<i32:f32, 2.000000e-01>>
   %1 = "ttir.requantize"(%arg0, %0) : (tensor<1x3x320x320x!quant.uniform<i32:f32, 1.000000e-01>>, tensor<1x3x320x320x!quant.uniform<i32:f32, 2.000000e-01>>) -> tensor<1x3x320x320x!quant.uniform<i32:f32, 2.000000e-01>>
   return %1 : tensor<1x3x320x320x!quant.uniform<i32:f32, 2.000000e-01>>
