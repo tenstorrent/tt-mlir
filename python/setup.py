ff43e6cbb2f63d02870a696d470face05dcf6231
vprajapati-tt <vprajapati@tenstorrent.com>
2025-04-22 20:25:53 +0000

    Update wheel building CI logic

--- python/setup.py
+++ python/setup.py
@@ -85,7 +85,7 @@ date = (
     .decode("ascii")
     .strip()
 )
-version = "0.1." + date + "+dev." + short_hash
+version = "0.1." + date + ".dev0+" + short_hash
 
 # Only the ttmlir package relies on the CMake build process
 ttmlir_c = TTExtension("ttmlir")
