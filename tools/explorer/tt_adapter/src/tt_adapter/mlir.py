9d76f467357ffa625582b2f158a47be1a995d263
madcampos <mcampos@tenstorrent.com>
2025-05-05 15:09:14 +0000

    chore: fix lint issues

--- tools/explorer/tt_adapter/src/tt_adapter/mlir.py
+++ tools/explorer/tt_adapter/src/tt_adapter/mlir.py
@@ -846,7 +846,9 @@ FILTERED_OPS = [
 ]
 
 
-def build_graph(module_path: str, module, perf_trace=None, memory_trace=None, golden_results=None):
+def build_graph(
+    module_path: str, module, perf_trace=None, memory_trace=None, golden_results=None
+):
     graph_id = Path(module_path).name
     output_connections = defaultdict(int)
     graph = graph_builder.Graph(id=graph_id)
