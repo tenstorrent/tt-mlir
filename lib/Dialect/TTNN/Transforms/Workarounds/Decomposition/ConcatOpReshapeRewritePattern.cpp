53ef2c06e978d55c56f659669083895f97414cc3
Muhammad Asif Manzoor <mmanzoor@tenstorrent.com>
2025-05-05 19:17:06 -0400

    Add workaround for concat op (#3188)
    ### Ticket
    https://github.com/tenstorrent/tt-mlir/issues/3193
    
    ### Problem description
    Concat op is failing for following two cases. 
    1. If all input tensors have shape of [1, 1] or [1, 1, 1] and concat is
    performed on last dimension then the generated output is incorrect. The
    first element of the output is correct while all other elements are
    zero.
    2. The second case is extension of the first one if any tensor's last
    dim is not `1` (but not for all tensors) then it crashes with `TT_FATAL`
    error. For example we want to concatenate 3 tensors and the shapes are
    [1, 32], [1, 2], [1, 1] then this will crash.
    
    ### What's changed
    Add one additional dimension to all the input tensor (using reshape) for
    the cases described above so that concatenation is not applied on the
    last dim of the input tensor. After concatenation; reshape backe the
    output tensor to original shape.
    
    ### Checklist
    - [X] New/Existing tests provide coverage for changes

--- lib/Dialect/TTNN/Transforms/Workarounds/Decomposition/ConcatOpReshapeRewritePattern.cpp
+++ lib/Dialect/TTNN/Transforms/Workarounds/Decomposition/ConcatOpReshapeRewritePattern.cpp
@@ -12,72 +12,82 @@
 #include "mlir/IR/Value.h"
 #include "mlir/Support/LLVM.h"
 #include "llvm/ADT/ArrayRef.h"
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallVector.h"
 
 namespace mlir::tt::ttnn::workarounds::decomposition {
+static bool isWorkaroundRequired(ttnn::ConcatOp srcOp) {
+  auto areAllDimsExceptLastOne = [](auto shape) {
+    return std::all_of(shape.begin(), shape.end() - 1,
+                       [](int64_t dim) { return dim == 1; });
+  };
+  // Check if we need to apply the workaround.
+  bool anyInputLastDimOne = false;
+  for (auto input : srcOp.getInputs()) {
+    auto shape =
+        mlir::dyn_cast<mlir::RankedTensorType>(input.getType()).getShape();
+    assert(!shape.empty());
 
+    // All dimensions except the last must be 1.
+    if (!areAllDimsExceptLastOne(shape)) {
+      return false;
+    }
+
+    // At least one input must have last dimension of size 1.
+    if (shape.back() == 1) {
+      anyInputLastDimOne = true;
+    }
+  }
+
+  return anyInputLastDimOne;
+}
+
+// Issue to track in tt-metal repo:
+// https://github.com/tenstorrent/tt-metal/issues/21581.
 LogicalResult ConcatOpReshapeRewritePattern::matchAndRewrite(
     ttnn::ConcatOp srcOp, PatternRewriter &rewriter) const {
   int64_t dim = srcOp.getDim();
-  mlir::RankedTensorType outputType =
-      mlir::dyn_cast<mlir::RankedTensorType>(srcOp->getResultTypes().front());
+  mlir::RankedTensorType outputType = srcOp.getResult().getType();
   int64_t rank = outputType.getRank();
-  if ((dim + 1) != rank) {
+
+  // Check if this is a concat on the last dimension with rank 2 or 3.
+  if (((dim + 1) != rank) || (rank != 2 && rank != 3)) {
     return failure();
   }
 
-  auto inputs = srcOp.getInputs();
-  bool isWorkaroundRequired = true;
-  llvm::SmallVector<int64_t, 4> lastDims;
-
-  for (auto input : inputs) {
-    auto inputShape =
-        mlir::dyn_cast<mlir::RankedTensorType>(input.getType()).getShape();
-    lastDims.emplace_back(inputShape.back());
-    isWorkaroundRequired &=
-        llvm::all_of(inputShape, [&](int64_t index) { return index == 1; });
-  }
-
-  auto countDims =
-      llvm::count_if(lastDims, [&](int64_t index) { return index == 1; });
-
-  isWorkaroundRequired |= (countDims > 0 && countDims < rank);
-  if (!isWorkaroundRequired) {
+  if (!isWorkaroundRequired(srcOp)) {
     return failure();
   }
 
-  llvm::SmallVector<mlir::Value, 2> adaptedInputs;
+  // Apply workaround: reshape inputs by adding an extra dimension.
+  llvm::SmallVector<mlir::Value> reshapedInputs;
+  for (auto input : srcOp.getInputs()) {
+    auto typedInput = dyn_cast<mlir::TypedValue<mlir::RankedTensorType>>(input);
+    llvm::SmallVector<int64_t> newShape(typedInput.getType().getShape());
+    newShape.push_back(1);
 
-  for (auto input : inputs) {
-    auto inputShape =
-        mlir::dyn_cast<mlir::RankedTensorType>(input.getType()).getShape();
-    llvm::SmallVector<int64_t, 4> adaptedShape(inputShape);
-    adaptedShape.emplace_back(1);
-    auto check = dyn_cast<mlir::TypedValue<mlir::RankedTensorType>>(input);
-    ReshapeOp adaptedInput =
-        ttir_to_ttnn::utils::generateReshape(check, adaptedShape, rewriter);
-    adaptedInputs.emplace_back(adaptedInput);
+    reshapedInputs.push_back(
+        ttir_to_ttnn::utils::generateReshape(typedInput, newShape, rewriter));
   }
 
-  auto outputShape = outputType.getShape();
-  llvm::SmallVector<int64_t, 4> adaptedOutputShape(outputShape);
-  adaptedOutputShape.emplace_back(1);
-  mlir::RankedTensorType adaptedOutputType =
-      mlir::RankedTensorType::Builder(outputType)
-          .setShape(adaptedOutputShape)
-          .setEncoding(
-              mlir::cast<ttnn::TTNNLayoutAttr>(outputType.getEncoding())
-                  .withTensorShape(adaptedOutputShape));
+  // Create output type with extra dimension.
+  llvm::SmallVector<int64_t> newOutputShape(outputType.getShape());
+  newOutputShape.push_back(1);
 
-  ConcatOp adaptedConcatOp = rewriter.create<mlir::tt::ttnn::ConcatOp>(
-      srcOp->getLoc(), adaptedOutputType, adaptedInputs, srcOp.getDim(),
+  auto newOutputType = mlir::RankedTensorType::get(
+      newOutputShape, outputType.getElementType(),
+      mlir::cast<ttnn::TTNNLayoutAttr>(outputType.getEncoding())
+          .withTensorShape(newOutputShape));
+
+  // Create new concat op with reshaped inputs and new output type.
+  auto newConcatOp = rewriter.create<ttnn::ConcatOp>(
+      srcOp->getLoc(), newOutputType, reshapedInputs, dim,
       /*memory_config=*/nullptr);
 
-  ReshapeOp concatOutput = ttir_to_ttnn::utils::generateReshape(
-      adaptedConcatOp, srcOp.getResult().getType().getShape(), rewriter);
-  rewriter.replaceOp(srcOp, concatOutput);
+  // Reshape back to the original output shape.
+  auto result = ttir_to_ttnn::utils::generateReshape(
+      newConcatOp, outputType.getShape(), rewriter);
 
+  rewriter.replaceOp(srcOp, result);
   return success();
 }
 
