ecdbc35e7ba1fb7cc2994ea01eb889721ed4cb63
jserbedzija <jserbedzija@tenstorrent.com>
2025-05-09 12:41:50 +0000

    Set use legacy flag in the runtime for binary ops

--- runtime/lib/ttnn/operations/eltwise/binary/binary.cpp
+++ runtime/lib/ttnn/operations/eltwise/binary/binary.cpp
@@ -9,7 +9,46 @@
 
 namespace tt::runtime::ttnn::operations::eltwise::binary {
 
-static void runEltwiseBinaryOp(
+// static void runEltwiseBinaryOp(
+//     const ::tt::target::ttnn::EltwiseBinaryOp *op,
+//     ProgramTensorPool &tensorPool,
+//     const std::function<::ttnn::Tensor(
+//         const ::ttnn::Tensor &, const ::ttnn::Tensor &,
+//         const std::optional<const ::ttnn::DataType> &,
+//         const std::optional<::ttnn::MemoryConfig> &,
+//         std::optional<::ttnn::Tensor>,
+//         tt::stl::Span<const ::ttnn::operations::unary::UnaryWithParam>,
+//         tt::stl::Span<const ::ttnn::operations::unary::UnaryWithParam>,
+//         tt::stl::Span<const ::ttnn::operations::unary::UnaryWithParam>)>
+//         &ttnnOp) {
+
+//   ::ttnn::Tensor *lhs = &(tensorPool.getTTNNTensorAndValidate(op->lhs()));
+//   ::ttnn::Tensor *rhs = &(tensorPool.getTTNNTensorAndValidate(op->rhs()));
+
+//   if (operations::utils::shouldSwapBinaryOperands(*lhs, *rhs)) {
+//     std::swap(lhs, rhs);
+//   }
+
+//   std::optional<::ttnn::DataType> outputDataType = std::nullopt;
+//   if (op->output_dtype()) {
+//     outputDataType =
+//         ::tt::runtime::ttnn::utils::toTTNNDataType(*(op->output_dtype()));
+//   }
+
+//   std::optional<::ttnn::MemoryConfig> outputMemoryConfig =
+//       ::tt::runtime::ttnn::utils::createMemoryConfigIfNeeded(
+//           op->memory_config());
+//   LOG_ASSERT(::tt::runtime::ttnn::utils::inSystemMemory(op->out()) ||
+//                  outputMemoryConfig.has_value(),
+//              "Memory config must exist for device tensors");
+
+//   ::ttnn::Tensor out = ttnnOp(*lhs, *rhs, outputDataType, outputMemoryConfig,
+//                               std::nullopt, {}, {}, {});
+
+//   tensorPool.insertTTNNTensorAndValidate(op->out(), out);
+// }
+
+static void runEltwiseNGBinaryOp(
     const ::tt::target::ttnn::EltwiseBinaryOp *op,
     ProgramTensorPool &tensorPool,
     const std::function<::ttnn::Tensor(
@@ -19,8 +58,8 @@ static void runEltwiseBinaryOp(
         std::optional<::ttnn::Tensor>,
         tt::stl::Span<const ::ttnn::operations::unary::UnaryWithParam>,
         tt::stl::Span<const ::ttnn::operations::unary::UnaryWithParam>,
-        tt::stl::Span<const ::ttnn::operations::unary::UnaryWithParam>)>
-        &ttnnOp) {
+        tt::stl::Span<const ::ttnn::operations::unary::UnaryWithParam>,
+        std::optional<bool>)> &ttnnOp) {
 
   ::ttnn::Tensor *lhs = &(tensorPool.getTTNNTensorAndValidate(op->lhs()));
   ::ttnn::Tensor *rhs = &(tensorPool.getTTNNTensorAndValidate(op->rhs()));
@@ -43,7 +82,7 @@ static void runEltwiseBinaryOp(
              "Memory config must exist for device tensors");
 
   ::ttnn::Tensor out = ttnnOp(*lhs, *rhs, outputDataType, outputMemoryConfig,
-                              std::nullopt, {}, {}, {});
+                              std::nullopt, {}, {}, {}, /* use_legacy */ false);
 
   tensorPool.insertTTNNTensorAndValidate(op->out(), out);
 }
@@ -54,55 +93,55 @@ void run(const ::tt::target::ttnn::EltwiseBinaryOp *op,
   switch (op->type()) {
   /* Eltwise Binary */
   case ::tt::target::ttnn::EltwiseBinaryOpType::Add: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::add);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::add);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::Multiply: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::multiply);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::multiply);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::Subtract: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::subtract);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::subtract);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::Equal: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::eq);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::eq);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::NotEqual: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::ne);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::ne);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::GreaterEqual: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::ge);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::ge);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::GreaterThan: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::gt);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::gt);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::LessEqual: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::le);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::le);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::LessThan: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::lt);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::lt);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::Divide: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::divide);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::divide);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::LogicalAnd: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::logical_and);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::logical_and);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::LogicalOr: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::logical_or);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::logical_or);
     break;
   }
   case ::tt::target::ttnn::EltwiseBinaryOpType::LogicalXor: {
-    runEltwiseBinaryOp(op, tensorPool, ::ttnn::logical_xor);
+    runEltwiseNGBinaryOp(op, tensorPool, ::ttnn::logical_xor);
     break;
   }
   }
