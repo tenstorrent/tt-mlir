76c2b621815ab0eee03c5bf8e600b22ab61951b3
jserbedzija <jserbedzija@tenstorrent.com>
2025-05-09 08:45:17 +0000

    Introduce changes to the ImplicitBroadcastFold pass

--- lib/Dialect/TTIR/Transforms/Broadcast.cpp
+++ lib/Dialect/TTIR/Transforms/Broadcast.cpp
@@ -4,6 +4,7 @@
 
 #include "ttmlir/Dialect/TT/IR/TT.h"
 #include "ttmlir/Dialect/TTIR/Transforms/Passes.h"
+#include "ttmlir/Dialect/TTIR/Utils/Utils.h"
 
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
@@ -50,16 +51,15 @@ public:
       }
     }
 
-    bool changed = false;
     if (op->hasTrait<PartiallyBroadcastable::Trait>()) {
       // This operation only support implicit broadcast for Operand 0.
       ttir::BroadcastOp broadcastOp =
           op->getOperand(0).getDefiningOp<ttir::BroadcastOp>();
       if (broadcastOp) {
-        Operation *newOp = rewriter.clone(*op);
-        newOp->setOperand(0, broadcastOp.getInput());
-        rewriter.replaceOp(op, newOp);
-        changed = true;
+        rewriter.modifyOpInPlace(
+            op, [&]() { op->setOperand(0, broadcastOp.getInput()); });
+
+        return success();
       }
     } else if (op->hasTrait<FullyBroadcastable::Trait>()) {
       // Check all operands of this op.
@@ -67,20 +67,73 @@ public:
           op->getOperand(0).getDefiningOp<ttir::BroadcastOp>();
       ttir::BroadcastOp broadcastOp1 =
           op->getOperand(1).getDefiningOp<ttir::BroadcastOp>();
-      if (broadcastOp0) {
+
+      if (broadcastOp0 && broadcastOp1) {
         Operation *newOp = rewriter.clone(*op);
+
+        // First operand can always be implicitly broadcast.
         newOp->setOperand(0, broadcastOp0.getInput());
-        rewriter.replaceOp(op, newOp);
-        changed = true;
-      } else if (broadcastOp1) {
-        Operation *newOp = rewriter.clone(*op);
-        newOp->setOperand(1, broadcastOp1.getInput());
-        rewriter.replaceOp(op, newOp);
-        changed = true;
+
+        if (broadcastOp0.getBroadcastDimensions() ==
+            broadcastOp1.getBroadcastDimensions()) {
+          // If the broadcast dimensions for the operands are the same, we can
+          // remove them and add a broadcast to the output of the operation.
+          newOp->setOperand(1, broadcastOp1.getInput());
+
+          rewriter.setInsertionPoint(newOp);
+          auto initTensor = rewriter.create<ttir::EmptyOp>(
+              op->getLoc(), broadcastOp0.getInput().getType().getShape(),
+              broadcastOp0.getInput().getType().getElementType());
+          newOp->setOperand(2, initTensor.getResult());
+          newOp->getResult(0).setType(broadcastOp0.getInput().getType());
+          rewriter.replaceOp(op, newOp);
+          rewriter.setInsertionPointAfter(newOp);
+
+          // Insert a broadcast to the output.
+          ::llvm::ArrayRef<int64_t> broadcastDims =
+              broadcastOp0.getBroadcastDimensions();
+          auto newBroadcastOp = ttir::utils::createDPSOp<ttir::BroadcastOp>(
+              rewriter, newOp->getLoc(), resultType, newOp->getResult(0),
+              broadcastDims);
+
+          rewriter.replaceAllUsesExcept(
+              newOp->getResult(0), newBroadcastOp.getResult(), newBroadcastOp);
+        } else {
+          // If the broadcast dimensions for the operands are different, we can
+          // only implicitly broadcast the second operand if there are no common
+          // elements between broadcast dimensions other than 1.
+          bool dimensionsCompatible =
+              !llvm::any_of(llvm::zip(broadcastOp0.getBroadcastDimensions(),
+                                      broadcastOp1.getBroadcastDimensions()),
+                            [](std::tuple<int64_t, int64_t> pair) {
+                              return std::get<0>(pair) == std::get<1>(pair) &&
+                                     std::get<0>(pair) != 1;
+                            });
+
+          if (dimensionsCompatible) {
+            newOp->setOperand(1, broadcastOp1.getInput());
+          }
+
+          rewriter.replaceOp(op, newOp);
+        }
+
+        return success();
+      }
+
+      if (broadcastOp0 || broadcastOp1) {
+        rewriter.modifyOpInPlace(op, [&]() {
+          if (broadcastOp0) {
+            op->setOperand(0, broadcastOp0.getInput());
+          } else if (broadcastOp1) {
+            op->setOperand(1, broadcastOp1.getInput());
+          }
+        });
+
+        return success();
       }
     }
 
-    return changed ? success() : failure();
+    return failure();
   }
 };
 
--- test/ttmlir/Dialect/TTNN/data_movement/repeat/repeat_workaround.mlir
+++ test/ttmlir/Dialect/TTNN/data_movement/repeat/repeat_workaround.mlir
@@ -1,14 +1,16 @@
 // RUN: ttmlir-opt --ttir-to-ttnn-backend-pipeline %s | FileCheck %s
 module {
-  func.func @main(%arg0: tensor<1x16x32xf32>, %arg1: tensor<1x1x32xf32>) -> tensor<1x16x32xf32> {
+  func.func @main(%arg0: tensor<1x1x32xf32>, %arg1: tensor<1x1x1xf32>) -> tensor<1x16x32xf32> {
     // CHECK: %[[VAL0:[0-9]+]] = "ttnn.full"
     // CHECK-SAME: fillValue = 0.000000e+00 : f32
     // CHECK: %{{[0-9]+}} = "ttnn.add"(%arg1, %{{[0-9]+}})
     // CHECK-NOT: "ttnn.repeat"
     %0 = ttir.empty() : tensor<1x16x32xf32>
-    %1 = "ttir.broadcast"(%arg1, %0) <{broadcast_dimensions = array<i64: 1, 16, 1>}> : (tensor<1x1x32xf32>, tensor<1x16x32xf32>) -> tensor<1x16x32xf32>
+    %1 = "ttir.broadcast"(%arg0, %0) <{broadcast_dimensions = array<i64: 1, 16, 1>}> : (tensor<1x1x32xf32>, tensor<1x16x32xf32>) -> tensor<1x16x32xf32>
     %2 = ttir.empty() : tensor<1x16x32xf32>
-    %3 = "ttir.multiply"(%arg0, %1, %2) : (tensor<1x16x32xf32>, tensor<1x16x32xf32>, tensor<1x16x32xf32>) -> tensor<1x16x32xf32>
-    return %3 : tensor<1x16x32xf32>
+    %3 = "ttir.broadcast"(%arg1, %0) <{broadcast_dimensions = array<i64: 1, 16, 32>}> : (tensor<1x1x1xf32>, tensor<1x16x32xf32>) -> tensor<1x16x32xf32>
+    %4 = ttir.empty() : tensor<1x16x32xf32>
+    %5 = "ttir.multiply"(%1, %3, %4) : (tensor<1x16x32xf32>, tensor<1x16x32xf32>, tensor<1x16x32xf32>) -> tensor<1x16x32xf32>
+    return %5 : tensor<1x16x32xf32>
   }
 }
