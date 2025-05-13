e87c87e8cac2d62b1d2eac0f3532ffade25a6cc3
jgrimTT <jgrim@tenstorrent.com>
2025-05-13 03:20:13 +0000

    Fixing import error

--- runtime/tools/ttrt/ttrt/runtime/__init__.py
+++ runtime/tools/ttrt/ttrt/runtime/__init__.py
@@ -43,6 +43,15 @@ try:
         get_op_loc_info,
         unregister_hooks,
     )
+
+    from ttrt.binary._binary_C import (
+        load_from_path,
+        load_binary_from_path,
+        load_binary_from_capsule,
+        load_system_desc_from_path,
+        Flatbuffer,
+        GoldenTensor,
+    )
 except ModuleNotFoundError:
     raise ImportError(
         "Error: Project was not built with runtime enabled, rebuild with: -DTTMLIR_ENABLE_RUNTIME=ON"
@@ -54,3 +63,15 @@ except ImportError:
     print(
         "Warning: not importing testing submodule since project was not built with runtime testing enabled. To enable, rebuild with: -DTTMLIR_ENABLE_RUNTIME_TESTS=ON"
     )
+
+import json
+
+
+def as_dict(bin):
+    tmp = bin.as_json()
+    # Flatbuffers emits 'nan' and 'inf'
+    # But Python's JSON accepts only 'NaN' and 'Infinity' and nothing else
+    # We include the comma to avoid replacing 'inf' in contexts like 'info'
+    tmp = tmp.replace("nan,", "NaN,")
+    tmp = tmp.replace("inf,", "Infinity,")
+    return json.loads(tmp)
