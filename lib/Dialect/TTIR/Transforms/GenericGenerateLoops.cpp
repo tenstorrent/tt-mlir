69860486b4951731c9af2520116696f85619008f
Nicholas Smith <nsmith@tenstorrent.com>
2025-05-15 19:29:31 +0000

    wip

--- lib/Dialect/TTIR/Transforms/GenericGenerateLoops.cpp
+++ lib/Dialect/TTIR/Transforms/GenericGenerateLoops.cpp
@@ -66,7 +66,7 @@ public:
 
     auto loopedGeneric = rewriter.create<GenericOp>(
         generic->getLoc(), generic.getResultTypes(), generic.getInputs(),
-        generic.getOutputs(), generic.getGrid(),
+        generic.getOutputs(), generic.getGrid(), generic.getBlockFactors(),
         /* indexing_maps */ rewriter.getArrayAttr({}),
         /* iterator_types */ rewriter.getArrayAttr({}), generic.getThreads(),
         generic.getNumRegions());
