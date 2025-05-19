ab1ffeb9d58afbd32591753d157e4e3a0fbf163d
Aleksandar Zecevic <azecevic@tenstorrent.com>
2025-05-19 07:23:42 +0000

    string_view funcName: dylib.h

--- runtime/include/tt/runtime/test/ttnn/dylib.h
+++ runtime/include/tt/runtime/test/ttnn/dylib.h
@@ -11,7 +11,7 @@ namespace tt::runtime::test::ttnn {
 
 void *openSo(std::string_view path);
 void closeSo(void *handle);
-std::vector<Tensor> runSoProgram(void *so, std::string funcName,
+std::vector<Tensor> runSoProgram(void *so, std::string_view funcName,
                                  std::vector<Tensor> inputs, Device device);
 bool compareOuts(std::vector<Tensor> &lhs, std::vector<Tensor> &rhs);
 } // namespace tt::runtime::test::ttnn
