ba74177584351ad56358d89e98e19720eab2376b
Jacob DeSousa <jdesousa@tenstorrent.com>
2025-05-20 20:06:40 +0000

    rename GenerateLLKHeaders to GenerateXXDHeader

--- cmake/modules/GenerateXXDHeader.cmake
+++ cmake/modules/GenerateXXDHeader.cmake
@@ -1,5 +1,5 @@
 # Function to generate C++ header files from input header files using xxd
-function(generate_llk_header INPUT_FILE OUTPUT_FILE VARIABLE_NAME)
+function(generate_xxd_header INPUT_FILE OUTPUT_FILE VARIABLE_NAME)
   # Create the output directory if it doesn't exist
   get_filename_component(OUTPUT_DIR ${OUTPUT_FILE} DIRECTORY)
   file(MAKE_DIRECTORY ${OUTPUT_DIR})
@@ -12,7 +12,7 @@ function(generate_llk_header INPUT_FILE OUTPUT_FILE VARIABLE_NAME)
     COMMAND bash -c "xxd -i < ${INPUT_FILE} >> ${OUTPUT_FILE}"
     COMMAND bash -c "echo \"}; const unsigned int ${VARIABLE_NAME}_len = sizeof(${VARIABLE_NAME});\" >> ${OUTPUT_FILE}"
     DEPENDS ${INPUT_FILE}
-    COMMENT "Generating llk header for ${INPUT_FILE}"
+    COMMENT "Generating xxd header for ${INPUT_FILE}"
     VERBATIM
   )
 endfunction()
