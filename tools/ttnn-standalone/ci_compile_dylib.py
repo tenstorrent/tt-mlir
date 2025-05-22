77429177b47ba08dd06c32d00a8534c47c7bcd94
jmcgrathTT <jmcgrath@tenstorrent.com>
2025-05-22 23:06:29 +0000

    fixing

--- tools/ttnn-standalone/ci_compile_dylib.py
+++ tools/ttnn-standalone/ci_compile_dylib.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
 
