d4b89e149c00b1bff008f8c42a366038a53faaf7
Vincent Wells <vwells@tenstorrent.com>
2025-05-23 19:58:06 +0000

    fresh hells

--- lib/Dialect/TTMetal/Pipelines/TTMetalPipelines.cpp
+++ lib/Dialect/TTMetal/Pipelines/TTMetalPipelines.cpp
@@ -103,8 +103,8 @@ void createTTIRToTTMetalBackendPipeline(
   pm.addPass(mlir::emitc::createFormExpressionsPass());
 }
 
-void createTTIRToTTMetalPipeline(
-    OpPassManager &pm, const TTIRToTTMetalBackendPipelineOptions &options) {
+void createTTIRToTTMetalPipeline(OpPassManager &pm,
+                                 const TTIRToTTMetalPipelineOptions &options) {
   // Create DeviceModule to wrap all ops.
   pm.addPass(tt::createTTCoreWrapDeviceModulePass());
   // Create CPUModuleOp to wrap hoisted ops (if any).
