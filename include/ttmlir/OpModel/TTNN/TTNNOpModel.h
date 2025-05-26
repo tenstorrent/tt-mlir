aeff52b5b77e465e1f66039dd1eac9bf8bf88a71
Guangyu Feng <157328249+gfengTT@users.noreply.github.com>
2025-05-26 14:05:14 -0400

    [OpModel] fix names of namespaces (#3559)
    For consistency, use op name + "OpInterface".

--- include/ttmlir/OpModel/TTNN/TTNNOpModel.h
+++ include/ttmlir/OpModel/TTNN/TTNNOpModel.h
@@ -327,7 +327,7 @@ getOpRuntime(llvm::ArrayRef<int64_t> inputShape,
 //===----------------------------------------------------------------------===//
 // MaxPool2D
 //===----------------------------------------------------------------------===//
-namespace MaxPool2DInterface {
+namespace MaxPool2DOpInterface {
 llvm::Expected<
     std::tuple<size_t, size_t, size_t, ::mlir::tt::ttnn::TTNNLayoutAttr>>
 getOpConstraints(GridAttr deviceGrid, llvm::ArrayRef<int64_t> inputShape,
@@ -349,12 +349,12 @@ getOpRuntime(llvm::ArrayRef<int64_t> inputShape,
              bool ceilMode, llvm::ArrayRef<int64_t> outputShape,
              mlir::tt::ttnn::TTNNLayoutAttr outputLayout);
 
-}; // namespace MaxPool2DInterface
+}; // namespace MaxPool2DOpInterface
 
 //===----------------------------------------------------------------------===//
 // ClampScalar
 //===----------------------------------------------------------------------===//
-namespace ClampScalarInterface {
+namespace ClampScalarOpInterface {
 llvm::Expected<
     std::tuple<size_t, size_t, size_t, ::mlir::tt::ttnn::TTNNLayoutAttr>>
 getOpConstraints(GridAttr deviceGrid, llvm::ArrayRef<int64_t> inputShape,
@@ -367,7 +367,7 @@ getOpRuntime(llvm::ArrayRef<int64_t> inputShape,
              mlir::tt::ttnn::TTNNLayoutAttr inputLayout, llvm::APFloat min,
              llvm::APFloat max, llvm::ArrayRef<int64_t> outputShape,
              mlir::tt::ttnn::TTNNLayoutAttr outputLayout);
-}; // namespace ClampScalarInterface
+}; // namespace ClampScalarOpInterface
 
 } // namespace mlir::tt::op_model::ttnn
 #endif // TTMLIR_OPMODEL_TTNN_TTNNOPMODEL_H
