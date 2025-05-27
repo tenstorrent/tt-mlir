941697dab969ee63f3798f58681543d1fbecf481
vprajapati-tt <vprajapati@tenstorrent.com>
2025-05-27 20:32:19 +0000

    Updated TTKernel Dialect and Pybound new types / enums

--- include/ttmlir-c/TTKernelTypes.h
+++ include/ttmlir-c/TTKernelTypes.h
@@ -14,8 +14,36 @@ extern "C" {
 MLIR_CAPI_EXPORTED MlirType ttmlirTTKernelCBTypeGet(MlirContext ctx,
                                                     MlirType memrefType);
 
+MLIR_CAPI_EXPORTED MlirType ttmlirTTKernelSemaphoreTypeGet(MlirContext ctx);
+
+MLIR_CAPI_EXPORTED MlirType ttmlirTTKernelNocAddrTypeGet(MlirContext ctx);
+
 MLIR_CAPI_EXPORTED MlirAttribute
 ttmlirTTKernelThreadTypeAttrGet(MlirContext ctx, uint32_t enumValue);
+
+MLIR_CAPI_EXPORTED MlirAttribute
+ttmlirTTKernelReduceTypeAttrGet(MlirContext ctx, uint32_t enumValue);
+
+MLIR_CAPI_EXPORTED MlirAttribute
+ttmlirTTKernelReduceDimAttrGet(MlirContext ctx, uint32_t enumValue);
+
+MLIR_CAPI_EXPORTED MlirType ttmlirTTKernelL1AddrTypeGet(MlirContext ctx);
+
+MLIR_CAPI_EXPORTED MlirType ttmlirTTKernelL1AddrPtrTypeGet(MlirContext ctx);
+
+MLIR_CAPI_EXPORTED MlirType
+ttmlirTTKernelInterleavedAddrGenFastTypeGet(MlirContext ctx);
+
+MLIR_CAPI_EXPORTED MlirType ttmlirTTKernelDataFormatTypeGet(MlirContext ctx);
+
+MLIR_CAPI_EXPORTED MlirAttribute ttmlirTTKernelArgAttrGet(MlirContext ctx,
+                                                          MlirType argType,
+                                                          size_t operandIndex,
+                                                          bool is_uniform);
+
+MLIR_CAPI_EXPORTED MlirAttribute ttmlirTTKernelArgSpecAttrGet(
+    MlirContext ctx, MlirAttribute *rt_args, size_t rt_args_size,
+    MlirAttribute *ct_args, size_t ct_args_size);
 
 #ifdef __cplusplus
 }
