4b003e2634d27e444889537daf1ed5adbcdf44e2
Collin Tod <collintod@tenstorrent.com>
2025-06-03 18:37:27 +0000

    Revert "Re-enable op level golden check"
    This reverts commit 12d18e93d2c0eea5880f72f950a45112ebfadfe0.

--- tools/ttir-builder/builder.py
+++ tools/ttir-builder/builder.py
@@ -255,7 +255,6 @@ class TTIRBuilder:
         inputs: List[torch.Tensor],
         outputs: Optional[List[torch.Tensor]] = None,
         override: bool = False,
-        check_level: GoldenCheckLevel = GoldenCheckLevel.GRAPH_LEVEL,
     ) -> None:
         """
         Records the input and output tensors for the graph.
@@ -270,7 +269,7 @@ class TTIRBuilder:
             self.id_golden_map[input_key] = Golden(tensor)
 
         if outputs is not None:
-            self.golden_check_level = check_level
+            self.golden_check_level = GoldenCheckLevel.GRAPH_LEVEL
             for index, tensor in enumerate(outputs):
                 output_key = f"output_{index}"
                 if not override and output_key in self.id_golden_map:
