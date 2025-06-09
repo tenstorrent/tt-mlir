2a70fd4bea1f094958541ad5b14c9343b75ace5d
Julia Grim <jgrim@tenstorrent.com>
2025-06-09 12:23:56 -0500

    Fixing ttir-builder PCC failures caused by dtype mismatch (#3709)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-mlir/issues/1221)
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-mlir/issues/1223)
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-mlir/issues/1224)
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-mlir/issues/1225)
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-mlir/issues/1226)
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-mlir/issues/1800)
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-mlir/issues/1801)
    
    ### Problem description
    TTIR `ttir.eq`, `ttir.ge`, `ttir.gt`, `ttir.le`, `ttir.lt`,
    `ttir.logical_not`, `ttir.logical_xor` ops are failing golden PCC
    checks.
    
    ### What's changed
    These TTIR ops were failing PCC because of golden functions returning a
    different datatype than the ttir ops. Optional `out` tensor keyword
    arguments were added to control datatype because these torch functions
    don't have a `dtype` keyword.
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- test/python/golden/test_ttir_ops.py
+++ test/python/golden/test_ttir_ops.py
@@ -934,7 +934,7 @@ def test_reverse(shape: Shape, dims: List[int], request):
     )
 
 
-@pytest.mark.run_error
+@pytest.mark.skip("See issue #3685")
 @pytest.mark.parametrize("shape", [(4, 4)])
 @pytest.mark.parametrize("dim_args", [[0, 1]])
 def test_reduce_and(shape: Shape, dim_args: List[int], request):
@@ -1442,7 +1442,7 @@ unary_ops = [
     expm1 | Marks(pytest.mark.skip_target("ttmetal")),
     floor | Marks(pytest.mark.skip_target("ttmetal")),
     abs | Marks(pytest.mark.skip_target("ttmetal")),
-    logical_not | Marks(pytest.mark.fails_golden, pytest.mark.skip_target("ttmetal")),
+    logical_not | Marks(pytest.mark.skip_target("ttmetal")),
     neg,
     sign | Marks(pytest.mark.skip_target("ttmetal")),
     cos,
@@ -1503,12 +1503,12 @@ def test_unary_ops(
         add,
         multiply,
         subtract,
-        eq | Marks(pytest.mark.fails_golden, pytest.mark.skip_target("ttmetal")),
-        ne | Marks(pytest.mark.fails_golden, pytest.mark.skip_target("ttmetal")),
-        le | Marks(pytest.mark.fails_golden, pytest.mark.skip_target("ttmetal")),
-        lt | Marks(pytest.mark.fails_golden, pytest.mark.skip_target("ttmetal")),
-        ge | Marks(pytest.mark.fails_golden, pytest.mark.skip_target("ttmetal")),
-        gt | Marks(pytest.mark.fails_golden, pytest.mark.skip_target("ttmetal")),
+        eq | Marks(pytest.mark.skip_target("ttmetal")),
+        ne | Marks(pytest.mark.skip_target("ttmetal")),
+        le | Marks(pytest.mark.skip_target("ttmetal")),
+        lt | Marks(pytest.mark.skip_target("ttmetal")),
+        ge | Marks(pytest.mark.skip_target("ttmetal")),
+        gt | Marks(pytest.mark.skip_target("ttmetal")),
         div | Marks(pytest.mark.skip_target("ttmetal")),
         remainder | Marks(pytest.mark.skip_target("ttmetal")),
         maximum,
@@ -1517,8 +1517,7 @@ def test_unary_ops(
         matmul | Marks(pytest.mark.skip_target("ttmetal")),
         logical_and | Marks(pytest.mark.skip_target("ttmetal")),
         logical_or | Marks(pytest.mark.skip_target("ttmetal")),
-        logical_xor
-        | Marks(pytest.mark.fails_golden, pytest.mark.skip_target("ttmetal")),
+        logical_xor | Marks(pytest.mark.skip_target("ttmetal")),
     ],
 )
 def test_binary_ops(
