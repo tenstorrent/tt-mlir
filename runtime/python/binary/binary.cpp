becb0a57eff739b58311f021e811781796800152
Julia Grim <jgrim@tenstorrent.com>
2025-06-11 08:57:58 -0500

    Changing ttrt json handling to improve efficiency (#3535)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-mlir/issues/3250)
    
    ### Problem description
    TTRT was loading full flatbuffer files to access information. Going
    through json to load flatbuffers with large golden tensors was causing
    extremely slow run times and high memory usage.
    
    ### What's changed
    Added the following functions to `ttrt.binary` to target information
    importing from flatbuffer: `get_system_desc_as_json`,
    `get_num_programs`, `get_program_name`, `get_program_ops_as_json`,
    `get_program_inputs_as_json `, `get_program_outputs_as_json`,
    `get_program_mlir_as_json`, `get_program_cpp`, `is_program_private`.
    Expanded `ttrt.binary.as_dict(bin)` into similar functions for each new
    C++ getter:
    `ttrt.binary.program_inputs_as_dict(binary.fbb, index)`
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- runtime/python/binary/binary.cpp
+++ runtime/python/binary/binary.cpp
@@ -35,6 +35,26 @@ void registerBinaryBindings(nb::module_ &m) {
       .def("store", &tt::runtime::Binary::store)
       .def("get_debug_info_golden", &::tt::runtime::Binary::getDebugInfoGolden,
            nb::rv_policy::reference)
+      .def("get_system_desc_as_json", &tt::runtime::Binary::getSystemDescAsJson,
+           nb::rv_policy::reference)
+      .def("get_num_programs", &tt::runtime::Binary::getNumPrograms,
+           nb::rv_policy::reference)
+      .def("get_program_name", &tt::runtime::Binary::getProgramName,
+           nb::rv_policy::reference)
+      .def("is_program_private", &tt::runtime::Binary::isProgramPrivate,
+           nb::rv_policy::reference)
+      .def("get_program_ops_as_json", &tt::runtime::Binary::getProgramOpsAsJson,
+           nb::rv_policy::reference)
+      .def("get_program_inputs_as_json",
+           &tt::runtime::Binary::getProgramInputsAsJson,
+           nb::rv_policy::reference)
+      .def("get_program_outputs_as_json",
+           &tt::runtime::Binary::getProgramOutputsAsJson,
+           nb::rv_policy::reference)
+      .def("get_program_mlir_as_json",
+           &tt::runtime::Binary::getProgramMlirAsJson, nb::rv_policy::reference)
+      .def("get_program_cpp", &tt::runtime::Binary::getProgramCpp,
+           nb::rv_policy::reference)
       .def(
           "get_tensor_cache",
           [](tt::runtime::Binary &bin) { return bin.getCache(); },
