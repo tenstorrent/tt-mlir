fa22f1d1c88b35a631022473d44d18ab6e32a1bf
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2025-06-11 08:06:56 +0000

    tweak to chisel compile script

--- runtime/tools/chisel/chisel_compile.sh
+++ runtime/tools/chisel/chisel_compile.sh
@@ -6,5 +6,5 @@ if [ -z "$1" ]; then
 fi
 
 ttmlir-opt --ttir-to-ttir-decomposition $1/ttir_wide.mlir > $1/ttir.mlir
-ttmlir-opt --mlir-print-debuginfo --tt-register-device="system-desc-path=./ttrt-artifacts/system_desc.ttsys" --ttir-to-ttnn-backend-pipeline="enable-const-eval=false" $1/ttir.mlir > $1/ttnn.mlir
+ttmlir-opt --mlir-print-debuginfo --tt-register-device="system-desc-path=./ttrt-artifacts/system_desc.ttsys" --ttir-to-ttnn-backend-pipeline="enable-const-eval=false enable-erase-inverse-ops-pass=false" $1/ttir.mlir > $1/ttnn.mlir
 ttmlir-translate --ttnn-to-flatbuffer $1/ttnn.mlir -o $1/fb.ttnn
