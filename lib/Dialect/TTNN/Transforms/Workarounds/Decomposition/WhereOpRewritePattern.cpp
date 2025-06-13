114cc23f0fcd19e75805c5f45b73a9f56393853c
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2025-06-13 14:40:12 +0000

    Code cleanup

--- lib/Dialect/TTNN/Transforms/Workarounds/Decomposition/WhereOpRewritePattern.cpp
+++ lib/Dialect/TTNN/Transforms/Workarounds/Decomposition/WhereOpRewritePattern.cpp
@@ -12,15 +12,12 @@
 #include "llvm/ADT/APFloat.h"
 
 #include "llvm/Support/ErrorHandling.h"
-#include "llvm/Support/raw_ostream.h"
-
-#include <iostream>
 
 namespace mlir::tt::ttnn::workarounds::decomposition {
 
+// Metal tracking issue: https://github.com/tenstorrent/tt-metal/issues/22308
 LogicalResult WhereOpWithNanConstantRewritePattern::matchAndRewrite(
     ttnn::WhereOp op, PatternRewriter &rewriter) const {
-  // Check if one of the operands is a result of a FullOp with NaN constant.
   auto isFullOpWithNan = [](Value value) -> bool {
     if (auto fullOp = value.getDefiningOp<ttnn::FullOp>()) {
       if (auto floatAttr =
@@ -30,44 +27,39 @@ LogicalResult WhereOpWithNanConstantRewritePattern::matchAndRewrite(
     }
     return false;
   };
+
   if (!isFullOpWithNan(op.getSecond()) && !isFullOpWithNan(op.getThird())) {
     return failure();
   }
-  // Create a new WhereOp with the NaN constant replaced by 1e+37 for float32
-  // or 65504 for bfloat16.
-  RankedTensorType inputType = op.getFirst().getType();
-  RankedTensorType outputType = op.getResult().getType();
-  auto newTrueValue = op.getSecond();
-  auto newFalseValue = op.getThird();
-  // Helper lambda to create a replacement FullOp with the correct value.
+
+  // Constants 1e37 and 65504 are mostly arbitrary
+  // both are finite values near the upper end of the range for their type.
   auto createReplacementFullOp = [&](Value origValue) -> Value {
     auto origFullOp = llvm::cast<ttnn::FullOp>(origValue.getDefiningOp());
-    if (inputType.getElementType().isF32()) {
-      std::cerr << "Test!\n";
-      return rewriter.create<ttnn::FullOp>(op.getLoc(), outputType,
+    RankedTensorType origType = origFullOp.getType();
+    if (origType.getElementType().isF32()) {
+      return rewriter.create<ttnn::FullOp>(op.getLoc(), origType,
                                            rewriter.getF32FloatAttr(1e+37),
                                            origFullOp.getDevice());
     }
-    if (inputType.getElementType().isBF16()) {
+    if (origType.getElementType().isBF16()) {
       auto bf16Type = rewriter.getBF16Type();
       auto floatAttr = rewriter.getFloatAttr(bf16Type, 65504.0);
-      return rewriter.create<ttnn::FullOp>(op.getLoc(), outputType, floatAttr,
+      return rewriter.create<ttnn::FullOp>(op.getLoc(), origType, floatAttr,
                                            origFullOp.getDevice());
     }
     llvm_unreachable("Unknown data type was used inside the rewrite pattern "
                      "for WhereOp with NaN constant input");
   };
 
-  // Iterate over the two operands (second and third), replacing if needed.
   Value operands[2] = {op.getSecond(), op.getThird()};
-  Value *newOperands[2] = {&newTrueValue, &newFalseValue};
   for (int i = 0; i < 2; ++i) {
     if (isFullOpWithNan(operands[i])) {
-      *newOperands[i] = createReplacementFullOp(operands[i]);
+      operands[i] = createReplacementFullOp(operands[i]);
     }
   }
   rewriter.replaceOpWithNewOp<ttnn::WhereOp>(op, outputType, op.getFirst(),
-                                             newTrueValue, newFalseValue);
+                                             operands[0], operands[1]);
   return success();
 }
 
