fda2c5c8f2227de08c99d89b66d59c6661102a25
Nick Smith <127986401+nsmithtt@users.noreply.github.com>
2025-06-25 09:08:00 -0500

    Support blocking over dst (#3886)
    This commit changes the OptimizeTensorLayout pass to additionally
    constrain layouts by programming
    the generic op's `block_factors` such that resulting block shapes never
    exceed a volume that fits into
    the destination register. These semantics are subject to change in the
    future, but they are sufficient for
    now in order to unblock matmul performance sweeps.
    
    ---------
    
    Co-authored-by: Vincent Wells <vwells@tenstorrent.com>

---
+++ test/python/golden/test_metal_blocking.py
@@ -0,0 +1,105 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+from typing import List
+
+from ttir_builder.utils import compile_to_flatbuffer
+from ttir_builder import Operand, TTIRBuilder, Shape
+from ttmlir.ir import *
+
+
+@pytest.mark.parametrize("grid_y", [1])
+@pytest.mark.parametrize("grid_x", [1])
+@pytest.mark.parametrize("shard_mul_y", [3])
+@pytest.mark.parametrize("shard_mul_x", [2])
+@pytest.mark.parametrize("dst_register_size_tiles", [8])
+def test_eltwise_blocking(
+    grid_y: int,
+    grid_x: int,
+    shard_mul_y: int,
+    shard_mul_x: int,
+    dst_register_size_tiles: int,
+    request,
+):
+    tile_size = 32
+    shape = (
+        grid_y * shard_mul_y * tile_size,
+        grid_x * shard_mul_x * tile_size,
+    )
+
+    def eltwise_blocking(
+        in0: Operand,
+        in1: Operand,
+        builder: TTIRBuilder,
+        unit_attrs: List[str] = None,
+    ):
+        return builder.add(in0, in1, unit_attrs=unit_attrs)
+
+    options = [
+        f"max-dst-register-size-tiles={dst_register_size_tiles}",
+        f"override-device-shape={grid_y},{grid_x}",
+    ]
+    compile_to_flatbuffer(
+        eltwise_blocking,
+        [shape, shape],
+        target="ttmetal",
+        custom_pipeline=f"ttir-to-ttmetal-pipeline{{{' '.join(options)}}}",
+        test_base=request.node.name,
+        output_root=request.config.getoption("--path"),
+        system_desc_path=request.config.getoption("--sys-desc"),
+    )
+
+
+@pytest.mark.fails_golden
+@pytest.mark.parametrize("grid_m", [1])
+@pytest.mark.parametrize("grid_k", [1])
+@pytest.mark.parametrize("grid_n", [1])
+@pytest.mark.parametrize("m", [3])
+@pytest.mark.parametrize("k", [4])
+@pytest.mark.parametrize("n", [3])
+@pytest.mark.parametrize("dst_register_size_tiles", [8])
+def test_matmul_blocking(
+    grid_m: int,
+    grid_k: int,
+    grid_n: int,
+    m: int,
+    k: int,
+    n: int,
+    dst_register_size_tiles: int,
+    request,
+):
+    tile_size = 32
+    lhs = (
+        grid_m * m * tile_size,
+        grid_k * k * tile_size,
+    )
+    rhs = (
+        grid_k * k * tile_size,
+        grid_n * n * tile_size,
+    )
+
+    def matmul_blocking(
+        in0: Operand,
+        in1: Operand,
+        builder: TTIRBuilder,
+        unit_attrs: List[str] = None,
+    ):
+        return builder.matmul(in0, in1, unit_attrs=unit_attrs)
+
+    options = [
+        f"max-dst-register-size-tiles={dst_register_size_tiles}",
+        f"override-device-shape={grid_m},{grid_n}",
+    ]
+    compile_to_flatbuffer(
+        matmul_blocking,
+        [lhs, rhs],
+        target="ttmetal",
+        custom_pipeline=f"ttir-to-ttmetal-pipeline{{{' '.join(options)}}}",
+        test_base=request.node.name,
+        print_ir=True,
+        output_root=request.config.getoption("--path"),
+        system_desc_path=request.config.getoption("--sys-desc"),
+    )
