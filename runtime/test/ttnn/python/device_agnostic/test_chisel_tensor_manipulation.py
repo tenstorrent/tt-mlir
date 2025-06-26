ff2dc36979cd642c5c88af73018e1502f13fae35
Nikola Drakulic <ndrakulic@tenstorrent.com>
2025-06-26 01:17:47 +0000

    feat: add BatchNorm and PrepareConv2dBias ops to TTNN runtime

--- runtime/test/ttnn/python/device_agnostic/test_chisel_tensor_manipulation.py
+++ runtime/test/ttnn/python/device_agnostic/test_chisel_tensor_manipulation.py
@@ -5,8 +5,8 @@ import pytest
 import ttrt
 import ttmlir
 import torch
-import pdb
 import os
+import tempfile
 
 from ttrt.common.api import API as RtApi
 from ttrt.common.util import Logger as RtLogger
@@ -20,41 +20,30 @@ from ttrt.runtime import (
     get_tensor,
     update_tensor,
     memcpy,
-    create_owned_tensor,
+    create_owned_host_tensor,
     DataType,
     unregister_hooks,
 )
+from ttir_builder import TTIRBuilder, Operand, GoldenCheckLevel
+from ttir_builder.utils import compile_to_flatbuffer
 from ..utils import TT_MLIR_HOME
 
-DIRECTORY_PATH = f"{TT_MLIR_HOME}/build/test/ttmlir/Silicon"
-
-DTYPE_TO_TORCH_DTYPE = {
-    DataType.Float32: torch.float32,
-    DataType.Float16: torch.float16,
-    DataType.BFloat16: torch.bfloat16,
-    # DataType.BFP_Float8
-    # DataType.BFP_BFloat8
-    # DataType.BFP_Float4
-    # DataType.BFP_BFloat4
-    # DataType.BFP_Float2
-    # DataType.BFP_BFloat2
-    DataType.UInt32: torch.uint32,
-    DataType.UInt16: torch.uint16,
-    DataType.UInt8: torch.uint8,
-    DataType.Int32: torch.int32,
-}
-
+# Constants
+INPUT_SHAPE = (10, 10)
+WEIGHT_SHAPE = (10, 10)
+BIAS_SHAPE = (1, 10)
+DTYPE = torch.float32
 
 def get_torch_tensor(tensor):
     rt_data_ptr = tensor.get_data_buffer()
     rt_dtype = tensor.get_dtype()
-    dtype = DTYPE_TO_TORCH_DTYPE[rt_dtype]
+    if rt_dtype is not DataType.Float32:
+        raise ValueError(f"Unsupported data type: {rt_dtype}")
+    dtype = torch.float32
     shape = tensor.get_shape()
     torch_tensor = torch.frombuffer(rt_data_ptr, dtype=dtype)
     torch_tensor = torch_tensor.reshape(shape)
-
     return torch_tensor
-
 
 def update_device_tensor(program_context, tensor_ref, dst_tensor, src_tensor):
     data_ptr = src_tensor.data_ptr()
@@ -62,33 +51,50 @@ def update_device_tensor(program_context, tensor_ref, dst_tensor, src_tensor):
     stride = dst_tensor.get_stride()
     dtype = dst_tensor.get_dtype()
     size = torch.numel(src_tensor)
-    tensor = create_owned_tensor(data_ptr, shape, stride, size, dtype)
+    tensor = create_owned_host_tensor(data_ptr, shape, stride, size, dtype)
     update_tensor(program_context, tensor_ref, tensor)
 
+def linear_model(in0: Operand, in1: Operand, in2: Operand, builder: TTIRBuilder):
+    builder._golden_check_level = GoldenCheckLevel.DISABLED
+    matmul_result = builder.matmul(in0, in1)
+    output = builder.add(matmul_result, in2)
+    return output
 
 @pytest.mark.parametrize(
-    "flatbuffer_path,golden_input_tensors,golden_out_tensors,update_in_tensors",
+    "golden_input_tensors,golden_out_tensors,update_in_tensors",
     [
         (
-            f"{DIRECTORY_PATH}/TTNN/n150/chisel/Output/test_tensor_manipulation.mlir.tmp.ttnn",
             {
-                0: torch.ones([10, 10]),
-                1: torch.ones([10, 10]),
-                2: torch.ones([10, 10]),
-                3: torch.ones([10, 10]),
-                4: torch.ones([10, 10]) * 10,
-                5: torch.ones([10]),
-                6: torch.ones([10, 10]) * 20,
-                7: torch.ones([10]),
+                0: torch.ones(INPUT_SHAPE),
+                1: torch.ones(WEIGHT_SHAPE),
+                2: torch.ones(BIAS_SHAPE),
+                3: torch.ones(WEIGHT_SHAPE),
+                4: torch.ones(WEIGHT_SHAPE) * 10,
+                5: torch.ones(BIAS_SHAPE),
+                6: torch.ones(WEIGHT_SHAPE) * 20,
+                7: torch.ones(BIAS_SHAPE),
             },
-            {0: torch.ones([10, 10]) * 10, 1: torch.ones([10, 10]) * 21},
-            {4: torch.ones([10, 10]) * 20},
+            {0: torch.ones(WEIGHT_SHAPE) * 10, 1: torch.ones(WEIGHT_SHAPE) * 21},
+            {4: torch.ones(WEIGHT_SHAPE) * 20},
         ),
     ],
 )
 def test_tensor_manipulation_apis(
-    flatbuffer_path, golden_input_tensors, golden_out_tensors, update_in_tensors
+    golden_input_tensors, golden_out_tensors, update_in_tensors
 ):
+    output_root = "."
+    test_base = "test_chisel_tensor_manipulation"
+    compile_to_flatbuffer(
+        linear_model,
+        [INPUT_SHAPE, WEIGHT_SHAPE, BIAS_SHAPE],
+        target="ttnn",
+        output_root=output_root,
+        test_base=test_base,
+        golden_check_level=GoldenCheckLevel.DISABLED,
+    )
+    flatbuffer_path = f"{output_root}/ttnn/{test_base}_ttnn.mlir.ttnn"
+    assert os.path.exists(flatbuffer_path)
+
     in_counter = 0
     out_counter = 0
 
@@ -100,6 +106,8 @@ def test_tensor_manipulation_apis(
             tensor = get_tensor(programContext, ref)
             torch_tensor = get_torch_tensor(tensor)
             if in_counter in golden_input_tensors:
+                print(f"Input {in_counter}: {torch_tensor}")
+                print(f"Golden {in_counter}: {golden_input_tensors[in_counter]}")
                 assert torch.all(torch_tensor == golden_input_tensors[in_counter])
             if in_counter in update_in_tensors:
                 update_device_tensor(
@@ -115,11 +123,11 @@ def test_tensor_manipulation_apis(
             return
         torch_tensor = get_torch_tensor(tensor)
         if out_counter in golden_out_tensors:
+            print(f"Output {out_counter}: {torch_tensor}")
+            print(f"Golden {out_counter}: {golden_out_tensors[out_counter]}")
             assert torch.all(torch_tensor == golden_out_tensors[out_counter])
         out_counter += 1
-
-    assert os.path.exists(flatbuffer_path)
-
+    
     args = {
         "binary": str(flatbuffer_path),
         "save-artifacts": True,
