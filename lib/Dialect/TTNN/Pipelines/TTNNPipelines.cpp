f7944e1416e339e78a7a1cca0956ffb9a5d2b6b2
Lewis Panos <lpanos@tenstorrent.com>
2025-06-27 19:22:27 +0000

    Update comment

--- lib/Dialect/TTNN/Pipelines/TTNNPipelines.cpp
+++ lib/Dialect/TTNN/Pipelines/TTNNPipelines.cpp
@@ -56,8 +56,10 @@ void createTTNNPipelineTTIRPasses(
   // Flattening sliding window ops for compatibility with conversion to TTNN
   pm.addPass(mlir::tt::ttir::createTTIRFlattenSlidingWindow());
 
-  // Add pass to erase inverse ops. This is enabled by default
-  // while the pass is experimental.
+  // Add pass to erase inverse ops. We will explicate TMs so that
+  // erase inverse ops can commute TMs through otherwise implicit
+  // broadcasts, and handle rank-changing reshape ops which are
+  // also otherwise implicit.
   if (options.eraseInverseOpsEnabled) {
     pm.addPass(mlir::tt::ttir::createTTIRExplicateTMs());
     pm.addPass(mlir::tt::ttir::createTTIREraseInverseOps());
