27ed242414cc25858d4ed08efb753b074dbc3ec5
jgrimTT <jgrim@tenstorrent.com>
2025-06-27 16:00:10 +0000

    Setting up sphinx build

--- cmake/modules/ConfigureSphinx.cmake
+++ cmake/modules/ConfigureSphinx.cmake
@@ -19,7 +19,7 @@ endif()
 
 # Configure paths
 set(SPHINX_SOURCE_DIR ${CMAKE_SOURCE_DIR}/docs/source)
-set(SPHINX_BUILD_DIR ${CMAKE_BINARY_DIR}/docs/book/autogen/md/Module)
+set(SPHINX_BUILD_DIR ${CMAKE_BINARY_DIR}/docs/book/sphinx)
 set(SPHINX_MD_DIR ${CMAKE_BINARY_DIR}/docs/src/autogen/md/Module)
 
 # Create directories if they don't exist
