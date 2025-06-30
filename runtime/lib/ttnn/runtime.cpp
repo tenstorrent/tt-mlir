204a8a10c215ba73c1881748cd83a7ae0af0e483
Nikola Drakulic <ndrakulic@tenstorrent.com>
2025-06-30 13:43:16 +0000

    small fix

--- runtime/lib/ttnn/runtime.cpp
+++ runtime/lib/ttnn/runtime.cpp
@@ -1073,10 +1073,6 @@ getOpOutputRef(OpContext opContextHandle,
     tensorRef = opContext.type_as_PrepareConv2dWeightsOp()->out();
     break;
   }
-  case ::tt::target::ttnn::OpType::PrepareConv2dBiasOp: {
-    tensorRef = opContext.type_as_PrepareConv2dBiasOp()->out();
-    break;
-  }
   case ::tt::target::ttnn::OpType::BatchNormOp: {
     tensorRef = opContext.type_as_BatchNormOp()->out();
     break;
@@ -1314,10 +1310,6 @@ getOpInputRefs(OpContext opContextHandle,
   }
   case ::tt::target::ttnn::OpType::PrepareConv2dWeightsOp: {
     tensorRefs = {opContext.type_as_PrepareConv2dWeightsOp()->weight_tensor()};
-    break;
-  }
-  case ::tt::target::ttnn::OpType::PrepareConv2dBiasOp: {
-    tensorRefs = {opContext.type_as_PrepareConv2dBiasOp()->bias_tensor()};
     break;
   }
   case ::tt::target::ttnn::OpType::BatchNormOp: {
