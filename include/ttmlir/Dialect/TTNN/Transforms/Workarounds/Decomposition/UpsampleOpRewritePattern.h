f7dc17d21292ad34e385508aa9853e3c1e10f890
Aleksandar Zecevic <azecevic@tenstorrent.com>
2025-07-01 21:38:27 +0000

    add docs

--- include/ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/UpsampleOpRewritePattern.h
+++ include/ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/UpsampleOpRewritePattern.h
@@ -11,7 +11,11 @@
 #include "mlir/Support/LogicalResult.h"
 
 namespace mlir::tt::ttnn::workarounds::decomposition {
-// TODO (azecevic): Add description!
+// UpsampleOp in `bilinear` mode expects the input to be in `HeightSharded`
+// memory layout. This pattern converts the input tensor to the target memory
+// layout, provided that the input tensor is already in a `RowMajor` layout and
+// has `BFloat16` data type, and that the channel axis is a multiple of the tile
+// width.
 class UpsampleOpBilinearShardingRewritePattern
     : public OpRewritePattern<ttnn::UpsampleOp> {
 public:
@@ -22,6 +26,9 @@ public:
                                 PatternRewriter &rewriter) const override;
 };
 
+// This pattern is a prerequisite (hence the `2` benefit) for the
+// `UpsampleOpBilinearShardingRewritePattern`. `Channel` axis of the input has
+// to be a multiple of the tile width.
 class UpsampleOpBilinearPaddingRewritePattern
     : public OpRewritePattern<ttnn::UpsampleOp> {
 public:
@@ -32,6 +39,9 @@ public:
                                 PatternRewriter &rewriter) const override;
 };
 
+// UpsampleOp expects the input to be in `RowMajor` layout and has `BFloat16`
+// data type. This pattern converts the input tensor to the target memory
+// configuration.
 class UpsampleOpLayoutRewritePattern
     : public OpRewritePattern<ttnn::UpsampleOp> {
 public:
@@ -41,7 +51,6 @@ public:
   LogicalResult matchAndRewrite(ttnn::UpsampleOp srcOp,
                                 PatternRewriter &rewriter) const override;
 };
-
 } // namespace mlir::tt::ttnn::workarounds::decomposition
 
 #endif // TTMLIR_DIALECT_TTNN_TRANSFORMS_WORKAROUNDS_DECOMPOSITION_UPSAMPLEOPREWRITEPATTERN_H
