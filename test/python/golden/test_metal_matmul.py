65ac4fa29188a768f9a545f87a770f21c40e267d
Jacob DeSousa <jdesousa@tenstorrent.com>
2025-07-03 11:23:28 +0000

    add d2m ttnn benchmark tests

---
+++ test/python/golden/test_metal_matmul.py
@@ -0,0 +1,67 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+from typing import List
+
+from ttir_builder.utils import compile_to_flatbuffer
+from ttir_builder import Operand, TTIRBuilder, Shape
+from ttmlir.ir import *
+
+
+@pytest.mark.parametrize(
+    "shape",
+    [
+        (512, 512, 512),
+        (512, 1024, 1024),
+        (512, 1024, 2048),
+        (1024, 1024, 1024),
+        (1024, 1024, 2048),
+        (1024, 2048, 2048),
+        (2048, 2048, 2048),
+        (2048, 2048, 3072),
+    ],
+)
+@pytest.mark.parametrize("dtype", [torch.float32])
+@pytest.mark.parametrize("dst_register_size_tiles", [8])
+def test_matmul_ttnn(
+    shape: tuple[int],
+    dtype: torch.dtype,
+    dst_register_size_tiles: int,
+    request,
+):
+    lhs = (
+        shape[0],
+        shape[1],
+    )
+    rhs = (
+        shape[1],
+        shape[2],
+    )
+
+    def matmul_blocking(
+        in0: Operand,
+        in1: Operand,
+        builder: TTIRBuilder,
+        unit_attrs: List[str] = None,
+    ):
+        return builder.matmul(in0, in1, unit_attrs=unit_attrs)
+
+    options = [
+        f"max-dst-register-size-tiles={dst_register_size_tiles}",
+        f"matmul-interchange=2,0,1",
+    ]
+    compile_to_flatbuffer(
+        matmul_blocking,
+        [lhs, rhs],
+        inputs_types=[dtype, dtype],
+        target="ttmetal",
+        custom_pipeline=f"ttir-to-ttmetal-pipeline{{{' '.join(options)}}}",
+        test_base=request.node.name,
+        module_dump=True,
+        print_ir=True,
+        output_root=request.config.getoption("--path"),
+        system_desc_path=request.config.getoption("--sys-desc"),
+    )
