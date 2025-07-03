ab7fbd7d69c5c43485c552f91d584fc1a0fad3e4
Julia Grim <jgrim@tenstorrent.com>
2025-07-03 17:17:40 -0500

    Sphinx doc generation for ttir-builder (#3959)

---
+++ cmake/modules/ConfigureSphinx.cmake
@@ -0,0 +1,9 @@
+# Configure paths
+set(SPHINX_SOURCE_DIR ${CMAKE_SOURCE_DIR}/docs/source)
+set(SPHINX_BUILD_DIR ${CMAKE_BINARY_DIR}/docs/book/sphinx)
+set(SPHINX_MD_DIR ${CMAKE_BINARY_DIR}/docs/src/autogen/md/Module)
+
+# Create directories if they don't exist
+file(MAKE_DIRECTORY ${SPHINX_BUILD_DIR})
+file(MAKE_DIRECTORY ${SPHINX_MD_DIR})
+file(MAKE_DIRECTORY ${SPHINX_SOURCE_DIR}/generated)
