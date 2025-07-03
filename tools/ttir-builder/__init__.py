777ec4fab2db5800ae0c2718d936e1fd009e9960
Julia Grim <jgrim@tenstorrent.com>
2025-07-03 17:17:40 -0500

    Sphinx doc generation for ttir-builder (#3959)

--- tools/ttir-builder/__init__.py
+++ tools/ttir-builder/__init__.py
@@ -7,4 +7,5 @@ TTIR Builder
 This module contains the `TTIRBuilder` class, which is the main entry point for creating TTIR from python.
 
 """
-from .builder import *
+from .apis import *
+from .ops import *
