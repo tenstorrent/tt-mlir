06bdac52f4b3978e89b165cbdadbab53284f4589
Hongseok Kwon <hkwon@tenstorrent.com>
2025-07-07 18:14:28 +0000

    optimize

--- runtime/lib/ttnn/operations/ccl/point_to_point.cpp
+++ runtime/lib/ttnn/operations/ccl/point_to_point.cpp
@@ -5,8 +5,8 @@
 #include "operations/ccl/point_to_point.h"
 #include "tt/runtime/detail/logger.h"
 #include "tt/runtime/detail/ttnn/utils.h"
+#include "ttnn/distributed/types.hpp"
 #include <optional>
-#include <ttnn/distributed/types.hpp>
 
 /*
 This is a temporary host fallback to ttnn::PointToPoint(..) API.
@@ -16,7 +16,7 @@ namespace tt::runtime::ttnn::operations::ccl {
 void run(const ::tt::target::ttnn::PointToPointOp *op,
          ProgramContext &context) {
   DEBUG_ASSERT(!::tt::runtime::ttnn::utils::inSystemMemory(op->in()),
-               "Calling ttnn::from_device on a host tensor");
+               "Input tensor of point to point must be on device.");
 
   ProgramTensorPool &tensorPool = context.getTensorPool();
   const ::ttnn::Tensor &inputTensor =
@@ -30,18 +30,15 @@ void run(const ::tt::target::ttnn::PointToPointOp *op,
   std::vector<::ttnn::Tensor> inputTensorsHost =
       extractShardsToHost(inputTensor);
 
-  ::ttnn::Tensor outputTensor;
-  bool hasUserProvidedOutputTensor = op->output_tensor();
+  std::vector<::ttnn::Tensor> outputTensorsHost bool
+      hasUserProvidedOutputTensor = op->output_tensor();
 
   if (hasUserProvidedOutputTensor) {
-    outputTensor = tensorPool.getTTNNTensorAndValidate(op->output_tensor());
+    outputTensorsHost = extractShardsToHost(
+        tensorPool.getTTNNTensorAndValidate(op->output_tensor()));
   } else {
-    outputTensor = ::tt::tt_metal::create_device_tensor(
-        inputTensor.tensor_spec(), inputTensor.mesh_device());
+    outputTensorsHost = inputTensorsHost;
   }
-
-  std::vector<::ttnn::Tensor> outputTensorsHost =
-      extractShardsToHost(outputTensor);
 
   outputTensorsHost[op->receiver_id()] = inputTensorsHost[op->sender_id()];
 
