23f90ae626086cc9c25281776af1ca80014df4fa
Tapasvi Patel <tpatel@tenstorrent.com>
2025-07-08 21:29:02 +0000

    wip

---
+++ include/ttmlir/Dialect/StableHLO/Utils/GspmdUtils.h
@@ -0,0 +1,93 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef TTMLIR_DIALECT_STABLEHLO_TRANSFORMS_GSPMDUTILS_H
+#define TTMLIR_DIALECT_STABLEHLO_TRANSFORMS_GSPMDUTILS_H
+
+#include "ttmlir/Dialect/StableHLO/Transforms/ShardyCCLToStableHLOCCL.h"
+#include "ttmlir/Dialect/StableHLO/Utils/MeshShardingUtils.h"
+
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Operation.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "mlir/Transforms/Passes.h"
+#include "llvm/ADT/SmallSet.h"
+#include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/SmallVector.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdefaulted-function-deleted"
+#include "shardy/dialect/sdy/transforms/propagation/aggressive_propagation.h"
+#pragma clang diagnostic pop
+
+#include "shardy/dialect/sdy/ir/constants.h"
+#include "shardy/dialect/sdy/ir/dialect.h"
+#include "shardy/dialect/sdy/ir/utils.h"
+#include "shardy/dialect/sdy/transforms/export/passes.h"
+#include "shardy/dialect/sdy/transforms/propagation/op_sharding_rule_builder.h"
+#include "shardy/dialect/sdy/transforms/propagation/passes.h"
+#include "stablehlo/dialect/StablehloOps.h"
+
+namespace mlir::tt::gspmd_utils {
+
+#ifdef TTMLIR_ENABLE_STABLEHLO
+
+// Sharding related string definitions from open-xla
+// https://github.com/openxla/xla/blob/main/xla/service/spmd/shardy/constants.h
+inline constexpr llvm::StringRef kShardingCustomCallTargetName = "Sharding";
+inline constexpr llvm::StringRef kSPMDFullToShardShapeCallTargetName =
+    "SPMDFullToShardShape";
+inline constexpr llvm::StringRef kSPMDShardToFullShapeCallTargetName =
+    "SPMDShardToFullShape";
+inline constexpr llvm::StringRef kXlaShardingAttr = "mhlo.sharding";
+
+// Parse meshes from the GSPMD module.
+llvm::Expected<llvm::SmallVector<llvm::SmallVector<int64_t>>>
+parseMeshesFromGspmdModule(mlir::ModuleOp &module);
+
+class GSPMDMeshSharding : public mesh_sharding_utils::MeshSharding {
+public:
+  // Static factory method
+  static llvm::Expected<GSPMDMeshSharding>
+  generate(llvm::StringRef opShardingStr, llvm::StringRef operandShardingStr,
+           mlir::tt::ttcore::ShardStatus shardStatus);
+  GSPMDMeshSharding(mlir::tt::ttcore::MeshShardDirection shardDirection,
+                    mlir::tt::ttcore::MeshShardType shardType,
+                    llvm::SmallVector<int64_t> shardShape,
+                    llvm::SmallVector<int64_t> shardDims,
+                    llvm::SmallVector<int64_t> meshShape,
+                    llvm::SmallVector<int64_t> deviceIds,
+                    mlir::tt::ttcore::ShardStatus shardStatus,
+                    std::string opShardingStr, std::string operandShardingStr,
+                    bool lastTileDimReplicate)
+      : MeshSharding(shardDirection, shardType, shardShape, shardDims,
+                     meshShape, deviceIds, shardStatus),
+        lastTileDimReplicate(lastTileDimReplicate),
+        opShardingStr(opShardingStr), operandShardingStr(operandShardingStr) {}
+
+  // Getters
+  bool getLastTileDimReplicate() const { return lastTileDimReplicate; }
+  std::string getOpShardingStr() const { return opShardingStr; }
+  std::string getOperandShardingStr() const { return operandShardingStr; }
+
+private:
+  // Member variables
+  bool lastTileDimReplicate;
+  std::string opShardingStr;
+  std::string operandShardingStr;
+};
+
+#endif // #ifdef TTMLIR_ENABLE_STABLEHLO
+
+} // namespace mlir::tt::gspmd_utils
+
+#endif // TTMLIR_DIALECT_STABLEHLO_TRANSFORMS_GSPMDUTILS_H
