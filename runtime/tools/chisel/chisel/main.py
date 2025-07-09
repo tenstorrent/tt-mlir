89deaf488d7d93ee22dda44d7c7840b9c1a7d8f4
Nikola Drakulic <ndrakulic@tenstorrent.com>
2025-07-09 09:19:09 +0000

    Chisel init

---
+++ runtime/tools/chisel/chisel/main.py
@@ -0,0 +1,51 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+from pathlib import Path
+import datetime
+import os
+
+from chisel.core.context import ChiselContext
+from chisel.core.enums import ExecutionType
+from ttmlir.ir import Operation
+
+def main():
+    """
+    To run this script:
+    You need to have ttir.mlir and ttnn.mlir files in the base directory.
+    To generate ttir.mlir and ttnn.mlir please run chisel.sh on your starting ttir file
+    """
+    model = "mlpmixer_trimmed"
+    base_dir = Path(f"/proj_sw/user_dev/ndrakulic/chisel/{model}")
+    main_fn = "main"
+    program_index = 0
+    tensor_folder = base_dir / "tensors"
+
+    input_dir = base_dir
+    output_dir = base_dir / "output"
+
+    ttnn_path = base_dir / "ttnn.mlir"
+    ttir_path = base_dir / "ttir.mlir"
+    flatbuffer_path = base_dir / "fb.ttnn"
+
+    chisel_context = ChiselContext(
+        ttir_text=ttir_path.read_text(),
+        ttnn_text=ttnn_path.read_text(),
+        output_dir=output_dir,
+        report_path=base_dir / f"report_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.csv",
+        main_fn=main_fn,
+        program_index=program_index,
+        flatbuffer_path=flatbuffer_path,
+        should_skip_op=lambda op: '%6 = "ttnn.matmul"' in op.get_asm(enable_debug_info=True)
+    )
+    chisel_context.bind_callbacks()
+    input_paths = [f"{tensor_folder}/{i}.pt" for i in range(len(os.listdir(tensor_folder)))]
+    print(f"Input paths:")
+    print(*input_paths, sep="\n")
+    chisel_context.load_inputs_from_disk(input_paths)
+    chisel_context.registry.load_all_ops()
+    chisel_context.run()
+
+
+if __name__ == "__main__":
+    main()
