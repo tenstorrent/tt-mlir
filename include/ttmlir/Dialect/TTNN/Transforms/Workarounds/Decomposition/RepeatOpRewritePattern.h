b8b97a78d8c148ea9e3e3772cd2774eecc3d51e0
Jovan Serbedzija <jserbedzija@tenstorrent.com>
2025-07-10 11:24:58 +0200

    [TTNN] Apply the workaround to ttnn.repeat only when the input has the Int32 dtype (#4042)
    ### What's changed
    Apply the workaround to `ttnn.repeat` only when the input has the
    `Int32` dtype. We can remove this workaround once the following issue
    gets resolved in tt-metal:
    https://github.com/tenstorrent/tt-metal/issues/24749.
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes

--- include/ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/RepeatOpRewritePattern.h
+++ include/ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/RepeatOpRewritePattern.h
@@ -7,22 +7,10 @@
 
 #include "ttmlir/Dialect/TTNN/IR/TTNNOps.h"
 
-#include "mlir/IR/BuiltinAttributes.h"
-#include "mlir/IR/BuiltinTypes.h"
-#include "mlir/IR/PatternMatch.h"
-#include "mlir/Support/LogicalResult.h"
-#include "llvm/ADT/SmallSet.h"
-#include "llvm/ADT/SmallVector.h"
-
 namespace mlir::tt::ttnn::workarounds::decomposition {
-// The RepeatOp currently does not support repeating the last dimension.
-// Furthermore due to exisiting issues
-// https://github.com/tenstorrent/tt-metal/issues/16701 and
-// https://github.com/tenstorrent/tt-metal/issues/16698, using RepeatOp might
-// cause PCC and ATOL mismatch errors. The purpose of this workaround is to
-// replace every RepeatOp with an AddOp with zero in order to fold the RepeatOp
-// with an implicit operation. This workaround should be removed once the above
-// mentioned issues have been fixed.
+// The RepeatOp currently does not support Int32 type for the input.
+// This is tracked in the following Metal issue:
+// https://github.com/tenstorrent/tt-metal/issues/24749.
 class TTNNRepeatFoldingWorkaround : public OpRewritePattern<ttnn::RepeatOp> {
 public:
   using OpRewritePattern<ttnn::RepeatOp>::OpRewritePattern;
