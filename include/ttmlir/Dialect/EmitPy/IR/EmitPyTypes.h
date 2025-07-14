a7481db29e8d35f11d26b33d13865ec7a5cdc0d3
Andjela Milovanovic <amilovanovic@tenstorrent.com>
2025-07-14 11:06:08 +0200

    [EmitPy] Implement Python Code Generation (Initial MNIST Support) (#3834)
    ### Problem description
    The ttmlir project currently leverages the EmitC dialect to represent
    C++ syntax, enabling the emission of C++ code via ttmlir-translate
    --mlir-to-cpp.
    
    However, the equivalent feature for Python currently does not exist.
    This presents a significant limitation, as Python is heavily used for
    rapid model development and seamless integration with libraries like
    ttnn. Without the ability to emit Python code directly from MLIR, models
    are currently implemented directly using the ttnn library in Python.
    
    To address this, we need to introduce a counterpart EmitPy dialect that
    accurately represents Python syntax within MLIR. This new dialect will
    then serve as the intermediate representation from which functional
    Python code can be generated.
    
    ### What's changed
    - Implemented the new EmitPy dialect to represent Python syntax in MLIR.
    - Enabled translation from TTIR and TTNN dialects to EmitPy. 
    - Added the ability to emit Python code directly from the EmitPy
    dialect.
    - **Initial MNIST Model Support:**
    Crucially, all current changes are focused on supporting the operations
    and types required for the MNIST model.
    - Integrated Pipeline: The full conversion pipeline from TTIR to EmitPy
    is invoked using ttmlir-opt --ttir-to-emitpy-pipeline, followed by the
    Python code emission using ttmlir-translate --mlir-to-python.
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

---
+++ include/ttmlir/Dialect/EmitPy/IR/EmitPyTypes.h
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef TTMLIR_DIALECT_EMITPY_IR_EMITPYTYPES_H
+#define TTMLIR_DIALECT_EMITPY_IR_EMITPYTYPES_H
+
+#include "ttmlir/Dialect/TTCore/IR/TTCoreOpsTypes.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "ttmlir/Dialect/EmitPy/IR/EmitPyOpsTypes.h.inc"
+
+#endif // TTMLIR_DIALECT_EMITPY_IR_EMITPYTYPES_H
---
+++ test/ttmlir/EmitPy/add.mlir
@@ -0,0 +1,7 @@
+// RUN: ttmlir-opt --ttir-to-emitpy-pipeline %s | ttmlir-translate --mlir-to-python > %t.py
+
+func.func @add(%arg0: tensor<64x128xf32>, %arg1: tensor<64x128xf32>) -> tensor<64x128xf32> {
+  %0 = ttir.empty() : tensor<64x128xf32>
+  %1 = "ttir.add"(%arg0, %arg1, %0) : (tensor<64x128xf32>, tensor<64x128xf32>, tensor<64x128xf32>) -> tensor<64x128xf32>
+  return %1 : tensor<64x128xf32>
+}
