572b7e3cdb2d22b7349ea11a8a192512e4effbbc
Anurag <anuragsingh@tenstorrent.com>
2025-07-17 00:48:24 +0000

    commute dequantize past add (next up, rewrite the add)

--- include/ttmlir/Dialect/TTIR/Utils/QuantUtils.h
+++ include/ttmlir/Dialect/TTIR/Utils/QuantUtils.h
@@ -5,10 +5,13 @@
 #ifndef TTMLIR_DIALECT_TTIR_UTILS_QUANTUTILS_H
 #define TTMLIR_DIALECT_TTIR_UTILS_QUANTUTILS_H
 
+#include "mlir/Dialect/Quant/IR/QuantTypes.h"
 #include "llvm/ADT/APInt.h"
 
 namespace mlir::tt::ttir::utils {
 
+// Returns the min/max representable values for the given integer type, or emits
+// an error if the bitwidth is invalid.
 inline mlir::FailureOr<std::pair<int64_t, int64_t>>
 getStorageTypeMinMax(IntegerType intType, Location loc) {
   unsigned bitWidth = intType.getWidth();
@@ -26,6 +29,37 @@ getStorageTypeMinMax(IntegerType intType, Location loc) {
     max = llvm::APInt::getMaxValue(bitWidth).getZExtValue();
   }
   return std::make_pair(min, max);
+}
+
+// Returns true if all values are ranked tensors with the same quantized element
+// type.
+inline bool
+areQuantizationParamsAligned(mlir::ArrayRef<mlir::Value> quantizedValues) {
+  if (quantizedValues.empty()) {
+    return true;
+  }
+
+  auto firstType =
+      mlir::dyn_cast<mlir::RankedTensorType>(quantizedValues[0].getType());
+  if (!firstType) {
+    return false;
+  }
+
+  auto firstQuantType =
+      mlir::dyn_cast<mlir::quant::QuantizedType>(firstType.getElementType());
+  if (!firstQuantType) {
+    return false;
+  }
+
+  return llvm::all_of(quantizedValues.drop_front(), [&](mlir::Value val) {
+    auto type = mlir::dyn_cast<mlir::RankedTensorType>(val.getType());
+    if (!type) {
+      return false;
+    }
+    auto qType =
+        mlir::dyn_cast<mlir::quant::QuantizedType>(type.getElementType());
+    return qType && qType == firstQuantType;
+  });
 }
 
 } // namespace mlir::tt::ttir::utils
