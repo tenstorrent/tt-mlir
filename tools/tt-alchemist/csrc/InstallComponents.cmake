99323b5b8af146e5d31be8053abd9fb941bc9bd7
svuckovic <svuckovic@tenstorrent.com>
2025-07-28 16:07:34 +0000

    Add workarounds for C++ standalone

--- tools/tt-alchemist/csrc/InstallComponents.cmake
+++ tools/tt-alchemist/csrc/InstallComponents.cmake
@@ -27,7 +27,7 @@ add_custom_command(
 # Install metalium-runtime component
 add_custom_command(
   OUTPUT ${TTNN_INSTALL_DIR}/.metalium-runtime-installed
-  DEPENDS ${TTNN_INSTALL_DIR}/.ttnn-dev-installed
+  DEPENDS ${TTNN_INSTALL_DIR}
   COMMAND cmake --install ${TT_METAL_BUILD_HOME} --prefix ${TTNN_INSTALL_DIR} --component metalium-runtime
   COMMAND ${CMAKE_COMMAND} -E touch ${TTNN_INSTALL_DIR}/.metalium-runtime-installed
   COMMENT "Installing metalium-runtime component"
@@ -45,7 +45,7 @@ add_custom_command(
 # Install ttnn-runtime component
 add_custom_command(
   OUTPUT ${TTNN_INSTALL_DIR}/.ttnn-runtime-installed
-  DEPENDS ${TTNN_INSTALL_DIR}
+  DEPENDS ${TTNN_INSTALL_DIR}/.metalium-runtime-installed
   COMMAND cmake --install ${TT_METAL_BUILD_HOME} --prefix ${TTNN_INSTALL_DIR} --component ttnn-runtime
   COMMAND ${CMAKE_COMMAND} -E touch ${TTNN_INSTALL_DIR}/.ttnn-runtime-installed
   COMMENT "Installing ttnn-runtime component"
@@ -60,14 +60,33 @@ add_custom_command(
   COMMENT "Installing ttnn-dev component"
 )
 
-# # Debug: Install all available components to see what's available
-# add_custom_command(
-#   OUTPUT ${TTNN_INSTALL_DIR}/.all-components-installed
-#   DEPENDS ${TTNN_INSTALL_DIR}/.metalium-dev-installed
-#   COMMAND cmake --install ${TT_METAL_BUILD_HOME} --prefix ${TTNN_INSTALL_DIR}
-#   COMMAND ${CMAKE_COMMAND} -E touch ${TTNN_INSTALL_DIR}/.all-components-installed
-#   COMMENT "DEBUG: Installing all available components"
-# )
+# Debug: Install all available components to see what's available
+add_custom_command(
+  OUTPUT ${TTNN_INSTALL_DIR}/.all-components-installed
+  DEPENDS ${TTNN_INSTALL_DIR}
+  COMMAND cmake --install ${TT_METAL_BUILD_HOME} --prefix ${TTNN_INSTALL_DIR}
+  COMMAND ${CMAKE_COMMAND} -E touch ${TTNN_INSTALL_DIR}/.all-components-installed
+  COMMENT "DEBUG: Installing all available components"
+)
+
+# Set of workarounds to copy around missing cpp/hpp files until ttnn/metalium install scripts are fixed
+add_custom_command(
+  OUTPUT ${TTNN_INSTALL_DIR}/.missing-headers-installed
+  DEPENDS ${TTNN_INSTALL_DIR}/.ttnn-dev-installed
+  # Copy tracy
+  COMMAND ${CMAKE_COMMAND} -E copy_directory ${TT_METAL_BUILD_HOME}/../tt_metal/third_party/tracy/public/tracy ${TTNN_INSTALL_DIR}/include/tracy
+  COMMAND ${CMAKE_COMMAND} -E copy_directory ${TT_METAL_BUILD_HOME}/../tt_metal/third_party/tracy/public/common ${TTNN_INSTALL_DIR}/include/common
+  COMMAND ${CMAKE_COMMAND} -E copy_directory ${TT_METAL_BUILD_HOME}/../tt_metal/third_party/tracy/public/client ${TTNN_INSTALL_DIR}/include/client
+  # Copy op-related missing hpp files
+  COMMAND ${CMAKE_COMMAND} -DSOURCE_DIR=${TT_METAL_BUILD_HOME}/../ttnn/cpp/ttnn/operations -DDEST_DIR=${TTNN_INSTALL_DIR}/include/ttnn/operations -P ${CMAKE_CURRENT_SOURCE_DIR}/CopyHppFiles.cmake
+  # Copy kernel-related missing hpp/cpp files (needed for kernel compilation in runtime)
+  COMMAND ${CMAKE_COMMAND} -DSOURCE_DIR=${TT_METAL_BUILD_HOME}/../ttnn/cpp/ttnn/operations -DDEST_DIR=${TTNN_INSTALL_DIR}/libexec/tt-metalium/ttnn/cpp/ttnn/operations -P ${CMAKE_CURRENT_SOURCE_DIR}/CopyHppFiles.cmake
+  COMMAND ${CMAKE_COMMAND} -DSOURCE_DIR=${TT_METAL_BUILD_HOME}/../ttnn/cpp/ttnn/operations -DDEST_DIR=${TTNN_INSTALL_DIR}/libexec/tt-metalium/ttnn/cpp/ttnn/operations -P ${CMAKE_CURRENT_SOURCE_DIR}/CopyCppFiles.cmake
+  COMMAND ${CMAKE_COMMAND} -E copy_directory ${TT_METAL_BUILD_HOME}/../tt_metal/api/tt-metalium ${TTNN_INSTALL_DIR}/libexec/tt-metalium/tt_metal/api/tt-metalium
+  #
+  COMMAND ${CMAKE_COMMAND} -E touch ${TTNN_INSTALL_DIR}/.missing-headers-installed
+  COMMENT "Installing missing headers for precompiled header"
+)
 
 # Target to install all components
 add_custom_target(install-ttnn-components
@@ -76,6 +95,7 @@ add_custom_target(install-ttnn-components
     ${TTNN_INSTALL_DIR}/.metalium-dev-installed
     ${TTNN_INSTALL_DIR}/.ttnn-runtime-installed
     ${TTNN_INSTALL_DIR}/.ttnn-dev-installed
-    # ${TTNN_INSTALL_DIR}/.all-components-installed
+    ${TTNN_INSTALL_DIR}/.missing-headers-installed
+    ${TTNN_INSTALL_DIR}/.all-components-installed
   COMMENT "Installing all required TT-NN components"
 )
