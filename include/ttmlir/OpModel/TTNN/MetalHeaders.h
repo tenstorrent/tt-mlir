3901b37613cb42900f0f8dc28c59175636c5a9e4
Chris Karavasilis <ckaravasilis@tenstorrent.com>
2025-07-30 17:13:55 +0000

    cumSumOp

--- include/ttmlir/OpModel/TTNN/MetalHeaders.h
+++ include/ttmlir/OpModel/TTNN/MetalHeaders.h
@@ -41,6 +41,7 @@ using IDevice = ::tt::tt_metal::IDevice;
 #include "ttnn/operations/embedding/embedding.hpp"
 #include "ttnn/operations/embedding_backward/embedding_backward.hpp"
 #include "ttnn/operations/matmul/matmul.hpp"
+#include "ttnn/operations/moreh/moreh_cumsum/moreh_cumsum.hpp"
 #include "ttnn/operations/normalization/softmax/softmax.hpp"
 #include "ttnn/operations/pool/generic/generic_pools.hpp"
 #include "ttnn/operations/pool/upsample/upsample.hpp"
