0da0a0e374322616d9a30fdf94f3444ced054197
gengelageTT <gengelage@tenstorrent.com>
2025-07-31 21:19:34 +0000

    softmax op definition changes

--- lib/Conversion/TTIRToTTNN/TTIRToTTNN.cpp
+++ lib/Conversion/TTIRToTTNN/TTIRToTTNN.cpp
@@ -489,7 +489,7 @@ public:
                   ConversionPatternRewriter &rewriter) const override {
     rewriter.replaceOpWithNewOp<ttnn::SoftmaxOp>(
         op, this->getTypeConverter()->convertType(op.getType()),
-        adaptor.getInput(), adaptor.getDimension());
+        adaptor.getInput(), adaptor.getDimension(), adaptor.getNumericStable());
     return success();
   }
 };
