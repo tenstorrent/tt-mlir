1d38d23e0aadf16635def586981e8ee5a0b28e87
Marina Vasiljevic <mvasiljevic@tenstorrent.com>
2025-08-01 10:21:33 +0200

    [Optimizer] Respect override when checking interleaved to sharded (#4188)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-mlir/issues/3749)
    
    ### Problem description
    When checking whether a reshard must be inserted before the first op in
    the L1 chain, or whether it supports interleaved input with sharded
    output, we assumed that the op has tiled input. However, if the op
    before it has row major output, that makes its input row major as well
    and there is a possibility it does not support sharded output in that
    case.
    
    ### What's changed
    The set of ops with row major output overrides is created in
    `TTNNOptimizer`, passed through all intermediate functions, and finally
    used in `ShardSolver::preprocessFirstOp`.
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- lib/Dialect/TTNN/Analysis/DFShardingPolicy.cpp
+++ lib/Dialect/TTNN/Analysis/DFShardingPolicy.cpp
@@ -152,7 +152,7 @@ void DFShardingPolicy::run() {
     progressTracker.startL1Chain(firstOp, chainIndex, numOpsInChain);
     ShardSolver shardSolver = l1ChainConfig.resolveWithSolver(
         tensorTypePossibleLayouts, legalConfigs, usableL1CacheSize,
-        overrideReshardEdges);
+        overrideReshardEdges, overrideOutputLayout);
 
     if (l1ChainConfig.getState() == L1ChainState::Failed) {
       TTMLIR_DEBUG(ttmlir::LogComponent::Optimizer,
