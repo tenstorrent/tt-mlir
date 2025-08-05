166930306f051787dd98d75c01bbef35eb0451ba
Brett Grady <bgrady@tenstorrent.com>
2025-08-05 17:00:02 +0000

    first cut tilized round trip tests

--- test/python/golden/test_metal_dma.py
+++ test/python/golden/test_metal_dma.py
@@ -45,51 +45,93 @@ def compile_dma_test(test_func, shape, request, write_mlir_to_file=None):
     )
 
 
-@pytest.mark.skip
-@pytest.mark.parametrize(
-    "shape", [(x, y) for x in (32, 128) for y in (32, 64, 128)] + [(256, 256)]
-)
-def test_dram_tilize(shape: Shape, request):
+@pytest.mark.parametrize("shape", [(256, 256)])
+@pytest.mark.parametrize("start_grid", [(1, 4), (4, 1), (2, 4), (4, 2)])
+@pytest.mark.parametrize("end_grid", [(1, 1), (4, 4)])
+@pytest.mark.parametrize("memory_space", [ttcore.MemorySpace.DeviceL1])
+def test_dram_tilize(
+    shape: Shape,
+    start_grid: tuple[int, int],
+    end_grid: tuple[int, int],
+    memory_space: ttcore.MemorySpace,
+    request,
+):
     def tilize(
         in0: Operand,
         builder: TTIRBuilder,
         unit_attrs: List[str] = None,
     ):
+        # derive sharded shapes
+        assert (
+            (shape[0] % start_grid[0] == 0) and (shape[1] % start_grid[1] == 0),
+            "shape must be divisible by start_grid",
+        )
+        start_shard_shape = (shape[0] // start_grid[0], shape[1] // start_grid[1])
+        assert (
+            (shape[0] % end_grid[0] == 0) and (shape[1] % end_grid[1] == 0),
+            "shard_shape must be divisible by end_grid",
+        )
+        end_shard_shape = (shape[0] // end_grid[0], shape[1] // end_grid[1])
 
-        to_device = builder.to_layout(
+        # tilize the tensor on a single worker
+        to_device = builder.tilize(
             in0,
             output_type=builder.metal_tensor_layout(
-                shape, tiled=False, memorySpace=ttcore.MemorySpace.DeviceDRAM
+                shape,
+                tiled=True,
+                memorySpace=ttcore.MemorySpace.DeviceL1,
             ),
             unit_attrs=unit_attrs,
         )
 
-        tilize_out = builder.tilize(
+        # WRITE tensor from L1 to initial shard layout
+        tensor_layoutA = builder.to_layout(
             to_device,
             output_type=builder.metal_tensor_layout(
-                shape, tiled=True, memorySpace=ttcore.MemorySpace.DeviceL1
+                start_shard_shape,
+                tiled=True,
+                memorySpace=memory_space,
+                grid=start_grid,
+            ),
+            unit_attrs=unit_attrs,
+        )
+
+        # READ sharded layout to final sharded layout
+        tensor_layoutB = builder.to_layout(
+            tensor_layoutA,
+            output_type=builder.metal_tensor_layout(
+                end_shard_shape,
+                tiled=True,
+                memorySpace=ttcore.MemorySpace.DeviceL1,
+                grid=end_grid,
             ),
             unit_attrs=unit_attrs,
         )
 
         untilize_out = builder.untilize(
-            tilize_out,
+            tensor_layoutB,
             output_type=in0.type,
             unit_attrs=unit_attrs,
         )
 
         return untilize_out
 
-    compile_dma_test(tilize, shape, request, write_mlir_to_file=None)
+    compile_dma_test(
+        tilize,
+        shape,
+        request,
+        write_mlir_to_file=f"test_dram_tilize_{start_grid[0]}x{start_grid[1]}_to_{end_grid[0]}x{end_grid[1]}_{memory_space}.mlir",
+    )
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize(
     "shape",
     [(256, 256)],
 )
 @pytest.mark.parametrize("start_grid", [(1, 1), (2, 2), (1, 2), (2, 1), (4, 4)])
 @pytest.mark.parametrize("end_grid", [(1, 1), (2, 2), (1, 2), (2, 1), (4, 4)])
-@pytest.mark.parametrize("memory_space", [ttcore.MemorySpace.DeviceDRAM])
+@pytest.mark.parametrize("memory_space", [ttcore.MemorySpace.DeviceL1])
 def test_dram_write(
     shape: Shape,
     start_grid: tuple[int, int],
