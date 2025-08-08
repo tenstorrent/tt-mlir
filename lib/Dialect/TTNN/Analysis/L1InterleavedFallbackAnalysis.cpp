0130e6b7f65fd53a99968d1a51124697e2ced7c1
Bojana Malesevic <bmalesevic@tenstorrent.com>
2025-08-08 15:19:29 +0000

    temp: ignore flag, just do the analysis regardless.

--- lib/Dialect/TTNN/Analysis/L1InterleavedFallbackAnalysis.cpp
+++ lib/Dialect/TTNN/Analysis/L1InterleavedFallbackAnalysis.cpp
@@ -224,9 +224,9 @@ L1InterleavedFallbackAnalysis::checkUpgradeToL1Interleaved(
                                    consumerOp->getName().getStringRef().data());
   }
 
-  bool l1UsageValid =
-      (producersL1OutputUsage + tensorUsage + cBUsagePeak) <
-      analysisInput.tensorL1UsageCap * analysisInput.usableL1CacheSize;
+  bool l1UsageValid = (producersL1OutputUsage + tensorUsage + cBUsagePeak) <
+                      0.8f * analysisInput.usableL1CacheSize;
+  // analysisInput.tensorL1UsageCap * analysisInput.usableL1CacheSize;
 
   if (!l1UsageValid) {
     TTMLIR_DEBUG(ttmlir::LogComponent::Optimizer,
