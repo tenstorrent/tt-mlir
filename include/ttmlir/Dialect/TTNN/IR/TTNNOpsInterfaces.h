277633522925cbea9752bde147462a42e01ddb7c
Aleksandar Zecevic <azecevic@tenstorrent.com>
2025-08-09 20:05:43 +0000

    add interfaces

---
+++ include/ttmlir/Dialect/TTNN/IR/TTNNOpsInterfaces.h
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef TTMLIR_DIALECT_TTNN_IR_TTNNOPSINTERFACES_H
+#define TTMLIR_DIALECT_TTNN_IR_TTNNOPSINTERFACES_H
+
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/OpDefinition.h"
+#include "llvm/ADT/ArrayRef.h"
+
+#include "ttmlir/Dialect/TTNN/IR/TTNNOpsInterfaces.h.inc"
+
+#endif
