b3df57eae909a780f2170fd699042262d3ce0e13
Jovan Serbedzija <jserbedzija@tenstorrent.com>
2025-08-11 16:55:24 +0200

    [Utils] Updated generateReshape to accept location parameter (#4422)
    ### Problem description
    When creating a reshape function using `generateReshape` from
    `lib/Conversion/TTIRToTTNN/Utils.cpp`, the newly created reshape will
    always inherit the location from the input. This is not always desirable
    because sometimes we want to reshape the input to a certain operation
    and inherit the location from that operation, not the input.
    
    This posed a problem when using the Chisle tool for golden verification
    because Chisle uses locations to track the relationship between ops in
    the TTNN dialect and the TTIR dialect from which they originated, for
    which we have custom golden implementations. The problem is that if a
    certain operation’s input is a function argument, the generated reshape
    will inherit the location of the input rather than the operation, and
    thus the Chisle tool will not know that the generated reshape originated
    from the operation.
    
    ### What's changed
    Updated the function to accept a location and changed its usage to
    adhere to the new signature.
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes

--- lib/Conversion/TTIRToTTNN/Utils.cpp
+++ lib/Conversion/TTIRToTTNN/Utils.cpp
@@ -17,16 +17,12 @@ namespace ttir_to_ttnn::utils {
 ttnn::ReshapeOp generateReshape(mlir::TypedValue<mlir::RankedTensorType> input,
                                 ArrayRef<int64_t> newShape,
                                 PatternRewriter &rewriter,
-                                llvm::StringRef locSuffix) {
+                                mlir::Location newLoc) {
   // With reshape op, the output layout changes due to new output shape, hence
   // we need to create a new output layout attribute with the new shape.
   RankedTensorType inputType = input.getType();
   RankedTensorType outputType =
       ttnn::utils::RankedTensorTypeFactory::create(inputType, newShape);
-
-  // Add suffix to keep the location name unique.
-  Location newLoc =
-      ttmlir::utils::appendLocationSuffix(input.getLoc(), locSuffix);
 
   llvm::SmallVector<int32_t> newShapeI32(newShape.begin(), newShape.end());
   return rewriter.create<ttnn::ReshapeOp>(newLoc, outputType, input,
@@ -36,14 +32,14 @@ ttnn::ReshapeOp generateReshape(mlir::TypedValue<mlir::RankedTensorType> input,
 
 ttnn::ReshapeOp
 generateNHWFlatten(mlir::TypedValue<mlir::RankedTensorType> input,
-                   PatternRewriter &rewriter, llvm::StringRef locSuffix) {
+                   PatternRewriter &rewriter, mlir::Location newLoc) {
   llvm::ArrayRef<int64_t> shape = input.getType().getShape();
 
   assert(shape.size() == 4 && "Must have 4-dim tensor as conv2d input");
 
   llvm::SmallVector<int64_t> newShape = {1, 1, shape[0] * shape[1] * shape[2],
                                          shape[3]};
-  return generateReshape(input, newShape, rewriter, locSuffix);
+  return generateReshape(input, newShape, rewriter, newLoc);
 }
 
 } // namespace ttir_to_ttnn::utils
