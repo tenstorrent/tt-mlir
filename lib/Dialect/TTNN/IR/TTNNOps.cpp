9e0bb3f181224e50f3e66f9e5deec815743a8403
Lewis Panos <lpanos@tenstorrent.com>
2025-08-20 14:13:16 +0000

    WIP transpose caches, force row major for indices tensors

--- lib/Dialect/TTNN/IR/TTNNOps.cpp
+++ lib/Dialect/TTNN/IR/TTNNOps.cpp
@@ -873,6 +873,15 @@ mlir::tt::ttnn::TypecastOp::canonicalize(TypecastOp typecastOp,
 }
 
 //===----------------------------------------------------------------------===//
+// PagedScaledDotProductAttentionDecodeOp
+//===----------------------------------------------------------------------===//
+
+::mlir::LogicalResult
+mlir::tt::ttnn::PagedScaledDotProductAttentionDecodeOp::verify() {
+  return success();
+}
+
+//===----------------------------------------------------------------------===//
 // ToDTypeOp
 //===----------------------------------------------------------------------===//
 
