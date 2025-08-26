0d722cc280a7e57d8754455b1c36366c64121db6
Armin Ale <arminale@tenstorrent.com>
2025-08-26 20:21:55 +0000

    Move experimental matmul block llk to the experimental llk flow

--- lib/Conversion/TTKernelToEmitC/TTKernelToEmitC.cpp
+++ lib/Conversion/TTKernelToEmitC/TTKernelToEmitC.cpp
@@ -807,7 +807,7 @@ public:
         TTKernelToEmitCOpaqueRewriter<ttkernel::MatmulTilesOp>,
         TTKernelToEmitCOpaqueRewriter<ttkernel::MatmulBlockInitOp>,
         TTKernelToEmitCOpaqueRewriter<ttkernel::MatmulBlockInitShortOp>,
-        TTKernelToEmitCOpaqueRewriter<ttkernel::MatmulBlockOp>,
+        TTKernelToEmitCOpaqueRewriter<ttkernel::ExperimentalMatmulBlockOp>,
         TTKernelToEmitCOpaqueRewriter<ttkernel::MulTilesInitOp>,
         TTKernelToEmitCOpaqueRewriter<ttkernel::MulTilesOp>,
         TTKernelToEmitCOpaqueRewriter<ttkernel::SubTilesInitOp>,
