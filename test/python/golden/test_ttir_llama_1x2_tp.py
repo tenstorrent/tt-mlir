c4509f48cdfe163370782a38d8852d0b10619898
Hongseok Kwon <hkwon@tenstorrent.com>
2025-08-28 18:04:05 +0000

    enable all the fails_golden cases

--- test/python/golden/test_ttir_llama_1x2_tp.py
+++ test/python/golden/test_ttir_llama_1x2_tp.py
@@ -162,8 +162,6 @@ def golden_part2(
     return output115
 
 
-# llama attention part 1 with 1x2
-@pytest.mark.skip(reason="failed with new CCL op - skip temporarily")
 @pytest.mark.parametrize(
     "shapes",
     [
@@ -371,8 +369,6 @@ def test_llama_attention_1x2_tp_part1(
     )
 
 
-# llama attention part 2 with 1x2
-@pytest.mark.skip(reason="failed with new CCL op - skip temporarily")
 @pytest.mark.parametrize(
     "shapes",
     [
