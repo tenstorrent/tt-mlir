6b6055f8cffe3c44cec9f5aa820ba6e8671f3a88
ajakovljevicTT <ajakovljevic@tenstorrent.com>
2025-09-02 09:35:27 +0000

    Added pipelines to tt-mlir

--- lib/Dialect/StableHLO/Pipelines/StableHLOPipelines.cpp
+++ lib/Dialect/StableHLO/Pipelines/StableHLOPipelines.cpp
@@ -6,13 +6,136 @@
 
 #include "mlir/Transforms/Passes.h"
 
+#ifdef TTMLIR_ENABLE_STABLEHLO
+#include "shardy/round_trip_import/pipelines.h"
+#include "shardy/round_trip_import/constants.h"
+#include "shardy/round_trip_import/utils.h"
+#include "ttmlir/Dialect/StableHLO/Utils/ShardyUtils.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Support/TypeID.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "stablehlo/dialect/StablehloOps.h"
+#endif // TTMLIR_ENABLE_STABLEHLO
+
 namespace mlir::tt::stablehlo {
+
+#ifdef TTMLIR_ENABLE_STABLEHLO
+
+struct ConditionalShardyRoundTripPass : public OperationPass<ModuleOp> {
+  ConditionalShardyRoundTripPass() : OperationPass(TypeID::get<ConditionalShardyRoundTripPass>()) {}
+  
+  void runOnOperation() override {
+    ModuleOp module = getOperation();
+    
+    // Check for SDY annotations (native format)  
+    bool needsRoundTripImport = mlir::tt::shardy_utils::sdyAnnotationsExist(module);
+    
+    // Also check for round-trip Shardy format
+    if (!needsRoundTripImport) {
+      // Check for round-trip attributes
+      if (mlir::sdy::tryGetFrontendAttr<mlir::DictionaryAttr>(module, mlir::sdy::kMeshesRoundTripAttr).has_value()) {
+        needsRoundTripImport = true;
+      }
+      
+      // Check for round-trip custom calls using simple iteration
+      if (!needsRoundTripImport) {
+        for (auto funcOp : module.getOps<mlir::func::FuncOp>()) {
+          funcOp.walk([&](::mlir::stablehlo::CustomCallOp customCall) {
+            llvm::StringRef targetName = customCall.getCallTargetName();
+            if (targetName == mlir::sdy::kGlobalToLocalShapeCallTargetName || 
+                targetName == mlir::sdy::kLocalToGlobalShapeCallTargetName ||
+                targetName == mlir::sdy::kFuncResultShardingTargetName) {
+              needsRoundTripImport = true;
+            }
+          });
+          if (needsRoundTripImport) break;
+        }
+      }
+    }
+    
+    // Only run round-trip import if module needs it
+    if (needsRoundTripImport) {
+      // Fix frontend attributes: transfer from custom calls to CallOp
+      // This is needed because JAX/XLA puts attributes on custom calls instead of CallOps
+      module.walk([&](mlir::func::FuncOp func) {
+        func.walk([&](::mlir::stablehlo::CustomCallOp globalToLocal) {
+          if (globalToLocal.getCallTargetName() == mlir::sdy::kGlobalToLocalShapeCallTargetName) {
+            for (auto user : globalToLocal->getResult(0).getUsers()) {
+              if (auto callOp = mlir::dyn_cast<mlir::func::CallOp>(user)) {
+                if (callOp.getCallee().contains(mlir::sdy::kManualComputationBodyFuncName)) {
+                  auto globalAttrs = globalToLocal->getAttrOfType<mlir::DictionaryAttr>(mlir::sdy::kFrontendAttributesAttr);
+                  
+                  mlir::DictionaryAttr localAttrs;
+                  for (auto callUser : callOp->getResult(0).getUsers()) {
+                    if (auto localToGlobal = mlir::dyn_cast<::mlir::stablehlo::CustomCallOp>(callUser)) {
+                      if (localToGlobal.getCallTargetName() == mlir::sdy::kLocalToGlobalShapeCallTargetName) {
+                        localAttrs = localToGlobal->getAttrOfType<mlir::DictionaryAttr>(mlir::sdy::kFrontendAttributesAttr);
+                        break;
+                      }
+                    }
+                  }
+                  
+                  if (globalAttrs && localAttrs) {
+                    llvm::SmallVector<mlir::NamedAttribute> combinedAttrs;
+                    
+                    if (auto inShardings = globalAttrs.get(mlir::sdy::kInShardings)) {
+                      combinedAttrs.push_back(mlir::NamedAttribute(
+                          mlir::StringAttr::get(globalToLocal->getContext(), mlir::sdy::kInShardings), inShardings));
+                    }
+                    if (auto manualAxes = globalAttrs.get(mlir::sdy::kManualAxes)) {
+                      combinedAttrs.push_back(mlir::NamedAttribute(
+                          mlir::StringAttr::get(globalToLocal->getContext(), mlir::sdy::kManualAxes), manualAxes));
+                    }
+                    if (auto outShardings = localAttrs.get(mlir::sdy::kOutShardings)) {
+                      combinedAttrs.push_back(mlir::NamedAttribute(
+                          mlir::StringAttr::get(globalToLocal->getContext(), mlir::sdy::kOutShardings), outShardings));
+                    }
+                    
+                    auto frontendAttrsDict = mlir::DictionaryAttr::get(globalToLocal->getContext(), combinedAttrs);
+                    callOp->setAttr(mlir::sdy::kFrontendAttributesAttr, frontendAttrsDict);
+                  }
+                }
+              }
+            }
+          }
+        });
+      });
+      
+      PassManager roundTripPM(module->getName());
+      mlir::sdy::addSdyRoundTripImportPipeline(roundTripPM);
+      
+      if (failed(roundTripPM.run(module))) {
+        signalPassFailure();
+      }
+    }
+  }
+  
+  StringRef getName() const override { return "ConditionalShardyRoundTripPass"; }
+  StringRef getArgument() const override { return "conditional-shardy-roundtrip"; }
+  StringRef getDescription() const override { 
+    return "Conditionally run Shardy round-trip import for modules with SDY annotations"; 
+  }
+  
+  std::unique_ptr<Pass> clonePass() const override {
+    return std::make_unique<ConditionalShardyRoundTripPass>();
+  }
+};
+
+#endif // TTMLIR_ENABLE_STABLEHLO
+
 //===----------------------------------------------------------------------===//
 // Pipeline implementation.
 //===----------------------------------------------------------------------===//
 
 void createStableHLOPipeline(OpPassManager &pm,
                              const StableHLOPipelineOptions &options) {
+#ifdef TTMLIR_ENABLE_STABLEHLO
+  
+  // Add conditional Shardy round-trip import pass
+  pm.addPass(std::make_unique<ConditionalShardyRoundTripPass>());
+
+#endif // TTMLIR_ENABLE_STABLEHLO
   // Inline all operations to make analysis easier.
   pm.addPass(mlir::createInlinerPass());
 
