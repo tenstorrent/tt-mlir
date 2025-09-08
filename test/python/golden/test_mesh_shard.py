7a966a7490ea66dfb9f81f59536b16e52c88232a
Hongseok Kwon <hkwon@tenstorrent.com>
2025-09-08 20:05:27 +0000

    Add 3D mesh shard testcase

--- test/python/golden/test_mesh_shard.py
+++ test/python/golden/test_mesh_shard.py
@@ -6,6 +6,8 @@ import pytest
 import torch
 from typing import List, Tuple, OrderedDict, Callable
 
+import itertools
+
 from builder.base.builder import Operand
 from builder.ttir.ttir_builder import TTIRBuilder
 from builder.base.builder_utils import compile_ttir_to_flatbuffer
@@ -182,6 +184,84 @@ def test_mesh_shard_devices(
         [input_shape],
         mesh_name="mesh",
         mesh_dict=OrderedDict([("x", mesh_shape[0]), ("y", mesh_shape[1])]),
+        test_base=request.node.name,
+        output_root=request.config.getoption("--path"),
+        system_desc_path=request.config.getoption("--sys-desc"),
+        target=target,
+    )
+
+
+@pytest.mark.parametrize(
+    "input_rank",
+    range(MIN_TEST_RANK + 1, MAX_TEST_RANK + 1),
+)
+@pytest.mark.parametrize(
+    "shard_dim_0, shard_dim_1, shard_dim_2",
+    list(itertools.permutations(range(-1, MAX_TEST_RANK), 3)),
+)
+@pytest.mark.parametrize("mesh_shape", [(2, 2, 2), (1, 1, 8), (1, 1, 2), (1, 2, 4)])
+@pytest.mark.parametrize("target", ["ttnn", "ttmetal"])
+@pytest.mark.parametrize("tile_size", [1, 32])
+@pytest.mark.parametrize(
+    "test_case",
+    [devices_devices, devices_replicate, replicate_devices],
+    ids=lambda f: f.__name__,
+)
+def test_mesh_shard_3D(
+    input_rank: int,
+    shard_dim_0: int,
+    shard_dim_1: int,
+    shard_dim_2: int,
+    mesh_shape: Tuple[int, ...],
+    target: str,
+    tile_size: int,
+    test_case: Callable,
+    request,
+):
+    if (
+        shard_dim_0 >= input_rank
+        or shard_dim_1 >= input_rank
+        or shard_dim_2 >= input_rank
+    ):
+        pytest.skip("shard_dim is out of range, skipping test.")
+    if (
+        shard_dim_0 == shard_dim_1
+        or shard_dim_0 == shard_dim_2
+        or shard_dim_1 == shard_dim_2
+    ):
+        pytest.skip("shard_dims need to be unique, skipping test.")
+
+    # Generate shard_shape from shard_dims.
+    shard_dims = (shard_dim_0, shard_dim_1, shard_dim_2)
+    shard_shape = [1] * input_rank
+    for mesh_axis, tensor_dim in enumerate(shard_dims):
+        if tensor_dim >= 0:
+            shard_shape[tensor_dim] = mesh_shape[mesh_axis]
+    if all(x == 1 for x in shard_shape):
+        pytest.skip("sharding is meaningless, skipping test.")
+
+    # Generate input_shape according to shard_shape.
+    # For the last two dimensions, ensure they are multiples of tile_size.
+    input_shape = [
+        n_shards if idx < input_rank - 2 else tile_size * n_shards
+        for idx, n_shards in enumerate(shard_shape)
+    ]
+    # In the case of the replicate_devices test_case,
+    # the input is replicated so there is no need to increase the input according to shard_shape,
+    # but for code simplicity, we just test it this way.
+
+    test_fn = lambda inputs, builder: test_case(
+        inputs, builder, shard_shape, shard_dims
+    )
+    test_fn.__name__ = test_case.__name__
+
+    compile_ttir_to_flatbuffer(
+        test_fn,
+        [input_shape],
+        mesh_name="mesh",
+        mesh_dict=OrderedDict(
+            [("x", mesh_shape[0]), ("y", mesh_shape[1]), ("z", mesh_shape[2])]
+        ),
         test_base=request.node.name,
         output_root=request.config.getoption("--path"),
         system_desc_path=request.config.getoption("--sys-desc"),
