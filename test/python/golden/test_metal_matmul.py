1fe2a4233ec7036ad0ba63b8a935b591029865f1
Jacob DeSousa <jdesousa@tenstorrent.com>
2025-09-08 19:03:40 +0000

    add double buffered matmul tests

--- test/python/golden/test_metal_matmul.py
+++ test/python/golden/test_metal_matmul.py
@@ -135,7 +135,7 @@ def test_matmul_ttnn_shapes(
         shape[2],
     )
 
-    def matmul_blocking(
+    def matmul(
         in0: Operand,
         in1: Operand,
         builder: TTIRBuilder,
@@ -149,7 +149,62 @@ def test_matmul_ttnn_shapes(
         f"use-tile-matmul={use_tile_matmul}",
     ]
     compile_ttir_to_flatbuffer(
-        matmul_blocking,
+        matmul,
+        [lhs, rhs],
+        target=target,
+        custom_pipeline=f"ttir-to-ttmetal-pipeline{{{' '.join(options)}}}",
+        test_base=request.node.name,
+        module_dump=True,
+        print_ir=True,
+        output_root=request.config.getoption("--path"),
+        system_desc_path=request.config.getoption("--sys-desc"),
+    )
+
+
+@pytest.mark.fails_golden
+@pytest.mark.parametrize(
+    "shape",
+    [
+        (512, 512, 512),
+        (512, 1024, 1024),
+        (512, 1024, 2048),
+        (1024, 1024, 1024),
+        (1024, 1024, 2048),
+    ],
+)
+@pytest.mark.parametrize("dst_register_size_tiles", [8])
+@pytest.mark.parametrize("target", ["ttmetal"])
+# Large matmuls, based on ttnn's matmul benchmarks
+def test_matmul_ttnn_shapes_double_buffered(
+    shape: tuple[int, ...],
+    dst_register_size_tiles: int,
+    target: str,
+    request,
+):
+    lhs = (
+        shape[0],
+        shape[1],
+    )
+    rhs = (
+        shape[1],
+        shape[2],
+    )
+
+    def matmul(
+        in0: Operand,
+        in1: Operand,
+        builder: TTIRBuilder,
+        unit_attrs: List[str] = None,
+    ):
+        return builder.matmul(in0, in1, unit_attrs=unit_attrs)
+
+    options = [
+        f"max-dst-register-size-tiles={dst_register_size_tiles}",
+        f"matmul-interchange=2,0,1",
+        f"num-stream-buffers=2",
+    ]
+    compile_ttir_to_flatbuffer(
+        matmul,
         [lhs, rhs],
         target=target,
         custom_pipeline=f"ttir-to-ttmetal-pipeline{{{' '.join(options)}}}",
