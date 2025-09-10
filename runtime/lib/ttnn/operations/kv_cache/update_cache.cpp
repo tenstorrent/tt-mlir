ab8d3d68e590658c639e66b5ae77ebb9cb085515
James Zian Xu <jameszianxu@tenstorrent.com>
2025-09-10 15:39:20 +0000

    Create workaround for update cache op to extract a replicated updateindex back to host

--- runtime/lib/ttnn/operations/kv_cache/update_cache.cpp
+++ runtime/lib/ttnn/operations/kv_cache/update_cache.cpp
@@ -7,6 +7,12 @@
 #include "tt/runtime/detail/common/logger.h"
 
 #include "tt/runtime/workarounds.h"
+#include "ttnn/distributed/api.hpp"
+#include "ttnn/distributed/distributed_tensor.hpp"
+// using ::ttnn::distributed::MeshComposerConfig;
+// using ::ttnn::distributed::MeshMapperConfig;
+// using ::ttnn::distributed::MeshToTensor;
+// using ::ttnn::distributed::TensorToMesh;
 
 namespace tt::runtime::ttnn::operations::kv_cache {
 void run(const ::tt::target::ttnn::UpdateCacheOp *op, ProgramContext &context) {
@@ -20,9 +26,35 @@ void run(const ::tt::target::ttnn::UpdateCacheOp *op, ProgramContext &context) {
   const ::ttnn::Tensor &updateIndex =
       tensorPool.getTTNNTensorAndValidate(op->update_index());
 
+
+  ::ttnn::Tensor indexOnHost;
+
   if (workaround::Env::get().readUpdateIndexFromDeviceForKVCache) {
 
-    const ::ttnn::Tensor indexOnHost = ::ttnn::from_device(updateIndex);
+    // ::ttnn::Tensor indexOnHost = ::ttnn::from_device(updateIndex);
+
+    // Handle distributed tensors by aggregating them to a single tensor
+    // updateIndex.tensor_topology().mesh_shape().mesh_size() -> this remains as 1, but still fails the get host buffer later.
+    // if (updateIndex.tensor_topology().mesh_shape().mesh_size() > 1) {
+    if(true){
+
+      // Get the mesh device from the tensor
+      auto mesh_device = updateIndex.device();
+
+      // Create a mesh composer to aggregate the distributed tensor # but it's not a distributed tensor...
+      auto composer = ::ttnn::distributed::create_mesh_composer(
+          *mesh_device,
+          ::ttnn::distributed::MeshComposerConfig{
+              .dims = {0}, // Concatenate along the first dimension. This is replicated so we'll only use the first element anyways.
+              .mesh_shape_override = ::ttnn::MeshShape(1), // Collapse to single device
+          });
+
+      // Aggregate the distributed tensor into a single tensor
+      indexOnHost = ::ttnn::distributed::aggregate_tensor(updateIndex, *composer);
+    }else{
+      indexOnHost = ::ttnn::from_device(updateIndex);
+    }
+
     const ::tt::tt_metal::HostBuffer buffer =
         ::tt::tt_metal::host_buffer::get_host_buffer(indexOnHost);
     const auto &buf = buffer.view_as<uint32_t>();
