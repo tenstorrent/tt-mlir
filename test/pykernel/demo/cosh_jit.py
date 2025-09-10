101549ad7bb959201d62db092dd44dca8d2b57b8
Vincent Tang <vtang@tenstorrent.com>
2025-09-10 15:28:08 +0000

    run test in ci

--- test/pykernel/demo/cosh_jit.py
+++ test/pykernel/demo/cosh_jit.py
@@ -5,16 +5,27 @@ import pykernel
 import ttnn
 
 
-@pykernel.jit(backend="ttnn", debug=True, dump_flatbuffer=True)
-def cosh(a):
+def _cosh(a):
     e_pos_x = exp(a)
     e_neg_x = exp(-a)
     nr_term = e_pos_x + e_neg_x
     return nr_term * 0.5
 
 
-input_tensor = ttnn.Tensor(
-    [-1, 0, 1, 2], shape=[1, 1, 1, 4], data_type=ttnn.float32, layout=ttnn.TILE_LAYOUT
-)
+def main():
+    input_tensor = ttnn.Tensor(
+        [-1, 0, 1, 2],
+        shape=[1, 1, 1, 4],
+        data_type=ttnn.float32,
+        layout=ttnn.TILE_LAYOUT,
+    )
 
-cosh(input_tensor)
+    cosh_ttnn = pykernel.jit(backend="ttnn", debug=True, dump_flatbuffer=True)(_cosh)
+    cosh_metal = pykernel.jit(backend="metal", debug=True, dump_flatbuffer=True)(_cosh)
+
+    cosh_ttnn(input_tensor)
+    cosh_metal(input_tensor)
+
+
+if __name__ == "__main__":
+    main()
