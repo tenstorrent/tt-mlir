f3f94e0c96b0d46e14f94ee3806bd9d2ae81ca3d
Bojana Malesevic <bmalesevic@tenstorrent.com>
2025-09-10 13:22:37 +0000

    [skip ci] Revert unnecessary import

--- lib/Dialect/TTNN/Utils/Utils.cpp
+++ lib/Dialect/TTNN/Utils/Utils.cpp
@@ -11,7 +11,6 @@
 #include "mlir/Dialect/Quant/IR/QuantTypes.h"
 #include "mlir/IR/Location.h"
 #include "mlir/IR/Value.h"
-#include "llvm/ADT/ArrayRef.h"
 #include "llvm/Support/Casting.h"
 
 #include <optional>
