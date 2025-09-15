56dba15565c85092cf04c1d32b29aafb971c4553
Vincent Tang <vtang@tenstorrent.com>
2025-09-15 18:33:10 +0000

    legalize ttnn in metal backend pipeline

--- lib/Conversion/TTIRToTTKernel/TTIRToTTKernelPass.cpp
+++ lib/Conversion/TTIRToTTKernel/TTIRToTTKernelPass.cpp
@@ -12,6 +12,7 @@
 #include "ttmlir/Dialect/TTKernel/IR/TTKernel.h"
 #include "ttmlir/Dialect/TTKernel/IR/TTKernelOpsTypes.h"
 #include "ttmlir/Dialect/TTMetal/IR/TTMetal.h"
+#include "ttmlir/Dialect/TTNN/IR/TTNN.h"
 
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/EmitC/IR/EmitC.h"
@@ -48,6 +49,7 @@ struct ConvertTTIRToTTKernel
     target.addLegalDialect<ttmetal::TTMetalDialect>();
     target.addLegalDialect<ttcore::TTCoreDialect>();
     target.addLegalDialect<ttkernel::TTKernelDialect>();
+    target.addLegalDialect<ttnn::TTNNDialect>();
     target.addLegalDialect<scf::SCFDialect>();
     target.addLegalDialect<emitc::EmitCDialect>();
     target.addIllegalDialect<math::MathDialect>();
@@ -81,6 +83,8 @@ struct ConvertTTIRToTTKernel
       return !op->hasAttr(ttir::ThreadAttr::name) ||
              (op.getFunctionType().getNumInputs() == 0);
     });
+
+    target.addLegalOp<ttir::TTNNToMetalLayoutCastOp>();
 
     TypeConverter typeConverter;
     typeConverter.addConversion([](Type type) { return type; });
