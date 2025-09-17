476d3eb387fae7145f8cff7c0d4bc92e70ac0491
Vincent Tang <vtang@tenstorrent.com>
2025-09-17 18:54:45 +0000

    fix cast op def + add flag

--- lib/Dialect/TTMetal/Pipelines/TTMetalPipelines.cpp
+++ lib/Dialect/TTMetal/Pipelines/TTMetalPipelines.cpp
@@ -103,6 +103,7 @@ void createTTIRToTTMetalMiddleendPipeline(
     allocateOptions.allowOutputSpilling = options.allowOutputSpilling;
   }
   pm.addPass(ttir::createTTIRAllocate(allocateOptions));
+  pm.addPass(ttir::createTTIRAllocate());
   pm.addPass(createCanonicalizerPassWithOptions(options));
   ttir::TTIRGenericApplyInterchangeOptions applyInterchangeOptions;
   {
