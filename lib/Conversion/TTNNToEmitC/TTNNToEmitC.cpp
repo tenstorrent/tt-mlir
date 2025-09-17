ff6c604a9c21a8d94e780e8d8db7dca33c01bcb7
Aleksandar Zecevic <azecevic@tenstorrent.com>
2025-09-17 12:46:32 +0000

    emitc support

--- lib/Conversion/TTNNToEmitC/TTNNToEmitC.cpp
+++ lib/Conversion/TTNNToEmitC/TTNNToEmitC.cpp
@@ -2982,6 +2982,7 @@ public:
 namespace {
 class DumpTensorOpConversionPattern
     : public TTNNToEmitCBaseOpConversionPattern<mlir::tt::ttnn::DumpTensorOp> {
+<<<<<<< HEAD
 private:
   std::string getPrefixSearchPattern() const override {
     return mlir::tt::ttnn::DumpTensorOp::getOperationName().str();
@@ -2991,6 +2992,8 @@ private:
     return "::tt::tt_metal::dump_tensor_flatbuffer";
   }
 
+=======
+>>>>>>> 1d3b1a971 (emitc support)
 public:
   using TTNNToEmitCBaseOpConversionPattern<
       mlir::tt::ttnn::DumpTensorOp>::TTNNToEmitCBaseOpConversionPattern;
@@ -3020,7 +3023,6 @@ private:
   std::string getPrefixSearchPattern() const override {
     return mlir::tt::ttnn::LoadTensorOp::getOperationName().str();
   }
-
   std::string getPrefixSwapPattern() const override {
     return "::tt::tt_metal::load_tensor_flatbuffer";
   }
@@ -3028,8 +3030,6 @@ private:
 public:
   using TTNNToEmitCBaseOpConversionPattern<
       mlir::tt::ttnn::LoadTensorOp>::TTNNToEmitCBaseOpConversionPattern;
-
-  LogicalResult
   matchAndRewrite(mlir::tt::ttnn::LoadTensorOp srcOp, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
 
