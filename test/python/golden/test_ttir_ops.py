1987389c54253361e480ed49cd1537acc356181d
ttdloke <dloke@tenstorrent.com>
2025-09-18 21:40:53 +0000

    cleaned up test file

--- test/python/golden/test_ttir_ops.py
+++ test/python/golden/test_ttir_ops.py
@@ -63,7 +63,6 @@ def logical_not(
 
 
 # TODO (wenbinlyuTT): test int32 once untilize issue is fixed
-# @pytest.mark.skip_config(["ttmetal", "p150"], reason="Issue #4079")
 @pytest.mark.parametrize("shape", [(128, 128)], ids=shape_str)
 @pytest.mark.parametrize("dtype", [torch.float32], ids=["f32"])
 @pytest.mark.parametrize("target", ["ttnn", "ttmetal"])
@@ -270,7 +269,6 @@ def leaky_relu(
     return builder.leaky_relu(in0, unit_attrs=unit_attrs)
 
 
-# @pytest.mark.skip_config(["ttmetal", "p150"], reason="Issue #4080")
 @pytest.mark.parametrize("shape", [(128, 128)], ids=shape_str)
 @pytest.mark.parametrize("dtype", [torch.float32], ids=["f32"])
 @pytest.mark.parametrize("target", ["ttnn", "ttmetal"])
@@ -299,7 +297,6 @@ def cbrt(in0: Operand, builder: TTIRBuilder, unit_attrs: Optional[List[str]] = N
     return builder.cbrt(in0, unit_attrs=unit_attrs)
 
 
-# @pytest.mark.skip_config(["ttmetal", "p150"], reason="Issue #4081")
 @pytest.mark.parametrize("shape", [(128, 128)], ids=shape_str)
 @pytest.mark.parametrize("dtype", [torch.float32], ids=["f32"])
 @pytest.mark.parametrize("target", ["ttnn", "ttmetal"])
@@ -562,7 +559,6 @@ def div(
 
 
 # TODO (wenbinlyuTT): fix f32 accuracy issue for small values
-# @pytest.mark.skip_config(["ttmetal", "p150"], reason="Issue #4082")
 @pytest.mark.parametrize("shape", [(128, 128)], ids=shape_str)
 @pytest.mark.parametrize("dtype", [torch.float32], ids=["f32"])
 @pytest.mark.parametrize("target", ["ttnn", "ttmetal"])
@@ -2253,16 +2249,13 @@ def test_hoisted_transpose(input_shape, dims, request, target: str):
 
 
 unary_ops = [
-    # exp | Marks(pytest.mark.skip_config(["ttmetal", "p150"], reason="Issue #4078")),
     exp,
     expm1 | Marks(pytest.mark.skip_config(["ttmetal"])),
     floor | Marks(pytest.mark.fails_golden),
     abs,
     neg,
     sign | Marks(pytest.mark.skip_config(["ttmetal"])),
-    # cos | Marks(pytest.mark.skip_config(["ttmetal", "p150"], reason="Issue #4083")),
     cos,
-    # sin | Marks(pytest.mark.skip_config(["ttmetal", "p150"], reason="Issue #4083")),
     sin,
     atan | Marks(pytest.mark.skip_config(["ttmetal"])),
     tanh | Marks(pytest.mark.skip_config(["ttmetal"])),
@@ -2276,7 +2269,6 @@ unary_ops = [
     sum | Marks(pytest.mark.skip_config(["ttmetal"])),
     mean | Marks(pytest.mark.skip_config(["ttmetal"])),
     max | Marks(pytest.mark.fails_golden, pytest.mark.skip_config(["ttmetal"])),
-    # max,
     min | Marks(pytest.mark.fails_golden, pytest.mark.skip_config(["ttmetal"])),
     get_dimension_size
     | Marks(
@@ -2455,9 +2447,6 @@ def test_bitwise_binary_ops(test_fn: Callable, shape: Shape, request):
     )
 
 
-@pytest.mark.skip_config(
-    ["ttmetal", "p150"], reason="https://github.com/tenstorrent/tt-mlir/issues/4910"
-)
 @pytest.mark.parametrize("shape", [(128, 128)], ids=shape_str)
 @pytest.mark.parametrize("dtype", [torch.float32], ids=["f32"])
 @pytest.mark.parametrize("target", ["ttnn", "ttmetal"])
