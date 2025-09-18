c2716a19767bb8f6e8573b7d25ef70a87d6938af
Lewis Panos <lpanos@tenstorrent.com>
2025-09-18 17:49:27 +0000

    Add verification methods for SDPADecode op. Convert cur_pos_tensor to a required argument

--- lib/Target/TTNN/TTNNToFlatbuffer.cpp
+++ lib/Target/TTNN/TTNNToFlatbuffer.cpp
@@ -1900,14 +1900,12 @@ createOp(FlatbufferObjectCache &cache, ScaledDotProductAttentionDecodeOp op) {
       getOperandThroughDPSOps(op.getKey()));
   auto value = cache.at<::tt::target::ttnn::TensorRef>(
       getOperandThroughDPSOps(op.getValue()));
+  auto curPosTensor = cache.at<::tt::target::ttnn::TensorRef>(
+      getOperandThroughDPSOps(op.getCurPosTensor()));
   auto attentionMask = op.getAttentionMask()
                            ? cache.at<::tt::target::ttnn::TensorRef>(
                                  getOperandThroughDPSOps(op.getAttentionMask()))
                            : 0;
-  auto curPosTensor = op.getCurPosTensor()
-                          ? cache.at<::tt::target::ttnn::TensorRef>(
-                                getOperandThroughDPSOps(op.getCurPosTensor()))
-                          : 0;
   auto attentionSink = op.getAttentionSink()
                            ? cache.at<::tt::target::ttnn::TensorRef>(
                                  getOperandThroughDPSOps(op.getAttentionSink()))
@@ -1919,7 +1917,7 @@ createOp(FlatbufferObjectCache &cache, ScaledDotProductAttentionDecodeOp op) {
   auto out = cache.getOrCreate(op.getResult(), tensorValueToFlatbuffer);
 
   return ::tt::target::ttnn::CreateScaledDotProductAttentionDecodeOp(
-      *cache.fbb, query, key, value, isCausal, attentionMask, curPosTensor,
+      *cache.fbb, query, key, value, curPosTensor, isCausal, attentionMask,
       attentionSink, scale.convertToFloat(), out, memoryConfig);
 }
 
