a101cf670014e51364d0a124f15466fde4986ce3
Tapasvi Patel <133996364+tapspatel@users.noreply.github.com>
2025-09-18 01:49:14 -0500

    Uplift tt-metal to 449ce3763f54e862f4094230914639406c2ca7d8 (#4963)

--- test/ttmlir/Silicon/TTNN/n150/fusing/resnet_pattern_fusing.mlir
+++ test/ttmlir/Silicon/TTNN/n150/fusing/resnet_pattern_fusing.mlir
@@ -10,7 +10,7 @@ module {
   func.func @main(%weight: tensor<64x64x3x3xf32> {ttcore.argument_type = #ttcore.argument_type<constant>}, %arg5: tensor<1x1x1x64xf32> {ttcore.argument_type = #ttcore.argument_type<constant>}, %arg6: tensor<1x1x1x64xf32>, %input: tensor<1x56x56x64xf32>) -> tensor<1x56x56x64xf32> {
     // CHECK: "ttnn.multiply"
     // CHECK: "ttnn.conv2d"
-    // CHECK-SAME: activation = "relu"
+    // CHECK-SAME: activation = <op_type = relu>
     %0 = ttir.empty() : tensor<1x56x56x64xf32>
     %1 = "ttir.conv2d"(%input, %weight, %0) <{dilation = array<i32: 1, 1>, groups = 1 : i32, padding = array<i32: 1, 1, 1, 1>, stride = array<i32: 1, 1>}> {channel_last = 1 : si32} : (tensor<1x56x56x64xf32>, tensor<64x64x3x3xf32>, tensor<1x56x56x64xf32>) -> tensor<1x56x56x64xf32>
     %2 = ttir.empty() : tensor<1x56x56x64xf32>
--- test/unittests/OpModel/TTNN/Op/TestOpModelInterface.cpp
+++ test/unittests/OpModel/TTNN/Op/TestOpModelInterface.cpp
@@ -2089,7 +2089,7 @@ TEST_F(OpModelBase, Conv2dInterfaceConfigs) {
   auto badConvConfig = Conv2dConfigAttr::get(
       &context,
       /*weights_dtype=*/ttcore::DataType::BFloat16,
-      /*activation=*/StringAttr::get(&context, ""),
+      /*activation=*/nullptr,
       /*deallocate_activation=*/BoolAttr::get(&context, false),
       /*reallocate_halo_output=*/BoolAttr::get(&context, true),
       /*act_block_h_override=*/0, /*act_block_w_div=*/1,
@@ -2101,7 +2101,6 @@ TEST_F(OpModelBase, Conv2dInterfaceConfigs) {
       /*output_layout=*/Layout::Tile,
       /*enable_act_double_buffer=*/BoolAttr::get(&context, false),
       /*enable_weights_double_buffer=*/BoolAttr::get(&context, false),
-      /*enable_split_reader=*/BoolAttr::get(&context, false),
       /*in_place=*/BoolAttr::get(&context, false));
 
   OpModel backend = dyn_cast<OpModel>(conv2d.getOperation());
@@ -2128,7 +2127,7 @@ TEST_F(OpModelBase, Conv2dInterfaceConfigs) {
   auto goodConvConfig = Conv2dConfigAttr::get(
       &context,
       /*weights_dtype=*/ttcore::DataType::BFloat16,
-      /*activation=*/StringAttr::get(&context, ""),
+      /*activation=*/nullptr,
       /*deallocate_activation=*/BoolAttr::get(&context, false),
       /*reallocate_halo_output=*/BoolAttr::get(&context, true),
       /*act_block_h_override=*/0, /*act_block_w_div=*/1,
@@ -2140,7 +2139,6 @@ TEST_F(OpModelBase, Conv2dInterfaceConfigs) {
       /*output_layout=*/Layout::Tile,
       /*enable_act_double_buffer=*/BoolAttr::get(&context, true),
       /*enable_weights_double_buffer=*/BoolAttr::get(&context, true),
-      /*enable_split_reader=*/BoolAttr::get(&context, false),
       /*in_place=*/BoolAttr::get(&context, false));
 
   constraintsExp = backend.getOpConstraints(
@@ -2275,7 +2273,7 @@ TEST_F(OpModelBase, ConvTranspose2dInterfaceConfigs) {
   auto goodConvConfig = Conv2dConfigAttr::get(
       &context,
       /*weights_dtype=*/ttcore::DataType::BFloat16,
-      /*activation=*/StringAttr::get(&context, ""),
+      /*activation=*/nullptr,
       /*deallocate_activation=*/BoolAttr::get(&context, false),
       /*reallocate_halo_output=*/BoolAttr::get(&context, true),
       /*act_block_h_override=*/0, /*act_block_w_div=*/1,
@@ -2287,7 +2285,6 @@ TEST_F(OpModelBase, ConvTranspose2dInterfaceConfigs) {
       /*output_layout=*/Layout::Tile,
       /*enable_act_double_buffer=*/BoolAttr::get(&context, true),
       /*enable_weights_double_buffer=*/BoolAttr::get(&context, true),
-      /*enable_split_reader=*/BoolAttr::get(&context, false),
       /*in_place=*/BoolAttr::get(&context, false));
 
   OpModel backend = dyn_cast<OpModel>(convTranspose2d.getOperation());
