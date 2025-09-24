9653e24707c51f6f046c7565dee097851def8159
Milan Topalovic <mtopalovic@tenstorrent.com>
2025-09-24 19:01:48 +0000

    Default queue for recording mesh events should be 0

--- runtime/lib/ttnn/runtime.cpp
+++ runtime/lib/ttnn/runtime.cpp
@@ -154,9 +154,8 @@ toHostSingleTensor(const ::tt::runtime::ttnn::TTNNTensorWrapper &tensorWrapper,
 
     std::optional<::ttnn::MeshEvent> meshEvent = std::nullopt;
     if (!blocking) {
-      meshEvent = ::ttnn::events::record_mesh_event(
-          meshDevice,
-          ::ttnn::QueueId(tt::tt_metal::GetCurrentCommandQueueIdForThread()));
+      meshEvent =
+          ::ttnn::events::record_mesh_event(meshDevice, ::ttnn::QueueId{0});
     }
 
     return utils::createRuntimeTensorFromTTNN(hostTensor, meshEvent,
@@ -183,9 +182,8 @@ toHostSingleTensor(const ::tt::runtime::ttnn::TTNNTensorWrapper &tensorWrapper,
   // if we don't need to untilize, then from_device can execute asynchronously
   // in this case we need to populate the event
   if (!untilize && !blocking) {
-    meshEvent = ::ttnn::events::record_mesh_event(
-        meshDevice,
-        ::ttnn::QueueId(tt::tt_metal::GetCurrentCommandQueueIdForThread()));
+    meshEvent =
+        ::ttnn::events::record_mesh_event(meshDevice, ::ttnn::QueueId{0});
   }
 
   return utils::createRuntimeTensorFromTTNN(hostTensor, /*meshEvent=*/meshEvent,
