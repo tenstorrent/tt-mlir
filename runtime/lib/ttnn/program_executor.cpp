4edc39f22ab864a5fab79298823f1ba7ab763762
Lewis Panos <lpanos@tenstorrent.com>
2025-09-25 11:17:52 -0400

    Implement `ttnn::transformers::scaled_dot_product_attention_decode` (#5018)
    ### Ticket
    https://github.com/tenstorrent/tt-mlir/issues/4265
    - This doesn't close the issue as there is another op for prefill we
    must implement too.
    
    ### Problem description
     - We do not model the ttnn `scaled_dot_product_attention_decode` op.
    
    ### What's changed
    - Model the `scaled_dot_product_attention_decode` in runtime, ttnn, ttir
    - Add stablehlo custom call conversion for this op for ease of use in
    vllm plugin.
    - Implement OpModel support for `scaled_dot_product_attention_decode`
    
    ### Checklist
    - [X] New/Existing tests provide coverage for changes
    
    @svuckovicTT For emitc, an empty `std::vector` needs to be passed as one
    of the arguments. I have not yet figured out how to do this. This should
    be solved before merge.

--- runtime/lib/ttnn/program_executor.cpp
+++ runtime/lib/ttnn/program_executor.cpp
@@ -71,6 +71,7 @@
 #include "operations/transformer/nlp_concat_heads.h"
 #include "operations/transformer/nlp_concat_heads_decode.h"
 #include "operations/transformer/rotary_embedding_llama.h"
+#include "operations/transformer/scaled_dot_product_attention_decode.h"
 #include "tt/runtime/debug.h"
 #include "tt/runtime/detail/ttnn/types/types.h"
 #include "tt/runtime/perf.h"
@@ -388,6 +389,10 @@ void ProgramExecutor::runOperation(const ::tt::target::ttnn::Operation *op) {
   }
   case ::tt::target::ttnn::OpType::GenericOp: {
     return operations::generic_op::run(op->type_as_GenericOp(), getContext());
+  }
+  case ::tt::target::ttnn::OpType::ScaledDotProductAttentionDecodeOp: {
+    return operations::transformer::run(
+        op->type_as_ScaledDotProductAttentionDecodeOp(), getContext());
   }
   case ::tt::target::ttnn::OpType::NONE: {
     LOG_FATAL("Unsupported operation type: ",
