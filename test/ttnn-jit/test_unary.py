eb580d8ac5cf9f952c35eb4761a7d5f52a422ec9
Vincent Tang <vtang@tenstorrent.com>
2025-09-29 20:45:41 +0000

    sweep shapes for unary + some test cleanup

--- test/ttnn-jit/test_unary.py
+++ test/ttnn-jit/test_unary.py
@@ -11,6 +11,69 @@ from math import *
 from utils import _get_ttnn_op
 
 
+def run_unary_op(device, h, w, max_grid, dtype, op):
+    # Note: both torch.float16 and torch.bfloat16 will convert to ttnn.bfloat16
+    torch_input_tensor = torch.randn((h, w), dtype=dtype)
+    golden_op = _get_ttnn_op(op)
+
+    start_coord = ttnn.CoreCoord(0, 0)
+    end_coord = ttnn.CoreCoord(max_grid[0], max_grid[1])
+    core_range = ttnn.CoreRange(start_coord, end_coord)
+    core_range_set = ttnn.CoreRangeSet([core_range])
+
+    shard_shape_x = h if max_grid[0] == 0 else h // (max_grid[0] + 1)
+    shard_shape_y = w if max_grid[1] == 0 else w // (max_grid[1] + 1)
+
+    shard_spec = ttnn.ShardSpec(
+        grid=core_range_set,
+        shard_shape=[
+            shard_shape_x,
+            shard_shape_y,
+        ],
+        shard_orientation=ttnn.ShardOrientation.ROW_MAJOR,
+        shard_mode=ttnn.ShardMode.PHYSICAL,
+    )
+
+    memory_config = ttnn.MemoryConfig(
+        memory_layout=ttnn.TensorMemoryLayout.BLOCK_SHARDED,
+        buffer_type=ttnn.BufferType.L1,
+        shard_spec=shard_spec,
+    )
+
+    print("torch_input_tensor: ", torch_input_tensor)
+    input_tensor = ttnn.from_torch(
+        torch_input_tensor,
+        layout=ttnn.TILE_LAYOUT,
+        device=device,
+        memory_config=memory_config,
+    )
+    print("input_ttnn_tensor: ", input_tensor)
+    op_jit = ttnn_jit.jit(backend="ttnn", debug=True, max_grid=max_grid)(op)
+    output_tensor = op_jit(input_tensor)
+    print("input tensor: ", input_tensor)
+    golden_tensor = golden_op(input_tensor)
+
+    print("--------------------------------")
+    print("input_tensor")
+    print(input_tensor)
+    print("--------------------------------")
+    print("output_tensor")
+    print(output_tensor)
+    print("--------------------------------")
+    print("golden_tensor")
+    print(golden_tensor)
+    print("--------------------------------")
+
+    all_close = torch.allclose(
+        output_tensor.cpu().to_torch(),
+        golden_tensor.cpu().to_torch(),
+        atol=1e-1,
+        rtol=1e-1,
+    )
+    print("all_close", all_close)
+    assert all_close
+
+
 def abs(input_tensor):
     return ttnn.abs(input_tensor)
 
@@ -31,7 +94,6 @@ def sin(input_tensor):
     return ttnn.sin(input_tensor)
 
 
-# always fails allclose
 def tan(input_tensor):
     return ttnn.tan(input_tensor)
 
@@ -117,70 +179,22 @@ def rsqrt(input_tensor):
         reciprocal,
         rsqrt,
         # cbrt, sign, erf, erfc, bitwise_not # <- not supported in TTIRToD2M
-        # sqrt # <- always fails allclose
+        # tan, sqrt # <- always fails allclose
     ],
 )
 def test_unary_op(device, h, w, max_grid, dtype, op):
-    print("dtype: ", dtype)
     if op in [log, ceil, floor, rsqrt] and dtype == torch.float32:
         pytest.xfail("failing allclose for some shapes for float32")
+    if op == abs and max_grid == (0, 0) and dtype == torch.bfloat16:
+        pytest.skip("already tested in test_unary_op_single_core_sweep")
 
-    # Note: both torch.float16 and torch.bfloat16 will convert to ttnn.bfloat16
-    torch_input_tensor = torch.randn((h, w), dtype=dtype)
-    print("torch_input_tensor dtype: ", torch_input_tensor.dtype)
-    golden_op = _get_ttnn_op(op)
+    run_unary_op(device, h, w, max_grid, dtype, op)
 
-    start_coord = ttnn.CoreCoord(0, 0)
-    end_coord = ttnn.CoreCoord(max_grid[0], max_grid[1])
-    core_range = ttnn.CoreRange(start_coord, end_coord)
-    core_range_set = ttnn.CoreRangeSet([core_range])
 
-    shard_shape_x = h if max_grid[0] == 0 else h // (max_grid[0] + 1)
-    shard_shape_y = w if max_grid[1] == 0 else w // (max_grid[1] + 1)
-
-    shard_spec = ttnn.ShardSpec(
-        grid=core_range_set,
-        shard_shape=[
-            shard_shape_x,
-            shard_shape_y,
-        ],
-        shard_orientation=ttnn.ShardOrientation.ROW_MAJOR,
-        shard_mode=ttnn.ShardMode.PHYSICAL,
-    )
-
-    memory_config = ttnn.MemoryConfig(
-        memory_layout=ttnn.TensorMemoryLayout.BLOCK_SHARDED,
-        buffer_type=ttnn.BufferType.L1,
-        shard_spec=shard_spec,
-    )
-
-    input_tensor = ttnn.from_torch(
-        torch_input_tensor,
-        layout=ttnn.TILE_LAYOUT,
-        device=device,
-        memory_config=memory_config,
-    )
-    print("input_tensor dtype: ", input_tensor.dtype)
-    op_jit = ttnn_jit.jit(backend="ttnn", debug=True, max_grid=max_grid)(op)
-    output_tensor = op_jit(input_tensor)
-    golden_tensor = golden_op(input_tensor)
-
-    print("--------------------------------")
-    print("input_tensor")
-    print(input_tensor)
-    print("--------------------------------")
-    print("output_tensor")
-    print(output_tensor)
-    print("--------------------------------")
-    print("golden_tensor")
-    print(golden_tensor)
-    print("--------------------------------")
-
-    all_close = torch.allclose(
-        output_tensor.cpu().to_torch(),
-        golden_tensor.cpu().to_torch(),
-        atol=1e-1,
-        rtol=1e-1,
-    )
-    print("all_close", all_close)
-    assert all_close
+# Note: anything bigger than 256x256 will fail allclose for bfloat16
+# looks like data corruption in the runtime..?
+@pytest.mark.parametrize(
+    "h, w", [(h, w) for h in range(32, 256, 32) for w in range(32, 256, 32)]
+)
+def test_unary_op_single_core_sweep(device, h, w):
+    run_unary_op(device, h, w, (0, 0), torch.bfloat16, abs)
