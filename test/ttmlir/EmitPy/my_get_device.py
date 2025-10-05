adceccd38ad60e0352d24d367d19cccdd8636569
svuckovic <svuckovic@tenstorrent.com>
2025-10-05 14:26:28 +0000

    wip

--- test/ttmlir/EmitPy/my_get_device.py
+++ test/ttmlir/EmitPy/my_get_device.py
@@ -19,3 +19,43 @@ class DeviceGetter:
                 device_id=0, l1_small_size=cls.l1_small_size
             )
         return cls._instance
+
+
+def mesh_shard(
+    input: ttnn.Tensor,
+    mesh_device,
+    shard_direction,
+    shard_type,
+    shard_shape,
+    shard_dims,
+):
+    # TODO: Convert all strings to enums
+
+    if shard_type == "Identity":
+        return
+
+    if shard_direction == "FullToShard":
+        sharded_input = ttnn.distribute_tensor(
+            input,
+            ttnn.create_mesh_mapper(
+                mesh_device,
+                ttnn.MeshMapperConfig(
+                    [ttnn.PlacementReplicate(), ttnn.PlacementShard(1)],
+                    ttnn.MeshShape(1, 8),
+                ),
+            ),
+        )
+    elif shard_direction == "ShardToFull":
+        output = ttnn.aggregate_tensor(
+            sharded_input,
+            ttnn.create_mesh_composer(
+                mesh_device, ttnn.MeshComposerConfig([-1, 1], ttnn.MeshShape(1, 8))
+            ),
+        )
+    else:
+        raise Exception(f"Unexpected shard_direction: {shard_direction}")
+
+    return
+
+
+ttnn.mesh_shard = mesh_shard
