5123fac10f90045dcbd72516de4b3a1d1357643d
jgrimTT <jgrim@tenstorrent.com>
2025-10-07 14:29:19 +0000

    Sprucing

--- test/python/golden/test_shardy_ops_n300.py
+++ test/python/golden/test_shardy_ops_n300.py
@@ -11,7 +11,7 @@ from collections import OrderedDict
 from builder.base.builder import Operand, Shape, TypeInfo
 from builder.stablehlo.stablehlo_builder import StableHLOBuilder
 from builder.base.builder_utils import compile_stablehlo_to_flatbuffer
-from test_utils import Marks, shape_str
+from test_utils import Marks, shape_str, sharding_str
 
 pytestmark = [pytest.mark.frontend("shlo"), pytest.mark.n300]
 
@@ -36,10 +36,9 @@ def sharding_constraint(
             ),
         ],
     )
-    sharding_attr = builder.create_sharding_attr_from_tuples(
-        "mesh", [("x", True), ("y", False)]
-    )
-    return builder.add(in0, in1, unit_attrs=unit_attrs, sharding_attr=sharding_attr)
+
+    builder.sharding_constraint(in0, tensor_sharding_attr=tensor_sharding_attr)
+    return builder.add(in0, in1, unit_attrs=unit_attrs)
 
 
 @pytest.mark.parametrize("shape", [(128, 128)], ids=shape_str)
@@ -60,6 +59,52 @@ def test_sharding_constraint(
 ):
     compile_stablehlo_to_flatbuffer(
         test_fn,
+        [shape, shape],
+        [dtype, dtype],
+        test_base=request.node.name,
+        output_root=request.config.getoption("--path"),
+        system_desc_path=request.config.getoption("--sys-desc"),
+        mesh_name="mesh",
+        mesh_dict=OrderedDict([("x", mesh_shape[0]), ("y", mesh_shape[1])]),
+    )
+
+
+@pytest.mark.parametrize("shape", [(2, 4, 8, 16)], ids=shape_str)
+@pytest.mark.parametrize("dtype", [torch.float32], ids=["f32"])
+@pytest.mark.parametrize("mesh_shape", [(1, 2)], ids=["1x2"])
+@pytest.mark.parametrize(
+    "sharding",
+    [
+        [("x", True), ("y", False), ("", False), ("", False)],
+        [("x", True), ("", False), ("y", False), ("", False)],
+        [("x", True), ("", False), ("", False), ("y", False)],
+        [("", False), ("x", True), ("", False), ("", False)],
+        [("", False), ("x", True), ("", False), ("y", False)],
+        [("", False), ("", False), ("x", True), ("y", False)],
+    ],
+    ids=sharding_str,
+)
+def test_sharding_annotation(
+    shape: Shape,
+    dtype: torch.dtype,
+    mesh_shape: Tuple[int, int],
+    sharding: List[Tuple[str, bool]],
+    request,
+):
+    def sharding_annotation(
+        in0: Operand,
+        in1: Operand,
+        builder: StableHLOBuilder,
+        unit_attrs: Optional[List[str]] = None,
+    ):
+        builder.set_graph_level_check(True)
+        sharding_attr = builder.create_sharding_attr_from_tuples("mesh", sharding)
+        return builder.add(in0, in1, unit_attrs=unit_attrs, sharding_attr=sharding_attr)
+
+    test_bundle = sharding_annotation_wrap_factory()
+
+    compile_stablehlo_to_flatbuffer(
+        test_bundle.test_fn,
         [shape, shape],
         [dtype, dtype],
         test_base=request.node.name,
