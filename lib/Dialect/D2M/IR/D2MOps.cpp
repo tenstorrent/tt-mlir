3f15b54c648316288b5668a463451d804d26c64c
Armin Ale <arminale@tenstorrent.com>
2025-10-07 19:42:52 +0000

    fix compile error and add tests

--- lib/Dialect/D2M/IR/D2MOps.cpp
+++ lib/Dialect/D2M/IR/D2MOps.cpp
@@ -1064,7 +1064,6 @@ static mlir::LogicalResult verifyAffineBlocking(
       Type operandType = operandTypes[arg.getArgNumber()];
       ArrayRef<int64_t> expectedShardShape;
       std::optional<Attribute> expectedMemorySpace;
-      bool isStream = false;
       if (RankedTensorType tensorType =
               mlir::dyn_cast<RankedTensorType>(operandType)) {
         if (!tensorType.getEncoding()) {
@@ -1086,7 +1085,6 @@ static mlir::LogicalResult verifyAffineBlocking(
             mlir::cast<mlir::tt::ttcore::DeviceLayoutInterface>(
                 memref.getLayout());
         expectedShardShape = layout.getShardShape(memref);
-        isStream = mlir::isa<ttcore::ViewLayoutAttr>(memref.getLayout());
       }
 
       if (auto blockMemref = mlir::dyn_cast<MemRefType>(blockArgType)) {
