8d5156f709fc9665c434b43924adc7122147069f
Nicholas Smith <nsmith@tenstorrent.com>
2025-10-10 12:55:37 -0500

    more skips

--- test/python/golden/test_stablehlo_ops.py
+++ test/python/golden/test_stablehlo_ops.py
@@ -160,6 +160,7 @@ def test_binary_ops(
     )
 
 
+@pytest.mark.skip("https://github.com/tenstorrent/tt-mlir/issues/5317")
 @pytest.mark.parametrize("shape", [(128, 128)], ids=shape_str)
 @pytest.mark.parametrize("dtype", [torch.float32], ids=["f32"])
 @pytest.mark.parametrize("target", ["ttnn"])
