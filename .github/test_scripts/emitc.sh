1b675dd287a9a07704cc135de59442ba5c2b0e5d
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-10-13 08:10:17 +0200

    more reports

--- .github/test_scripts/emitc.sh
+++ .github/test_scripts/emitc.sh
@@ -8,4 +8,4 @@ set -e -o pipefail
 llvm-lit -sv --xunit-xml-output $TEST_REPORT_PATH $BUILD_DIR/test/ttmlir/EmitC/TTNN
 ttrt run --emitc $BUILD_DIR/test/ttmlir/EmitC
 cp run_results.json ${TTRT_REPORT_PATH} || true
-cp ttrt_report.xml ${TEST_REPORT_PATH} || true
+cp ttrt_report.xml ${TEST_REPORT_PATH%_*}_emitc_${TEST_REPORT_PATH##*_} || true
