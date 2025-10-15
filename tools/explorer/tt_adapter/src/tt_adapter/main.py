230afcd63c177eba27ae1b38083666074eca2cbe
Marco Campos <mcampos@tenstorrent.com>
2025-10-15 20:42:17 +0000

    feat: send collection of graphs instead of single graph

--- tools/explorer/tt_adapter/src/tt_adapter/main.py
+++ tools/explorer/tt_adapter/src/tt_adapter/main.py
@@ -157,7 +157,7 @@ class TTAdapter(model_explorer.Adapter):
         ir_dumps_dir = 'ir_dumps'
 
         if not os.path.exists(ir_dumps_dir) or not os.path.isdir(ir_dumps_dir):
-            return utils.to_adapter_format({"graphPaths": []})
+            return utils.to_adapter_collection_format({"graphPaths": []}, label="Preload graphs")
 
         graph_paths = []
         for file in os.listdir(ir_dumps_dir):
@@ -166,7 +166,7 @@ class TTAdapter(model_explorer.Adapter):
             if file.endswith('.mlir') and os.path.isfile(file_path) and os.access(file_path, os.R_OK):
                 graph_paths.append(os.path.abspath(file_path))
 
-        return utils.to_adapter_format({"graphPaths": graph_paths})
+        return utils.to_adapter_collection_format({"graphPaths": graph_paths}, label="Preload graphs")
 
     def convert(
         self, model_path: str, settings: Dict
