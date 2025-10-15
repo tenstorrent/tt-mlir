a06d92251c2eaf349d3b34bb6289549f4e6c03f8
Jacob DeSousa <jdesousa@tenstorrent.com>
2025-10-15 22:25:01 +0000

    remove fails_golden on reduction tests and constrain inputs to TF32 friendly vals

--- tools/builder/base/builder_utils.py
+++ tools/builder/base/builder_utils.py
@@ -1841,6 +1841,8 @@ def execute_fb(
             _, _, cal_pcc, _ = get_atol_rtol_pcc(
                 golden_tensor_torch,
                 output_tensor_torch,
+                atol,
+                rtol,
                 logging,
             )
             pcc_fail = cal_pcc < pcc
