44dead2ec8d9fc59a76a7032de32cd046b97491f
Vincent Tang <vtang@tenstorrent.com>
2025-10-16 00:45:41 +0000

    jit cache unit tests

--- .github/test_scripts/pykernel.sh
+++ .github/test_scripts/pykernel.sh
@@ -13,5 +13,5 @@ ln -sf $INSTALL_DIR/tt-metal third_party/tt-metal/src/tt-metal
 
 echo "Running PyKernel tests..."
 pytest -v $WORK_DIR/test/pykernel/demo/test.py --junit-xml=$TEST_REPORT_PATH
-pytest -v $WORK_DIR/test/ttnn-jit/test_eltwise.py --junit-xml=${TEST_REPORT_PATH%_*}_ttnn_jit_${TEST_REPORT_PATH##*_}
+pytest -v $WORK_DIR/test/ttnn-jit/ --junit-xml=${TEST_REPORT_PATH%_*}_ttnn_jit_${TEST_REPORT_PATH##*_}
 rm -rf third_party/tt-metal
--- test/ttnn-jit/utils.py
+++ test/ttnn-jit/utils.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 import ttnn
+import torch
 from typing import Callable, Optional, Dict, Iterable
 
 
@@ -22,3 +23,50 @@ def _build_golden_map(ops: Iterable[Callable]) -> Dict[Callable, Callable]:
         if ttnn_op is not None:
             result[op] = ttnn_op
     return result
+
+
+def _create_sharded_tile_tensor(device, h, w, max_grid, dtype):
+    torch.manual_seed(0)
+    torch_tensor = torch.randn((h, w), dtype=dtype)
+
+    start_coord = ttnn.CoreCoord(0, 0)
+    end_coord = ttnn.CoreCoord(max_grid[0], max_grid[1])
+    core_range = ttnn.CoreRange(start_coord, end_coord)
+    core_range_set = ttnn.CoreRangeSet([core_range])
+
+    shard_shape_x = h if max_grid[0] == 0 else h // (max_grid[0] + 1)
+    shard_shape_y = w if max_grid[1] == 0 else w // (max_grid[1] + 1)
+
+    shard_spec = ttnn.ShardSpec(
+        grid=core_range_set,
+        shard_shape=[shard_shape_x, shard_shape_y],
+        shard_orientation=ttnn.ShardOrientation.ROW_MAJOR,
+    )
+
+    memory_config = ttnn.MemoryConfig(
+        memory_layout=ttnn.TensorMemoryLayout.BLOCK_SHARDED,
+        buffer_type=ttnn.BufferType.L1,
+        shard_spec=shard_spec,
+    )
+
+    return ttnn.from_torch(
+        torch_tensor,
+        layout=ttnn.TILE_LAYOUT,
+        device=device,
+        memory_config=memory_config,
+    )
+
+
+def _create_dram_tensor(device, h, w, dtype):
+    torch.manual_seed(0)
+    torch_tensor = torch.randn((h, w), dtype=dtype)
+    memory_config = ttnn.MemoryConfig(
+        memory_layout=ttnn.TensorMemoryLayout.INTERLEAVED,
+        buffer_type=ttnn.BufferType.DRAM,
+    )
+    return ttnn.from_torch(
+        torch_tensor,
+        layout=ttnn.TILE_LAYOUT,
+        device=device,
+        memory_config=memory_config,
+    )
