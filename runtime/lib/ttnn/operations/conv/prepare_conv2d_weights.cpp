9637fe5466a661b2c261d337ea30d87f9824f1f3
Jovan Serbedzija <jserbedzija@tenstorrent.com>
2025-10-17 19:22:56 +0200

    [TTNN] Add DeviceComputeKernelConfig attribute to Conv ops (#5339)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-mlir/issues/3994)
    
    ### What's changed
    Added support for specifying `DeviceComputeKernelConfig` attribute to
    `PrepareConv2dWeightsOp`, `PrepareConv2dBiasOp`, and
    `ConvTranspose2dOp`.
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes

--- runtime/lib/ttnn/operations/conv/prepare_conv2d_weights.cpp
+++ runtime/lib/ttnn/operations/conv/prepare_conv2d_weights.cpp
@@ -60,6 +60,12 @@ void run(const ::tt::target::ttnn::PrepareConv2dWeightsOp *op,
     conv2dConfig = utils::createConv2dConfig(op->conv2d_config());
   }
 
+  std::optional<::ttnn::DeviceComputeKernelConfig> computeConfig;
+  if (op->compute_config()) {
+    computeConfig =
+        utils::createDeviceComputeKernelConfig(op->compute_config());
+  }
+
   ::ttnn::MeshDevice &targetDevice = context.getMeshDevice();
 
   std::optional<::ttnn::operations::conv::conv2d::Conv2dSliceConfig>
@@ -74,7 +80,7 @@ void run(const ::tt::target::ttnn::PrepareConv2dWeightsOp *op,
       op->weights_format()->str(), op->in_channels(), op->out_channels(),
       op->batch_size(), op->input_height(), op->input_width(), kernelSize,
       stride, padding, dilation, op->has_bias(), op->groups(), &targetDevice,
-      inputDtype, outputDtype, conv2dConfig, std::nullopt, sliceConfig);
+      inputDtype, outputDtype, conv2dConfig, computeConfig, sliceConfig);
 
   tensorPool.insertTTNNTensorAndValidate(op->out(), out);
 }
