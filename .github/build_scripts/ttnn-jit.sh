0583f3948548d603f6f99f3ad29850b65e6a4bed
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-10-17 17:14:21 +0200

    fixes to the merge

--- .github/build_scripts/ttnn-jit.sh
+++ .github/build_scripts/ttnn-jit.sh
@@ -7,7 +7,7 @@ set -e -o pipefail
 cd $WORK_DIR/tools/ttnn-jit
 mkdir -p dist
 pip install build setuptools wheel
-python3 -m build --wheel -q
+python3 -m build --wheel
 
 # upload artifact
 echo "{\"name\":\"ttnn-jit-whl-$BUILD_NAME\",\"path\":\"$WORK_DIR/tools/ttnn-jit/dist/ttnn_jit*.whl\"}," >> $UPLOAD_LIST
