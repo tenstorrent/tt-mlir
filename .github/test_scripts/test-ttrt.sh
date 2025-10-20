972a2ea16dbb1ed31d96b1a4082f1862c6962ec8
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-10-20 15:21:21 +0000

    fix failing tests

--- .github/test_scripts/test-ttrt.sh
+++ .github/test_scripts/test-ttrt.sh
@@ -5,9 +5,6 @@
 
 set -e -o pipefail
 
-# mv ttrt-artifacts ttrt-artifacts_backup
 pytest -ssv "test/python/op_by_op" --junit-xml=$TEST_REPORT_PATH
-# rm -rf ttrt-artifacts
-# mv ttrt-artifacts_backup ttrt-artifacts
 # restore system desc as ttrt test clears it
 ttrt query --save-artifacts
