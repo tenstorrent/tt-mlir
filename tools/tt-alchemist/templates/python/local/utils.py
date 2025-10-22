acf54dd268ad4fc2a1dc1b3cad4ca2b03f24ad73
Sasa Vuckovic <svuckovic@tenstorrent.com>
2025-10-22 15:20:38 +0200

    [EmitPy] Add CCL support (#5387)
    ### Ticket
    Closes https://github.com/tenstorrent/tt-mlir/issues/5356
    
    ### Problem description
    No CCL op support in EmitPy path.
    
    ### What's changed
    Added CCL op support.
    
    - Added CCL dialect conversions from TTNN to EmitPy - thanks @hkwonTT!
    - Added `ttnn_supplemental.py` which implements CCL ops in Python
    - Monkey-patched `ttnn_supplemental` into `ttnn` module in `utils.py`,
    so that `main.py` remains clean
    - Removed `my_get_device.py` and moved device logic to `utils.py`
    - Unlinked `test/ttmlir/EmitPy/utils.py` as the symlink was hardcoded
    
    No tests added (op-level tests being added in ttir-builder atm).
    Manually tested these implementations on 1x2/1x4/1x8 configs for mnist
    and alexnet models.
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Hongseok Kwon <hkwon@tenstorrent.com>

--- tools/tt-alchemist/templates/python/local/utils.py
+++ tools/tt-alchemist/templates/python/local/utils.py
@@ -2,6 +2,35 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 import ttnn
+import ttnn_supplemental
+
+
+# Monkey-patch ttnn with ttnn_supplemental objects
+ttnn.MeshShardDirection = ttnn_supplemental.MeshShardDirection
+ttnn.MeshShardType = ttnn_supplemental.MeshShardType
+ttnn.mesh_shard = ttnn_supplemental.mesh_shard
+ttnn.all_gather = ttnn_supplemental.all_gather
+ttnn.reduce_scatter = ttnn_supplemental.reduce_scatter
+ttnn.collective_permute = ttnn_supplemental.collective_permute
+ttnn.point_to_point = ttnn_supplemental.point_to_point
+
+
+class DeviceGetter:
+    _instance = None
+    l1_small_size = 1 << 15
+
+    def __init__(self):
+        raise RuntimeError("This is Singleton, invoke get_device() instead.")
+
+    @classmethod
+    def get_device(cls):
+        if cls._instance == None:
+            cls._instance = ttnn.open_mesh_device(
+                mesh_shape=ttnn.MeshShape(1, 1),
+                l1_small_size=cls.l1_small_size,
+            )
+            print(f"Device: {cls._instance}")
+        return cls._instance
 
 
 def get_scalar_from_tensor(tensor: ttnn.Tensor) -> int:
