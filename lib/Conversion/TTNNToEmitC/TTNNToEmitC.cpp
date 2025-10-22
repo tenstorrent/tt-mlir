1db5168c4a0586affe1a967f2220484caae27722
Defne Dilbaz <ddilbaz@tenstorrent.com>
2025-10-22 12:31:49 -0400

    Uplift ttnn scatter op and add coverage (#4913)
    ### Ticket
    #4531 
    #4912 
    
    ### Problem description
    Current stablehlo scatter op is not lowered into ttnn scatter op as this
    was not implemented in tt-metal. It is a binary op and it provides
    coverage for a subset of scatter operations.
    
    ### What's changed
    - Uplifted tt-metal scatter op implementation.
    - Removed binary op references for scatter op and lowered ttir scatter
    to ttnn scatter.
    - Wrote conversion pattern for scatter op which supports element-wise
    scatter and multi-dimensional scatter.
    - Added scatter decomposition workaround in ttnn due to the following
    restriction
    (third_party/tt-metal/src/tt-metal/ttnn/cpp/ttnn/operations/data_movement/scatter/scatter.cpp)
    ```
    index_shape[dim] < to_layout_int32_scatter_axis_max_length (256)
    ```
    - Added scatter workaround pass in ttnn due to the following restriction
    (third_party/tt-metal/src/tt-metal/ttnn/cpp/ttnn/operations/data_movement/scatter/scatter.cpp)
    ```
        TT_FATAL(
            !(input_dtype == DataType::FLOAT32 && input_layout == Layout::TILE),
            "Scatter doesn't work for fp32 tiled tensors yet (see to_layout issue #) - input tensor is {} {}.",
            input_dtype,
            input_layout);
        TT_FATAL(
            !(source_dtype == DataType::FLOAT32 && source_layout == Layout::TILE),
            "Scatter doesn't work for fp32 tiled tensors yet (see to_layout issue #) - source tensor is {} {}.",
            source_dtype,
            source_layout);
    ```
    
    
    Conversion logic:
    - scatter_dims_to_operand_dims: 
    - if len == 1 --> simple scatter operation. dim =
    scatter_dims_to_operand_dims[0]. We are expecting dim == 0.
    - if len > 1 --> multi-dimensional scatter operation. Need to flatten
    all tensors to 1D and use custom logic to calculate linear indices.
     - If simple scatter:
    - indices need to match updates shape --> reshape by adding 1d singleton
    dimensions at the beginning if needed
         - indices are broadcasted based on `update_window_dims` if needed
    - If multi-dimensional scatter
        - inputs and updates are flattened into 1d
        - 1d indices are created using strides
    
    
    ### Checklist
    - [X] New/Existing tests provide coverage for changes
    - [ ] TODO: Add scatter op constraints again since they are removed in
    this PR.
    - [ ] TODO: Add window-based scatter support.
    - [ ] TODO: Simplify multi-dimensional scatter to use multiple scatters
    instead of flattened scatter.

--- lib/Conversion/TTNNToEmitC/TTNNToEmitC.cpp
+++ lib/Conversion/TTNNToEmitC/TTNNToEmitC.cpp
@@ -2258,6 +2258,35 @@ public:
 };
 } // namespace
 
+namespace {
+class ScatterOpConversionPattern
+    : public TTNNToEmitCBaseOpConversionPattern<mlir::tt::ttnn::ScatterOp> {
+public:
+  using TTNNToEmitCBaseOpConversionPattern<
+      mlir::tt::ttnn::ScatterOp>::TTNNToEmitCBaseOpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(mlir::tt::ttnn::ScatterOp srcOp,
+                  mlir::tt::ttnn::ScatterOp::Adaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    ttnn_to_emitc::EmitCTTNNEmitter<mlir::tt::ttnn::ScatterOp> emitter(
+        srcOp, adaptor, rewriter);
+    llvm::SmallVector<mlir::Attribute> args{
+        emitter.emit(srcOp.getInput()),
+        emitter.emit(srcOp.getDim()),
+        emitter.emit(srcOp.getIndex()),
+        emitter.emit(srcOp.getSource()),
+        emitter.emit(std::nullopt) |
+            emitter.getMemoryConfig(srcOp.getResult()), // mem config
+        emitter.emit(std::nullopt)                      // opt_reduction
+    };
+
+    emitter.replaceOp(*this, args);
+    return success();
+  }
+};
+} // namespace
+
 // CollectivePermuteOp conversion pattern
 //
 namespace {
@@ -3646,7 +3675,6 @@ void populateTTNNToEmitCPatterns(mlir::MLIRContext *ctx,
       EltwiseBinaryNGCompositeOpConversionPattern<mlir::tt::ttnn::MaximumOp>,
       EltwiseBinaryNGCompositeOpConversionPattern<mlir::tt::ttnn::MinimumOp>,
       EltwiseBinaryOpConversionPattern<mlir::tt::ttnn::DivideOp>,
-      EltwiseBinaryCompositeOpConversionPattern<mlir::tt::ttnn::ScatterOp>,
       EltwiseBinaryCompositeOpConversionPattern<
           mlir::tt::ttnn::LogicalLeftShiftOp>,
       EltwiseBinaryCompositeOpConversionPattern<mlir::tt::ttnn::RemainderOp>,
@@ -3716,6 +3744,7 @@ void populateTTNNToEmitCPatterns(mlir::MLIRContext *ctx,
   //
   patterns.add<AllGatherOpConversionPattern>(typeConverter, ctx);
   patterns.add<ReduceScatterOpConversionPattern>(typeConverter, ctx);
+  patterns.add<ScatterOpConversionPattern>(typeConverter, ctx);
   patterns.add<CollectivePermuteOpConversionPattern>(typeConverter, ctx);
   patterns.add<MeshShardOpConversionPattern>(typeConverter, ctx);
   patterns.add<PointToPointOpConversionPattern>(typeConverter, ctx);
