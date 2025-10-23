8d1f89f8e0500f3966105fe3356997d2dc863b79
Collin Tod <collintod@tenstorrent.com>
2025-10-23 11:10:58 -0500

    Exclude `to_layout` ops in `perf` reports (#4759)
    ### Ticket
    Closes #4558 
    
    ### Problem description
    At runtime, `to_layout` ops are prepended to input operands to ensure
    they are in the correct format. We don't wish to include those in perf
    reports, and wish to be able to track them
    
    ### What's changed
    `to_layout` ops inserted at runtime are now tagged and optionally
    excluded from the csv depending on what is passed into `--filter`
    
    Also, incorporated the small change from #4570

--- runtime/python/runtime/runtime.cpp
+++ runtime/python/runtime/runtime.cpp
@@ -578,6 +578,8 @@ void registerRuntimeBindings(nb::module_ &m) {
       .def_static("get", &tt::runtime::perf::Env::get, nb::rv_policy::reference)
       .def("set_program_metadata", &tt::runtime::perf::Env::setProgramMetadata)
       .def("tracy_log_op_location", &tt::runtime::perf::Env::tracyLogOpLocation)
+      .def("tracy_log_input_layout_conversion",
+           &tt::runtime::perf::Env::tracyLogInputLayoutConversion)
       .def("tracy_log_const_eval_program",
            &tt::runtime::perf::Env::tracyLogConstEvalProgram)
       .def("tracy_log_program_metadata",
