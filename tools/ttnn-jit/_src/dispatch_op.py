b4199ae792df500f29aa0c212a2a7dbc68192d0e
Vincent Tang <vtang@tenstorrent.com>
2025-10-24 19:17:31 +0000

    try import

--- tools/ttnn-jit/_src/dispatch_op.py
+++ tools/ttnn-jit/_src/dispatch_op.py
@@ -2,16 +2,27 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from ttnn_jit.runtime._ttmlir_runtime.runtime import (
+# from _ttmlir_runtime.runtime import (
+#     submit,
+#     set_compatible_device_runtime,
+#     get_current_device_runtime,
+# )
+# from _ttmlir_runtime.binary import (
+#     load_binary_from_path,
+#     load_binary_from_capsule,
+# )
+# from _ttmlir_runtime.utils import (
+#     create_runtime_device_from_ttnn,
+#     create_runtime_tensor_from_ttnn,
+#     get_ttnn_tensor_from_runtime_tensor,
+# )
+
+from . import (
     submit,
     set_compatible_device_runtime,
     get_current_device_runtime,
-)
-from ttnn_jit.runtime._ttmlir_runtime.binary import (
     load_binary_from_path,
     load_binary_from_capsule,
-)
-from ttnn_jit.runtime._ttmlir_runtime.utils import (
     create_runtime_device_from_ttnn,
     create_runtime_tensor_from_ttnn,
     get_ttnn_tensor_from_runtime_tensor,
