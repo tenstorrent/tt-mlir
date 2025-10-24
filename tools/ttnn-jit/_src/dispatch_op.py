e88bb14fa896be9b87386fcd850e48e6da9518d2
Vincent Tang <157417475+vtangTT@users.noreply.github.com>
2025-10-24 17:39:56 -0400

    [ttnn.jit] fix runtime import path (#5501)
    ### Ticket
    https://github.com/tenstorrent/tt-mlir/issues/5236
    
    ### What's changed
    - try import from wheel, if not import from local

--- tools/ttnn-jit/_src/dispatch_op.py
+++ tools/ttnn-jit/_src/dispatch_op.py
@@ -2,16 +2,12 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from ttnn_jit.runtime._ttmlir_runtime.runtime import (
+from ttnn_jit._src import (
     submit,
     set_compatible_device_runtime,
     get_current_device_runtime,
-)
-from ttnn_jit.runtime._ttmlir_runtime.binary import (
     load_binary_from_path,
     load_binary_from_capsule,
-)
-from ttnn_jit.runtime._ttmlir_runtime.utils import (
     create_runtime_device_from_ttnn,
     create_runtime_tensor_from_ttnn,
     get_ttnn_tensor_from_runtime_tensor,
