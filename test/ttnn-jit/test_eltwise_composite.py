c54dd04b05bd090e3d9a41c3d7047c8379eb4000
Brandon Rapanan <brapanan@tenstorrent.com>
2025-10-24 16:52:45 +0000

    Rebase + added memory_configs_equal assert to composite tests.

--- test/ttnn-jit/test_eltwise_composite.py
+++ test/ttnn-jit/test_eltwise_composite.py
@@ -9,9 +9,10 @@ import pytest
 
 from utils import (
     _get_ttnn_op,
-    _all_close_check,
-    _create_dram_tensor,
-    _create_sharded_tile_tensor,
+    all_close_check,
+    memory_configs_equal,
+    create_dram_tensor,
+    create_sharded_tile_tensor,
 )
 
 COMMON_SHAPE_GRID_PARAMS = [
@@ -34,11 +35,11 @@ def run_op_test(
 ):
     if buffer_type == ttnn.BufferType.L1:
         inputs = [
-            _create_sharded_tile_tensor(device, h, w, max_grid, dtype)
+            create_sharded_tile_tensor(device, h, w, max_grid, dtype)
             for _ in range(num_inputs)
         ]
     else:
-        inputs = [_create_dram_tensor(device, h, w, dtype) for _ in range(num_inputs)]
+        inputs = [create_dram_tensor(device, h, w, dtype) for _ in range(num_inputs)]
     print("inputs", inputs)
     golden_op = _get_ttnn_op(op)
 
@@ -46,7 +47,10 @@ def run_op_test(
     output_tensor = op_jit(*inputs)
     golden_tensor = (golden_op or op)(*inputs)
 
-    _all_close_check(output_tensor, golden_tensor)
+    assert memory_configs_equal(
+        output_tensor.memory_config(), golden_tensor.memory_config()
+    )
+    assert all_close_check(output_tensor, golden_tensor)
 
 
 # ------------------------------------------------------------
@@ -88,56 +92,6 @@ def test_composite_ops(device, h, w, max_grid, dtype, op):
     )
 
 
-# should be covered by test_composite_ops
-""" @pytest.mark.parametrize("h, w, max_grid", COMMON_SHAPE_GRID_PARAMS)
-@pytest.mark.parametrize("dtype", [torch.float32, torch.bfloat16])
-def test_muladd_jit_l1(device, h, w, max_grid, dtype):
-    A = _create_sharded_tile_tensor(device, h, w, max_grid, dtype)
-    B = _create_sharded_tile_tensor(device, h, w, max_grid, dtype)
-    C = _create_sharded_tile_tensor(device, h, w, max_grid, dtype)
-
-    # JIT path
-    op_jit = ttnn_jit.jit(backend="ttnn", debug=True, max_grid=max_grid)(mul_add)
-    interop_result = op_jit(A, B, C)
-
-    # Golden path
-    golden_result = mul_add(A, B, C)
-
-    _all_close_check(interop_result, golden_result)
-
-
-@pytest.mark.parametrize(
-    "h, w",
-    [
-        (32, 32),
-        (32, 64),
-        (64, 64),
-        (64, 128),
-        (128, 128),
-        (256, 256),
-        (256, 512),
-    ],
-)
-@pytest.mark.parametrize("dtype", [torch.float32, torch.bfloat16])
-def test_muladd_jit_dram(device, h, w, dtype):
-    if (h, w) == (256, 512):
-        pytest.xfail("OOM error.")
-    max_grid = (0, 0)
-
-    A = _create_dram_tensor(device, h, w, dtype)
-    B = _create_dram_tensor(device, h, w, dtype)
-    C = _create_dram_tensor(device, h, w, dtype)
-
-    # JIT path
-    op_jit = ttnn_jit.jit(backend="ttnn", debug=True, max_grid=max_grid)(mul_add)
-    interop_result = op_jit(A, B, C)
-
-    # Golden path
-    golden_result = mul_add(A, B, C)
-
-    _all_close_check(interop_result, golden_result) """
-
-
 passing_configs_l1 = {
     torch.bfloat16: [
         (512, 2048),
@@ -161,9 +115,9 @@ def test_large_muladd_nice_seq_len_jit_l1(device, seq_len, hidden_dim, dtype):
 
     max_grid = (7, 7)
 
-    A = _create_sharded_tile_tensor(device, seq_len, hidden_dim, max_grid, dtype)
-    B = _create_sharded_tile_tensor(device, seq_len, hidden_dim, max_grid, dtype)
-    C = _create_sharded_tile_tensor(device, seq_len, hidden_dim, max_grid, dtype)
+    A = create_sharded_tile_tensor(device, seq_len, hidden_dim, max_grid, dtype)
+    B = create_sharded_tile_tensor(device, seq_len, hidden_dim, max_grid, dtype)
+    C = create_sharded_tile_tensor(device, seq_len, hidden_dim, max_grid, dtype)
 
     # JIT path
     op_jit = ttnn_jit.jit(backend="ttnn", debug=True, max_grid=max_grid)(mul_add)
@@ -172,7 +126,10 @@ def test_large_muladd_nice_seq_len_jit_l1(device, seq_len, hidden_dim, dtype):
     # Golden path
     golden_result = mul_add(A, B, C)
 
-    _all_close_check(interop_result, golden_result, atol=1e-1, rtol=1e-1)
+    assert memory_configs_equal(
+        interop_result.memory_config(), golden_result.memory_config()
+    )
+    assert all_close_check(interop_result, golden_result, atol=1e-1, rtol=1e-1)
 
 
 @pytest.mark.parametrize("seq_len", [2048, 4096, 8192, 16384, 32768, 65536])
@@ -182,9 +139,9 @@ def test_large_muladd_nice_seq_len_jit_dram(device, seq_len, hidden_dim, dtype):
     pytest.xfail("All large DRAM test configurations are failing.")
     max_grid = (0, 0)
 
-    A = _create_dram_tensor(device, seq_len, hidden_dim, dtype)
-    B = _create_dram_tensor(device, seq_len, hidden_dim, dtype)
-    C = _create_dram_tensor(device, seq_len, hidden_dim, dtype)
+    A = create_dram_tensor(device, seq_len, hidden_dim, dtype)
+    B = create_dram_tensor(device, seq_len, hidden_dim, dtype)
+    C = create_dram_tensor(device, seq_len, hidden_dim, dtype)
 
     # JIT path
     op_jit = ttnn_jit.jit(backend="ttnn", debug=True, max_grid=max_grid)(mul_add)
@@ -193,7 +150,10 @@ def test_large_muladd_nice_seq_len_jit_dram(device, seq_len, hidden_dim, dtype):
     # Golden path
     golden_result = mul_add(A, B, C)
 
-    _all_close_check(interop_result, golden_result, atol=1e-1, rtol=1e-1)
+    assert memory_configs_equal(
+        interop_result.memory_config(), golden_result.memory_config()
+    )
+    assert all_close_check(interop_result, golden_result, atol=1e-1, rtol=1e-1)
 
 
 @pytest.mark.parametrize("h, w, max_grid", COMMON_SHAPE_GRID_PARAMS)
@@ -206,10 +166,10 @@ def test_muladd_broadcast_jit_l1(device, h, w, max_grid, dtype):
         "Broadcasting requires either h or w to be 1, but sharded tensor must be at least 32 x 32. Assert error."
     )
 
-    A = _create_sharded_tile_tensor(device, h, w, max_grid, dtype)
-    B = _create_sharded_tile_tensor(device, h, w, max_grid, dtype)
+    A = create_sharded_tile_tensor(device, h, w, max_grid, dtype)
+    B = create_sharded_tile_tensor(device, h, w, max_grid, dtype)
     # broadcast C
-    C = _create_sharded_tile_tensor(device, 1, w, max_grid, dtype)
+    C = create_sharded_tile_tensor(device, 1, w, max_grid, dtype)
 
     print("A:", A.cpu().to_torch())
     print("A shape:", A.shape)
@@ -225,7 +185,10 @@ def test_muladd_broadcast_jit_l1(device, h, w, max_grid, dtype):
     # Golden path
     golden_result = mul_add(A, B, C)
 
-    _all_close_check(interop_result, golden_result)
+    assert memory_configs_equal(
+        interop_result.memory_config(), golden_result.memory_config()
+    )
+    assert all_close_check(interop_result, golden_result)
 
 
 @pytest.mark.parametrize(
@@ -240,10 +203,10 @@ def test_muladd_bcast_jit_dram(device, h, w, dtype):
             f"Broadcasted shape is incorrectly chosen to be (32, {w}) for all shapes, not ({h}, {w})."
         )
     max_grid = (0, 0)
-    A = _create_dram_tensor(device, h, w, dtype)
-    B = _create_dram_tensor(device, h, w, dtype)
+    A = create_dram_tensor(device, h, w, dtype)
+    B = create_dram_tensor(device, h, w, dtype)
     # broadcast C
-    C = _create_dram_tensor(device, 1, w, dtype)
+    C = create_dram_tensor(device, 1, w, dtype)
 
     print("A:", A.cpu().to_torch())
     print("A shape:", A.shape)
@@ -259,4 +222,7 @@ def test_muladd_bcast_jit_dram(device, h, w, dtype):
     # Golden path
     golden_result = mul_add(A, B, C)
 
-    _all_close_check(interop_result, golden_result)
+    assert memory_configs_equal(
+        interop_result.memory_config(), golden_result.memory_config()
+    )
+    assert all_close_check(interop_result, golden_result)
