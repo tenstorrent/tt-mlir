b40af9a82738595cd34b5a76050d29e9527ffd3e
Lewis Panos <lpanos@tenstorrent.com>
2025-10-27 21:59:03 +0000

    Add emitc conversion and  test

--- lib/Conversion/TTNNToEmitC/TTNNToEmitC.cpp
+++ lib/Conversion/TTNNToEmitC/TTNNToEmitC.cpp
@@ -3716,7 +3716,35 @@ public:
         emitter.emit(srcOp.getDtype())};
 
     emitter.replaceOp(*this, args);
+    return success();
+  }
+};
+} // namespace
 
+namespace {
+class PagedUpdateCacheOpConversionPattern
+    : public TTNNToEmitCBaseOpConversionPattern<
+          mlir::tt::ttnn::PagedUpdateCacheOp> {
+public:
+  using TTNNToEmitCBaseOpConversionPattern<
+      mlir::tt::ttnn::PagedUpdateCacheOp>::TTNNToEmitCBaseOpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(mlir::tt::ttnn::PagedUpdateCacheOp srcOp, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+
+    ttnn_to_emitc::EmitCTTNNEmitter<mlir::tt::ttnn::PagedUpdateCacheOp> emitter(
+        srcOp, adaptor, rewriter);
+
+    llvm::SmallVector<mlir::Attribute> args{
+        emitter.emit(srcOp.getCache()),
+        emitter.emit(srcOp.getInput()),
+        emitter.emit(srcOp.getUpdateIndex()),
+        emitter.emit(srcOp.getShareCache()),
+        emitter.emit(srcOp.getPageTable()),
+    };
+
+    emitter.replaceOp(*this, args);
     return success();
   }
 };
@@ -3906,6 +3934,7 @@ void populateTTNNToEmitCPatterns(mlir::MLIRContext *ctx,
   // KV Cache ops
   //
   patterns.add<UpdateCacheOpConversionPattern>(typeConverter, ctx);
+  patterns.add<PagedUpdateCacheOpConversionPattern>(typeConverter, ctx);
   patterns.add<DefaultOpConversionPattern<mlir::tt::ttnn::FillCacheOp>>(
       typeConverter, ctx);
 
