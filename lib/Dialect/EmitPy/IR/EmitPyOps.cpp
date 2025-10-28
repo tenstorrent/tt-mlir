331fc37ab659cde669a2d489077ccb716b39c299
Andjela Milovanovic <amilovanovic@tenstorrent.com>
2025-10-28 15:44:48 +0000

    Change assignGlobalOp  type.

--- lib/Dialect/EmitPy/IR/EmitPyOps.cpp
+++ lib/Dialect/EmitPy/IR/EmitPyOps.cpp
@@ -12,6 +12,7 @@
 #include "llvm/Support/LogicalResult.h"
 #include "llvm/Support/raw_ostream.h"
 #include <llvm/Support/LogicalResult.h>
+#include <mlir/IR/OpImplementation.h>
 
 using namespace mlir;
 using namespace mlir::tt::emitpy;
@@ -399,14 +400,14 @@ LogicalResult ConstantOp::verify() {
 
 static void printEmitPyGlobalOpInitialValue(OpAsmPrinter &p, GlobalOp op,
                                             Attribute initialValue) {
-  p << "= ";
+  p << ": ";
   p.printAttributeWithoutType(initialValue);
 }
 
 static ParseResult parseEmitPyGlobalOpInitialValue(OpAsmParser &parser,
                                                    Attribute &initialValue) {
-  if (parser.parseEqual()) {
-    return failure();
+  if (parser.parseColon()) {
+    return parser.emitError(parser.getNameLoc(), "expected ':' after symbol name");
   }
 
   if (parser.parseAttribute(initialValue)) {
@@ -487,6 +488,26 @@ GetGlobalOp::verifySymbolUses(SymbolTableCollection &symbolTable) {
 //===----------------------------------------------------------------------===//
 // AssignGlobalOp
 //===----------------------------------------------------------------------===//
+
+/* static void printEmitPyAssignGlobalOpInitialValue(OpAsmPrinter &p, AssignGlobalOp op,
+                                                  FlatSymbolRefAttr name,
+                                                  Value value) {
+  p << name << " = ";
+  p.printOperand(value);
+}
+
+static ParseResult parseEmitPyAssignGlobalOpInitialValue(OpAsmParser &parser,
+                                                         FlatSymbolRefAttr &name) {
+  if (parser.parseAttribute(name)) {
+    return failure();
+  }
+  
+  if (parser.parseEqual()) {
+    return parser.emitError(parser.getNameLoc(), "expected '=' after symbol name");
+  }
+
+  return success();
+} */
 
 LogicalResult
 AssignGlobalOp::verifySymbolUses(SymbolTableCollection &symbolTable) {
