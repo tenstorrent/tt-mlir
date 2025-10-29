887e8509f96dcc284122ccf6d3b1d0d2e864c6c5
Marco Campos <mcampos@tenstorrent.com>
2025-10-29 21:13:11 +0000

    feat: move ir dumps folder and model extensions to utils

--- tools/explorer/tt_adapter/src/tt_adapter/utils.py
+++ tools/explorer/tt_adapter/src/tt_adapter/utils.py
@@ -14,6 +14,11 @@ import torch
 TTRT_INSTALLED = importlib.util.find_spec("ttrt") is not None
 
 
+# TODO(ctr-mcampos): update path to be configurable
+IR_DUMPS_DIR = 'ir_dumps'
+MODEL_EXTENSIONS = ['.ttir', '.mlir', '.ttnn']
+
+
 def parse_mlir_str(module_str):
     with ttmlir.ir.Context() as ctx:
         module = ttmlir.ir.Module.parse(module_str, ctx)
