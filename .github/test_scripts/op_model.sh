432862a8687ccaed80699b45d56ec1326539a402
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-10-30 08:46:06 +0100

    move lit to every run, fix optimizer

--- .github/test_scripts/op_model.sh
+++ .github/test_scripts/op_model.sh
@@ -11,9 +11,9 @@ echo "Running op-model test Lib"
 $BUILD_DIR/test/unittests/OpModel/TTNN/Lib/TestOpModelLib
 echo "Running op-model test Interface"
 $BUILD_DIR/test/unittests/OpModel/TTNN/Op/TestOpModelInterface
+echo "Run Optimizer Models Perf Tests"
+llvm-lit -v --filter="optimizer" --xunit-xml-output $TEST_REPORT_PATH --param TTMLIR_ENABLE_OPTIMIZER_MODELS_PERF_TESTS=1 $BUILD_DIR/test/ttmlir/Silicon/TTNN/n150/optimizer/models_perf_tests
 echo "Running op-model ttrt test"
-ttrt run --non-zero Silicon/TTNN/n150/optimizer
+ttrt run --non-zero $BUILD_DIR/test/ttmlir/Silicon/TTNN/n150/optimizer
 cp run_results.json ${TTRT_REPORT_PATH} || true
 cp ttrt_report.xml ${TEST_REPORT_PATH%_*}_optimizer_${TEST_REPORT_PATH##*_} || true
-echo "Run Optimizer Models Perf Tests"
-llvm-lit -v --xunit-xml-output $TEST_REPORT_PATH --param TTMLIR_ENABLE_OPTIMIZER_MODELS_PERF_TESTS=1 $BUILD_DIR/test/ttmlir/Silicon/TTNN/n150/optimizer/models_perf_tests
