81c79acdb4c931a09c4a2743820f8e20dfd3430b
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-10-30 12:42:04 +0100

    enable opmodel on tracy

--- .github/test_scripts/op_model_ttrt.sh
+++ .github/test_scripts/op_model_ttrt.sh
@@ -7,6 +7,6 @@ set -e -o pipefail
 
 echo
 echo "Running op-model ttrt test"
-ttrt $1 --non-zero $BUILD_DIR/test/ttmlir/Silicon/TTNN/n150/optimizer
+ttrt $1 $BUILD_DIR/test/ttmlir/Silicon/TTNN/n150/optimizer
 cp ${1}_results.json ${TTRT_REPORT_PATH} || true
 cp ttrt_report.xml ${TEST_REPORT_PATH%_*}_optimizer_${TEST_REPORT_PATH##*_} || true
