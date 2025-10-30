f5e2bf40f9bb4346754ee10a556afb1fdefd33b9
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-10-30 13:19:24 +0100

    fixes

--- .github/test_scripts/op_model.sh
+++ .github/test_scripts/op_model.sh
@@ -13,4 +13,4 @@ echo "Running op-model test Interface"
 $BUILD_DIR/test/unittests/OpModel/TTNN/Op/TestOpModelInterface
 echo
 echo "Run Optimizer Models Perf Tests"
-llvm-lit -v --filter="optimizer" --xunit-xml-output $TEST_REPORT_PATH $BUILD_DIR/test/ttmlir/Silicon/TTNN/n150/optimizer
+llvm-lit -v --xunit-xml-output $TEST_REPORT_PATH --param TTMLIR_ENABLE_OPTIMIZER_MODELS_PERF_TESTS=1 $BUILD_DIR/test/ttmlir/Silicon/TTNN/n150/optimizer/models_perf_tests
