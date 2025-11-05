92e35dd44bc9495694e5b2c2429bc80fec50a236
Lewis Panos <lpanos@tenstorrent.com>
2025-11-05 18:54:42 +0000

    Remove commented code

--- lib/Dialect/TTNN/IR/TTNNOps.cpp
+++ lib/Dialect/TTNN/IR/TTNNOps.cpp
@@ -2921,11 +2921,6 @@ mlir::tt::ttnn::CollectivePermuteOp::fold(FoldAdaptor adaptor) {
 //===----------------------------------------------------------------------===//
 
 ::mlir::LogicalResult FillCacheOp::verify() {
-  // if (getBatchOffset() != 0) {
-  //   return emitOpError(
-  //       "Only single-batch is supported. Batch offset must be 0");
-  // }
-
   const ::mlir::RankedTensorType cacheType = getCache().getType();
   const ::mlir::RankedTensorType inputType = getInput().getType();
 
