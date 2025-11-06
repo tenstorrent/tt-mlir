58ccf34084bd1242f70ce411eef83c7f0c8368e4
Brandon Rapanan <brapanan@tenstorrent.com>
2025-11-06 16:33:27 +0000

    Removed some failing OOM tests. Broadcasting test broadcast dim correctly but fails allclose.

--- test/ttnn-jit/test_eltwise_composite.py
+++ test/ttnn-jit/test_eltwise_composite.py
@@ -13,6 +13,7 @@ from utils import (
     memory_configs_equal,
     create_dram_tensor,
     create_sharded_tile_tensor,
+    run_op_test
 )
 
 COMMON_SHAPE_GRID_PARAMS = [
@@ -30,29 +31,6 @@ COMMON_SHAPE_GRID_PARAMS = [
 ]
 
 COMMON_SHAPE_PARAMS = [(32, 32), (32, 64), (64, 64), (64, 128), (128, 128)]
-
-
-def run_op_test(
-    device, h, w, max_grid, dtype, op, num_inputs, buffer_type=ttnn.BufferType.L1
-):
-    if buffer_type == ttnn.BufferType.L1:
-        inputs = [
-            create_sharded_tile_tensor(device, h, w, max_grid, dtype)
-            for _ in range(num_inputs)
-        ]
-    else:
-        inputs = [create_dram_tensor(device, h, w, dtype) for _ in range(num_inputs)]
-    print("inputs", inputs)
-    golden_op = _get_ttnn_op(op)
-
-    op_jit = ttnn_jit.jit(debug=True, max_grid=max_grid)(op)
-    output_tensor = op_jit(*inputs)
-    golden_tensor = (golden_op or op)(*inputs)
-
-    assert memory_configs_equal(
-        output_tensor.memory_config(), golden_tensor.memory_config()
-    )
-    assert all_close_check(output_tensor, golden_tensor)
 
 
 # ------------------------------------------------------------
@@ -97,48 +75,33 @@ def test_composite_ops_l1(device, h, w, max_grid, dtype, op):
 @pytest.mark.parametrize("h , w", COMMON_SHAPE_PARAMS)
 @pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32])
 @pytest.mark.parametrize("op", [cosh, sinh, mul_add])
-@pytest.mark.skip(
-    reason="Seeing failure with DRAM composite ops: /tt-mlir/lib/Dialect/TTCore/IR/TTCoreOpsTypes.cpp:1374: Unsupported memory space"
-)
 def test_composite_ops_dram(device, h, w, dtype, op):
-    for h, w in COMMON_SHAPE_PARAMS:
-        for dtype in [torch.bfloat16, torch.float32]:
-            for op in [cosh, sinh, mul_add]:
-                num_inputs = 1
-                if op is mul_add:
-                    num_inputs = 3
-                run_op_test(
-                    device,
-                    h,
-                    w,
-                    max_grid=(0, 0),
-                    dtype=dtype,
-                    op=op,
-                    num_inputs=num_inputs,
-                    buffer_type=ttnn.BufferType.DRAM,
-                )
+    num_inputs = 1
+    if op is mul_add:
+        num_inputs = 3
+    run_op_test(
+        device,
+        h,
+        w,
+        max_grid=(0, 0),
+        dtype=dtype,
+        op=op,
+        num_inputs=num_inputs,
+        buffer_type=ttnn.BufferType.DRAM,
+    )
 
 
-passing_configs_l1 = {
-    torch.bfloat16: [
-        (512, 2048),
-        (512, 4096),
-        (512, 8192),
-        (1024, 2048),
-        (1024, 4096),
-    ],
-}
+PASSING_LARGE_SHAPES_DTYPES_L1 = [
+        (512, 2048, torch.bfloat16),
+        (512, 4096, torch.bfloat16),
+        (512, 8192, torch.bfloat16),
+        (1024, 2048, torch.bfloat16),
+        (1024, 4096, torch.bfloat16),
+]
 
 
-@pytest.mark.parametrize("seq_len", [2048, 4096, 8192, 16384, 32768, 65536])
-@pytest.mark.parametrize("hidden_dim", [512, 1024, 2048, 4096])
-@pytest.mark.parametrize("dtype", [torch.float32, torch.bfloat16])
-def test_large_muladd_nice_seq_len_jit_l1(device, seq_len, hidden_dim, dtype):
-    # most test cases fail
-    if (hidden_dim, seq_len) not in passing_configs_l1.get(dtype, []):
-        pytest.xfail(
-            f"Most large L1 configs fail. Skipping ({hidden_dim}, {seq_len}) with dtype {dtype}."
-        )
+@pytest.mark.parametrize("hidden_dim, seq_len, dtype", PASSING_LARGE_SHAPES_DTYPES_L1)
+def test_large_shapes_muladd_l1(device, hidden_dim, seq_len, dtype):
 
     max_grid = (7, 7)
 
@@ -159,13 +122,29 @@ def test_large_muladd_nice_seq_len_jit_l1(device, seq_len, hidden_dim, dtype):
     assert all_close_check(interop_result, golden_result, atol=1e-1, rtol=1e-1)
 
 
-@pytest.mark.parametrize("seq_len", [2048, 4096, 8192, 16384, 32768, 65536])
-@pytest.mark.parametrize("hidden_dim", [512, 1024, 2048, 4096])
-@pytest.mark.parametrize("dtype", [torch.float32, torch.bfloat16])
-@pytest.mark.skip(
-    reason="Seeing failure with DRAM composite ops: /tt-mlir/lib/Dialect/TTCore/IR/TTCoreOpsTypes.cpp:1374: Unsupported memory space"
-)
-def test_large_muladd_nice_seq_len_jit_dram(device, seq_len, hidden_dim, dtype):
+PASSING_LARGE_SHAPES_DTYPES_DRAM = [
+   
+    (512, 2048, torch.float32),
+    (512, 4096, torch.float32), 
+    (512, 8192, torch.float32),
+    (1024, 2048, torch.float32),
+    (1024, 4096, torch.float32),
+    (2048, 2048, torch.float32),
+    (4096, 1024, torch.float32),
+    
+    (512, 2048, torch.bfloat16),
+    (512, 4096, torch.bfloat16),
+    (512, 8192, torch.bfloat16),
+    (512, 16384, torch.bfloat16),
+    (1024, 2048, torch.bfloat16),
+    (1024, 4096, torch.bfloat16),
+    (1024, 8192, torch.bfloat16),
+    (2048, 4096, torch.bfloat16),
+    
+]
+
+@pytest.mark.parametrize("hidden_dim, seq_len, dtype", PASSING_LARGE_SHAPES_DTYPES_DRAM)
+def test_large_shapes_muladd_dram(device, hidden_dim, seq_len, dtype):
 
     A = create_dram_tensor(device, seq_len, hidden_dim, dtype)
     B = create_dram_tensor(device, seq_len, hidden_dim, dtype)
@@ -201,13 +180,6 @@ def test_muladd_broadcast_jit_l1(device, h, w, max_grid, dtype):
     # broadcast C
     C = create_sharded_tile_tensor(device, 1, w, max_grid, dtype)
 
-    print("A:", A.cpu().to_torch())
-    print("A shape:", A.shape)
-    print("B:", B.cpu().to_torch())
-    print("B shape:", B.shape)
-    print("C:", C.cpu().to_torch())
-    print("C shape:", C.shape)
-
     # JIT path
     op_jit = ttnn_jit.jit(debug=True, max_grid=max_grid)(mul_add)
     interop_result = op_jit(A, B, C)
@@ -225,9 +197,7 @@ def test_muladd_broadcast_jit_l1(device, h, w, max_grid, dtype):
     "h, w", [(32, 32), (64, 64), (128, 128), (256, 256), (512, 512)]
 )
 @pytest.mark.parametrize("dtype", [torch.float32, torch.bfloat16])
-@pytest.mark.skip(
-    reason="Seeing failure with DRAM composite ops: /tt-mlir/lib/Dialect/TTCore/IR/TTCoreOpsTypes.cpp:1374: Unsupported memory space"
-)
+@pytest.mark.xfail(reason="All tests failing allclose.")
 def test_muladd_broadcast_jit_dram(device, h, w, dtype):
 
     max_grid = (0, 0)
@@ -235,13 +205,6 @@ def test_muladd_broadcast_jit_dram(device, h, w, dtype):
     B = create_dram_tensor(device, h, w, dtype)
     # broadcast C
     C = create_dram_tensor(device, 1, w, dtype)
-
-    print("A:", A.cpu().to_torch())
-    print("A shape:", A.shape)
-    print("B:", B.cpu().to_torch())
-    print("B shape:", B.shape)
-    print("C:", C.cpu().to_torch())
-    print("C shape:", C.shape)
 
     # JIT path
     op_jit = ttnn_jit.jit(debug=True, max_grid=max_grid)(mul_add)
