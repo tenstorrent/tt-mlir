ebe02dee01f3c09c53dffb580a5bbe1d3fc68f81
Brandon Rapanan <brapanan@tenstorrent.com>
2025-11-07 16:52:02 +0000

    added rank 3 and 4 tensors to test_layouts.py

--- test/ttnn-jit/test_layouts.py
+++ test/ttnn-jit/test_layouts.py
@@ -6,6 +6,7 @@ import ttnn
 import torch
 
 import pytest
+import itertools
 
 from utils import (
     _get_ttnn_op,
@@ -18,18 +19,45 @@ from utils import (
 
 # Generates all shapes with 1 to 4 tiles per core in each dimension, with every grid from single core to 8x8.
 # TTNN grids are (Width, Height), while tensor shapes are (Height, Width).
-BLOCK_SHARDED_SHAPE_GRIDS = [
-    ((h * 32 * (grid_h + 1), w * 32 * (grid_w + 1)), (grid_w, grid_h))
-    for h in range(1, 5)
-    for w in range(1, 5)
-    for grid_h in range(8)
-    for grid_w in range(8)
-]
+BLOCK_SHARDED_SHAPE_GRIDS = []
+
+# 2D shapes
+BLOCK_SHARDED_SHAPE_GRIDS.extend(
+    [
+        ((h * 32 * (grid_h + 1), w * 32 * (grid_w + 1)), (grid_w, grid_h))
+        for h, w, grid_h, grid_w in itertools.product(
+            range(1, 5), range(1, 5), range(8), range(8)
+        )
+    ]
+)
+
+# 3D shapes
+BLOCK_SHARDED_SHAPE_GRIDS.extend(
+    [
+        ((batch, h * 32 * (grid_h + 1), w * 32 * (grid_w + 1)), (grid_w, grid_h))
+        for batch, h, w, grid_h, grid_w in itertools.product(
+            [1, 2, 4, 8], range(1, 3), range(1, 3), range(0, 4), range(0, 4)
+        )
+    ]
+)
+
+# 4D shapes
+BLOCK_SHARDED_SHAPE_GRIDS.extend(
+    [
+        ((batch1, batch2, h * 32 * (grid_h + 1), w * 32 * (grid_w + 1)), (grid_w, grid_h))
+        for batch1, batch2, h, w, grid_h, grid_w in itertools.product(
+            [1, 2], [1, 2, 4], range(1, 3), range(1, 3), range(0, 3), range(0, 3)
+        )
+    ]
+)
 
 # TODO (5415): These grids fail for all shapes.
 GRIDS_FAILING_ALL_SHAPES = [(1, 1), (1, 2), (1, 3), (3, 2), (3, 3), (5, 3)]
 
-DRAM_INTERLEAVED_SHAPE_GRIDS = (
+DRAM_INTERLEAVED_SHAPE_GRIDS = []
+
+# 2D shapes
+DRAM_INTERLEAVED_SHAPE_GRIDS.extend(
     [(32 * (2**x), 2048) for x in range(6)]
     + [(2048, 32 * (2**x)) for x in range(6)]
     + [(2**x, 2**x) for x in range(5, 12)]
@@ -38,6 +66,35 @@ DRAM_INTERLEAVED_SHAPE_GRIDS = (
         (64, 32),
         (32, 256),
         (256, 32),
+    ]
+)
+
+# 3D shapes
+DRAM_INTERLEAVED_SHAPE_GRIDS.extend(
+    [
+        (batch, h, w)
+        for batch, h, w in itertools.product(
+            [1, 2, 4, 8], [32, 64, 128, 256, 512], [32, 64, 128, 256, 512]
+        )
+    ]
+)
+
+DRAM_INTERLEAVED_SHAPE_GRIDS.extend(
+    [
+        (batch, h, w)
+        for batch, h, w in itertools.product(
+            [1, 2, 4, 8], [32, 64, 128, 256, 512], [32, 64, 128, 256, 512]
+        )
+    ]
+)
+
+# 4D shapes
+DRAM_INTERLEAVED_SHAPE_GRIDS.extend(
+    [
+        (batch1, batch2, h, w)
+        for batch1, batch2, h, w in itertools.product(
+            [1, 2], [1, 2, 4], [32, 64, 128, 256], [32, 64, 128, 256]
+        )
     ]
 )
 
@@ -70,17 +127,16 @@ def test_l1_block_sharded_shapes(device, shape, max_grid, op):
 
 
 @pytest.mark.parametrize(
-    "h , w",
+    "shape",
     DRAM_INTERLEAVED_SHAPE_GRIDS,
-    ids=[f"{h}-{w}" for h, w in DRAM_INTERLEAVED_SHAPE_GRIDS],
+    ids=[f"{shape}" for shape in DRAM_INTERLEAVED_SHAPE_GRIDS],
 )
 @pytest.mark.parametrize("op", [abs])
-def test_dram_interleaved_shapes(device, h, w, op):
+def test_dram_interleaved_shapes(device, shape, op):
     max_grid = (0, 0)
     run_op_test(
         device,
-        h,
-        w,
+        shape,
         max_grid,
         torch.float16,
         op,
