51b4e1eb9677c5c0ae58783cb43bd062a0445296
vkovinicTT <vkovinic@tenstorrent.com>
2025-11-07 15:32:38 +0000

    ttir.convolution -> ttir.conv3d

--- lib/Dialect/TTNN/IR/TTNNOps.cpp
+++ lib/Dialect/TTNN/IR/TTNNOps.cpp
@@ -481,7 +481,7 @@ static bool isDefinedByOp(mlir::Value value) {
     return mlir::failure();
   }
 
-  if (verifyOutputDimensions(this, inputDims, weightDims, biasDims, outputDims,
+if (verifyOutputDimensions(this, inputDims, weightDims, biasDims, outputDims,
                              params)
           .failed()) {
     return mlir::failure();
