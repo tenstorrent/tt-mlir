efc98206bd19d654de974eb257e671f62dc59dce
Milan Topalovic <mtopalovic@tenstorrent.com>
2025-11-10 20:51:35 +0000

    Adding rewrite pattern for rope

---
+++ include/ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/RotaryEmbeddingOpRewritePattern.h
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef TTMLIR_DIALECT_TTNN_TRANSFORMS_PASSES_TTNNFUSINGOPREWRITEPATTERN_H
+#define TTMLIR_DIALECT_TTNN_TRANSFORMS_PASSES_TTNNFUSINGOPREWRITEPATTERN_H
+
+#include "ttmlir/Dialect/TTNN/IR/TTNNOps.h"
+
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Support/LogicalResult.h"
+
+namespace mlir::tt::ttnn::workarounds::decomposition {
+class RotaryEmbeddingOpRewritePattern
+    : public OpRewritePattern<RotaryEmbeddingOp> {
+public:
+  using OpRewritePattern<RotaryEmbeddingOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(RotaryEmbeddingOp srcOp,
+                                PatternRewriter &rewriter) const override;
+};
+} // namespace mlir::tt::ttnn::workarounds::decomposition
+
+#endif // TTMLIR_DIALECT_TTNN_TRANSFORMS_PASSES_TTNNFUSINGOPREWRITEPATTERN_H
---
+++ lib/Dialect/TTNN/Transforms/Workarounds/Decomposition/RotaryEmbeddingOpRewritePattern.cpp
@@ -0,0 +1,53 @@
+// // SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/RotaryEmbeddingOpRewritePattern.h"
+#include "ttmlir/Dialect/TTNN/Types/Types.h"
+#include "ttmlir/Dialect/TTNN/Utils/Utils.h"
+
+// Workaround which adds padding to RotaryEmbedding seq_len
+// dimension to make it a multiple of tile size.
+// Metal issue reference: https://github.com/tenstorrent/tt-metal/issues/31567
+namespace mlir::tt::ttnn::workarounds::decomposition {
+LogicalResult RotaryEmbeddingOpRewritePattern::matchAndRewrite(
+    RotaryEmbeddingOp srcOp, PatternRewriter &rewriter) const {
+  RankedTensorType resultType = srcOp.getType();
+  ArrayRef<int64_t> resultShape = resultType.getShape();
+  if (resultShape.size() < 2) {
+    return failure();
+  }
+
+  if (resultShape[resultShape.size() - 2] % TILE_HEIGHT == 0) {
+    return failure();
+  }
+
+  int64_t originalSeqLen = resultShape[resultShape.size() - 2];
+  SmallVector<int64_t> paddedResultShape(resultShape);
+  paddedResultShape[paddedResultShape.size() - 2] =
+      llvm::divideCeil(originalSeqLen, TILE_HEIGHT) * TILE_HEIGHT;
+
+  auto paddedType =
+      utils::RankedTensorTypeFactory::create(resultType, paddedResultShape);
+
+  auto rope = rewriter.create<RotaryEmbeddingOp>(
+      srcOp.getLoc(), paddedType, srcOp.getInput(), srcOp.getCosCache(),
+      srcOp.getSinCache(), srcOp.getTokenIndexAttr(),
+      srcOp.getMemoryConfigAttr(), srcOp.getComputeConfigAttr());
+
+  // Slice to original shape
+  SmallVector<int32_t> begins(resultShape.size(), 0);
+  SmallVector<int32_t> ends(paddedResultShape.begin(), paddedResultShape.end());
+  SmallVector<int32_t> steps(resultShape.size(), 1);
+  ends[ends.size() - 2] = originalSeqLen;
+
+  auto sliced = rewriter.create<ttnn::SliceStaticOp>(
+      srcOp.getLoc(), resultType, rope.getResult(),
+      rewriter.getI32ArrayAttr(begins), rewriter.getI32ArrayAttr(ends),
+      rewriter.getI32ArrayAttr(steps));
+
+  rewriter.replaceOp(srcOp, sliced);
+
+  return success();
+}
+} // namespace mlir::tt::ttnn::workarounds::decomposition
