a0a2a4a98293d905f42fe5a3cc89f08345810b1e
Collin Tod <collintod@tenstorrent.com>
2025-11-11 20:58:07 +0000

    Linear batching workaround

--- lib/Dialect/TTNN/Transforms/Workarounds/Decomposition/LinearOpRewritePattern.cpp
+++ lib/Dialect/TTNN/Transforms/Workarounds/Decomposition/LinearOpRewritePattern.cpp
@@ -14,19 +14,20 @@
 
 namespace mlir::tt::ttnn::workarounds::decomposition {
 
-static bool isBatchedLinearOp(ttnn::LinearOp linearOp) {
-  RankedTensorType inputBType = linearOp.getB().getType();
-  auto inputBShape = inputBType.getShape();
-  int64_t rank = inputBShape.size();
-
-  // if rank <= 2, it cannot be batched. Return false.
-  // Check if batched: any dimension before the last 2 has size > 1.
-  // i.e. <1x3x128x32xbf16> is batched because of 3.
-  // i.e. <1x1x128x32xbf16> is not batched because all dims before last 2 are 1.
-  // i.e. <128xbf16> is not batched because rank < 2.
-  return rank > 2 && llvm::any_of(inputBShape.drop_back(2),
-                                  [](int64_t dim) { return dim > 1; });
-}
+// static bool isBatchedLinearOp(ttnn::LinearOp linearOp) {
+//   RankedTensorType inputBType = linearOp.getB().getType();
+//   auto inputBShape = inputBType.getShape();
+//   int64_t rank = inputBShape.size();
+//
+//   // if rank <= 2, it cannot be batched. Return false.
+//   // Check if batched: any dimension before the last 2 has size > 1.
+//   // i.e. <1x3x128x32xbf16> is batched because of 3.
+//   // i.e. <1x1x128x32xbf16> is not batched because all dims before last 2
+//   are 1.
+//   // i.e. <128xbf16> is not batched because rank < 2.
+//   return rank > 2 && llvm::any_of(inputBShape.drop_back(2),
+//                                   [](int64_t dim) { return dim > 1; });
+// }
 
 // Calculate the output shape of a matmul operation following tt-metal's logic.
 // Reference: ttnn/cpp/ttnn/operations/matmul/matmul.cpp
@@ -36,10 +37,11 @@ computeMatmulOutputShape(llvm::ArrayRef<int64_t> shapeA, bool transposeA,
   int64_t rankA = shapeA.size();
   int64_t rankB = shapeB.size();
 
-  if (rankA == 1 || rankB == 1) {
-    TT_assertv(false,
-               "Should not reach linear op workaround if rankA or rankB is 1");
-  }
+  // if (rankA == 1 || rankB == 1) {
+  //   TT_assertv(false,
+  //              "Should not reach linear op workaround if rankA or rankB is
+  //              1");
+  // }
 
   SmallVector<int64_t> outputShape;
   SmallVector<int64_t> batchShapeA(shapeA.begin(), shapeA.end() - 2);
@@ -69,11 +71,6 @@ computeMatmulOutputShape(llvm::ArrayRef<int64_t> shapeA, bool transposeA,
 LogicalResult
 LinearOpRewritePattern::matchAndRewrite(ttnn::LinearOp srcOp,
                                         PatternRewriter &rewriter) const {
-
-  // Only decompose if input B is batched AND bias exists
-  if (!isBatchedLinearOp(srcOp) || !srcOp.getBias()) {
-    return failure();
-  }
 
   RankedTensorType inputAType = srcOp.getA().getType();
   RankedTensorType inputBType = srcOp.getB().getType();
