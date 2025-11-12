a45583e1e71ac479829fb59f310ed1ce77e6928e
Lewis Panos <lpanos@tenstorrent.com>
2025-11-12 19:34:54 +0000

    Add verification for curPosTensor to be an integer tensor

--- lib/Dialect/TTIR/IR/TTIROps.cpp
+++ lib/Dialect/TTIR/IR/TTIROps.cpp
@@ -4752,6 +4752,9 @@ mlir::tt::ttir::PagedScaledDotProductAttentionDecodeOp::verify() {
   }
 
   if (getCurPosTensor()) {
+    if (!getCurPosTensor().getType().getElementType().isInteger()) {
+      return emitOpError("Cur pos tensor must be an integer tensor.");
+    }
     if (getCurPosTensor().getType().getShape().size() != 1) {
       return emitOpError("Cur pos tensor must be a 1D tensor.");
     }
