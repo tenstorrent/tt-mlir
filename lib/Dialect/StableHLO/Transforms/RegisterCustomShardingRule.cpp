0d1ea72e33fc3760aa7d5cf12114302df579056c
Sungjoon Shon <sshon@tenstorrent.com>
2025-11-13 18:31:02 +0000

    Add pass for register custom sharding rule
    This pass registers custom sharding rules in the current MLIRContext so that Shardy can correctly propagate shardings through operations that do not have built-in sharding rules in Shardy.
    Starting from stablehlo.custom_call op, we can define any newly introduced op by our own rule.

---
+++ lib/Dialect/StableHLO/Transforms/RegisterCustomShardingRule.cpp
@@ -0,0 +1,124 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "shardy/dialect/sdy/transforms/propagation/op_sharding_rule_builder.h"
+#include "stablehlo/dialect/StablehloOps.h"
+#include "ttmlir/Dialect/StableHLO/Transforms/Passes.h"
+
+#include "llvm/Support/Error.h"
+
+namespace mlir::tt::stablehlo {
+#define GEN_PASS_DEF_REGISTERCUSTOMSHARDINGRULEPASS
+#include "ttmlir/Dialect/StableHLO/Transforms/Passes.h.inc"
+
+static constexpr llvm::StringLiteral sdpaTargetName =
+    "tt.scaled_dot_product_attention";
+
+static mlir::sdy::OpShardingRuleAttr
+getSDPAShardingRule(mlir::stablehlo::CustomCallOp op) {
+  auto qType = llvm::dyn_cast<RankedTensorType>(op.getOperand(0).getType());
+  auto kType = llvm::dyn_cast<RankedTensorType>(op.getOperand(1).getType());
+  auto vType = llvm::dyn_cast<RankedTensorType>(op.getOperand(2).getType());
+  auto outType = llvm::dyn_cast<RankedTensorType>(op.getResult(0).getType());
+
+  // SDPA requires Q/K/V and output to have identical shapes.
+  if (qType.getShape() != kType.getShape() ||
+      qType.getShape() != vType.getShape() ||
+      qType.getShape() != outType.getShape()) {
+    return mlir::sdy::OpShardingRuleAttr();
+  }
+
+  ArrayRef<int64_t> shape = qType.getShape();
+
+  // SDPA is assumed to operate on 4D tensors: [B, H, S, D]
+  // If the shape does not match, conservatively fallback to a pointwise rule.
+  if (shape.size() != 4) {
+    return mlir::sdy::OpShardingRuleBuilder::buildPointwise(op);
+  }
+
+  // SDPA can shard batch (B) and head (H) dimensions freely.
+  // Sequence (S) and hidden (D) dimensions generally require replication,
+  // unless a more advanced distributed attention algorithm is implemented.
+  //
+  // Dimension assignment:
+  // dim 0 -> Batch
+  // dim 1 -> Head
+  // dim 2 -> Sequence length
+  // dim 3 -> Hidden size
+  auto getFactorType = [&](int64_t dim) -> mlir::sdy::FactorType {
+    if (dim == 0 || dim == 1) {
+      // Allow sharding
+      return mlir::sdy::FactorType::kPassThrough;
+    }
+    // Disallow sharding, require replication
+    return mlir::sdy::FactorType::kNeedReplication;
+  };
+
+  // Build the final sharding rule:
+  // - Pass-through on B/H dims
+  // - Replication on S/D dims
+  return mlir::sdy::OpShardingRuleBuilder(op)
+      .addPointwise(shape, getFactorType)
+      .build();
+}
+
+struct StablehloCustomCallShardingModel
+    : public mlir::sdy::ShardingRuleOpInterface::ExternalModel<
+          StablehloCustomCallShardingModel, ::mlir::stablehlo::CustomCallOp> {
+
+  mlir::sdy::OpShardingRuleAttr getShardingRule(mlir::Operation *op) const {
+    auto custom = llvm::cast<mlir::stablehlo::CustomCallOp>(op);
+    assert(custom && "StablehloCustomCallShardingModel must be attached to "
+                     "CustomCallOp only");
+    return getCustomCallShardingRule(custom);
+  }
+
+  bool shouldKeepOutputShardingsDivisible(mlir::Operation *) const {
+    return true;
+  }
+
+private:
+  mlir::sdy::OpShardingRuleAttr
+  getCustomCallShardingRule(mlir::stablehlo::CustomCallOp op) const {
+    llvm::StringRef target = op.getCallTargetName();
+
+    auto it = customCallShardingRules.find(target);
+    if (it != customCallShardingRules.end()) {
+      return it->second(op);
+    }
+
+    op.getOperation()->emitWarning()
+        << "StableHLO CustomCallOp sharding rule is not defined for target '"
+        << target << "'";
+    return mlir::sdy::OpShardingRuleAttr();
+  }
+
+  // Map from CustomCall target names to their corresponding sharding rule
+  // functions.
+  llvm::DenseMap<llvm::StringRef, std::function<mlir::sdy::OpShardingRuleAttr(
+                                      mlir::stablehlo::CustomCallOp)>>
+      customCallShardingRules = {
+          {sdpaTargetName, getSDPAShardingRule},
+      };
+};
+
+class RegisterCustomShardingRulePass
+    : public impl::RegisterCustomShardingRulePassBase<
+          RegisterCustomShardingRulePass> {
+public:
+  using impl::RegisterCustomShardingRulePassBase<
+      RegisterCustomShardingRulePass>::RegisterCustomShardingRulePassBase;
+
+  void runOnOperation() final {
+    mlir::ModuleOp rootModule = getOperation();
+    MLIRContext *context = rootModule.getContext();
+
+    context->loadDialect<mlir::stablehlo::StablehloDialect>();
+    // Register for stablehlo.CustomCallOp
+    mlir::stablehlo::CustomCallOp::attachInterface<
+        StablehloCustomCallShardingModel>(*context);
+  }
+};
+
+} // namespace mlir::tt::stablehlo
