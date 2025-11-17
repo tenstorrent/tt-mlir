514f40790c14414b53b935a203fa65faf32d0938
Zoe Carver <zcarver@tenstorrent.com>
2025-11-17 10:20:53 -0500

    review comments

--- lib/Dialect/D2M/IR/D2MGenericRegionOps.cpp
+++ lib/Dialect/D2M/IR/D2MGenericRegionOps.cpp
@@ -546,7 +546,7 @@ void TileUntilizeBlockOp::getEffects(
 }
 
 //===----------------------------------------------------------------------===//
-// YieldOp / WaitOp / ReserveOp
+// YieldOp / WaitOp / ReserveOp / PushOp / PopOp
 //===----------------------------------------------------------------------===//
 
 mlir::LogicalResult YieldOp::verify() {
@@ -557,4 +557,70 @@ mlir::LogicalResult YieldOp::verify() {
   }
 
   return ::mlir::success();
+}
+
+bool PushOp::bufferizesToMemoryRead(
+    mlir::OpOperand &, const mlir::bufferization::AnalysisState &) {
+  return false;
+}
+
+bool PushOp::bufferizesToMemoryWrite(
+    mlir::OpOperand &, const mlir::bufferization::AnalysisState &) {
+  return false;
+}
+
+mlir::bufferization::AliasingValueList
+PushOp::getAliasingValues(mlir::OpOperand &,
+                          const mlir::bufferization::AnalysisState &) {
+  mlir::bufferization::AliasingValueList result;
+  return result;
+}
+
+mlir::FailureOr<mlir::bufferization::BufferLikeType> PushOp::getBufferType(
+    mlir::Value, const mlir::bufferization::BufferizationOptions &,
+    const mlir::bufferization::BufferizationState &,
+    ::llvm::SmallVector<mlir::Value> &) {
+  llvm_unreachable(
+      "intentionally unimplemented, this op can only accept block arguments "
+      "which should have already been converted");
+}
+
+mlir::LogicalResult
+PushOp::bufferize(mlir::RewriterBase &rewriter,
+                  const mlir::bufferization::BufferizationOptions &options,
+                  mlir::bufferization::BufferizationState &) {
+  return mlir::success();
+}
+
+bool PopOp::bufferizesToMemoryRead(
+    mlir::OpOperand &, const mlir::bufferization::AnalysisState &) {
+  return false;
+}
+
+bool PopOp::bufferizesToMemoryWrite(
+    mlir::OpOperand &, const mlir::bufferization::AnalysisState &) {
+  return false;
+}
+
+mlir::bufferization::AliasingValueList
+PopOp::getAliasingValues(mlir::OpOperand &,
+                         const mlir::bufferization::AnalysisState &) {
+  mlir::bufferization::AliasingValueList result;
+  return result;
+}
+
+mlir::FailureOr<mlir::bufferization::BufferLikeType> PopOp::getBufferType(
+    mlir::Value, const mlir::bufferization::BufferizationOptions &,
+    const mlir::bufferization::BufferizationState &,
+    ::llvm::SmallVector<mlir::Value> &) {
+  llvm_unreachable(
+      "intentionally unimplemented, this op can only accept block arguments "
+      "which should have already been converted");
+}
+
+mlir::LogicalResult
+PopOp::bufferize(mlir::RewriterBase &rewriter,
+                 const mlir::bufferization::BufferizationOptions &options,
+                 mlir::bufferization::BufferizationState &) {
+  return mlir::success();
 }
