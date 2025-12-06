1625ac99315c93e923d88c75132a0a3770675e2b
Hongseok Kwon <hkwon@tenstorrent.com>
2025-12-06 01:05:39 +0000

    P2P workaround - decompose for 2D transport

--- lib/Dialect/TTNN/Transforms/Workarounds/TTNNWorkaroundsPatterns.cpp
+++ lib/Dialect/TTNN/Transforms/Workarounds/TTNNWorkaroundsPatterns.cpp
@@ -24,6 +24,7 @@
 #include "ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/LinearOpRewritePattern.h"
 #include "ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/MultiplyOpDecompositionRewritePattern.h"
 #include "ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/PagedUpdateCacheOpRewritePattern.h"
+#include "ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/PointToPointOpRewritePattern.h"
 #include "ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/ReduceScatterOpRewritePattern.h"
 #include "ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/RotaryEmbeddingOpRewritePattern.h"
 #include "ttmlir/Dialect/TTNN/Transforms/Workarounds/Decomposition/ScaledDotProductAttentionDecodeConfigRewritePattern.h"
@@ -586,7 +587,8 @@ public:
           workarounds::decomposition::
               ScaledDotProductAttentionDecodeConfigRewritePattern,
           workarounds::decomposition::
-              ScaledDotProductAttentionPadTileDimsRewritePattern>(
+              ScaledDotProductAttentionPadTileDimsRewritePattern,
+          workarounds::decomposition::PointToPointOpRewritePattern>(
           &getContext());
 
       runRewritePatterns(std::move(patterns),
