8dcbc3c1e2869dd6db8e5992fa609a60fc724eb5
mbagherbeikTT <mbagherbeik@tenstorrent.com>
2025-12-09 00:33:31 +0000

    adding ttnn jit fusion tests

--- test/ttnn-jit/test_eltwise_composite.py
+++ test/ttnn-jit/test_eltwise_composite.py
@@ -322,3 +322,418 @@ def test_nested_composite_dram(device, shape, dtype):
         buffer_type=ttnn.BufferType.DRAM,
         graph_capture=True,
     )
+
+
+# ------------------------------------------------------------
+# Fusion Tests
+# ------------------------------------------------------------
+
+# Chain can be made arbitrarily long when using JIT (as long as the 
+# instructions fit on the tensix I-SRAM) and executed as one compute
+# kernel. With a sufficiently long chain and large enough input size,
+# regular ttnn will run out of memory while ttnn.jit will not.
+def long_unary_chain(input_tensor_a):
+    res_0 = ttnn.abs(input_tensor_a)
+    res_1 = ttnn.sin(res_0)
+    res_2 = ttnn.neg(res_1)
+    res_3 = ttnn.exp(res_2)
+
+    res_4 = ttnn.abs(res_3)
+    res_5 = ttnn.cos(res_4)
+    res_6 = ttnn.neg(res_5)
+    res_7 = ttnn.exp(res_6)
+
+    return res_7
+    # replace the above return with the code below
+    # to see shapes/grids where ttnn runs out of memory 
+    # but ttnn.jit, through fusion, does not.
+    
+    # res_8 = ttnn.neg(res_7)
+    # return res_8
+
+
+@pytest.mark.parametrize(
+    "shape, max_grid, memory_layout",
+    SHARDED_SHAPE_GRID_LAYOUTS,
+    ids=[
+        f"shape_{shape}_grid_{grid}_{layout}"
+        for shape, grid, layout in SHARDED_SHAPE_GRID_LAYOUTS
+    ],
+)
+@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32])
+def test_long_unary_chain_l1(device, shape, max_grid, dtype, memory_layout):
+    num_inputs = 1
+
+    run_op_test(
+        device,
+        shape,
+        max_grid,
+        dtype,
+        long_unary_chain,
+        num_inputs,
+        buffer_type=ttnn.BufferType.L1,
+        memory_layout=memory_layout,
+    )
+
+
+@pytest.mark.parametrize("shape", DRAM_SHAPES)
+@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32])
+def test_long_unary_chain_dram(device, shape, dtype):
+    num_inputs = 1
+
+    run_op_test(
+        device,
+        shape,
+        max_grid=(0, 0),
+        dtype=dtype,
+        op=long_unary_chain,
+        num_inputs=num_inputs,
+        buffer_type=ttnn.BufferType.DRAM,
+    )
+
+
+### ------------------------------------------------------------------------ ###
+
+
+# Example of fusing 2 unary op chains that eventually join through a binary op.
+# Unary chains can be made arbitrarily long with jit.
+def join_unary_chains(in0, in1):
+    chain_0_0 = ttnn.abs(in0)
+    chain_0_1 = ttnn.exp(chain_0_0)
+    chain_0_2 = ttnn.neg(chain_0_1)
+
+    chain_1_0 = ttnn.abs(in1)
+    chain_1_1 = ttnn.exp(chain_1_0)
+    chain_1_2 = ttnn.neg(chain_1_1)
+
+    return ttnn.add(chain_0_2, chain_1_2)
+
+
+@pytest.mark.parametrize(
+    "shape, max_grid, memory_layout",
+    SHARDED_SHAPE_GRID_LAYOUTS,
+    ids=[
+        f"shape_{shape}_grid_{grid}_{layout}"
+        for shape, grid, layout in SHARDED_SHAPE_GRID_LAYOUTS
+    ],
+)
+@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32])
+def test_join_unary_chains_l1(device, shape, max_grid, dtype, memory_layout):
+    num_inputs = 2
+
+    run_op_test(
+        device,
+        shape,
+        max_grid,
+        dtype,
+        join_unary_chains,
+        num_inputs,
+        buffer_type=ttnn.BufferType.L1,
+        memory_layout=memory_layout,
+        graph_capture=False
+    )
+
+
+@pytest.mark.parametrize("shape", DRAM_SHAPES)
+@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32])
+def test_join_unary_chains_dram(device, shape, dtype):
+    num_inputs = 2
+
+    run_op_test(
+        device,
+        shape,
+        max_grid=(0, 0),
+        dtype=dtype,
+        op=join_unary_chains,
+        num_inputs=num_inputs,
+        buffer_type=ttnn.BufferType.DRAM,
+        graph_capture=False
+    )
+
+
+### ------------------------------------------------------------------------ ###
+
+
+# The largest BALANCED op tree that can be fused into one d2m.generic and fully DST fused
+# when in 32b DST mode.
+def add_tree_7_to_1(
+    in0, in1, in2, in3, 
+    in4,in5, in6
+):
+    add_0_0 = ttnn.add(in0, in1)
+    add_0_1 = ttnn.add(in2, in3)
+    add_0_2 = ttnn.add(in4, in5)
+
+    add_1_0 = ttnn.add(add_0_0, add_0_1)
+    add_1_1 = ttnn.add(add_0_2, in6)
+
+    add_2_0 = ttnn.add(add_1_0, add_1_1)
+
+    return add_2_0
+
+
+@pytest.mark.parametrize(
+    "shape, max_grid, memory_layout",
+    SHARDED_SHAPE_GRID_LAYOUTS,
+    ids=[
+        f"shape_{shape}_grid_{grid}_{layout}"
+        for shape, grid, layout in SHARDED_SHAPE_GRID_LAYOUTS
+    ],
+)
+@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32])
+def test_add_tree_7_to_1_l1(device, shape, max_grid, dtype, memory_layout):
+    num_inputs = 7
+
+    if shape == (2, 512, 2048):
+        pytest.xfail(
+            "add_tree_7_to_1 runs out of memory in regular TTNN path"
+        )
+
+    run_op_test(
+        device,
+        shape,
+        max_grid,
+        dtype,
+        add_tree_7_to_1,
+        num_inputs,
+        buffer_type=ttnn.BufferType.L1,
+        memory_layout=memory_layout,
+        graph_capture=False
+    )
+
+
+@pytest.mark.parametrize("shape", DRAM_SHAPES)
+@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32])
+def test_add_tree_7_to_1_dram(device, shape, dtype):
+    num_inputs = 7
+
+    run_op_test(
+        device,
+        shape,
+        max_grid=(0, 0),
+        dtype=dtype,
+        op=add_tree_7_to_1,
+        num_inputs=num_inputs,
+        buffer_type=ttnn.BufferType.DRAM,
+        graph_capture=False
+    )
+
+
+### ------------------------------------------------------------------------ ###
+
+
+# The largest BALANCED op tree that can be fused into one d2m.generic and fully DST fused
+# when in 16b DST mode. Works with 32b as well but results in more ops/kernels
+def add_tree_31_to_1(
+    in0, in1, in2, in3, in4, in5, in6, in7,
+    in8, in9, in10, in11, in12, in13, in14, in15,
+    in16, in17, in18, in19, in20, in21, in22, in23,
+    in24, in25, in26, in27, in28, in29, in30
+):
+    # Level 0: 15 pairs + 1 unpaired (31 -> 16)
+    add_0_0 = ttnn.add(in0, in1)
+    add_0_1 = ttnn.add(in2, in3)
+    add_0_2 = ttnn.add(in4, in5)
+    add_0_3 = ttnn.add(in6, in7)
+    add_0_4 = ttnn.add(in8, in9)
+    add_0_5 = ttnn.add(in10, in11)
+    add_0_6 = ttnn.add(in12, in13)
+    add_0_7 = ttnn.add(in14, in15)
+    add_0_8 = ttnn.add(in16, in17)
+    add_0_9 = ttnn.add(in18, in19)
+    add_0_10 = ttnn.add(in20, in21)
+    add_0_11 = ttnn.add(in22, in23)
+    add_0_12 = ttnn.add(in24, in25)
+    add_0_13 = ttnn.add(in26, in27)
+    add_0_14 = ttnn.add(in28, in29)
+
+    # Level 1: 8 pairs (16 -> 8)
+    add_1_0 = ttnn.add(add_0_0, add_0_1)
+    add_1_1 = ttnn.add(add_0_2, add_0_3)
+    add_1_2 = ttnn.add(add_0_4, add_0_5)
+    add_1_3 = ttnn.add(add_0_6, add_0_7)
+    add_1_4 = ttnn.add(add_0_8, add_0_9)
+    add_1_5 = ttnn.add(add_0_10, add_0_11)
+    add_1_6 = ttnn.add(add_0_12, add_0_13)
+    add_1_7 = ttnn.add(add_0_14, in30)
+
+    # Level 2: 4 pairs (8 -> 4)
+    add_2_0 = ttnn.add(add_1_0, add_1_1)
+    add_2_1 = ttnn.add(add_1_2, add_1_3)
+    add_2_2 = ttnn.add(add_1_4, add_1_5)
+    add_2_3 = ttnn.add(add_1_6, add_1_7)
+
+    # Level 3: 2 pairs (4 -> 2)
+    add_3_0 = ttnn.add(add_2_0, add_2_1)
+    add_3_1 = ttnn.add(add_2_2, add_2_3)
+
+    # Level 4: 1 pair (2 -> 1)
+    add_4_0 = ttnn.add(add_3_0, add_3_1)
+
+    return add_4_0
+
+
+@pytest.mark.parametrize(
+    "shape, max_grid, memory_layout",
+    SHARDED_SHAPE_GRID_LAYOUTS,
+    ids=[
+        f"shape_{shape}_grid_{grid}_{layout}"
+        for shape, grid, layout in SHARDED_SHAPE_GRID_LAYOUTS
+    ],
+)
+@pytest.mark.parametrize("dtype", [torch.bfloat16])
+def test_add_tree_31_to_1_l1(device, shape, max_grid, dtype, memory_layout):
+    num_inputs = 31
+
+    size_limit = 256 * 256
+
+    elements = 1
+    for dim in shape:
+        elements *= dim
+        
+    if elements > size_limit:
+        pytest.xfail(
+            "add_tree_31_to_1 runs out of memory"
+        )
+    
+    if shape == (2, 32, 384):
+        pytest.xfail(
+            "add_tree_31_to_1 fails all close"
+        )
+
+    run_op_test(
+        device,
+        shape,
+        max_grid,
+        dtype,
+        add_tree_31_to_1,
+        num_inputs,
+        buffer_type=ttnn.BufferType.L1,
+        memory_layout=memory_layout,
+        graph_capture=False
+    )
+
+
+@pytest.mark.parametrize("shape", DRAM_SHAPES)
+@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32])
+def test_add_tree_31_to_1_dram(device, shape, dtype):
+    num_inputs = 31
+
+    run_op_test(
+        device,
+        shape,
+        max_grid=(0, 0),
+        dtype=dtype,
+        op=add_tree_31_to_1,
+        num_inputs=num_inputs,
+        buffer_type=ttnn.BufferType.DRAM,
+        graph_capture=False
+    )
+
+
+### ------------------------------------------------------------------------ ###
+
+
+# A ladder pattern where each add output is summed with the next input sequentially.
+# Unlike the balanced tree, this creates a long dependency chain.
+def adder_ladder_31(
+    in0, in1, in2, in3, in4, in5, in6, in7,
+    in8, in9, in10, in11, in12, in13, in14, in15,
+    in16, in17, in18, in19, in20, in21, in22, in23,
+    in24, in25, in26, in27, in28, in29, in30
+):
+    add_0 = ttnn.add(in0, in1)
+    add_1 = ttnn.add(add_0, in2)
+    add_2 = ttnn.add(add_1, in3)
+    add_3 = ttnn.add(add_2, in4)
+    add_4 = ttnn.add(add_3, in5)
+    add_5 = ttnn.add(add_4, in6)
+    add_6 = ttnn.add(add_5, in7)
+    add_7 = ttnn.add(add_6, in8)
+    add_8 = ttnn.add(add_7, in9)
+    add_9 = ttnn.add(add_8, in10)
+    add_10 = ttnn.add(add_9, in11)
+    add_11 = ttnn.add(add_10, in12)
+    add_12 = ttnn.add(add_11, in13)
+    add_13 = ttnn.add(add_12, in14)
+    add_14 = ttnn.add(add_13, in15)
+    add_15 = ttnn.add(add_14, in16)
+    add_16 = ttnn.add(add_15, in17)
+    add_17 = ttnn.add(add_16, in18)
+    add_18 = ttnn.add(add_17, in19)
+    add_19 = ttnn.add(add_18, in20)
+    add_20 = ttnn.add(add_19, in21)
+    add_21 = ttnn.add(add_20, in22)
+    add_22 = ttnn.add(add_21, in23)
+    add_23 = ttnn.add(add_22, in24)
+    add_24 = ttnn.add(add_23, in25)
+    add_25 = ttnn.add(add_24, in26)
+    add_26 = ttnn.add(add_25, in27)
+    add_27 = ttnn.add(add_26, in28)
+    add_28 = ttnn.add(add_27, in29)
+    add_29 = ttnn.add(add_28, in30)
+
+    return add_29
+
+
+@pytest.mark.parametrize(
+    "shape, max_grid, memory_layout",
+    SHARDED_SHAPE_GRID_LAYOUTS,
+    ids=[
+        f"shape_{shape}_grid_{grid}_{layout}"
+        for shape, grid, layout in SHARDED_SHAPE_GRID_LAYOUTS
+    ],
+)
+@pytest.mark.parametrize("dtype", [torch.float32])
+def test_adder_ladder_31_l1(device, shape, max_grid, dtype, memory_layout):
+    num_inputs = 31
+
+    if dtype is torch.bfloat16:
+        pytest.xfail(
+            "adder_ladder_31 fails all close"
+        )
+
+    size_limit = 256 * 256
+    elements = 1
+
+    for dim in shape:
+        elements *= dim
+        
+    if elements > size_limit or (shape == (64, 128) and max_grid == (0,0)):
+        pytest.xfail(
+            "adder_ladder_31 runs out of memory"
+        )
+
+    run_op_test(
+        device,
+        shape,
+        max_grid,
+        dtype,
+        adder_ladder_31,
+        num_inputs,
+        buffer_type=ttnn.BufferType.L1,
+        memory_layout=memory_layout,
+        graph_capture=False
+    )
+
+
+@pytest.mark.parametrize("shape", DRAM_SHAPES)
+@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float32])
+def test_adder_ladder_31_dram(device, shape, dtype):
+    num_inputs = 31
+
+    if dtype is torch.bfloat16:
+        pytest.xfail(
+            "adder_ladder_31 fails all close"
+        )
+
+    run_op_test(
+        device,
+        shape,
+        max_grid=(0, 0),
+        dtype=dtype,
+        op=adder_ladder_31,
+        num_inputs=num_inputs,
+        buffer_type=ttnn.BufferType.DRAM,
+        graph_capture=False
+    )
