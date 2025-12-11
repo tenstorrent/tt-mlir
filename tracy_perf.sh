fe01ecdb732e443e57f2f740dc34dd4d8447956d
Brandon Rapanan <brapanan@tenstorrent.com>
2025-12-11 20:45:33 +0000

    updated perf script

--- tracy_perf.sh
+++ tracy_perf.sh
@@ -3,7 +3,8 @@ set -e
 
 source env/activate
 export TT_METAL_DEVICE_PROFILER=1
-export TT_METAL_PROFILER_DIR=/localdev/brapanan/tt-mlir/generated/profiler_jit_muladd/
+#export TT_METAL_PROFILER_DIR=/localdev/brapanan/tt-mlir/generated/profiler_jit_muladd/
+export TT_METAL_PROFILER_DIR=/localdev/brapanan/tt-mlir/generated/profiler_jit_matmul/
 export TT_METAL_HOME=/localdev/brapanan/tt-mlir/third_party/tt-metal/src/tt-metal
 export PYTHONPATH="${PYTHONPATH}:${TT_METAL_HOME}/ttnn:${TT_METAL_HOME}:${TT_METAL_HOME}/tools"
 
@@ -25,6 +26,6 @@ export PYTHONPATH="${PYTHONPATH}:${TT_METAL_HOME}/ttnn:${TT_METAL_HOME}:${TT_MET
 
 for test_params in "512-512-512" "512-1024-1024" "512-1024-2048" "1024-1024-1024" "1024-1024-2048" "1024-2048-2048" "2048-2048-2048"; do
     python -m tracy -r -m pytest "test/ttnn-jit/test_matmul_trace.py::test_matmul_block_sharded_trace[$test_params]"
-    python3 generated/extract_perf_columns.py -d $TT_METAL_PROFILER_DIR -o ${TT_METAL_PROFILER_DIR}/${test_params}_clean.csv
-    python3 generated/extract_total_durations.py ${TT_METAL_PROFILER_DIR}/${test_params}_clean.csv -b 1
+    python3 generated/extract_perf_columns.py -d $TT_METAL_PROFILER_DIR -o ${TT_METAL_PROFILER_DIR}/${test_params}_matmul_f32_clean.csv
+    python3 generated/extract_total_durations.py ${TT_METAL_PROFILER_DIR}/${test_params}_matmul_f32_clean.csv -b 1
 done
