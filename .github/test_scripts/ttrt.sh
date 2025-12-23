d8c6bd5a223a7ad941cb24b9a441a4cfe7bd13ff
jgrimTT <jgrim@tenstorrent.com>
2025-12-23 22:05:09 +0000

    restoring ttrt.sh

--- .github/test_scripts/ttrt.sh
+++ .github/test_scripts/ttrt.sh
@@ -11,12 +11,5 @@ set -e -o pipefail
 
 echo "Running TTRT tests"
 eval ttrt "$1" "$BUILD_DIR/test/ttmlir/$2" "$3"
-
-# Copy results only if files exist
-if [ -f "${1}_results.json" ]; then
-	cp "${1}_results.json" "${TTRT_REPORT_PATH}"
-fi
-
-if [ -f "ttrt_report.xml" ]; then
-	cp "ttrt_report.xml" "$TEST_REPORT_PATH"
-fi
+cp ${1}_results.json ${TTRT_REPORT_PATH} || true
+cp ttrt_report.xml $TEST_REPORT_PATH || true
