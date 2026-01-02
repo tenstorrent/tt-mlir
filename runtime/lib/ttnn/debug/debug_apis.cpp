e064b3ea6e1f09679932a92da03e88cd1ef30a1e
James Xu <jameszianxu@tenstorrent.com>
2026-01-02 20:43:39 +0000

    [tmp] save het identity shard w/a

--- runtime/lib/ttnn/debug/debug_apis.cpp
+++ runtime/lib/ttnn/debug/debug_apis.cpp
@@ -15,50 +15,50 @@ namespace tt::runtime::ttnn::debug {
 void checkTensorRefMatchesTTNNTensor(
     const ::tt::target::ttnn::TensorRef *tensorRef,
     const ::ttnn::Tensor &ttnnTensor) {
-  ::ttnn::Layout expectedLayout =
-      ::tt::runtime::ttnn::utils::inferLayoutFromTileShape(tensorRef);
-  ::ttnn::Layout actualLayout = ttnnTensor.layout();
-  DEBUG_ASSERT(expectedLayout == actualLayout, "Layout mismatch, expected ",
-               toString(expectedLayout), ", got ", toString(actualLayout));
+//   ::ttnn::Layout expectedLayout =
+//       ::tt::runtime::ttnn::utils::inferLayoutFromTileShape(tensorRef);
+//   ::ttnn::Layout actualLayout = ttnnTensor.layout();
+//   DEBUG_ASSERT(expectedLayout == actualLayout, "Layout mismatch, expected ",
+//                toString(expectedLayout), ", got ", toString(actualLayout));
 
-  ::ttnn::DataType expectedDataType =
-      ::tt::runtime::ttnn::utils::toTTNNDataType(
-          tensorRef->desc()->layout()->memory_desc()->data_type());
-  ::ttnn::DataType actualDataType = ttnnTensor.dtype();
-  DEBUG_ASSERT(expectedDataType == actualDataType,
-               "DataType mismatch, expected ", toString(expectedDataType),
-               ", got ", toString(actualDataType));
+//   ::ttnn::DataType expectedDataType =
+//       ::tt::runtime::ttnn::utils::toTTNNDataType(
+//           tensorRef->desc()->layout()->memory_desc()->data_type());
+//   ::ttnn::DataType actualDataType = ttnnTensor.dtype();
+//   DEBUG_ASSERT(expectedDataType == actualDataType,
+//                "DataType mismatch, expected ", toString(expectedDataType),
+//                ", got ", toString(actualDataType));
 
-  ::ttnn::StorageType expectedStorageType =
-      ::tt::runtime::ttnn::utils::toTTNNStorageType(
-          tensorRef->desc()->layout()->memory_desc()->storage_type());
-  ::ttnn::StorageType actualStorageType = ttnnTensor.storage_type();
+//   ::ttnn::StorageType expectedStorageType =
+//       ::tt::runtime::ttnn::utils::toTTNNStorageType(
+//           tensorRef->desc()->layout()->memory_desc()->storage_type());
+//   ::ttnn::StorageType actualStorageType = ttnnTensor.storage_type();
 
-  // With TT-Mesh backend, single device tensors are also
-  // MULTI_DEVICE_HOST_STORAGE Therefore, for host tensors we loosen the
-  // constraint a bit and just check that the storage type is on host
-  if (utils::isOnHost(expectedStorageType)) {
-    DEBUG_ASSERT(
-        utils::isOnHost(actualStorageType), "Storage type mismatch, expected ",
-        toString(expectedStorageType), ", got ", toString(actualStorageType));
-  } else {
-    DEBUG_ASSERT(expectedStorageType == actualStorageType,
-                 "Storage type mismatch, expected ",
-                 toString(expectedStorageType), ", got ",
-                 toString(actualStorageType));
-  }
+//   // With TT-Mesh backend, single device tensors are also
+//   // MULTI_DEVICE_HOST_STORAGE Therefore, for host tensors we loosen the
+//   // constraint a bit and just check that the storage type is on host
+//   if (utils::isOnHost(expectedStorageType)) {
+//     DEBUG_ASSERT(
+//         utils::isOnHost(actualStorageType), "Storage type mismatch, expected ",
+//         toString(expectedStorageType), ", got ", toString(actualStorageType));
+//   } else {
+//     DEBUG_ASSERT(expectedStorageType == actualStorageType,
+//                  "Storage type mismatch, expected ",
+//                  toString(expectedStorageType), ", got ",
+//                  toString(actualStorageType));
+//   }
 
-  if (!::tt::runtime::ttnn::utils::inSystemMemory(tensorRef)) {
-    const ::tt::target::ttnn::MemoryConfig *memcfg =
-        ::tt::runtime::ttnn::utils::getTensorRefMemoryConfig(tensorRef);
-    DEBUG_ASSERT(memcfg, "Device tensor must have memory config");
-    ::ttnn::MemoryConfig expectedMemoryConfig =
-        ::tt::runtime::ttnn::utils::createMemoryConfigIfNeeded(memcfg).value();
-    ::ttnn::MemoryConfig actualMemoryConfig = ttnnTensor.memory_config();
-    DEBUG_ASSERT(expectedMemoryConfig == actualMemoryConfig,
-                 "Memory config mismatch, expected ", expectedMemoryConfig,
-                 ", got ", actualMemoryConfig);
-  }
+//   if (!::tt::runtime::ttnn::utils::inSystemMemory(tensorRef)) {
+//     const ::tt::target::ttnn::MemoryConfig *memcfg =
+//         ::tt::runtime::ttnn::utils::getTensorRefMemoryConfig(tensorRef);
+//     DEBUG_ASSERT(memcfg, "Device tensor must have memory config");
+//     ::ttnn::MemoryConfig expectedMemoryConfig =
+//         ::tt::runtime::ttnn::utils::createMemoryConfigIfNeeded(memcfg).value();
+//     ::ttnn::MemoryConfig actualMemoryConfig = ttnnTensor.memory_config();
+//     DEBUG_ASSERT(expectedMemoryConfig == actualMemoryConfig,
+//                  "Memory config mismatch, expected ", expectedMemoryConfig,
+//                  ", got ", actualMemoryConfig);
+//   }
 }
 
 } // namespace tt::runtime::ttnn::debug
