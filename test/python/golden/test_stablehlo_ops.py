d1e99de2906b40272312b674a99c6a78abc0209e
Djordje Stefanovic <djstefanovic@tenstorrent.com>
2026-01-05 10:44:49 +0000

    Fixes.

--- test/python/golden/test_stablehlo_ops.py
+++ test/python/golden/test_stablehlo_ops.py
@@ -1329,6 +1329,7 @@ def test_all_gather(target: str, request, device):
         mesh_dict=OrderedDict([("x", 1), ("y", 1)]),
     )
 
+
 @pytest.mark.parametrize(
     "shapes,stride,padding,dilation,groups",
     [
