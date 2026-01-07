3c2258ba99af2224c85c717e38ed5b60c00051e5
Armin Ale <arminale@tenstorrent.com>
2026-01-07 09:38:01 -0500

    Fix Incorrect Parsing of MemoryConfigAttr Params By Adding a Custom Parser and Printer (#6512)
    ### Problem description
    The `MemoryConfigAttr` has optional `ShardSpecAttr` and
    `NDShardSpecAttr` params. These params are mutually exclusive. Currently
    when `NDShardSpecAttr` is present in a `MemoryConfigAttr`, it is
    serialized as:
    ```
    #nd_shard_spec = ...
    #l1 = ...
    #ttnn.memory_config<#l1, <block_sharded>,  #nd_shard_spec>
    ```
    Upon deserialization, the `nd_shard_spec` will be incorrectly loaded as
    the `shardSpec` parameter. In other words, serializing and deserializing
    a `MemoryConfigAttr` with an `NdShardSpec` **always** results in an
    attribute that has:
    ```
    static_cast<bool>(getNdShardSpec()) == false
    static_cast<bool>(getShardSpec()) == true
    ```
    This is an unanticipated side effect of MLIRs parsing logic and this
    attribute's `assemblyFormat`. MLIR always matches optional groups in
    order, thereby matching the serialized `nd_shard_spec` to the
    `shardSpec` parameter on the object.
    
    ### What's changed
    - Created a custom parser and printer for `MemoryConfigAttr`
      - Printing:
    - `bufferType`, `tensorMemoryLayout` and `shardSpec` behave as before
    - If `NdShardSpec` is not null, it is printed with a prefix:
    `ndShardSpec = #ttnn.nd_shard_spec<...>`
      - Parsing:
        - Always expect to parse `bufferType`
        - Greedily attempt to parse `tensorMemoryLayout` and `shardSpec`
        - Only attempt to parse `ndShardSpec` if the prefix is found
    - All existing serialized `MemoryConfigAttr` objects in lit tests are
    still valid and interpreted as before
    - Updated the `nd_layout_encoding` IR test to a parametrization that
    will fail if the `NDShardSpec` is misinterpreted as a `ShardSpec`. The
    previous parametrization passed by accident
    
    ### Evaluated and rejected alternatives
    #### Adding the `ndShardSpec` prefix in the `assemblyFormat`
    ```
    let assemblyFormat = "`<` $bufferType (`,` $tensorMemoryLayout^ )? (`,` $shardSpec^ )? (`,` `ndShardSpec` `=` $ndShardSpec^ )? `>`";
    ```
    The generated parser incorrectly asserts that there are mismatched
    brackets whenever an ndShardSpec is printed.
    
    #### Adding prefixes for all optional params in `assemblyFormat`
    ```
    let assemblyFormat = "`<` $bufferType (`,` `memoryLayout` `=` $tensorMemoryLayout^ )? (`,` `shardSpec` `=` $shardSpec^ )? (`,` `ndShardSpec` `=` $ndShardSpec^ )? `>`";
    ```
    The generated parser incorrectly asserts that later prefixes can only be
    present if earlier ones exist. i.e. the following IR snippet fails with
    `expected "shardSpec"` error:
    ```
    memory_config = #ttnn.memory_config<#l1, <block_sharded>, ndShardSpec = #nd_shard_spec>
    ```
    
    #### Always printing all params with a prefix, even if null
    ```
    let assemblyFormat = "`<` $bufferType `,` `memoryLayout` `=` $tensorMemoryLayout `,` `shardSpec` `=` $shardSpec `ndShardSpec` `=` $ndShardSpec `>`";
    ```
    The parser fails to interpret MemoryConfigAttr objects with null
    memoryLayout, i.e `ttnn.memory_config<#system_memory, memoryLayout = ,
    shardSpec = , ndShardSpec = >`. This is likely because
    `tensorMemoryLayout` is an enum attribute.
    
    #### Using a `struct` in the assemblyFormat
    Does not seem to support optional params.
    
    ### Checklist
    - [X] New/Existing tests provide coverage for changes

--- lib/Dialect/TTNN/IR/TTNNOpsAttrs.cpp
+++ lib/Dialect/TTNN/IR/TTNNOpsAttrs.cpp
@@ -712,6 +712,88 @@ MemoryConfigAttr MemoryConfigAttr::get(
                              .succeeded());
 }
 
+// Manually parse MemoryConfigAttr to avoid various issues with the tablegen
+// parser in dealing with multiple optional parameters. See PR #6512 for more
+// details.
+mlir::Attribute MemoryConfigAttr::parse(::mlir::AsmParser &parser,
+                                        ::mlir::Type type) {
+  ::llvm::SMLoc loc = parser.getCurrentLocation();
+  if (parser.parseLess()) {
+    return {};
+  }
+
+  BufferTypeAttr bufferType;
+  if (parser.parseCustomAttributeWithFallback(bufferType)) {
+    return {};
+  }
+
+  TensorMemoryLayoutAttr tensorMemoryLayout;
+  std::optional<ShardSpecAttr> shardSpec;
+  std::optional<NDShardSpecAttr> ndShardSpec;
+
+  while (parser.parseOptionalComma().succeeded()) {
+    // Attempt to parse the optional param as a shardSpecAttr.
+    ShardSpecAttr maybeShardSpec;
+    OptionalParseResult shardSpecResult =
+        parser.parseOptionalAttribute(maybeShardSpec);
+    if (shardSpecResult.has_value()) {
+      if (succeeded(*shardSpecResult)) {
+        shardSpec = maybeShardSpec;
+        continue;
+      }
+    }
+
+    // Attempt to parse the optional param as a tensorMemoryLayoutAttr.
+    TensorMemoryLayoutAttr tml;
+    if (succeeded(parser.parseCustomAttributeWithFallback(tml))) {
+      tensorMemoryLayout = tml;
+      continue;
+    }
+
+    // Only attempt to parse the param as an NDShardSpecAttr if the keyword is
+    // explicitly specified.
+    if (parser.parseOptionalKeyword("ndShardSpec").succeeded()) {
+      if (parser.parseEqual()) {
+        return {};
+      }
+      NDShardSpecAttr attr;
+      if (parser.parseCustomAttributeWithFallback(attr)) {
+        return {};
+      }
+      ndShardSpec = attr;
+      continue;
+    }
+
+    return {};
+  }
+
+  if (parser.parseGreater()) {
+    return {};
+  }
+
+  // getChecked outputs verification errors instead of just crashing.
+  return MemoryConfigAttr::getChecked(
+      [loc, &parser]() { return parser.emitError(loc); }, parser.getContext(),
+      tensorMemoryLayout, bufferType, shardSpec, ndShardSpec);
+}
+
+void MemoryConfigAttr::print(::mlir::AsmPrinter &p) const {
+  p << "<";
+  p.printStrippedAttrOrType(getBufferType());
+  if (getTensorMemoryLayout()) {
+    p << ", ";
+    p.printStrippedAttrOrType(getTensorMemoryLayout());
+  }
+  if (getShardSpec()) {
+    p << ", ";
+    p.printStrippedAttrOrType(getShardSpec());
+  } else if (getNdShardSpec()) {
+    p << ", ndShardSpec = ";
+    p.printStrippedAttrOrType(getNdShardSpec());
+  }
+  p << ">";
+}
+
 bool CoreRangeAttr::intersects(CoreRangeAttr other) const {
   bool thisEndsBeforeOtherStarts =
       this->getEndCoord().getX() < other.getStartCoord().getX();
