df6359ff4a0ec440625207029b6cb2708569df65
svuckovic <svuckovic@tenstorrent.com>
2026-01-14 15:21:37 +0000

    Fix tt-alchemist's generate-python API for TTNN IR input

--- include/ttmlir/Dialect/TTNN/Pipelines/TTNNPipelines.h
+++ include/ttmlir/Dialect/TTNN/Pipelines/TTNNPipelines.h
@@ -533,6 +533,9 @@ void createTTIRToEmitCPipeline(OpPassManager &pm,
 void createTTIRToEmitPyPipeline(OpPassManager &pm,
                                 const TTIRToEmitPyPipelineOptions &options);
 
+void createWorkaroundMixedTTIRTTNNToEmitPyPipeline(
+    OpPassManager &pm, const TTNNToEmitPyDevicePipelineOptions &options);
+
 void registerTTNNPipelines();
 } // namespace mlir::tt::ttnn
 
