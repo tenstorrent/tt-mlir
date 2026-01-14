5136a16d67b05dc6c41de0acb08181c9297e1cbc
Tapasvi Patel <tpatel@tenstorrent.com>
2026-01-14 20:15:14 +0000

    Support presharded/unsharded graphs in builder

--- tools/golden/mapping.py
+++ tools/golden/mapping.py
@@ -188,6 +188,11 @@ class GoldenMapTensor:
     def __rsub__(self, other):
         return self._binary_map(other, lambda a, b: operator.sub(b, a))
 
+    def __str__(self) -> str:
+        return (
+            f"GoldenMapTensor(mesh_shape={self._mesh_shape}, shards={self._shard_map})"
+        )
+
     @staticmethod
     def _walk_tree(*trees) -> Iterable:
         # Yield leaves in a nested structure.
@@ -3920,6 +3925,8 @@ def ttir_mesh_shard_golden(
     if shard_direction == ttcore.ir.MeshShardDirection.FullToShard:
         if shard_type == ttcore.ir.MeshShardType.Replicate:
             shard_dims = [None] * len(mesh_shape)
+        elif shard_type == ttcore.ir.MeshShardType.Identity:
+            return input.clone().to(output_dtype)
         return apply_sharding(input, mesh_shape, shard_dims)
     elif shard_direction == ttcore.ir.MeshShardDirection.ShardToFull:
         if shard_type == ttcore.ir.MeshShardType.Replicate:
