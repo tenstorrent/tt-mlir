125692e5baf16c62dc014f33d0483c5609569438
Vladimir Milosevic <157983820+vmilosevic@users.noreply.github.com>
2026-01-19 18:09:36 +0100

    Uplift third_party/tt-metal to acd0707afc9ca0e276a3f1eafecccb534a0c7772 2026-01-15 (#6643)
    This PR uplifts the third_party/tt-metal to the
    acd0707afc9ca0e276a3f1eafecccb534a0c7772
    
    **Note** Dockerfile.base is modified due to dependency updates
    
    ### Checklist
    - **Frontend CI passing links**
    - [X]
    [tt-forge-fe](https://github.com/tenstorrent/tt-forge-fe/actions/runs/21089792998):
    - [X] [tt-xla
    (mlir-uplift-qualification)](https://github.com/tenstorrent/tt-xla/actions/runs/21080934271):
    - **Follow-up Actions**
      - [X] **Issues filed** to follow up on incomplete changes (if any):
    - tt-metal's umd uplift `0a1325d` causing issues on tt-mlir build
    https://github.com/tenstorrent/tt-umd/issues/1864
      - [X] **Frontend fix PRs** ready (if needed by this uplift):
          - https://github.com/tenstorrent/tt-xla/pull/2895
          - https://github.com/tenstorrent/tt-forge-onnx/pull/3148
    
    ---------
    
    Co-authored-by: jazpurTT <200671331+jazpurTT@users.noreply.github.com>
    Co-authored-by: Jonathan Azpur <jazpur@tenstorrent.com>

--- lib/OpModel/TTNN/TTNNOpModel.cpp
+++ lib/OpModel/TTNN/TTNNOpModel.cpp
@@ -91,7 +91,8 @@ executeConstraintQuery(Callable &callable) {
             query.error_message.value_or("<error message not set>"));
   }
 
-  if (!query.output_tensor_spec.has_value()) {
+  if (!query.output_tensor_specs.has_value() ||
+      query.output_tensor_specs->empty()) {
     return llvm::createStringError(llvm::inconvertibleErrorCode(),
                                    "Op constraint query missing output tensor");
   }
@@ -129,13 +130,13 @@ llvm::Expected<OpConstraints> getOpConstraints(MLIRContext *context,
 
   ::ttnn::graph::ConstraintQueryResponse response = query.get();
 
-  return OpConstraints(
-      response.resource_usage.cb_peak_size_per_core,
-      response.resource_usage.l1_buffers_peak_per_core,
-      response.resource_usage.peak_memory_usage_per_core,
-      response.resource_usage.l1_output_buffer_per_core,
-      conversion::getLayoutAttrFromTensorSpec(
-          context, response.output_tensor_spec.value(), deviceGrid.getShape()));
+  return OpConstraints(response.resource_usage.cb_peak_size_per_core,
+                       response.resource_usage.l1_buffers_peak_per_core,
+                       response.resource_usage.peak_memory_usage_per_core,
+                       response.resource_usage.l1_output_buffer_per_core,
+                       conversion::getLayoutAttrFromTensorSpec(
+                           context, response.output_tensor_specs.value()[0],
+                           deviceGrid.getShape()));
 }
 
 template <class Callable>
@@ -645,8 +646,9 @@ llvm::Expected<::ttnn::TensorSpec> getPrepareConv2dWeightsOpOutputTensorSpec(
     return output.takeError();
   }
 
-  assert(output.get().output_tensor_spec.has_value());
-  return output.get().output_tensor_spec.value();
+  assert(output.get().output_tensor_specs.has_value() &&
+         !output.get().output_tensor_specs->empty());
+  return output.get().output_tensor_specs.value()[0];
 }
 
 // Returns the output tensor spec of the prepared bias for a conv2d op.
@@ -744,8 +746,9 @@ getPrepareConv2dBiasOpOutputTensorSpec(
     return output.takeError();
   }
 
-  assert(output.get().output_tensor_spec.has_value());
-  return output.get().output_tensor_spec.value();
+  assert(output.get().output_tensor_specs.has_value() &&
+         !output.get().output_tensor_specs->empty());
+  return output.get().output_tensor_specs.value().at(0);
 }
 
 #endif // TTMLIR_ENABLE_OPMODEL
@@ -6528,17 +6531,18 @@ llvm::Expected<OpConstraints> OpModel<UpsampleOp>::getOpConstraints(
       SingletonDeviceContext::getInstance().getDevice();
 
   // Convert params
-  std::variant<int, ::tt::tt_metal::Array2D> convertedScaleFactor;
+  std::variant<int, std::array<int, 2>, float, std::array<float, 2>>
+      convertedScaleFactor;
   if (auto value = mlir::dyn_cast<mlir::IntegerAttr>(scaleFactor)) {
     convertedScaleFactor = static_cast<int>(value.getSInt());
   } else if (auto tuple =
                  mlir::dyn_cast<::mlir::detail::DenseArrayAttrImpl<int32_t>>(
                      scaleFactor);
              tuple.size() == 2) {
-    std::array<uint32_t, 2> arr;
-    arr[0] = static_cast<uint32_t>(tuple[0]);
-    arr[1] = static_cast<uint32_t>(tuple[1]);
-    convertedScaleFactor = ::tt::tt_metal::Array2D(arr);
+    std::array<int, 2> arr;
+    arr[0] = static_cast<int>(tuple[0]);
+    arr[1] = static_cast<int>(tuple[1]);
+    convertedScaleFactor = arr;
   } else {
     return llvm::createStringError("Invalid scaleFactor");
   }
@@ -6574,17 +6578,18 @@ llvm::Expected<size_t> OpModel<UpsampleOp>::getOpRuntime(
       SingletonDeviceContext::getInstance().getDevice();
 
   // Convert parameters
-  std::variant<int, ::tt::tt_metal::Array2D> convertedScaleFactor;
+  std::variant<int, std::array<int, 2>, float, std::array<float, 2>>
+      convertedScaleFactor;
   if (auto value = mlir::dyn_cast<mlir::IntegerAttr>(scaleFactor)) {
     convertedScaleFactor = static_cast<int>(value.getSInt());
   } else if (auto tuple =
                  mlir::dyn_cast<::mlir::detail::DenseArrayAttrImpl<int32_t>>(
                      scaleFactor);
              tuple.size() == 2) {
-    std::array<uint32_t, 2> arr;
-    arr[0] = static_cast<uint32_t>(tuple[0]);
-    arr[1] = static_cast<uint32_t>(tuple[1]);
-    convertedScaleFactor = ::tt::tt_metal::Array2D(arr);
+    std::array<int, 2> arr;
+    arr[0] = static_cast<int>(tuple[0]);
+    arr[1] = static_cast<int>(tuple[1]);
+    convertedScaleFactor = arr;
   } else {
     return llvm::createStringError("Invalid scaleFactor");
   }
