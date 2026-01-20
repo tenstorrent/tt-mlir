1513ef636085fe741e10ab509a0d3dd49cef89fe
Jacob DeSousa <jdesousa@tenstorrent.com>
2026-01-20 23:56:05 +0000

    implement spill&scratch pass and fpuloopfission pass

--- lib/Dialect/TTMetal/Pipelines/TTMetalPipelines.cpp
+++ lib/Dialect/TTMetal/Pipelines/TTMetalPipelines.cpp
@@ -170,7 +170,8 @@ void createTTIRToTTMetalMiddleendPipeline(
     ;
   }
   pm.addPass(d2m::createD2MOpScheduler(opSchedulerOptions));
-
+  pm.addPass(d2m::createD2MSpillAndScratch());
+  pm.addPass(d2m::createD2MFPUScratchFission());
   d2m::D2MInsertDstRegisterAccessOptions insertDstRegisterAccessOptions;
   {
     insertDstRegisterAccessOptions.useTileMatmul = options.useTileMatmul;
