c73c9394553e00f5ebab192117ca2feabe934edc
Het Shah <hshah@tenstorrent.com>
2026-01-21 05:45:50 +0000

    first try at adding new ttir and ttnn op for mesh_partition

--- runtime/lib/ttnn/runtime.cpp
+++ runtime/lib/ttnn/runtime.cpp
@@ -1218,6 +1218,10 @@ getOpOutputRef(OpContext opContextHandle,
     tensorRef = opContext.type_as_MeshShardOp()->out();
     break;
   }
+  case ::tt::target::ttnn::OpType::MeshPartitionOp: {
+    tensorRef = opContext.type_as_MeshPartitionOp()->out();
+    break;
+  }
   case ::tt::target::ttnn::OpType::ArangeOp: {
     tensorRef = opContext.type_as_ArangeOp()->out();
     break;
@@ -1618,6 +1622,10 @@ getOpInputRefs(OpContext opContextHandle,
   }
   case ::tt::target::ttnn::OpType::MeshShardOp: {
     tensorRefs = {opContext.type_as_MeshShardOp()->in()};
+    break;
+  }
+  case ::tt::target::ttnn::OpType::MeshPartitionOp: {
+    tensorRefs = {opContext.type_as_MeshPartitionOp()->out()};
     break;
   }
   case ::tt::target::ttnn::OpType::UpsampleOp: {
