087f54c39b3e1362598d2bf24d163ec0506821fa
Tapasvi Patel <133996364+tapspatel@users.noreply.github.com>
2026-01-27 09:19:26 -0600

    Support presharded input and output execution in ttrt (#6638)
    This PR support execution of mlir graphs that have presharded
    inputs/outputs via ttrt.
    
    In introduces a RuntimeTensorShardingAttr for tensor types, which encode
    the shard status of that tensor and it's local shape. Shard status can
    be one of presharded/unsharded and local shape is the shape of the
    tensor on device. This information gets captured during lowering in
    stablehlo-pipeline during `createApplyArgumentShardStatusPass`. Then,
    when translating to flatbuffer executable, each input/output tensor will
    get appended with this information, so runtime knows how to generate
    these inputs and collect the outputs.

--- include/ttmlir/Target/Utils/FuncOpToProgram.h
+++ include/ttmlir/Target/Utils/FuncOpToProgram.h
@@ -72,15 +72,54 @@ funcOpToProgram(FlatbufferObjectCache &cache, func::FuncOp entry, FnT fn,
   program.name = entry.getSymName().data();
 
   for (auto &input : entry.getBody().getArguments()) {
-    program.inputs.push_back(cache.getOrCreate(input, tensorValueToFlatbuffer));
+    // Get argument encoding to determine sharding status.
+    mlir::DictionaryAttr argAttrDict =
+        entry.getArgAttrDict(input.getArgNumber());
+    ttcore::ShardStatus shardStatus = ttcore::ShardStatus::Unsharded;
+    mlir::RankedTensorType localShape =
+        mlir::cast<mlir::RankedTensorType>(input.getType());
+
+    if (argAttrDict) {
+      auto runtimeTensorShardingAttr =
+          argAttrDict.get(mlir::tt::ttcore::RuntimeTensorShardingAttr::name);
+      if (runtimeTensorShardingAttr) {
+        auto rtsAttr = mlir::cast<mlir::tt::ttcore::RuntimeTensorShardingAttr>(
+            runtimeTensorShardingAttr);
+        shardStatus = rtsAttr.getShardStatus().getValue();
+        localShape = rtsAttr.getLocalShape();
+      }
+    }
+
+    program.inputs.push_back(cache.getOrCreate(input, tensorValueToFlatbuffer,
+                                               shardStatus, localShape));
   }
 
   mlir::AsmState printState(entry, printFlags);
   entry.getBody().walk([&](mlir::Operation *op) {
     if (auto returnOp = dyn_cast_if_present<func::ReturnOp>(op); returnOp) {
-      for (auto output : returnOp.getOperands()) {
-        program.outputs.push_back(cache.at<::tt::target::ttnn::TensorRef>(
-            getOperandThroughDPSOps(output)));
+      for (auto [i, output] : llvm::enumerate(returnOp.getOperands())) {
+        ttcore::ShardStatus shardStatus = ttcore::ShardStatus::Unsharded;
+        mlir::RankedTensorType localShape =
+            mlir::cast<mlir::RankedTensorType>(output.getType());
+
+        auto resultAttrs = mlir::DictionaryAttr::get(op->getContext(),
+                                                     entry.getResultAttrs(i));
+        if (resultAttrs) {
+          auto runtimeTensorShardingAttr = resultAttrs.get(
+              mlir::tt::ttcore::RuntimeTensorShardingAttr::name);
+          if (runtimeTensorShardingAttr) {
+            auto rtsAttr =
+                mlir::cast<mlir::tt::ttcore::RuntimeTensorShardingAttr>(
+                    runtimeTensorShardingAttr);
+            shardStatus = rtsAttr.getShardStatus().getValue();
+            localShape = rtsAttr.getLocalShape();
+          }
+        }
+
+        auto tensorRefResult =
+            cache.getOrCreate(getOperandThroughDPSOps(output),
+                              tensorValueToFlatbuffer, shardStatus, localShape);
+        program.outputs.push_back(tensorRefResult);
       }
     } else {
       std::string debugStr = getOpDebugString(op, printState);
