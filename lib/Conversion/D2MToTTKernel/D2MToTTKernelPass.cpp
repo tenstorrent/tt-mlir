e70265d6fefb4b62ab49309854cfe0d3748f738e
Hongseok Kwon <hkwon@tenstorrent.com>
2026-01-28 17:44:11 +0000

    add spatial op as legal to D2MToTTKernel pass

--- lib/Conversion/D2MToTTKernel/D2MToTTKernelPass.cpp
+++ lib/Conversion/D2MToTTKernel/D2MToTTKernelPass.cpp
@@ -78,6 +78,7 @@ struct ConvertD2MToTTKernel
     target.addLegalOp<d2m::MeshShardOp>();
     target.addLegalOp<d2m::CreateGlobalSemaphoreOp>();
     target.addLegalOp<d2m::ResetGlobalSemaphoreOp>();
+    target.addLegalOp<d2m::SpatialOp>();
 
     if (ttnnMode) {
       target.addLegalOp<ttir::TTNNMetalLayoutCastOp>();
