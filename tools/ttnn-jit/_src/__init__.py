dabdc1f5cc79d6eea18c70e8b53aa7673f73e95c
Vincent Tang <vtang@tenstorrent.com>
2026-01-29 20:02:43 +0000

    wip

--- tools/ttnn-jit/_src/__init__.py
+++ tools/ttnn-jit/_src/__init__.py
@@ -4,22 +4,22 @@
 
 """TTNN-JIT internal utilities and implementation."""
 
-import sys
+import ttnn
 
-from _ttmlir_runtime.runtime import (
+from ttnn_jit.runtime._ttmlir_runtime.runtime import (
     submit,
     set_compatible_device_runtime,
     get_current_device_runtime,
     get_current_system_desc,
     DispatchCoreType,
 )
-from _ttmlir_runtime.binary import (
+from ttnn_jit.runtime._ttmlir_runtime.binary import (
     load_binary_from_path,
     load_binary_from_capsule,
 )
-from _ttmlir_runtime.utils import (
+from ttnn_jit.runtime._ttmlir_runtime.utils import (
     create_runtime_device_from_ttnn,
     create_runtime_tensor_from_ttnn,
     get_ttnn_tensor_from_runtime_tensor,
 )
-from ttnn_jit._ttnn_jit import JitCache
+from ttnn_jit.runtime._ttnn_jit import JitCache
