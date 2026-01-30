89c1fdc3f64b1b285b28de929b9f19444aaa9557
Mohamed Atef <mohamedatef1698@gmail.com>
2026-01-30 21:01:54 +0200

    Fix K's range

--- lib/Dialect/TTIR/IR/TTIROps.cpp
+++ lib/Dialect/TTIR/IR/TTIROps.cpp
@@ -4879,7 +4879,7 @@ verifyReduceOp(llvm::function_ref<mlir::InFlightDiagnostic()> emitOpError,
                          << ", but got: " << dim;
   }
 
-  if (K <= 0 || K >= inputType.getDimSize(normalizedDim)) {
+  if (K <= 0 || K > inputType.getDimSize(normalizedDim)) {
     return emitOpError() << "K should be between 1 and the size of the "
                             "specified dimension ("
                          << normalizedDim << "), but got: " << K;
--- lib/Dialect/TTNN/IR/TTNNOps.cpp
+++ lib/Dialect/TTNN/IR/TTNNOps.cpp
@@ -4936,7 +4936,7 @@ void mlir::tt::ttnn::D2MSubgraphOp::getEffects(
                          << ", but got: " << dim;
   }
 
-  if (K <= 0 || K >= inputType.getDimSize(normalizedDim)) {
+  if (K <= 0 || K > inputType.getDimSize(normalizedDim)) {
     return emitOpError() << "K should be between 1 and the size of the "
                             "specified dimension ("
                          << normalizedDim << "), but got: " << K;
