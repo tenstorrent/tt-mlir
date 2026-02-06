ba7557dfbec6030b42cecd42caaf7bdae795d9e7
Brandon Rapanan <brapanan@tenstorrent.com>
2026-02-06 19:15:10 +0000

    get max core grid from tensor_arg's device

--- tools/ttnn-jit/_src/utils.py
+++ tools/ttnn-jit/_src/utils.py
@@ -103,10 +103,10 @@ def get_dispatch_core_type():
     return dispatch_core_type
 
 
-def get_maximal_block_sharding_grid(shape):
+def get_maximal_block_sharding_grid(shape, core_grid):
     """Infer a TTNN grid/end coord for block sharding the given logical tensor shape"""
 
-    # collapse dims [0, -1)
+    # Collapse dims [0, -1)
     if len(shape) > 2:
         collapsed_dim = 1
         for i in range(len(shape) - 1):
@@ -116,9 +116,14 @@ def get_maximal_block_sharding_grid(shape):
     tile_shape = [collapsed_dim // 32, shape[-1] // 32]
 
     grid = []
-    for dim in tile_shape:
-        for grid_dim in reversed(range(8)):
+    for dim, max_grid in zip(tile_shape, core_grid):
+        for grid_dim in reversed(range(max_grid + 1)):
             if dim % (grid_dim + 1) == 0:
                 grid.append(grid_dim)
                 break
     return list(reversed(grid))
+
+
+def get_core_grid_from_tensor_arg(tensor_arg):
+    device = tensor_arg.device
+    return (device.core_grid.x, device.core_grid.y)
