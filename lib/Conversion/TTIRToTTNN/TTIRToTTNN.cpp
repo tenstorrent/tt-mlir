c702eb40e40b1f983bf61fdba1d0d0ec7b04001f
Aleks Knezevic <aknezevic@tenstorrent.com>
2026-02-07 01:51:10 +0000

    fp32 accum for reduce scatter

--- lib/Conversion/TTIRToTTNN/TTIRToTTNN.cpp
+++ lib/Conversion/TTIRToTTNN/TTIRToTTNN.cpp
@@ -2031,7 +2031,8 @@ public:
         /*sub_device_id=*/nullptr,
         /*memory_config=*/nullptr,
         /*num_links=*/nullptr,
-        /*topology=*/nullptr);
+        /*topology=*/nullptr,
+        /*compute_config=*/nullptr);
 
     return success();
   }
