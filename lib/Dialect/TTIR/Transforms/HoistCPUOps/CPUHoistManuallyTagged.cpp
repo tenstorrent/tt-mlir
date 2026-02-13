65e889ba37311985c999048b2c020cd57b6e89d6
David Milinkovic <dmilinkovic@tenstorrent.com>
2026-02-13 12:15:14 +0000

    Restructure CPU-hoisting passes

---
+++ lib/Dialect/TTIR/Transforms/HoistCPUOps/CPUHoistManuallyTagged.cpp
@@ -0,0 +1,45 @@
+// SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ttmlir/Dialect/TTCore/IR/TTCore.h"
+#include "ttmlir/Dialect/TTIR/Transforms/HoistCPUOps/HoistCPUOps.h"
+#include "ttmlir/Dialect/TTIR/Transforms/Passes.h"
+
+namespace mlir::tt::ttir {
+#define GEN_PASS_DEF_CPUHOISTMANUALLYTAGGEDTRANSFORM
+#include "ttmlir/Dialect/TTIR/Transforms/Passes.h.inc"
+
+namespace {
+
+// Transform pass to hoist ops manually tagged with ttir.should_hoist.
+class CPUHoistManuallyTaggedTransform
+    : public impl::CPUHoistManuallyTaggedTransformBase<
+          CPUHoistManuallyTaggedTransform> {
+public:
+  using CPUHoistManuallyTaggedTransformBase::
+      CPUHoistManuallyTaggedTransformBase;
+
+  void runOnOperation() final {
+    mlir::ModuleOp rootModule = getOperation();
+    mlir::ModuleOp deviceInnerModule = getDeviceInnerModule(rootModule);
+    if (!deviceInnerModule) {
+      return;
+    }
+
+    llvm::SmallVector<CPUHoistedOpsDescriptor> descriptors;
+    deviceInnerModule.walk([&](func::FuncOp funcOp) {
+      auto result =
+          createDescriptorsWithPredicate(funcOp, [](mlir::Operation *op) {
+            return op->hasAttr(ttir::ShouldHoistAttr::name);
+          });
+      descriptors.append(std::make_move_iterator(result.begin()),
+                         std::make_move_iterator(result.end()));
+    });
+
+    runCPUHoist(rootModule, std::move(descriptors));
+  }
+};
+
+} // namespace
+} // namespace mlir::tt::ttir
