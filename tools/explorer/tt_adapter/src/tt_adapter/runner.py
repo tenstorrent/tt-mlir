75a387a5d16d94b2ed737bb45a458a8d75b017c6
David Milinkovic <dmilinkovic@tenstorrent.com>
2026-02-13 15:03:32 +0000

    Restructure TTNN pipelines

--- tools/explorer/tt_adapter/src/tt_adapter/runner.py
+++ tools/explorer/tt_adapter/src/tt_adapter/runner.py
@@ -361,7 +361,7 @@ class ModelRunner:
             )
             emitc_command = [
                 f"{self._build_dir}/bin/ttmlir-opt",
-                "--ttnn-to-emitc-device-pipeline",
+                "--ttnn-common-to-emitc-pipeline",
                 ttnn_ir_file,
                 "-o",
                 emitc_ir_file,
