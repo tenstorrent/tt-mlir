c19f030521c774f8d04760c378028761d0058aff
Sohaib Nadeem <sohaibnadeem@tenstorrent.com>
2026-02-18 04:56:04 +0000

    fix liveness issue

--- lib/Dialect/D2M/Transforms/Allocate.cpp
+++ lib/Dialect/D2M/Transforms/Allocate.cpp
@@ -485,9 +485,8 @@ class D2MAllocate final : public impl::D2MAllocateBase<D2MAllocate> {
       analysis.sequencing.operationMap[op] = position;
       analysis.sequencing.positionMap.emplace_back(op);
 
-      if (llvm::isa<memref::AllocOp, d2m::ViewLayoutOp,
-                    d2m::StreamLayoutOp>( // , d2m::CreateGlobalSemaphoreOp
-              op)) {
+      if (llvm::isa<memref::AllocOp, d2m::ViewLayoutOp, d2m::StreamLayoutOp,
+                    d2m::CreateGlobalSemaphoreOp>(op)) {
         // Skip memref.alloc operations that have a genericOp as parent
         if (llvm::isa<memref::AllocOp>(op) &&
             op->getParentOfType<d2m::GenericOp>()) {
@@ -1756,7 +1755,8 @@ class D2MAllocate final : public impl::D2MAllocateBase<D2MAllocate> {
 
     for (Operation *user : op->getResult(0).getUsers()) {
       if (graph.contains(user)) {
-        if (llvm::isa<d2m::ViewLayoutOp, d2m::StreamLayoutOp>(user)) {
+        if (llvm::isa<d2m::ViewLayoutOp, d2m::StreamLayoutOp,
+                      d2m::CreateGlobalSemaphoreOp>(user)) {
           last = std::max(last, resolve(user, graph));
         }
       }
