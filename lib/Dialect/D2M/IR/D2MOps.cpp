fd2af6a6d1cbe784a0a407c23463b2fe4778ac75
Sohaib Nadeem <sohaibnadeem@tenstorrent.com>
2026-02-18 14:04:24 +0000

    fix clang-tidy check + cleanup

--- lib/Dialect/D2M/IR/D2MOps.cpp
+++ lib/Dialect/D2M/IR/D2MOps.cpp
@@ -142,6 +142,7 @@ mlir::LogicalResult d2m::CreateGlobalSemaphoreOp::bufferize(
     return maybeInput;
   }
 
+  // NOLINTNEXTLINE(clang-analyzer-core.StackAddressEscape)
   rewriter.replaceOpWithNewOp<CreateGlobalSemaphoreOp>(
       *this, getResult().getType(), *maybeInput, getValue());
 
