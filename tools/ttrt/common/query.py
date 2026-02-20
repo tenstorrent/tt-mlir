96090c423381c82f0cfa298f86e794430db09a8a
Vladimir Milosevic <157983820+vmilosevic@users.noreply.github.com>
2026-02-20 10:32:50 +0100

    Upgrade to python 3.12 (#6851)
    ### Ticket
    Closes #6890
    
    ### Problem description
    Upgrade python version to 3.12
    
    ### What's changed
    Python 3.11 -> Python 3.12
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- tools/ttrt/common/query.py
+++ tools/ttrt/common/query.py
@@ -4,17 +4,7 @@
 
 import os
 import json
-import importlib.machinery
-import sys
-import signal
 import os
-import io
-import subprocess
-import time
-import socket
-from pkg_resources import get_distribution
-import shutil
-import atexit
 
 from ttrt.common.util import *
 
