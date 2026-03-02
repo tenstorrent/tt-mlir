7a2093a49232b28e2e0682c2bede305fc10ca26c
Vincent Wells <vwells@tenstorrent.com>
2026-03-02 21:15:35 +0000

    passing

--- test/ttnn-jit/lit/test_control_flow.py
+++ test/ttnn-jit/lit/test_control_flow.py
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# TODO(#CB-removal): Fix LowerLoadStoreOpsToExplicitCBForm for remote_store conversion.
-# XFAIL: *
 # RUN: %python %s | FileCheck %s
 # REQUIRES: ttnn-jit
 
