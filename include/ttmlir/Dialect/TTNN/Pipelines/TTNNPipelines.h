19e419e0e0c76f5064b12719f50f15ac3e5bd9a0
David Milinkovic <dmilinkovic@tenstorrent.com>
2026-03-03 17:12:27 +0000

    Enable CPU-hoisted consteval by default

--- include/ttmlir/Dialect/TTNN/Pipelines/TTNNPipelines.h
+++ include/ttmlir/Dialect/TTNN/Pipelines/TTNNPipelines.h
@@ -330,7 +330,7 @@ struct TTIRToTTNNDevicePipelineOptions
   Option<bool> enableCPUHoistedConstEval{
       *this, "enable-cpu-hoisted-const-eval",
       llvm::cl::desc("Enable hoisting const-eval ops to CPU module."),
-      llvm::cl::init(false)};
+      llvm::cl::init(true)};
 
   // Force const-eval function inputs to system memory.
   Option<bool> enableConstEvalInputsToSystemMemory{
