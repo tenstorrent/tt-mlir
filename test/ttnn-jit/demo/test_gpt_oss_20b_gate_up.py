d37343bdd081e1762e01321a2ee8017d878c6848
Brandon Rapanan <brapanan@tenstorrent.com>
2026-03-05 20:56:28 +0000

    add subgraph diagram

--- test/ttnn-jit/demo/test_gpt_oss_20b_gate_up.py
+++ test/ttnn-jit/demo/test_gpt_oss_20b_gate_up.py
@@ -9,6 +9,64 @@ import pytest
 
 from utils import create_dram_tensor, all_close_check
 
+r"""
+GPT-OSS gate up proj activation subgraph:
+
+%416 (input)        %25 (gate_up_proj weight)
+             \                /
+              \              /
+         +------------------+
+         |  %417 = matmul   |
+         +------------------+
+                  |
+                  |  + %17 (bias)
+                  v
+         +------------------+
+         |  %418 = add      |
+         +------------------+
+              /          \
+    odd indices           even indices
+    (step=2,start=1)      (step=2,start=0)
+            /                  \
+   +-----------------+   +-----------------+
+   | %419 = slice    |   | %422 = slice    |
+   +-----------------+   +-----------------+
+           |                     |
+------------JIT'ed subgraph starts here-------------
+           v                     v
+   +-----------------+   +-----------------+
+   | %420 = clamp    |   | %423 = clamp    |
+   |  [-7, 7]        |   |  [-inf, 7]      |
+   +-----------------+   +-----------------+
+           |                  /     \
+           |  + %30 (scalar) /       \  * %2 (scale)
+           v                /         \
+   +-----------------+     |    +-----------------+
+   | %421 = add      |     |    | %424 = multiply |
+   +-----------------+     |    +-----------------+
+           |               |            |
+           |               |            v
+           |               |    +-----------------+
+           |               |    | %425 = sigmoid  |
+           |               |    +-----------------+
+           |               |            |
+           |                \          /
+           |                 \        /
+           |            +-----------------+
+           |            | %426 = multiply |  x * sigmoid(x) = SiLU
+           |            +-----------------+
+            \                /
+             \              /
+         +------------------+
+         | %427 = multiply  |  gate * SiLU(up)
+         +------------------+
+                  |
+                  v
+              (output)
+
+------------JIT'ed subgraph ends here-------------
+"""
+
 
 @ttnn_jit.jit(debug=True)
 def gptoss_gateup_subgraph(
