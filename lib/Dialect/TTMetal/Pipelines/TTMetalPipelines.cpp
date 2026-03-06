16638a4991c1c83c090fccf55cf6c50e504597e5
Vincent Tang <vtang@tenstorrent.com>
2026-03-06 16:16:08 +0000

    scaffold

--- lib/Dialect/TTMetal/Pipelines/TTMetalPipelines.cpp
+++ lib/Dialect/TTMetal/Pipelines/TTMetalPipelines.cpp
@@ -237,6 +237,7 @@ void createTTIRToTTMetalMiddleendPipeline(
   pm.addPass(d2m::createD2MScheduleDMA());
   pm.addPass(d2m::createD2MLowerLoadStoreOpsToDMA());
   pm.addPass(d2m::createD2MExpandDMAReadCompositeView());
+  pm.addPass(d2m::createD2MOptimizeDMA());
   pm.addPass(d2m::createD2MLowerDMAToFullyIndexedForm());
 
   createOptimizationPasses(pm, options);
