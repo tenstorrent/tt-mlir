f6abb9eb4e357417ef777472976a0bb8e18f9ddc
Aditi Pandey <apandey@tenstorrent.com>
2026-03-06 14:50:15 +0000

    Changed the pipeline to count the ccls from the optimized stablhlo graph instead of the ttnn graph

--- include/ttmlir/Dialect/StableHLO/Utils/PassOverrides.h
+++ include/ttmlir/Dialect/StableHLO/Utils/PassOverrides.h
@@ -16,7 +16,6 @@ struct OptionNames {
       "automatic-arg-analysis";
   static constexpr llvm::StringRef enableAutoSharding =
       "enable-auto-sharding";
-  static constexpr llvm::StringRef systemDescPath = "system-desc-path";
   static constexpr llvm::StringRef dumpVariants = "dump-variants";
   static constexpr llvm::StringRef dumpDir = "dump-dir";
 };
