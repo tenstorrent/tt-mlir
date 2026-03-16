b0baff491b69423853e8651003577094b1c8101c
Sohaib Nadeem <sohaibnadeem@tenstorrent.com>
2026-03-16 17:12:46 +0000

    more changes

--- lib/Dialect/D2M/Utils/Utils.cpp
+++ lib/Dialect/D2M/Utils/Utils.cpp
@@ -779,4 +779,32 @@ collapseToPhysicalGrid2D(ArrayRef<int64_t> gridShape,
   return result;
 }
 
+llvm::SmallVector<int64_t>
+computeOptimalBlockShardedGrid(ArrayRef<int64_t> physicalShape,
+                               ArrayRef<int64_t> targetSquareGridShape) {
+  llvm::SmallVector<int64_t> grid(physicalShape.size(), 1);
+
+  TT_assert(targetSquareGridShape.size() == 2u);
+  TT_assert(physicalShape.size() >= targetSquareGridShape.size());
+
+  // For tensors with rank > 2, only shard across the last two dimensions
+  // (which correspond to the 2D worker grid).
+  const size_t dimOffset = physicalShape.size() - targetSquareGridShape.size();
+
+  for (size_t i = 0; i < targetSquareGridShape.size(); ++i) {
+    const int64_t dim = physicalShape[dimOffset + i];
+    TT_assert(dim > 0);
+    // Search downward from the target grid size to find the largest divisor.
+    for (int64_t g = targetSquareGridShape[i]; g > 0; g--) {
+      if (dim % g == 0) {
+        grid[dimOffset + i] = g;
+        break;
+      }
+    }
+  }
+
+  TT_assert(grid.size() == physicalShape.size());
+  return grid;
+}
+
 } // namespace mlir::tt::d2m::utils
