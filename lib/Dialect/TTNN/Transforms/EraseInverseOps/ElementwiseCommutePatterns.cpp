3e434ccdbdfeb1b76d060c1e6e3347073e9d1816
Milan Topalovic <mtopalovic@tenstorrent.com>
2026-03-19 11:28:11 +0000

    Add EIO to TTNN

---
+++ lib/Dialect/TTNN/Transforms/EraseInverseOps/ElementwiseCommutePatterns.cpp
@@ -0,0 +1,165 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ttmlir/Dialect/TTNN/IR/TTNNOps.h"
+#include "ttmlir/Dialect/TTNN/IR/TTNNOpsInterfaces.h"
+#include "ttmlir/Dialect/TTNN/Transforms/EraseInverseOps/EraseInverseOps.h"
+
+namespace mlir::tt::ttnn {
+
+namespace {
+template <typename TMOpType, typename ElementwiseInterfaceType,
+          CommuteDirection commuteDirection>
+class TTNNCommuteTmsThroughElementwiseRewriter
+    : public TTNNCommuteOpInterfaceRewritePattern<
+          TMOpType, ElementwiseInterfaceType, commuteDirection> {
+public:
+  using TTNNCommuteOpInterfaceRewritePattern<
+      TMOpType, ElementwiseInterfaceType,
+      commuteDirection>::TTNNCommuteOpInterfaceRewritePattern;
+
+  void performCommuteUpwardsRewrite(ElementwiseInterfaceType op,
+                                    TMOpType tmUser,
+                                    PatternRewriter &rewriter) const override {
+
+    llvm::SmallVector<Operation *> users(op->getUsers());
+    auto oldEltwiseType = cast<RankedTensorType>(op->getResult(0).getType());
+    auto oldTMResultType = tmUser.getResult().getType();
+
+    auto newEltwiseType = RankedTensorType::get(oldTMResultType.getShape(),
+                                                oldEltwiseType.getElementType(),
+                                                oldTMResultType.getEncoding());
+
+    SmallVector<Value> newEltwiseOperands;
+    SmallVector<RankedTensorType> newTMResultTypes;
+    for (uint32_t operandIdx = 0; operandIdx < op->getNumOperands();
+         operandIdx++) {
+
+      // The new TM will have the same shape as before, but if the eltwise op
+      // was a typecast, it will have the element type of the original operand
+      // of the eltwise. So we need to generate a new type for the TM keeping
+      // this in mind.
+      auto operandType =
+          cast<RankedTensorType>(op->getOperand(operandIdx).getType());
+
+      newTMResultTypes.push_back(
+          oldTMResultType.clone(operandType.getElementType()));
+
+      mlir::Location newLoc = ttmlir::utils::appendLocationSuffix(
+          tmUser->getLoc(), "_tm" + std::to_string(operandIdx));
+      auto newTM = rewriter.create<TMOpType>(
+          newLoc, newTMResultTypes[operandIdx], op->getOperand(operandIdx),
+          tmUser->getAttrs());
+
+      newEltwiseOperands.push_back(newTM);
+    }
+
+    Operation *newEltwise = rewriter.create(
+        op->getLoc(), rewriter.getStringAttr(op->getName().getStringRef()),
+        newEltwiseOperands, newEltwiseType, op->getAttrs());
+
+    // This only works when all the users are an identical TM
+    // In the future this function may be called when this is not
+    // the case, and we'll need to insert user clones on the
+    // user edges that do not have an inverse on them.
+    assert(llvm::all_of(users,
+                        [&](Operation *user) {
+                          return checkIdenticalTms(tmUser, user);
+                        }) &&
+           "isCommuteUpwardsViable/Favorable should have ensured all users "
+           "are identical TMs");
+
+    for (auto *user : users) {
+      rewriter.replaceOp(user, newEltwise);
+    }
+  }
+
+  void
+  performCommuteDownwardsRewrite(ElementwiseInterfaceType op,
+                                 TMOpType tmOperand,
+                                 PatternRewriter &rewriter) const override {
+    RankedTensorType newEltwiseType =
+        cast<RankedTensorType>(op->getResult(0).getType())
+            .clone(tmOperand.getInput().getType().getShape());
+
+    SmallVector<Value> newEltwiseOperands;
+    for (auto operand : op->getOperands()) {
+      if (operand.getDefiningOp() == tmOperand) {
+        newEltwiseOperands.push_back(tmOperand.getInput());
+        continue;
+      }
+      newEltwiseOperands.push_back(getInverseTM(tmOperand, operand, rewriter));
+    }
+
+    Operation *newEltwise = rewriter.create(
+        op->getLoc(), rewriter.getStringAttr(op->getName().getStringRef()),
+        newEltwiseOperands, newEltwiseType, op->getAttrs());
+
+    RankedTensorType newTMType =
+        cast<RankedTensorType>(op->getResult(0).getType())
+            .clone(tmOperand.getType().getShape());
+    TMOpType newUserTM = rewriter.create<TMOpType>(op->getLoc(), newTMType,
+                                                   newEltwise->getResult(0),
+                                                   tmOperand->getAttrs());
+
+    rewriter.replaceOp(op, newUserTM);
+  }
+
+private:
+  bool isCommuteUpwardsViable(ElementwiseInterfaceType op,
+                              TMOpType) const override {
+    // We can always commute a TM above an elementwise op
+    return true;
+  }
+
+  bool isCommuteUpwardsFavorable(ElementwiseInterfaceType op,
+                                 TMOpType) const override {
+    // If all users of an elementwise binary op are identical tms, then it is
+    // typically favorable to commute them above it. In some cases we may not
+    // be able to erase/consteval one or both of the commuted operand TMs.
+    SmallVector<Operation *> users(op->getUsers());
+    return !users.empty() && checkAllUsersAreIdenticalTms(users);
+  }
+
+  bool isCommuteDownwardsViable(ElementwiseInterfaceType op,
+                                TMOpType) const override {
+    // We can always commute a TM below an elementwise op
+    return true;
+  }
+
+  bool isCommuteDownwardsFavorable(ElementwiseInterfaceType op,
+                                   TMOpType tmOperand) const override {
+    // Commuting downwards is favorable if all other operands are an identical
+    // TM.
+    for (auto operand : op->getOperands()) {
+      if (checkIdenticalTms(operand.getDefiningOp(), tmOperand)) {
+        continue;
+      }
+      return false;
+    }
+    return true;
+  }
+};
+} // namespace
+
+template <CommuteDirection commuteDirection>
+void populateElementwiseCommutePatterns(MLIRContext *ctx,
+                                        RewritePatternSet &patterns) {
+  patterns.add<
+      TTNNCommuteTmsThroughElementwiseRewriter<PermuteOp, ElementwiseUnary,
+                                               commuteDirection>,
+      TTNNCommuteTmsThroughElementwiseRewriter<ReshapeOp, ElementwiseUnary,
+                                               commuteDirection>,
+      TTNNCommuteTmsThroughElementwiseRewriter<PermuteOp, ElementwiseBinary,
+                                               commuteDirection>,
+      TTNNCommuteTmsThroughElementwiseRewriter<ReshapeOp, ElementwiseBinary,
+                                               commuteDirection>>(ctx);
+}
+
+template void populateElementwiseCommutePatterns<CommuteDirection::UPWARDS>(
+    MLIRContext *ctx, RewritePatternSet &patterns);
+template void populateElementwiseCommutePatterns<CommuteDirection::DOWNWARDS>(
+    MLIRContext *ctx, RewritePatternSet &patterns);
+
+} // namespace mlir::tt::ttnn
