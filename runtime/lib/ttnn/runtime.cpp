a725b7e74ce40e72913b10eb106e9f593055ce8c
Aleksa Orlovic <aorlovic@tenstorrent.com>
2026-03-27 11:17:14 +0000

    Added TTNN supppot and tests

--- runtime/lib/ttnn/runtime.cpp
+++ runtime/lib/ttnn/runtime.cpp
@@ -1018,6 +1018,10 @@ getOpOutputRef(OpContext opContextHandle,
     tensorRef = opContext.type_as_ToLayoutOp()->out();
     break;
   }
+  case ::tt::target::ttnn::OpType::BitcastConvertOp: {
+    tensorRef = opContext.type_as_BitcastConvertOp()->out();
+    break;
+  }
   case ::tt::target::ttnn::OpType::TypecastOp: {
     tensorRef = opContext.type_as_TypecastOp()->out();
     break;
@@ -1444,6 +1448,10 @@ getOpInputRefs(OpContext opContextHandle,
   }
   case ::tt::target::ttnn::OpType::ToLayoutOp: {
     tensorRefs = {opContext.type_as_ToLayoutOp()->in()};
+    break;
+  }
+  case ::tt::target::ttnn::OpType::BitcastConvertOp: {
+    tensorRefs = {opContext.type_as_BitcastConvertOp()->in()};
     break;
   }
   case ::tt::target::ttnn::OpType::TypecastOp: {
