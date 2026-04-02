370babb2ec69b5bffea06b2af5d22abf20a0886f
David Milinkovic <dmilinkovic@tenstorrent.com>
2026-04-02 23:09:20 +0000

    Fix emitpy builder infra

--- tools/builder/base/builder_runtime.py
+++ tools/builder/base/builder_runtime.py
@@ -968,7 +968,7 @@ def execute_py(
                 and node.name[0:18] != "create_inputs_for_"
                 and not node.name.__contains__("_const_eval_")
                 # TODO(dmilinkovic): this is getting out of hand, issue #6386.
-                and not node.name.__contains__("hoisted_")
+                and not node.name.startswith("cpu_hoisted_")
                 and not node.name.startswith("consteval_")
             ):
                 program_names.append(node.name)
