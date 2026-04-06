fe2f1215195705ce82e576a802300c209e765f0a
Het Shah <hshah@tenstorrent.com>
2026-04-06 19:39:14 +0000

    rename ttir.gather_dim op to ttir.gather

--- tools/builder/ttir/ttir_builder.py
+++ tools/builder/ttir/ttir_builder.py
@@ -14923,10 +14923,10 @@ class TTIRBuilder(Builder):
 
         return topk_module, topk_builder
 
-    ############### ttir.GatherDimOp ###############
+    ############### ttir.GatherOp ###############
 
-    @tag(ttir.GatherDimOp)
-    def gather_dim(
+    @tag(ttir.GatherOp)
+    def gather(
         self,
         in0: Operand,
         index: Operand,
@@ -14935,7 +14935,7 @@ class TTIRBuilder(Builder):
         loc: Optional[str] = None,
         unit_attrs: Optional[List[str]] = None,
     ) -> OpResult:
-        ttir_op = self.get_opview_from_method(TTIRBuilder.gather_dim)
+        ttir_op = self.get_opview_from_method(TTIRBuilder.gather)
 
         if output_type is None:
             mlir_output_type = self.get_type(in0)
@@ -14977,10 +14977,10 @@ class TTIRBuilder(Builder):
 
         return op_result
 
-    @parse(ttir.GatherDimOp)
+    @parse(ttir.GatherOp)
     def gather_dim_parser(
         self,
-        old_op: ttir.GatherDimOp,
+        old_op: ttir.GatherOp,
         global_dict: Dict[Operand, Operand],
     ) -> Tuple[Operation, Dict[OpResult, OpResult]]:
         ttir_op = self.get_opview_from_parser(TTIRBuilder.gather_dim_parser)
@@ -15014,10 +15014,10 @@ class TTIRBuilder(Builder):
         op_map_dictionary[old_op.result] = new_op_result
         return new_op, op_map_dictionary
 
-    @split(ttir.GatherDimOp)
+    @split(ttir.GatherOp)
     def gather_dim_split(
         self,
-        old_op: ttir.GatherDimOp,
+        old_op: ttir.GatherOp,
     ) -> Tuple[Module, TTIRBuilder]:
         ttir_op = self.get_opview_from_split(TTIRBuilder.gather_dim_split)
 
