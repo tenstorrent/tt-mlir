1b1c59ad543a34b6fd88052666dfa5b93532652b
Nikola Drakulic <ndrakulic@tenstorrent.com>
2026-04-07 10:35:15 +0000

    feat(chisel): add package exports

--- tools/chisel/chisel/__init__.py
+++ tools/chisel/chisel/__init__.py
@@ -1,3 +1,8 @@
 # SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
+from chisel.context import ChiselContext
+from chisel.callbacks import (
+    chisel_pre_op_callback,
+    chisel_post_op_callback,
+)
