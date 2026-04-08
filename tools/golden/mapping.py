6c5d3a3487877f0cd9e4b1b01b5c8238d095c80a
Julia Grim <jgrim@tenstorrent.com>
2026-04-08 12:00:27 -0500

    Support TTNNBuilder split for CCLs (#7867)
    ### Ticket
    Closes [#7866](https://github.com/tenstorrent/tt-mlir/issues/7866) 
    Closes [#7243](https://github.com/tenstorrent/tt-mlir/issues/7243) 
    Closes [#7262](https://github.com/tenstorrent/tt-mlir/issues/7262) 
    Closes [#7264](https://github.com/tenstorrent/tt-mlir/issues/7243) 
    
    ### Problem description
    TTNNBuilder split doesn't support graphs that use ttnn.get_device
    because the device output gets passed into ops as an operand, but mlir
    graph structure doesn't allow for ttcore.devices to be passed into or
    returned from modules.
    CCL ops in TTNNBuilder don't have split functions.
    
    ### What's changed
    Added `ttnn.get_device` op creation to split functions that require a
    device as an input operand
    Added split functions for CCLs
    Miscellaneous CCL op/golden function cleanup
    Removed unusable ttnn.deallocate split function.
    Added mesh_shape handling to TTNNBuilder `from_module`
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- tools/golden/mapping.py
+++ tools/golden/mapping.py
@@ -3230,6 +3230,8 @@ def apply_sharding(
     shard_dims: Tuple[Union[int, None]],
 ) -> GoldenMapTensor:
     shards = [tensor.shard_at(0).clone()]
+    if len(mesh_shape) != len(shard_dims):
+        raise ValueError("mesh_shape and shard_dims must have the same length")
     for dim_size, shard_dim in zip(mesh_shape, shard_dims):
         temp_shards = []
         if shard_dim is None or shard_dim == -1:
@@ -6531,27 +6533,27 @@ def ttnn_reduce_scatter_golden(
 
 
 def ttnn_to_layout_golden(
-    input_tensor: GoldenMapTensor, output_ranked_tensor_type: RankedTensorType
+    input_tensor: GoldenMapTensor,
+    layout_attr: Attribute,
+    output_type_mlir: Type,
 ) -> GoldenMapTensor:
-    casted_type = ttcore.ir.TileType.maybe_downcast(
-        output_ranked_tensor_type.element_type
-    )
-
-    if casted_type:
-        output_dtype = mlir_datatype_to_torch_dtype(casted_type.data_type)
-    else:
-        output_dtype = mlir_type_to_torch_dtype(output_ranked_tensor_type.element_type)
-
+    output_dtype = mlir_type_to_torch_dtype(output_type_mlir)
     output_tensor = input_tensor.clone()
     return output_tensor.to(output_dtype)
 
 
-def ttnn_to_device_golden(input_tensor: GoldenMapTensor, **kwargs) -> GoldenMapTensor:
-    return input_tensor.clone()
+def ttnn_to_device_golden(
+    input_tensor: GoldenMapTensor, output_type_mlir: Type
+) -> GoldenMapTensor:
+    output_dtype = mlir_type_to_torch_dtype(output_type_mlir)
+    return input_tensor.clone().to(output_dtype)
 
 
-def ttnn_from_device_golden(input_tensor: GoldenMapTensor, **kwargs) -> GoldenMapTensor:
-    return input_tensor.clone()
+def ttnn_from_device_golden(
+    input_tensor: GoldenMapTensor, output_type_mlir: Type
+) -> GoldenMapTensor:
+    output_dtype = mlir_type_to_torch_dtype(output_type_mlir)
+    return input_tensor.clone().to(output_dtype)
 
 
 ################ Debug Op Golden Functions ###############
