684027649794bd55f57a66bf51473f9773cf44ed
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-12 19:22:35 +0000

    Comment out exalens ref

--- env/install-tt-triage.sh
+++ env/install-tt-triage.sh
@@ -9,7 +9,7 @@ mkdir temp
 cd temp
 
 wget -O install_debugger.sh "https://raw.githubusercontent.com/tenstorrent/tt-metal/${TT_METAL_VERSION}/scripts/install_debugger.sh"
-wget -O ttexalens_ref.txt "https://raw.githubusercontent.com/tenstorrent/tt-metal/${TT_METAL_VERSION}/scripts/ttexalens_ref.txt"
+#wget -O ttexalens_ref.txt "https://raw.githubusercontent.com/tenstorrent/tt-metal/${TT_METAL_VERSION}/scripts/ttexalens_ref.txt"
 wget -O requirements.txt "https://raw.githubusercontent.com/tenstorrent/tt-metal/${TT_METAL_VERSION}/tools/triage/requirements.txt"
 
 chmod u+x install_debugger.sh
