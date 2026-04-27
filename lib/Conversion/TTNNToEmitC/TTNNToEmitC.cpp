9de603f76fd5acbe01a6313625b73c6976bcc027
Mohamed Atef <mohamedatef1698@gmail.com>
2026-04-28 01:47:39 +0300

    Add runtime support for round
    (cherry picked from commit bba170ab02fd6dc2d05c5bde2efbcb768b238b3e)

--- lib/Conversion/TTNNToEmitC/TTNNToEmitC.cpp
+++ lib/Conversion/TTNNToEmitC/TTNNToEmitC.cpp
@@ -5856,6 +5856,7 @@ void populateTTNNToEmitCPatterns(mlir::MLIRContext *ctx,
            EltwiseUnaryWithOutputAndApproxModeOpConversionPattern<
                mlir::tt::ttnn::TanhOp>,
            EltwiseUnaryOpConversionPattern<mlir::tt::ttnn::AtanOp>,
+           EltwiseUnaryOpConversionPattern<mlir::tt::ttnn::RoundOp>,
            EltwiseUnaryWithFastAndApproximateModeOpConversionPattern<
                mlir::tt::ttnn::LogOp>>(typeConverter, ctx);
 
