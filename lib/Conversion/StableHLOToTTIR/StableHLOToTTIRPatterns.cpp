6938f79e4b2bd602132fe28e1c3d0204754a7d59
sdjukicTT <sdjukic@tenstorrent.com>
2026-04-27 11:58:26 +0000

    remove unnecessary check

--- lib/Conversion/StableHLOToTTIR/StableHLOToTTIRPatterns.cpp
+++ lib/Conversion/StableHLOToTTIR/StableHLOToTTIRPatterns.cpp
@@ -6157,11 +6157,6 @@ private:
           "TTIR single dimensional scatter requires index_vector_dim to be 1");
     }
 
-    if (!multiDimensionalScatter && scatterDimsToOperandDims[0] != 0) {
-      return rewriter.notifyMatchFailure(
-          op, "TTIR single dimensional scatter currently only supports "
-              "scattering along dimension 0");
-    }
     return success();
   }
 
