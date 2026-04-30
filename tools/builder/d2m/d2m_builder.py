b9e3a2cdd025e4ef1caea85cf05fd66ec7a4103d
hong kwon <hkwon@tenstorrent.com>
2026-04-30 20:11:44 +0000

    additional_args for generic API

--- tools/builder/d2m/d2m_builder.py
+++ tools/builder/d2m/d2m_builder.py
@@ -323,6 +323,7 @@ class D2MBuilder(Builder):
     def _create_generic(
         self,
         operands,
+        additional_args,
         grid,
         block_factors,
         indexing_maps,
@@ -347,7 +348,7 @@ class D2MBuilder(Builder):
             [ret_type],
             inputs,
             outputs,
-            [],  # additional_args
+            additional_args,
             ttcore.ir.GridAttr.get(ctx, grid),
             block_factors,
             list(map(affine_map_from_lambda, indexing_maps)),
@@ -414,8 +415,10 @@ class D2MBuilder(Builder):
                 nonlocal iterator_types
                 nonlocal skip_grid_selection
 
+                additional_args = kwargs.pop("additional_args", [])
                 generic = self._create_generic(
                     args,
+                    additional_args,
                     grid,
                     block_factors,
                     indexing_maps,
