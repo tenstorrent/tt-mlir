1c31e187f4b8f88474a4de06ac5cbfdcd63a801c
Chris Karavasilis <ckaravasilis@tenstorrent.com>
2026-05-04 14:55:14 +0000

    cleanup

--- include/ttmlir/Target/TTKernel/LLKs/experimental_reg_api.h
+++ include/ttmlir/Target/TTKernel/LLKs/experimental_reg_api.h
@@ -8,7 +8,7 @@
 namespace experimental {
 
 // Initializes the PACK_DONE semaphore used by unpack_stall_on_pack. This should
-// run once at compute kernel startup, not at every synchronization point.
+// run once at compute kernel startup.
 ALWI void unpack_stall_on_pack_init() {
   PACK(t6_semaphore_init(semaphore::PACK_DONE, 0, 1));
 }
@@ -17,7 +17,6 @@ ALWI void unpack_stall_on_pack_init() {
 // committed to L1. Call this before unpacking data that was just packed
 // by the previous linalg.generic to guarantee L1 read-after-write ordering.
 ALWI void unpack_stall_on_pack() {
-  PACK(t6_semaphore_init(semaphore::PACK_DONE, 0, 1));
   PACK(t6_semaphore_post<>(semaphore::PACK_DONE));
   UNPACK(t6_semaphore_wait_on_zero<p_stall::STALL_SYNC>(semaphore::PACK_DONE));
   UNPACK(t6_semaphore_get<>(semaphore::PACK_DONE));
