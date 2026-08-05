39b6fcbc70423a3642ba51e558a96a5822d95245
Carson Farmer <example@example.com>
2026-08-05 11:49:52 -0400

    prototype: use active Python for MLIR toolchain

--- cmake/modules/TTMLIRPythonSitePackages.cmake
+++
@@ -1,5 +0,0 @@
-execute_process(
-  COMMAND python -c "from distutils import sysconfig; print(sysconfig.get_python_lib(prefix='', plat_specific=True))"
-  OUTPUT_VARIABLE PYTHON_SITE_PACKAGES
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
-set(TTMLIR_PYTHON_SITE_PACKAGES "${TTMLIR_TOOLCHAIN_DIR}/venv/${PYTHON_SITE_PACKAGES}" CACHE STRING "Path to the Python site-packages directory")
